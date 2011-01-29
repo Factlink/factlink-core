c58d1bfc05691fa45a7a39d52f0f3a3b81bc68cb
Tom de Vries <tom@codigy.nl>
2011-01-29 14:54:46 +0100

    Search. Factlinks. Factlink partial

--- core/app/controllers/home_controller.rb
+++ core/app/controllers/home_controller.rb
@@ -3,14 +3,12 @@ class HomeController < ApplicationController
   def index
     search_query = params[:search]
 
-    @factlinks = Factlink.first
-    if search_query
-      
+    @factlinks = []
+
+    if search_query      
       q = /#{search_query}/i
       @factlinks = Factlink.where(:title => q)
     end
-    
-    puts "\n\nCOUNT: #{@factlinks.count}"
   end
 
 end
