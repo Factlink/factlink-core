e806684f63d6124952b060d54ce3bc7a58620b1e
Tom de Vries <tom@codigy.nl>
2011-04-29 20:58:46 +0200

    :(

--- core/config/environment.rb
+++ core/config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'yaml' 
-YAML::ENGINE.yamler= 'syck'
-
 # Initialize the rails application
 FactlinkUI::Application.initialize!
 
