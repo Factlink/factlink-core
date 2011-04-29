ee35f38b0322e919c45139801b765b3976db123e
Tom de Vries <tom@codigy.nl>
2011-04-29 20:56:27 +0200

    Use a Syck YAML parser

--- core/config/environment.rb
+++ core/config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require 'yaml' 
+YAML::ENGINE.yamler= 'syck'
+
 # Initialize the rails application
 FactlinkUI::Application.initialize!
+
