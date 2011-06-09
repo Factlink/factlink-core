a4c8b4b8208963a960011e3fa0fe5e75b7e22b12
Tom de Vries <tom@codigy.nl>
2011-06-09 15:55:16 +0200

    Remove Session debug information

--- core/app/controllers/users/sessions_controller.rb
+++ core/app/controllers/users/sessions_controller.rb
@@ -3,17 +3,14 @@ class Users::SessionsController < Devise::SessionsController
   layout "accounting"
 
   def new
-    puts "\n\n\n:::::::\nNew session\n\n\n"
     super
   end
   
   def create
-    puts "\n\n\n:::::::\nCreating session\n\n\n"
     super
   end
   
   def destroy
-    puts "\n\n\n:::::::\nDestroying session\n\n\n"
     super
   end
   
