3ec098102bc21a29702454c9ad8b8c8aa7e2de31
Tom de Vries <tom@codigy.nl>
2011-06-24 10:57:48 +0200

    Added files

---
+++ core/app/models/factlink_searcher.rb
@@ -0,0 +1,12 @@
+# class FactlinkSearcheraaa
+#   
+#   def search(keyword)
+# 
+#     result = Sunspot.search(Factlink) do
+#       keywords keyword, :fields => [:displaystring]
+#     end
+#     
+#     result
+#   end
+#   
+# end
