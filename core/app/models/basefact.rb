39cc9656de9d9ca44e04ff433de9784383dbbdc6
Mark IJbema <markijbema@gmail.com>
2011-07-18 14:55:10 +0200

    code metrics toegevoegd

--- core/app/models/basefact.rb
+++ core/app/models/basefact.rb
@@ -63,6 +63,12 @@ class Basefact < OurOhm
   # end
 
 
+  set :people_beliefs, User
+  set :people_doubts, User
+  set :people_disbeliefs, User
+  def people_who(type)
+    self.send("people_#{type}")
+  end
 
   def to_s
     self.data.displaystring || ""
@@ -70,11 +76,7 @@ class Basefact < OurOhm
 
   # Return a nice looking url, only subdomain + domain + top level domain
   def pretty_url
-    begin
-      self.site.url.gsub(/http(s?):\/\//,'').split('/')[0]
-    rescue
-      self.url.gsub(/http(s?):\/\//,'').split('/')[0]
-    end
+    self.site.url.gsub(/http(s?):\/\//,'').split('/')[0]
   end
 
   def set_added_to_factlink(user)
@@ -83,7 +85,7 @@ class Basefact < OurOhm
   end
 
   def delete_added_to_factlink()
-    self.added_to_factlink.delete
+    #self.added_to_factlink.delete
   end
 
   def toggle_opinion(type, user)
@@ -97,60 +99,23 @@ class Basefact < OurOhm
     end
   end
 
+  
+
   def add_opinion(type, user)
-    # Remove the old opinions
     remove_opinions(user)
-
-    # Add user to believers of this Fact
-    $redis.zadd(self.redis_key(type), user.authority, user.id)
-
-    # Add the belief type to user
+    people_who(type).add(user)
     user.update_opinion(type, self)
   end
 
   def remove_opinions(user)
     user.remove_opinions(self)
     [:beliefs, :doubts, :disbeliefs].each do |type|
-      $redis.zrem(self.redis_key(type), user.id)
+      people_who(type).remove(user)
     end
   end
 
 
-  def opiniated_ids(type)
-    $redis.zrange(self.redis_key(type), 0, -1)
-  end
 
-  def opiniated_count(type)
-    opiniated_ids(type).count
-  end
-
-  def opiniated(type)
-    User.where(:_id.in => self.opiniated_ids(type))
-  end
-
-  # All interacting users on this Fact
-  def interacting_user_ids
-    tmp_key = "factlink:#{self.id}:interacting_users:tmp"
-
-    $redis.zunionstore(tmp_key, [self.redis_key(:beliefs), self.redis_key(:doubts), self.redis_key(:disbeliefs)])
-
-    $redis.zrange(tmp_key, 0, -1)
-  end
-
-  def interacting_user_count
-    self.interacting_user_ids.count
-  end
-
-  def interacting_users
-    User.where(:_id.in => self.interacting_user_ids)
-  end
-
-
-  protected
-  # Helper method to generate redis keys
-  def redis_key(str)
-    "fact:#{self.id}:#{str}"
-  end
 
   def get_opinion
     opinions = []
