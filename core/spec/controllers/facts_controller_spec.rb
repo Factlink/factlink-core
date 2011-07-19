2e2742307ffacaa217fafa0d166067ba63b490b5
Tom de Vries <tom@codigy.nl>
2011-07-19 15:52:50 +0200

    Added tests for toggling opinion on fact

--- core/spec/controllers/facts_controller_spec.rb
+++ core/spec/controllers/facts_controller_spec.rb
@@ -4,9 +4,18 @@ describe FactsController do
 
   # TODO factor out, because each controller needs this
   def authenticate_user!
-     @user = FactoryGirl.create(:user)
-     request.env['warden'] = mock(Warden, :authenticate => @user, :authenticate! => @user)
-   end
+    @user = FactoryGirl.create(:user)
+    request.env['warden'] = mock(Warden, :authenticate => @user, :authenticate! => @user)
+  end
+
+  def create_fact_relation
+    @fact     = FactoryGirl.create(:fact)
+    @evidence = FactoryGirl.create(:fact)
+    @fr       = FactoryGirl.create(:fact_relation)
+    # Set the relation
+    @fr.from_fact.value = @fact.id
+    @fr.fact.value = @evidence.id
+  end
 
   describe :store_fact_for_non_signed_in_user do
     it "should work"
@@ -15,7 +24,7 @@ describe FactsController do
   describe :factlinks_for_url do
     it "should work" do
       @site = FactoryGirl.create(:site, :url => "http://en.wikipedia.org/wiki/Batman")
-      get :factlinks_for_url, :url => @site.url      
+      get :factlinks_for_url, :url => @site.url
       response.body.should eq("[]")
     end
   end
@@ -25,9 +34,9 @@ describe FactsController do
       @fact = FactoryGirl.create(:fact)
       get :show, :id => @fact.id
       response.should be_succes
-    end  
+    end
   end
-  
+
   describe :new do
     it "should return a new Fact object" do
       authenticate_user!
@@ -35,22 +44,22 @@ describe FactsController do
       assigns[:fact].should be_a_new(Fact)
     end
   end
-  
+
   describe :edit do
     it "should work"
   end
-  
+
   describe :prepare do
     it "should render the correct template" do
       get :prepare
       response.should render_template("prepare")
     end
   end
-  
+
   describe :intermediate do
     it "should work"
   end
-  
+
   describe :create do
     it "should work" do
       pending
@@ -59,56 +68,78 @@ describe FactsController do
       response.should redirect_to(factlink_path)
     end
   end
-  
+
   describe :add_supporting_evidence do
     it "should respond to XHR" do
-      authenticate_user!      
-      xhr :get, :add_supporting_evidence, 
-                  :fact_id => FactoryGirl.create(:fact).id,
-                  :evidence_id => FactoryGirl.create(:fact).id
-      
+      authenticate_user!
+      xhr :get, :add_supporting_evidence,
+        :fact_id => FactoryGirl.create(:fact).id,
+        :evidence_id => FactoryGirl.create(:fact).id
+
       response.code.should eq("200")
     end
   end
-  
+
   describe :add_weakening_evidence do
     it "should respond to XHR" do
-      authenticate_user!      
-      xhr :get, :add_supporting_evidence, 
-                  :fact_id => FactoryGirl.create(:fact).id,
-                  :evidence_id => FactoryGirl.create(:fact).id
-      
+      authenticate_user!
+      xhr :get, :add_supporting_evidence,
+        :fact_id => FactoryGirl.create(:fact).id,
+        :evidence_id => FactoryGirl.create(:fact).id
+
       response.code.should eq("200")
     end
   end
-  
+
   describe :remove_factlink_from_parent do
     it "should work"
   end
-  
+
   describe :update do
     it "should work"
   end
-  
+
   describe :toggle_opinion_on_fact do
-    it "should work"
+
+    it "should not respond to non-allowed types" do
+      authenticate_user!
+
+      create_fact_relation
+
+      get "toggle_opinion_on_fact", { :fact_relation_id => @fr.id, :type => "baron_is_not_allowed" }
+      parsed_content = JSON.parse(response.body)
+      parsed_content.should have_key("error")
+      parsed_content['error'].should == "type not allowed"
+    end
+
+    it "should respond to allowed types" do
+      authenticate_user!
+
+      create_fact_relation
+
+      # Check for all available types
+      [:beliefs, :doubts, :disbeliefs].each do |type|
+        xhr :get, "toggle_opinion_on_fact", { :fact_relation_id => @fr.id, :type => type }
+        response.code.should eq("200")
+      end
+    end
   end
-  
+
   describe :toggle_relevance_on_fact_relation do
     it "should work"
   end
-  
+
   describe :interaction_users_for_factlink do
     it "should work"
   end
-  
+
   describe :search do
     it "should work"
   end
-  
+
   describe :indication do
     it "should work"
   end
-  
-  
+
+
 end
