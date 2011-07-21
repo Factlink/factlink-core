e97c4e88aaae5bfe85e99f6c8df9c315b547ac10
Tom de Vries <tom@codigy.nl>
2011-07-21 14:59:41 +0200

    Clean

--- core/app/controllers/facts_controller.rb
+++ core/app/controllers/facts_controller.rb
@@ -4,25 +4,25 @@ class FactsController < ApplicationController
 
   before_filter :store_fact_for_non_signed_in_user, :only => [:create]
 
-  # Change this to :except, in stead of :only. 
-  before_filter :authenticate_user!, :only => [:new, 
-    :edit, 
-    :create, 
-    :update,
-    :add_factlink_to_parent,
-    :remove_factlink_from_parent,
-    :believe,
-    :doubt,
-    :disbelieve,
-    :set_opinion,
-    :add_supporting_evidence,
-    :add_weakening_evidence,
-    :toggle_opinion_on_fact,
-    :toggle_relevance_on_fact_relation
-    ]
-  
+  # Change this to :except, in stead of :only.
+  before_filter :authenticate_user!, :only => [:new,
+                                               :edit,
+                                               :create,
+                                               :update,
+                                               :add_factlink_to_parent,
+                                               :remove_factlink_from_parent,
+                                               :believe,
+                                               :doubt,
+                                               :disbelieve,
+                                               :set_opinion,
+                                               :add_supporting_evidence,
+                                               :add_weakening_evidence,
+                                               :toggle_opinion_on_fact,
+                                               :toggle_relevance_on_fact_relation
+                                               ]
+
   layout "client"
-  
+
   # Check if the user is signed in before adding a Fact.
   # If this is not the case, store the params in a session variable,
   # so the Fact can be created after logging in.
@@ -35,16 +35,16 @@ class FactsController < ApplicationController
   def factlinks_for_url
     url = params[:url]
     site = Site.find(:url => url)[0]
-    
-    @facts = if site
-             then site.facts
-             else []
-             end
 
-    # Render the result with callback, 
+    @facts = if site
+    then site.facts
+    else []
+    end
+
+    # Render the result with callback,
     # so JSONP can be used (for Internet Explorer)
-    render :json => @facts.to_json( :only => [:_id, :displaystring], :methods => :score_dict_as_percentage  ), 
-                                        :callback => params[:callback]  
+    render :json => @facts.to_json( :only => [:_id, :displaystring], :methods => :score_dict_as_percentage  ),
+      :callback => params[:callback]
   end
 
   def show
@@ -52,22 +52,22 @@ class FactsController < ApplicationController
     #TODO potential evidence should be a list of facts which can be added as supporting or weakening evidence
     @potential_evidence = Fact.all
   end
-  
+
   def new
     @fact = Fact.new
   end
-  
+
   def edit
     @fact = Fact.find(params[:id])
-    end
-  
+  end
+
   # Prepare for create
   def prepare
     render :template => 'facts/prepare', :layout => nil
   end
-  
+
   # Prepare for create
-  def intermediate    
+  def intermediate
     # TODO: Sanitize for XSS
     @url = params[:url]
     @passage = params[:passage]
@@ -79,7 +79,7 @@ class FactsController < ApplicationController
   def create
     # Creating a Fact requires a url and fact ( > displaystring )
     # TODO: Refactor 'fact' to 'displaystring' for internal consistency
-    
+
     # Get or create the website on which the Fact is located
     site = Site.find_or_create_by(:url => params[:url])
 
@@ -91,33 +91,33 @@ class FactsController < ApplicationController
     else
       displaystring = params[:factlink][:displaystring]
     end
-    
+
     # Create the Fact
-    @factlink = Fact.create!(:displaystring => displaystring, 
+    @factlink = Fact.create!(:displaystring => displaystring,
                              :created_by => current_user,
                              :site => site)
 
     # Redirect to edit action
     redirect_to :action => "edit", :id => @factlink.id
   end
-  
+
   def add_supporting_evidence
     # Add existing evidence to a Fact
     @fact     = Fact.find(params[:fact_id])
     @evidence = Fact.find(params[:evidence_id])
 
     @fact_relation = @fact.add_evidence(:supporting, @evidence, current_user)
-    
+
     render "add_source_to_factlink"
   end
-  
+
   def add_weakening_evidence
     # Add existing evidence to a Fact
     @fact     = Fact.find(params[:fact_id])
     @evidence = Fact.find(params[:evidence_id])
 
     @fact_relation = @fact.add_evidence(:weakening, @evidence, current_user)
-    
+
     render "add_source_to_factlink"
   end
 
@@ -130,14 +130,14 @@ class FactsController < ApplicationController
     # Add a Fact as source for another Fact
     @fact     = Fact.find(params[:fact_id])
     @evidence = Fact.find(params[:evidence_id])
-    
+
     # Is this correct?
     @factlink = FactRelation.get_or_create(@evidence, :supporting, @fact, current_user)
-    
+
     render "add_factlink_to_parent"
   end
-  
-  
+
+
   # Adding the current fact to another existing fact as evidence
   # Is this still the way we want to use this in the future UI?
 
@@ -149,20 +149,20 @@ class FactsController < ApplicationController
 
     # Is this correct?
     @factlink = FactRelation.get_or_create(@evidence, :weakening, @fact, current_user)
-    
+
     render "add_factlink_to_parent"
   end
-  
-  
-  
+
+
+
   def remove_factlink_from_parent
-    
+
     # TODO: Only allow if user added the source earlier on
-    
+
     # Remove a Fact from it's parent
     @factlink = Fact.find(params[:factlink_id])
     parent    = Fact.find(params[:parent_id])
-    
+
     if @factlink.added_to_parent_by_current_user(parent, current_user)
       # Only remove if the user added this source
       puts "Removing child"
@@ -170,41 +170,41 @@ class FactsController < ApplicationController
       parent.save
     end
   end
-  
+
 
   def update
     @factlink = Fact.find(params[:id])
 
     respond_to do |format|
       if @factlink.update_attributes(params[:factlink])
-        format.html { redirect_to(@factlink, 
-                      :notice => 'Factlink top was successfully updated.') }
+        format.html { redirect_to(@factlink,
+                                  :notice => 'Factlink top was successfully updated.') }
       else
         format.html { render :action => "edit" }
       end
     end
   end
-  
+
 
   # Set or unset the opinion on a Factrelation
   def toggle_opinion_on_fact
     allowed_types = ["beliefs", "doubts", "disbeliefs"]
     type = params[:type]
-    
+
     if allowed_types.include?(type)
       @fact_relation = FactRelation.find(params[:fact_relation_id])
       @fact_relation.get_from_fact.toggle_opinion(type, current_user)
-    else   
+    else
       render :json => {"error" => "type not allowed"}
       return false
     end
   end
-  
+
   # Set or unset the relevance on a Factrelation
   def toggle_relevance_on_fact_relation
     allowed_types = ["beliefs", "doubts", "disbeliefs"]
     type = params[:type]
-    
+
     if allowed_types.include?(type)
       @fact_relation = FactRelation.find(params[:fact_relation_id])
       @fact_relation.toggle_opinion(type, current_user)
@@ -213,23 +213,23 @@ class FactsController < ApplicationController
       return false
     end
   end
-  
-  
+
+
   # Users that interacted with this Fact
   def interaction_users_for_factlink
     @fact         = Fact.find(params[:factlink_id])
-    
+
     @believers    = @fact.opiniated(:beliefs)
     @doubters     = @fact.opiniated(:doubts)
-    @disbelievers = @fact.opiniated(:disbeliefs)    
-    
+    @disbelievers = @fact.opiniated(:disbeliefs)
+
   end
-  
-  # Search 
+
+  # Search
   def search
     @row_count = 50
     row_count = @row_count
-        
+
     if params[:s]
       solr_result = Fact.search() do
 
@@ -242,40 +242,40 @@ class FactsController < ApplicationController
         end
 
       end
-      
+
       @factlinks = solr_result.results
     else
       # will_paginate sorting doesn't work very well on arrays.. Fixed it..
       @factlinks = WillPaginate::Collection.create( params[:page] || 1, row_count ) do |pager|
         start = (pager.current_page-1)*row_count
-        
+
         # Sorting & filtering done by mongoid
         results = Fact.all(:sort => [[sort_column, sort_direction]]).skip(start).limit(row_count).to_a
-        
+
         pager.replace(results)
       end
     end
-        
+
     respond_to do |format|
       format.html { render :layout => "accounting" }# search.html.erb
       format.js
     end
   end
-  
+
   def indication
     respond_to do |format|
       format.js
     end
   end
-  
+
   private
-  def sort_column
-    Fact.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
-  
-  
+    def sort_column
+      Fact.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
+
+
 end
