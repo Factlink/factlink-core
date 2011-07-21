1d17833b3514967f34618000b648bc8047fd8d54
Remon Oldenbeuving <remon@codigy.nl>
2011-07-21 13:58:10 +0200

    Internet Explorer support has been removed because the rangy dependency was not loaded anyway.
    Need to add this later

--- js-library/src/js/search.js
+++ js-library/src/js/search.js
@@ -32,52 +32,6 @@ Factlink.search = function(searchString){
         // Maybe we later need to store the current selection before 
         // processing the document, so we can reset it afterwards
         window.getSelection().removeAllRanges();
-    } else if (document.body.createTextRange) { // IE
-            // Create an empty range object
-        range = document.body.createTextRange();
-        
-        if ( range.findText ) { // IE
-            // IE8 Doesn't support search over multiple elements...
-            if (document.documentMode && document.documentMode === 8) { // IE8
-                alert( "Your browser is currently running in IE8 Mode, this rendering " +
-                       "mode of IE8 has a bug which may cause Factlink from not finding " + 
-                       "all the available Factlinks on this page.");
-            } 
-
-            while ( range.findText(searchString) ) {
-                // We need to encapsulate the following statements in a try-catch
-                // because IE will throw an error (800a025e) when trying to select
-                // a non-visible range
-                // TODO: Find out if we can surpass this by using the internal rangy
-                //       functions which will parse a normal range into a rangy-range
-                try {
-                    // Select the current range, necessary because of the ierange 
-                    // we're using
-                    range.select();
-                    
-                        // Because of IE's shitty implementation of the Range and 
-                        // Selection objects, we have to refresh the range (this 
-                        // time the rangy module is used)
-                    selection = rangy.getSelection();
-
-                        // the rangy object
-                    ierange = selection.getRangeAt(0);
-
-                    // Push the range to the results
-                    results.push(ierange);
-
-                    // Move on
-                    range.collapse(false);
-                    selection.removeAllRanges();
-                } catch (e) {
-                    // We need to collapse the range because otherwise IE will 
-                    // keep matching this range
-                    range.collapse(false);
-                }
-            }
-        } else { // No findText available
-            return false;
-        }
     } else { // No window.find and createTextRange
         return false;
     }
