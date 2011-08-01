0670041625790813997f9b80f7bf9e9a42eb562f
Remon Oldenbeuving <remon@codigy.nl>
2011-08-01 11:19:36 +0200

    Changed notation of FactlinkConfig.api

--- js-library/src/js/xdm.js
+++ js-library/src/js/xdm.js
@@ -2,7 +2,7 @@
 // Initiate the easyXDM object
 Factlink.remote = new easyXDM.Rpc({
         // The URL to load
-		remote: window.location.protocol + FactlinkConfig.api + "factlink/intermediate",
+		remote: window.location.protocol + '//' + FactlinkConfig.api + "factlink/intermediate",
 		// The iFrame where the intermediate should be loaded in
 		container: "factlink-modal-frame"
 	}, {
