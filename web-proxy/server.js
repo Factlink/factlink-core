992cb57e793b327b9aba6c11b791e048fba2b86b
Tom de Vries <tom@codigy.nl>
2011-08-04 16:35:07 +0200

    Proxy now supports visiting urls of the following type: example.org, www.example.org, http(s)://www.example.org

--- web-proxy/server.js
+++ web-proxy/server.js
@@ -19,7 +19,22 @@ server.set('view engine', 'jade');
 
 server.get('/parse', function(req, res) {
     var site = req.query.url;
-    
+
+		// Quick fixes for visiting sites without http(s) or www
+		http_regex = new RegExp("^http(s?)");    
+		if (http_regex.test(site) === false) {
+
+			// Check if start with www
+			www_regex = new RegExp("^www.");
+			if (www_regex.test(site) === false) {
+				site = "www." + site
+			}
+
+			// Not starting with http(s), so add:
+			site = "http://" + site
+		}
+
+
     console.info( "Serving: " + site );
 
     // The actual request using restler so we can follow redirects
