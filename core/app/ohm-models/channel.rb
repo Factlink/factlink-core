d1555051c36e859397880cd26d1b5166b97a2021
Tom de Vries <tom@codigy.nl>
2011-08-09 14:42:47 +0200

    Set of facts. Don't allow empty :title

--- core/app/ohm-models/channel.rb
+++ core/app/ohm-models/channel.rb
@@ -3,8 +3,13 @@ class Channel < OurOhm
   attribute :title
   attribute :description
   
-  collection :facts, Fact
+  set :facts, Fact
   
   reference :user, GraphUser
+  
+  def validate
+    assert_present :title
+    # assert_present :user # User always needed?
+  end
   
 end
