cc147f7e39c994e0e6db9ec87c1e8c3bf56672fd
Mark IJbema <markijbema@gmail.com>
2011-08-10 14:49:21 +0200

    added merging/forking functionality to channels

--- core/app/ohm-models/channel.rb
+++ core/app/ohm-models/channel.rb
@@ -3,7 +3,18 @@ class Channel < OurOhm
   attribute :title
   attribute :description
   
-  set :facts, Fact
+  set :contained_channels, Channel
+  
+  set :internal_facts, Fact
+  set :delete_facts, Fact
+  
+  def facts
+    fs = internal_facts
+    contained_channels.each do |ch|
+      fs |= ch.facts
+    end
+    fs - delete_facts
+  end
   
   reference :created_by, GraphUser
   
@@ -13,14 +24,33 @@ class Channel < OurOhm
   end
   
   def add_fact(fact)
-    self.facts.add(fact)
+    self.internal_facts.add(fact)
   end
   
   def remove_fact(fact)
-    self.facts.delete(fact)
+    if self.internal_facts.include?(fact)
+      self.internal_facts.delete(fact)
+    else
+      self.delete_facts.add(fact)
+    end
   end
   
   def to_s
     self.id
   end
+  
+  def fork(user)
+    newtitle = title
+    if user == created_by
+      newtitle += "'"
+    end
+    c = Channel.create(:created_by => user, :title => newtitle, :description => description)
+    c.add_channel(self)
+    c
+  end
+  
+  def add_channel(channel)
+    contained_channels << channel
+  end
+  
 end
