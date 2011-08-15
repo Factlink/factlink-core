93fabd26b32db12b3ed38e58c86306eb65b33224
Mark IJbema <markijbema@gmail.com>
2011-08-15 14:43:16 +0200

    added structure of channel export to some_facts

--- core/db/init/some_facts.rb
+++ core/db/init/some_facts.rb
@@ -1,3 +1,6 @@
+user "mark"
+user "merijn"
+
 fact 'Batman is a fictional character created by the artist Bob Kane and writer Bill Finger',"http://en.wikipedia.org/wiki/Batman"
 fact 'Batman\'s secret identity is Bruce Wayne',"http://en.wikipedia.org/wiki/Batman"
 fact 'Batman operates in the fictional American Gotham City',"http://en.wikipedia.org/wiki/Batman"
@@ -20,3 +23,18 @@ fact 'industrialist',"http://en.wikipedia.org/wiki/Batman"
 fact "continuous war on crime","http://en.wikipedia.org/wiki/Batman"
 fact 'revenge on crime',"http://en.wikipedia.org/wiki/Batman"
 fact 'philanthropist',"http://en.wikipedia.org/wiki/Batman"
+
+user "mark"
+channel "foo"
+  add_fact "playboy"
+  add_fact "industrialist"
+  del_fact "je moeder"
+  sub_channel "merijn", "foobar"
+  sub_channel "tom", "dinges"
+
+user "merijn"
+channel "foobar"
+
+user "mark"
+  channel "foobar"
+    sub_channel "merijn", "foobar"
