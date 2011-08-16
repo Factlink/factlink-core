57a5f1c41c17f0defb3264ba3066f1721191cf40
Tom de Vries <tom@codigy.nl>
2011-08-16 12:30:08 +0200

    Created the 'fork_label' > duplicate for own, add for channel of someone else

--- core/app/helpers/channels_helper.rb
+++ core/app/helpers/channels_helper.rb
@@ -1,2 +1,11 @@
 module ChannelsHelper
+  
+  def fork_label(channel)
+    if channel.created_by == current_user.graph_user
+      return "duplicate"
+    else
+      return "add"
+    end
+  end
+  
 end
