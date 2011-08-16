388e6ff568fbc0892883bc4ef1337bcbf5afd7d8
Tom de Vries <tom@codigy.nl>
2011-08-16 11:02:08 +0200

    Spaces fix

--- web-proxy/server.js
+++ web-proxy/server.js
@@ -18,7 +18,7 @@ var fs = require('fs');
 server.set('view engine', 'jade');
 
 server.get('/parse', function(req, res) {
-  var site = req.query.url;
+	var site = req.query.url;
 
 	// Quick fixes for visiting sites without http
 	http_regex = new RegExp("^http(s?)");
@@ -27,52 +27,54 @@ server.get('/parse', function(req, res) {
 		site = "http://" + site
 	}
 
-  console.info( "Serving: " + site );
+ 	console.info( "Serving: " + site );
 
   // The actual request using restler so we can follow redirects
-  var req = require('restler').get( site );
+	var req = require('restler').get( site );
 
-  // When the request is finished, handle the response data
 
+	// Error handling
 	req.on("error", function(data) {
 		var html = "<html><body><span>An error occured when trying to visit " + site + ".<br/><br/>Please check the URL or <a href='http://www.google.com/'>do a web-search</a>.</span></body></html>";
 		console.log("[ERROR] Faild on " + site);
-		
+
 		res.write( html );
 		res.end(); 
 	});
-	
-  req.on('complete', function(data) {
-	
-    // Replace the closing head tag with a base tag
-    var html = data.replace('<head>', '<head><base href="' + site + '" />');
-    html = html.replace('</head>', '<script src="' + STATIC_URL + 'proxy/scripts/proxy.js"></script></head>');
 
-    // Write the replaced HTML to 
-    res.write( html );
-    res.end();
-  });
+
+	// When the request is finished, handle the response data	
+	req.on('complete', function(data) {
+
+		// Replace the closing head tag with a base tag
+		var html = data.replace('<head>', '<head><base href="' + site + '" />');
+		html = html.replace('</head>', '<script src="' + STATIC_URL + 'proxy/scripts/proxy.js"></script></head>');
+
+		// Write the replaced HTML to 
+		res.write( html );
+		res.end();
+	});
 
 });
 
 server.get('/header', function(req, res) {
-    res.render('header', {
-        layout: false,
-        locals: {
-            static_url: STATIC_URL,
-            url: req.query.url
-        }
-    });
+	res.render('header', {
+		layout: false,
+		locals: {
+			static_url: STATIC_URL,
+			url: req.query.url
+		}
+	});
 });
 
 server.get('/', function(req, res) {
-    res.render('index', {
-        layout: false,
-        locals: {
-            proxy_url: PROXY_URL,
-            url: req.query.url
-        }
-    });
+	res.render('index', {
+		layout: false,
+		locals: {
+			proxy_url: PROXY_URL,
+			url: req.query.url
+		}
+	});
 });
 
 server.listen(8080);
