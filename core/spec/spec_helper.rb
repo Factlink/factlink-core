5f2a8dd2a3df9309dcbdde7d76b923daf01b6e70
Mark IJbema <markijbema@gmail.com>
2011-08-23 16:31:44 +0200

    fixed the loaddsl

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -18,25 +18,9 @@ require 'cover_me'
 
 RSpec.configure do |config|
   # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  #config.use_transactional_fixtures = true
-
   require 'database_cleaner'
 
-  #TODO: ook iets slims doen met Redis
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
