2cc646b92be5e137c0f87cf59df127a7c4d59906
Mark IJbema <markijbema@gmail.com>
2011-08-24 16:10:56 +0200

    added twitter account to our test users

--- core/db/init/users.rb
+++ core/db/init/users.rb
@@ -3,12 +3,12 @@
 load_fact_data do
 
 
-user "merijn", "merijn@gmail.com", "merijn481"
-user "tomdev", "tom@codigy.nl", "123hoi"
-user "jordin", "jordin@factlink.com", "Jordin87"
-user "remon", "remon@factlink.com", "remon123"
-user "salvador", "salvador@factlink.com", "salvador123"
-user "mark", "mark@factlink.com"
-user "joel", "joel@factlink.com", "joel123"
-
+user "merijn", "merijn@gmail.com", "merijn481", "merijn481"
+user "tomdev", "tom@codigy.nl", "123hoi", "tomdev"
+user "jordin", "jordin@factlink.com", "Jordin87", "vanzwoljj"
+user "remon", "remon@factlink.com", "remon123", "R51"
+user "salvador", "salvador@factlink.com", "salvador123", "salvadorven"
+user "mark", "mark@factlink.com", "123hoi", "markijbema"
+user "joel", "joel@factlink.com", "joel123", "joelkuiper"
+user "luit", "luit@factlink.com", "luitwachtwoord", "LuitvD"
 end
