237a2ad804d57d9fd3b908772cd21b8baa190f4f
Tom de Vries <tom@codigy.nl>
2011-09-05 13:01:38 +0200

    :host for e-mailing

--- core/config/environments/production.rb
+++ core/config/environments/production.rb
@@ -36,7 +36,8 @@ FactlinkUI::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => "factlink.com" }
+  
   # Enable threaded mode
   # config.threadsafe!
 
