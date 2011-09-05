9e51f80693cbb33ad9ff399edb204addea029654
Tom de Vries <tom@codigy.nl>
2011-09-05 17:05:43 +0200

    Update 404

--- core/app/controllers/users_controller.rb
+++ core/app/controllers/users_controller.rb
@@ -13,11 +13,13 @@ class UsersController < ApplicationController
     if @user      
       @activities = @user.graph_user.activities.sort(:order => "DESC")
       
+      puts "\n\nGOT USER\n\n"
+      
       respond_to do |format|
         format.html # show.html.erb
       end
     else
-      render :file => "#{Rails.root}/public/404.html", :status => :not_found
+      render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false
     end
   end
   
