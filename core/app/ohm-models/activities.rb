83c7d7d7386a4e44e5c7c41d8afae63b78121425
Mark IJbema <markijbema@gmail.com>
2011-09-06 11:00:49 +0200

    start of refactor of activities

--- core/app/ohm-models/activities.rb
+++ core/app/ohm-models/activities.rb
@@ -9,6 +9,53 @@ class Activity < OurOhm
 
   #TODO opruimen
 
+  def self.generic_reference(name)
+    reader = :"#{name}_id"
+    writer = :"#{name}_id="
+
+    reader_c = :"#{name}_class"
+    writer_c = :"#{name}_class="
+
+    attributes(self) << reader   unless attributes.include?(reader)
+    attributes(self) << reader_c unless attributes.include?(reader_c)
+
+    index reader
+    index reader_c
+
+    define_memoized_method(name) do
+      klass = Kernel.const_get(send(reader_c))
+      if klass == NilClass
+        return nil
+      else
+        return klass[send(reader)]
+      end
+    end
+
+    define_method(:"#{name}=") do |value|
+      @_memo.delete(name)
+      send(writer, value ? value.id : nil)
+      send(writer_c, value ? value.class : nil)
+    end
+
+    define_method(reader) do
+      read_local(reader)
+    end
+
+    define_method(writer) do |value|
+      @_memo.delete(name)
+      write_local(reader, value)
+    end
+
+    define_method(reader_c) do
+      read_local(reader_c)
+    end
+
+    define_method(writer_c) do |value|
+      @_memo.delete(name)
+      write_local(reader_c, value)
+    end
+  end
+
   # generic_reference subject  
   index :subject_id
   index :subject_class
