95dad582359c743bc192be08d0906ae2f42a694f
Mark IJbema <markijbema@gmail.com>
2011-09-12 13:56:18 +0200

    fixed the infinite opinions in redis issue, partly rollbacking the config changes

---
+++ core/config/initializers/proxy.rb
@@ -0,0 +1,2 @@
+proxy_conf = YAML::load_file(Rails.root.join('config/factlink.yml'))[Rails.env]['proxy']
+FactlinkUI::Application.config.proxy_url = "http://" + proxy_conf['hostname'] + ':' + proxy_conf['port'].to_s
