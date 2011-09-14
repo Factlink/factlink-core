f8f0ebd60dc3a0c8658c1c403318bb1ebd1afafc
Mark IJbema <markijbema@gmail.com>
2011-09-14 15:33:27 +0200

    moved the static server

--- core/script/static.rb
+++ core/script/static.rb
@@ -4,14 +4,14 @@ require "bundler/setup"
 require 'webrick'
 require 'rack'
 
-coredir = File.dirname(File.expand_path(__FILE__))
+coredir = File.join(File.dirname(File.expand_path(__FILE__)),'..')
 railsenv = ENV['RAILS_ENV'] || 'development'
 static_conf = YAML::load_file(File.join(coredir,'config/static.yml'))[railsenv]['static']
 serverport = static_conf['port']
 
 #TODO save this in the static settings when we want to use this elsewhere
 paths = [
-  {:path => '/lib', :filepath => File.join(coredir, '../static/factlink-js-library')},
+  {:path => '/lib',   :filepath => File.join(coredir, '../static/factlink-js-library')},
   {:path => '/proxy', :filepath => File.join(coredir, '../web-proxy/static')}
 ]
 
