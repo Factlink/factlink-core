1d14cab49c701dcd59d691757760562bc1add5f9
Tom de Vries <tom@codigy.nl>
2011-09-20 17:27:15 +0200

    export NODE_ENV is needed

--- core/config/deploy/production.rb
+++ core/config/deploy/production.rb
@@ -2,6 +2,8 @@ server 'demo.factlink.com', :app, :web, :primary => true
 
 set :deploy_env, 'testserver'
 
+set :branch, "master"
+
 role :web, "demo.factlink.com"                          # Your HTTP server, Apache/etc
 role :app, "demo.factlink.com"                          # This may be the same as your `Web` server
 role :db,  "demo.factlink.com", :primary => true # This is where Rails migrations will run
