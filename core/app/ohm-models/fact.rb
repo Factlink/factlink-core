43521de681dc7e9ba05c901634da5b0da2148ae1
Tom de Vries <tom@codigy.nl>
2011-09-27 11:03:00 +0200

    Removed deprecated methods

--- core/app/ohm-models/fact.rb
+++ core/app/ohm-models/fact.rb
@@ -25,20 +25,6 @@ class Fact < Basefact
 
   reference :data, lambda { |id| FactData.find(id) }
   
-  def require_data # dit ook doen met zo'n aftercreategebeuren    
-    if not self.data_id
-      localdata = FactData.new
-      localdata.save    # FactData now has an ID
-      self.data = localdata
-      if self.save
-        localdata.fact_id = self.id
-        localdata.save
-      else
-        raise StandardException, "the object could not be saved, but this is required before a require_data can be executed"
-      end
-    end
-  end
-  
   def require_saved_data
     if not self.data_id
       localdata = FactData.new
@@ -72,12 +58,7 @@ class Fact < Basefact
   def to_hash
     super.merge(:_id => id, :displaystring => self.data.displaystring, :score_dict_as_percentage => get_opinion.as_percentages)
   end
-  
-  deprecate
-  def fact_relations_ids
-    fact_relations.map { |fr| fr.id }
-  end
-  
+
   def fact_relations
     supporting_facts | weakening_facts
   end
