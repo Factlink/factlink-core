fcf5aef6526597395d497324b0c2ac9524f9c494
Tom de Vries <tom@codigy.nl>
2011-10-19 11:03:13 +0200

    current_graph_user helper is not available in controller

--- core/app/controllers/channels_controller.rb
+++ core/app/controllers/channels_controller.rb
@@ -156,7 +156,8 @@ class ChannelsController < ApplicationController
     # @channel is fetched in load_channel    
     @partial = "channels/related_users"
     
-    @locals = { :related_users => @channel.related_users(:without=>[current_graph_user]).andand.map{|x| x.user }, :excluded_users => [@channel.created_by]}
+    
+    @locals = { :related_users => @channel.related_users(:without=>[current_user.andand.graph_user]).andand.map{|x| x.user }, :excluded_users => [@channel.created_by]}
     respond_to do |format|
       format.html { render :template => "home/partial_renderer", :layout => "ajax" }
     end
