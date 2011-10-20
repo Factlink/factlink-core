9b24afe68de617b5eb0c853b48fc3968a0851cd8
Jan Salvador van der Ven <factlink@jansalvador.nl>
2011-10-20 15:27:09 +0200

    somewhat nicer layout

--- core/config/application.rb
+++ core/config/application.rb
@@ -19,7 +19,14 @@ if ['test', 'development'].include? Rails.env
   
   require 'simplecov'
   require 'simplecov-rcov-text'
-  SimpleCov.formatter = SimpleCov::Formatter::RcovTextFormatter
+  
+  class SimpleCov::Formatter::MergedFormatter
+      def format(result)
+         SimpleCov::Formatter::HTMLFormatter.new.format(result)
+         SimpleCov::Formatter::RcovTextFormatter.new.format(result)
+      end
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
 	
   MetricFu::Configuration.run do |config|  
     #config.metrics -= [:churn]  
