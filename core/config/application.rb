670302443c3ab068cc93eac420c3047b0bd5ba0d
Mark IJbema <markijbema@gmail.com>
2011-10-25 14:43:30 +0200

    refactoring working towards mustachios!

--- core/config/application.rb
+++ core/config/application.rb
@@ -101,6 +101,8 @@ module FactlinkUI
       Channel
     ]
     require "#{config.root}/lib/mustache_rails.rb"
+
+    autoload "Facts::UsersPopup", "#{config.root}/app/views/facts/_users_popup.rb"
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
