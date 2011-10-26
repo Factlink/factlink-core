3aa7ed0fd4800d08bec9fd0bd4fb3757d01eee4f
Joël Kuiper <me@joelkuiper.eu>
2011-10-26 12:18:05 +0200

    Slower scrolling

--- js-library/src/js/scrollto.js
+++ js-library/src/js/scrollto.js
@@ -1,5 +1,5 @@
 (function(Factlink, $, _, easyXDM, undefined) {
   Factlink.scrollTo = function(fact_id){
-    $('body')._scrollable().scrollTo("span[data-factid="+fact_id+"]", 500);
+    $('body')._scrollable().scrollTo("span[data-factid="+fact_id+"]", 800);
   };
 })(window.Factlink, Factlink.$, Factlink._, Factlink.easyXDM);
