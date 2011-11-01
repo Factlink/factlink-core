0d093beb556c1b7a33afb1f1c59bca8a3662d53e
Tom de Vries <tom@codigy.nl>
2011-11-01 13:22:11 +0100

    Oh, sorry Remon for refixing your fixes (no commits from you found...)

--- core/app/controllers/templates_controller.rb
+++ core/app/controllers/templates_controller.rb
@@ -1,4 +1,5 @@
 class TemplatesController < ApplicationController
+
   def get
     respond_to do |format| 
       format.html {render "templates/" + params[:name], :layout => "templates", :content_type => "text/javascript"}
