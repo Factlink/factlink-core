3b59bf7a8b70c44fe8ca09684aeb8ca8973f3c7d
Tom de Vries <tom@codigy.nl>
2011-11-01 13:12:51 +0100

    Refactored out the @users used in the template

--- core/app/controllers/users/registrations_controller.rb
+++ core/app/controllers/users/registrations_controller.rb
@@ -1,15 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
   layout "frontend"
-
-  def new    
-    @users = User.all[0..10]
-    super
-  end
-  
-  def create
-    @users = User.all[0..10]
-    super
-  end
   
 end
