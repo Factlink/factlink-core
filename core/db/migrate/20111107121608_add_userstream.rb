35c8945f7ea10d27e67eb862afb233a797edd540
Mark IJbema <markijbema@gmail.com>
2011-11-08 10:31:42 +0100

    improved migration

--- core/db/migrate/20111107121608_add_userstream.rb
+++ core/db/migrate/20111107121608_add_userstream.rb
@@ -1,3 +1,11 @@
+class GraphUser < OurOhm
+  reference :stream, UserStream
+  def create_stream
+    self.stream = UserStream.create(:created_by => self)
+    save
+  end
+end
+
 class AddUserstream < Mongoid::Migration
   def self.up
     GraphUser.all.each do |gu|
