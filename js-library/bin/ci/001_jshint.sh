34156155fd3f481f8738798ed498f542dc3f1d3f
Mark IJbema <markijbema@gmail.com>
2011-11-08 15:29:44 +0100

    fail if jshint is uninstallable via npm

--- js-library/bin/ci/001_jshint.sh
+++ js-library/bin/ci/001_jshint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm install jshint
+npm install jshint || exit 1
 ls src/js/*.js | grep -vE '(intro|outro)' | xargs node_modules/jshint/bin/hint || exit 1
 node_modules/jshint/bin/hint src/js/lib/*.js || exit 1
 node_modules/jshint/bin/hint src/js/scripts/*.js || exit 1
