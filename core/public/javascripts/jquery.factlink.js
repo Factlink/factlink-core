484d5f5885522ccd1056edf26f711cbd5604565b
Tom de Vries <tom@codigy.nl>
2011-11-08 17:46:08 +0100

    Started new styling

--- core/public/javascripts/jquery.factlink.js
+++ core/public/javascripts/jquery.factlink.js
@@ -176,7 +176,7 @@
             $t.trigger("factlink:switchTabAction", [active]);
           }); 
         }
-        function addEventHandlersTabs($t){
+        function addEventHandlersTabs($t){          
           $t.find("ul.evidence li").click(function() {
             $t.find(".tab_content, div.add-action").hide(); 
             var activeTab = $(this).find("a").attr("class"); 
