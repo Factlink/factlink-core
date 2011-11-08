6e26289974bdcdfa1c7b9e670c69b6910ad670a8
Tom de Vries <tom@codigy.nl>
2011-11-08 11:23:42 +0100

    Moved files to server dir

---
+++ core/bin/server/start_recalculate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Fact Graph
+fact_graph_count=`ps aux | grep -c 'rake fact_graph:recalculate'`
+
+if (( $fact_graph_count < 2 )); then
+   cd /applications/factlink-core/current/
+   /usr/local/rvm/bin/rake fact_graph:recalculate RAILS_ENV=testserver &
+fi
+
+
+# Channels
+channels_count=`ps aux | grep -c 'rake channels:recalculate'`
+
+if (( $channels_count < 2 )); then
+   cd /applications/factlink-core/current/
+   /usr/local/rvm/bin/rake channels:recalculate RAILS_ENV=testserver &
+fi
+
+exit 0
