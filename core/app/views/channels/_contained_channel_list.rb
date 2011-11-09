d2ed6bc59e98660cab3a9bf05e4ca957f517a7dd
Mark IJbema <markijbema@gmail.com>
2011-11-09 16:18:02 +0100

    fixed the link

--- core/app/views/channels/_contained_channel_list.rb
+++ core/app/views/channels/_contained_channel_list.rb
@@ -2,7 +2,7 @@ module Channels
   class ContainedChannelList < Mustache::Railstache
     def channels_with_link
       self[:channels].to_a.map do |ch|
-        ch_path = channel_path(channel.created_by.user.username, @channel.id)
+        ch_path = channel_path(ch.created_by.user.username, ch.id)
         ch.class_eval """
           def link
             '#{ch_path}'
