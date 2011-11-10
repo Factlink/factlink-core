038dfa053dc209f3a11aa7b7c0b3f829c23c0145
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-11-10 15:43:32 +0100

    Updates to blacklist

--- core/app/classes/blacklist.rb
+++ core/app/classes/blacklist.rb
@@ -4,6 +4,7 @@ class Blacklist
       #@TODO: These regexes are just temporary, please fix a nice regex which will match all possible URLS on the given site (also subdomains & https)
       /^http(s)?:\/\/([^\/]+\.)?facebook\.com\//,
       /^http(s)?:\/\/([^\/]+\.)?factlink\.com\//,
+      /^http(s)?:\/\/([^\/]+\.)?twitter\.com\//,
       /^http:\/\/localhost[:\/]/,
       /^http(s)?:\/\/([^\/]+\.)?google\.([a-z.]{2,6})\//,
       /^http(s)?:\/\/([^\/]+\.)?gmail\.com\//,
