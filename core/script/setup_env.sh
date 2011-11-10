6da6f75bd6c49d0f29c9f3be986321bf8518e6c2
Mark IJbema <markijbema@gmail.com>
2011-11-10 16:11:47 +0100

    do not use v as version prefix for git flow

--- core/script/setup_env.sh
+++ core/script/setup_env.sh
@@ -11,7 +11,7 @@ npm install supervisor -g
 
 cd factlink-core
 	git checkout develop
-	echo -e "master\ndevelop\n\n\n\n\nv\n" | git flow init
+	echo -e "master\ndevelop\n\n\n\n\n\n" | git flow init
 	bundle install
 	rake db:migrate
 	mkdir -p log
@@ -25,19 +25,19 @@ cd ..
 
 cd web-proxy
 	git checkout develop
-	echo -e "master\ndevelop\n\n\n\n\nv\n" | git flow init
+	echo -e "master\ndevelop\n\n\n\n\n\n" | git flow init
 	npm install
 cd ..
 
 cd factlink-chrome-extension
 	git checkout develop
-	echo -e "master\ndevelop\n\n\n\n\nv\n" | git flow init
+	echo -e "master\ndevelop\n\n\n\n\n\n" | git flow init
 	./release_repo.sh
 cd ..
 
 cd factlink-js-library
 	git checkout develop
-	echo -e "master\ndevelop\n\n\n\n\nv\n" | git flow init
+	echo -e "master\ndevelop\n\n\n\n\n\n" | git flow init
 	git submodule init
 	git submodule update
 	make all
