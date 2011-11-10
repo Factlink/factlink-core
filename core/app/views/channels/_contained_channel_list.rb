fd706d8b56d3f695600d9eaed310c7b21cad19cc
Joël Kuiper <me@joelkuiper.eu>
2011-11-10 15:05:29 +0100

    more channel styling

--- core/app/views/channels/_contained_channel_list.rb
+++ core/app/views/channels/_contained_channel_list.rb
@@ -4,14 +4,9 @@ module Channels
       self[:channels].to_a.map do |ch|
         user = ch.created_by.user
         ch_path = channel_path(user.username, ch.id)
-        user_path = channel_path(user.username)
         ch.class_eval """
           def link
             '#{escape_single_quotes(ch_path)}'
-          end
-          
-          def user_link
-            '#{escape_single_quotes(user_path)}'
           end
           
           def user_avatar
