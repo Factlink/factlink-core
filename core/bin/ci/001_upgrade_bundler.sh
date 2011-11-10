24fc029ad947b1462306a5101cecc06930fb8489
Jan Salvador van der Ven <factlink@jansalvador.nl>
2011-11-10 12:17:07 +0100

    changed descriptions of the ci scripts

--- core/bin/ci/001_upgrade_bundler.sh
+++ core/bin/ci/001_upgrade_bundler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Start running bundle install and tests"
+echo "Start running bundle install"
 source "$HOME/.rvm/scripts/rvm" || exit 1
 rvm use --default 1.9.2-p290 || exit 1
 gem install bundler || exit 1
