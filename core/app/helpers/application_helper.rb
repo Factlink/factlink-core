eaec4a09894539243fd51fd07a75b17ff79af1a4
Mark IJbema <markijbema@gmail.com>
2011-11-10 15:24:59 +0100

    removed helper which i previously backported from rails 3.1, and therefore is not needed anymore.

--- core/app/helpers/application_helper.rb
+++ core/app/helpers/application_helper.rb
@@ -7,27 +7,6 @@ module ApplicationHelper
     abs_path
   end
 
-  # This is included in rails 3.1.0, and now we're forwards compatible ;)
-  # TODO remove when upgrading to 3.1
-  # File actionpack/lib/action_view/helpers/form_tag_helper.rb, line 458
-  def button_tag(content_or_options = nil, options = nil, &block)
-    options = content_or_options if block_given? && content_or_options.is_a?(Hash)
-    options ||= {}
-    options.stringify_keys!
-
-    if disable_with = options.delete("disable_with")
-      options["data-disable-with"] = disable_with
-    end
-
-    if confirm = options.delete("confirm")
-      options["data-confirm"] = confirm
-    end
-
-    options.reverse_merge! 'name' => 'button', 'type' => 'submit'
-
-    content_tag :button, content_or_options || 'Button', options, &block
-  end
-  
   def template_as_string(filename)
     data = ''
     filename = Rails.root.join('app','templates',filename)
