1f7d0af19d78036529edebfb478f35f6017d0d53
Mark IJbema <markijbema@gmail.com>
2011-11-10 11:21:48 +0100

    refactored not to give a hash, but actual objects

--- core/app/classes/channel_manager.rb
+++ core/app/classes/channel_manager.rb
@@ -1,3 +1,23 @@
+class ChannelForFact
+
+  def initialize(channel,fact)
+    @channel = channel
+    @fact = fact
+  end
+  
+  def to_hash
+    @channel.to_hash.merge({
+      :created_by => @channel.created_by.user.username,
+      :checked => @channel.include?(@fact)
+    })
+  end
+
+  private
+     def method_missing(method, *args, &block)
+       @channel.send(method, *args, &block)
+     end  
+end
+
 class ChannelManager
   def initialize(gu)
     @gu = gu
@@ -5,15 +25,7 @@ class ChannelManager
   
   def editable_channel_hash_for(fact)
     @channels = @gu.channels.to_a
-    
     @channels.reject! { |channel| ! channel.editable? }
-    
-    @channels.map do |channel|
-      channel.to_hash.merge({
-        :created_by => channel.created_by.user.username,
-        :checked => channel.include?(@fact)
-      })
-    end
+    @channels.map {|ch| ChannelForFact.new(ch,fact)}
   end
-  
 end
