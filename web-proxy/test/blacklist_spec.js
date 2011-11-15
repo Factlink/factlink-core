4464453340636154d108383503c96b78db43d055
Tom de Vries <tom@codigy.nl>
2011-11-15 18:01:29 +0100

    Added tests

--- web-proxy/test/blacklist_spec.js
+++ web-proxy/test/blacklist_spec.js
@@ -2,6 +2,7 @@ var config = require('../lib/config').get(process.env);
 var blacklist = require('../lib/blacklist');
 
 blacklist.set_API_URL(config.API_URL);
+blacklist.set_API_OPTIONS(config.API_OPTIONS);
 
 exports['should call something'] = function(beforeExit, assert){
   var x = 'moi'
--- web-proxy/test/server_spec.js
+++ web-proxy/test/server_spec.js
@@ -1,6 +1,37 @@
 var config = require('../lib/config').get(process.env);
 var server = require('../lib/server').getServer(config);
 
+config_path = process.env.CONFIG_PATH || '../config/';
+
+testserver_env = {};
+testserver_env.CONFIG_PATH = config_path;
+testserver_env.NODE_ENV = 'testserver';
+
+development_env = {};
+development_env.CONFIG_PATH = config_path;
+development_env.NODE_ENV = 'development';
+
+production_env = {};
+production_env.CONFIG_PATH = config_path;
+production_env.NODE_ENV = 'production';
+
+
+var testserver_config   = require('../lib/config').get(testserver_env);
+var development_config  = require('../lib/config').get(development_env);
+var production_config   = require('../lib/config').get(production_env);
+
+/** Config */
+exports['htpasswd should NOT be set on development env'] = function(beforeExit, assert){
+  assert.eql(development_config.core.htpasswd.username, "");
+};
+exports['htpasswd should be set on testserver env'] = function(beforeExit, assert){
+  assert.isDefined(testserver_config.core.htpasswd.username, "proxyuser");
+};
+exports['htpasswd should be set on production env'] = function(beforeExit, assert){
+  assert.eql(production_config.core.htpasswd.username, "proxyuser");
+};
+
+/** Server */
 exports['The index should render succesful'] = function(beforeExit, assert){
   assert.response(server, {
       url: '/',
