8f935c36f1cde14a4efa54234687a2c2789ec91d
Tom de Vries <tom@codigy.nl>
2011-11-15 16:39:34 +0100

    Cleaned the server, added blacklist

--- web-proxy/lib/server.js
+++ web-proxy/lib/server.js
@@ -1,110 +1,80 @@
 /* jslint node: true */
+function getServer(config) {
 
-function getServer(config_path) {
-  
-  // The actual server
+  /**
+   *  The actual server
+   */
   var server = require('express').createServer();
   server.configure(function() {
     server.set('views', __dirname + '/../views');
   });
 
-
   /**
-   *	We execute our requests using Restler
-   *  because it follows redirects if needed
+   *	We execute our requests using Restler (supports redirects)
    */
   var restler       = require('restler');
   var urlvalidation = require('./urlvalidation');
-  var fs            = require('fs');
+  var blacklist     = require('./blacklist');
 
-  config = require('./read_config').read_conf(config_path, fs, server.settings.env);
-
-  var STATIC_URL  = config['static'].protocol + config['static'].hostname + ':' + config['static'].port;
-  var API_URL     = config.core.protocol + config.core.hostname + ':' + config.core.port;
-  var PROXY_URL   = config.proxy.protocol + config.proxy.hostname + ':' + config.proxy.port;
-  var LIB_URL     = STATIC_URL + "/lib";
-  var INTERNAL_PROXY_PORT = parseInt(config.proxy.internal_port, 10);
-  
-  server.INTERNAL_PROXY_PORT = INTERNAL_PROXY_PORT;
-  
-  // Use Jade as templating engine
+  /**
+   *  Use Jade as templating engine
+   */
   server.set('view engine', 'jade');
 
-  function get_modus(modus){
-    if (modus === undefined){
-      return 'default';
-    } else {
-      return modus;
-    }
-  
-  }
+  /**
+   *	Routes and request handling
+   */
+  server.get('/',       render_page('index'));
+  server.get('/header', render_page('header'));
+  server.get('/search', get_search);
+  server.get('/parse',  get_parse);
+  server.get('/submit', get_submit);
 
   /**
    * Add base url and inject proxy.js, and return the proxied site
    */
-
   function injectFactlinkJs(html_in, site, scrollto, modus, successFn, errorFn) {
     var FactlinkConfig = {
       modus: modus,
-      api: API_URL,
-      lib: LIB_URL,
-      proxy: PROXY_URL,
+      api: config.API_URL,
+      lib: config.LIB_URL,
+      proxy: config.PROXY_URL,
       url: site
     };
-  
-    restler.get(API_URL + '/sites/' + encodeURIComponent(site) + '/info', {parser:restler.parsers.json}).on('complete', function(data) {
-      if (data.blacklisted !== true) {
-        var html = html_in.replace(/<head[^>]*>/i, '$&<base href="' + site + '" />');
 
-        if (scrollto !== undefined && !isNaN(parseInt(scrollto, 10))) {
-          FactlinkConfig.scrollto = parseInt(scrollto, 10);
-        }
-        var set_urls = '<script>window.FactlinkConfig = ' + JSON.stringify(FactlinkConfig) + '</script>';
+    blacklist.if_allowed(site,function() {
+      var html = html_in.replace(/<head[^>]*>/i, '$&<base href="' + site + '" />');
 
-        var load_proxy_js = '<script src="' + STATIC_URL + '/proxy/scripts/proxy.js?' + Number(new Date()) + '"></script>';
-        html = html.replace(/<\/head>/i, set_urls + load_proxy_js + '$&');
-        successFn(html);
-      } else {
-        errorFn(html_in);
+      if (scrollto !== undefined && !isNaN(parseInt(scrollto, 10))) {
+        FactlinkConfig.scrollto = parseInt(scrollto, 10);
       }
+
+      var set_urls      = '<script>window.FactlinkConfig = ' + JSON.stringify(FactlinkConfig) + '</script>';
+      var load_proxy_js = '<script src="' + config.STATIC_URL + '/proxy/scripts/proxy.js?' + Number(new Date()) + '"></script>';
+      html = html.replace(/<\/head>/i, set_urls + load_proxy_js + '$&');
+      successFn(html);
+    },function(){
+      successFn(html_in);
     });
   }
 
-
-
-
   function handleProxyRequest(res, url, scrollto, modus, form_hash) {
-    if (url === undefined){
+    // Welcome page
+    if (url === undefined) {
       res.render('welcome.jade',{
         layout:false,
         locals: {
-          static_url: STATIC_URL,
-          proxy_url: PROXY_URL,
-          factlinkModus : modus
+          proxy_url:      config.PROXY_URL,
+          static_url:     config.STATIC_URL,
+          factlinkModus:  modus
         }
       });
       return;
     }
+    
+    modus = get_modus(modus);
 
-    errorhandler = function(data) {
-      console.error(new Date().toString() + " : Failed on: " + url);
-
-      res.render('something_went_wrong', {
-        layout: false,
-        locals: {
-          static_url: STATIC_URL,
-          proxy_url: PROXY_URL,
-          site: url,
-          factlinkModus : modus
-        }
-      });
-    };
-
-    if (modus !== "addToFact") {
-      modus = "default";
-    }
-
-
+    // A site is needed
     site = urlvalidation.clean_url(url);
     if (site === undefined) {
       errorhandler({});
@@ -112,59 +82,35 @@ function getServer(config_path) {
     }
 
     var request = restler.get(site, form_hash);
+        
+    errorhandler = function(data) {
+      console.error(new Date().toString() + " : Failed on: " + url);
+      res.render('something_went_wrong', {
+        layout: false,
+        locals: {
+          static_url: config.STATIC_URL,
+          proxy_url: config.PROXY_URL,
+          site: url,
+          factlinkModus : modus
+        }
+      });
+    };
 
-    request.on('complete', function(data) {
+    completehandler = function(data) {
       injectFactlinkJs(data, site, scrollto, modus, function(html) {
         res.write(html);
-      
         res.end();
       }, function(html) {
         console.info( html );
-        res.write(html);
-      
+        res.write(html);      
         res.end();
       });
-    });
-
-
-    request.on("error", errorhandler);
+    };
+    
+    // Handle states
+    request.on('complete',  completehandler);
+    request.on('error',     errorhandler);
   }
-
-  /** 
-   *	Regular get requests
-   */
-  server.get('/parse', function(req, res) {
-    console.info("\nGET /parse");
-    var site = req.query.url;
-    var scrollto = req.query.scrollto;
-    var modus = get_modus(req.query.factlinkModus);
-
-    handleProxyRequest(res, site, scrollto, modus, {});
-  });
-
-
-  /** 
-   * Handling forms
-   * --------------
-   * Forms get posted with a hidden 'factlinkFormUrl' field,
-   * which is added by the proxy (in proxy.js). This is the 'action' URL which
-   * the form normally submits its form to.
-   *
-   */
-  server.get('/submit', function(req, res) {
-    var form_hash = req.query;
-
-    var modus = get_modus(form_hash.factlinkModus);
-    delete form_hash.factlinkModus;
-
-    var site = form_hash.factlinkFormUrl;
-    delete form_hash.factlinkFormUrl;
-
-    handleProxyRequest(res, site, undefined, modus, {
-      'query': form_hash
-    });
-  });
-
 
   function create_url(base, query) {
     var url;
@@ -187,16 +133,19 @@ function getServer(config_path) {
     return url;
   }
 
+  /**
+   *  Render a jade template
+   */
   function render_page(pagename) {
     return function(req, res) {
-      var header_url = create_url(PROXY_URL + "/header", req.query);
-      var parse_url = create_url(PROXY_URL + "/parse", req.query);
+      var header_url = create_url(config.PROXY_URL + "/header", req.query);
+      var parse_url = create_url(config.PROXY_URL + "/parse", req.query);
       res.render(pagename, {
         layout: false,
         locals: {
-          static_url: STATIC_URL,
-          proxy_url: PROXY_URL,
-          core_url: API_URL,
+          static_url: config.STATIC_URL,
+          proxy_url: config.PROXY_URL,
+          core_url: config.API_URL,
           page_url: req.query.url,
           factlinkModus: get_modus(req.query.factlinkModus),
           header_url: header_url,
@@ -206,24 +155,59 @@ function getServer(config_path) {
     };
   }
 
-  /**
-   *	Render the header with the url bar
-   */
-  server.get('/header', render_page('header'));
-  server.get('/', render_page('index'));
 
+  /**
+   *  Proxy modus
+   */
+  function get_modus(modus) {
+    if (modus === undefined){
+      return 'default';
+    } else {
+      return modus;
+    }
+  }
+
+  /**
+   *  Search on Factlink enabled Google
+   */
+  function get_search(req, res) {
+    console.info("\nGET /search");
+    var query       = req.query.query;
+    var google_url  = "http://google.com/search?as_q=" + encodeURIComponent(query);
+    var redir_url   = config.PROXY_URL + "/?url=" + google_url + "&factlinkModus=" + get_modus(req.query.factlinkModus);
+
+    console.info("\nGET /search to: " + redir_url);
+    res.redirect(redir_url);
+  }
+
+  /**
+   *  Inject Factlink in regular get requests
+   */
+  function get_parse(req, res) {
+    console.info("\nGET /parse");
+    var site      = req.query.url;
+    var scrollto  = req.query.scrollto;
+    var modus     = get_modus(req.query.factlinkModus);
+
+    handleProxyRequest(res, site, scrollto, modus, {});
+  }
 
   /** 
-   *	Search with google (top frame)
+   * Forms get posted with a hidden 'factlinkFormUrl' field,
+   * which is added by the proxy (in proxy.js). This is the 'action' URL which
+   * the form normally submits its form to.
    */
-  server.get('/search', function(req, res) {
-    console.info("\nGET /search");
-    var query = req.query.query;
-    var google_url = "http://google.com/search?as_q=" + encodeURIComponent(query);
-    var redir_url = PROXY_URL + "/?url=" + google_url + "&factlinkModus=" + get_modus(req.query.factlinkModus);
-    console.info("\n to "+redir_url);
-    res.redirect(redir_url);
-  });
+  function get_submit(req, res) {
+    var form_hash = req.query;
+    var modus     = get_modus(form_hash.factlinkModus);
+    delete form_hash.factlinkModus;
+    var site      = form_hash.factlinkFormUrl;
+    delete form_hash.factlinkFormUrl;
+
+    handleProxyRequest(res, site, undefined, modus, {
+      'query': form_hash
+    });
+  }
 
   return server;
 }
