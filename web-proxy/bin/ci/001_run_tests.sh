7802caa473d97b2e67bb7273b40ff7ad6f82bd03
Tom de Vries <tom@codigy.nl>
2011-11-16 09:40:04 +0100

    Exports on it's own line

--- web-proxy/bin/ci/001_run_tests.sh
+++ web-proxy/bin/ci/001_run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 npm install || exit 1
-export NODE_ENV=testserver; export CONFIG_PATH=/home/jetty/jobs/factlink-core/workspace/config/; node_modules/expresso/bin/expresso || exit 1
+export NODE_ENV=testserver;
+export CONFIG_PATH=/home/jetty/jobs/factlink-core/workspace/config/;
+node_modules/expresso/bin/expresso || exit 1
 exit
