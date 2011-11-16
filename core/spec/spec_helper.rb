bb40979be5b5be4bb512a307b9f9105419329a14
Tom de Vries <tom@codigy.nl>
2011-11-16 17:07:07 +0100

    One little capybara slipped throught my fingers... Kill the beast! Now we can be testing one, two, soundcheck everything seems okay again!

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -8,11 +8,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-
-# Add this to load Capybara integration:
-require 'capybara/rspec'
-require 'capybara/rails'
-
 RSpec.configure do |config|
   #mix in FactoryGirl methods
   config.include Factory::Syntax::Methods
