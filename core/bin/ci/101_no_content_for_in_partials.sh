8685d6d4a23edf6d28e8493c1003f727ddded0ce
Mark IJbema <markijbema@gmail.com>
2011-11-17 17:11:11 +0100

    changes hoping to fix the ci check

--- core/bin/ci/101_no_content_for_in_partials.sh
+++ core/bin/ci/101_no_content_for_in_partials.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-banner "partial content_for check"
-ERROR_FILES=`ack content_for -l | grep '/_' | wc -l`
+banner "partial content for check"
 
+ERROR_FILES=`ack-grep content_for -l | grep "/_" | wc -l`
+echo $ERROR_FILES
 if [ "$ERROR_FILES" -gt "0" ]; then
   echo "Use no content_for in partials."
   echo "Fix those files:"
-  ack content_for -l | grep '/_'
+  ack-grep content_for -l | grep '/_'
   exit 1
 else
   exit 0
