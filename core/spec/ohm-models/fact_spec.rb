e6ba076522b7df59357f25fa8730887ade652141
Tom de Vries <tom@codigy.nl>
2011-11-21 10:25:51 +0100

    added tests for :both .evidence on Fact
    Signed-off-by: Tom de Vries <tom@codigy.nl>

--- core/spec/ohm-models/fact_spec.rb
+++ core/spec/ohm-models/fact_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+def other_one(this)
+  if this == :supporting
+    :weakening
+  else
+    :supporting
+  end
+end
+
 describe Fact do
 
   subject {FactoryGirl.create(:fact)}
@@ -7,6 +15,7 @@ describe Fact do
   before(:each) do
     @parent = FactoryGirl.create(:fact)
     @factlink = FactoryGirl.create(:fact)
+    @factlink2 = FactoryGirl.create(:fact)
 
     @user1 = FactoryGirl.create(:user)
     @user2 = FactoryGirl.create(:user)
@@ -52,16 +61,32 @@ describe Fact do
     @gu.created_facts.to_a.should =~ [subject]
   end
 
+  
+  context "evidence" do
+    describe "should initially be empty" do
 
-  [:supporting, :weakening].each do |relation|
-    def other_one(this)
-      if this == :supporting
-        :weakening
-      else
-        :supporting
+      [:supporting, :weakening].each do |relation|
+        it "should have no evidence for a type " do
+          subject.evidence(relation).count.should == 0
+        end
+      end
+
+      it "should have no evidence for :both type " do
+        subject.evidence(:both).count.should == 0
       end
     end
+  end
 
+  [:supporting, :weakening].each do |relation|
+
+    describe ".evidence" do
+      [:supporting, :weakening, :both].each do |relation|
+        it "should initially be empty" do
+          subject.evidence(relation).count.should == 0
+        end
+      end
+    end
+    
     describe ".add_evidence" do
 
       context "with one #{relation} fact" do
@@ -71,6 +96,15 @@ describe Fact do
 
         its(:get_opinion) {should be_a(Opinion)}
 
+        describe "should have one evidence" do
+          it "for the relation #{relation}" do
+            subject.evidence(relation).count.should == 1
+          end 
+          it "for :both" do
+            subject.evidence(:both).count.should == 1
+          end
+        end
+        
         describe ".delete the fact, which has a #{relation} fact" do
           before do
             @subject_id = subject.id
@@ -104,6 +138,37 @@ describe Fact do
           it "should remove the #{relation} factrelation" do
             FactRelation[@relation_id].should be_nil
           end
+        end
+      end
+      
+      context "with two #{relation} fact" do
+        before do
+          @fr = subject.add_evidence(relation,@factlink,@user1)
+          @fr2 = subject.add_evidence(relation,@factlink2,@user1)
+        end
+        
+        it "should have two #{relation} facts" do
+          subject.evidence(relation).count.should == 2
+        end
+        it "should have two facts for :both" do
+          subject.evidence(:both).count.should == 2
+        end
+      end
+      
+      context "with one #{relation} fact and one #{other_one(relation)} fact" do
+        before do
+          @fr = subject.add_evidence(relation,@factlink,@user1)
+          @fr2 = subject.add_evidence(other_one(relation),@factlink2,@user1)
+        end
+        
+        it "should have one #{relation} fact" do
+          subject.evidence(relation).count.should == 1
+        end
+        it "should have one #{other_one(relation)} fact" do
+          subject.evidence(other_one(relation)).count.should == 1
+        end
+        it "should have two facts for :both" do
+          subject.evidence(:both).count.should == 2
         end
       end
     end
