21a999a1383b0f818667f573824755080dc3506e
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-11-22 17:54:08 +0100

    The Channels collection should know which Channel is the current active!

--- core/public/javascripts/backbone/collections/channels.js
+++ core/public/javascripts/backbone/collections/channels.js
@@ -1,8 +1,20 @@
 window.ChannelList = Backbone.Collection.extend({
   model: Channel,
   
+  initialize: function() {
+    this.bind('activate', this.setActiveChannel);
+  },
+  
   url: function() {
     return '/' + Router.getUsername() + '/channels/';
+  },
+  
+  setActiveChannel: function(channel) {
+    if ( this.activeChannel && this.activeChannel.id !== channel.id ) {
+      this.activeChannel.trigger('deactivate');
+    }
+    
+    this.activeChannel = channel;
   }
 });
 
