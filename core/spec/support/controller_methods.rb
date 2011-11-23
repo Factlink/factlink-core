c836ffadf0fa338e9784504ccd7943358b3dca86
Mark IJbema <markijbema@gmail.com>
2011-11-23 10:26:46 +0100

    better namespacing

--- core/spec/support/controller_methods.rb
+++ core/spec/support/controller_methods.rb
@@ -1,5 +1,5 @@
 module ControllerMethods
-  include Devise::TestHelpers
+  include ::Devise::TestHelpers
   def authenticate_user!(user)
     request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
   end
