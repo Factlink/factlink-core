b8163ae4a32737ea283eb6ce9605ae4877910eb9
Tom de Vries <tom@codigy.nl>
2011-11-24 13:17:56 +0100

    Added config for the new staging environment
    Signed-off-by: Tom de Vries <tom@codigy.nl>

---
+++ core/config/deploy/staging.rb
@@ -0,0 +1,9 @@
+server 'staging.factlink.com', :app, :web, :primary => true
+
+set :deploy_env, 'production'
+
+set :branch, "master"
+
+role :web, "staging.factlink.com"                          # Your HTTP server, Apache/etc
+role :app, "staging.factlink.com"                          # This may be the same as your `Web` server
+role :db,  "staging.factlink.com", :primary => true # This is where Rails migrations will run
