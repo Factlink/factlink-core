dd709449d1b782c5ff0c76b7925cf22c3ebf9307
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-11-24 11:39:18 +0100

    Added remove/add actions to subchannel controller

--- core/app/controllers/subchannels_controller.rb
+++ core/app/controllers/subchannels_controller.rb
@@ -2,12 +2,38 @@ class SubchannelsController < ChannelsController
   before_filter :get_user
 
   before_filter :load_channel
+  
+  before_filter :load_subchannel,
+    :except => [
+      :index,
+    ]
+    
+  before_filter :is_authorized?,
+    :except => [
+      :index,
+    ]
       
   def index
     @contained_channels = @channel.contained_channels
     
     respond_to do |format|
       format.json { render :json => @contained_channels.map {|ch| Subchannels::SubchannelItem.for_channel_and_view(ch,self)} }
+    end
+  end
+  
+  def add
+    @channel.add_channel(@subchannel)
+    
+    respond_to do |format|
+      format.json { render :json => @channel.contained_channels.map {|ch| Subchannels::SubchannelItem.for_channel_and_view(ch,self)}, :location => @channel }
+    end
+  end
+  
+  def remove
+    @channel.remove_channel(@subchannel)
+    
+    respond_to do |format|
+      format.json { render :json => @channel.contained_channels.map {|ch| Subchannels::SubchannelItem.for_channel_and_view(ch,self)}, :location => @channel }
     end
   end
   
