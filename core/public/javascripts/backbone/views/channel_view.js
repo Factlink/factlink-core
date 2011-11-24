543c01f8a5b352928c8b4ff48d65efc060900c42
Joël Kuiper <me@joelkuiper.eu>
2011-11-24 15:38:52 +0100

    Hover Intent behavior for the add to channels menu

--- core/public/javascripts/backbone/views/channel_view.js
+++ core/public/javascripts/backbone/views/channel_view.js
@@ -28,14 +28,34 @@ window.ChannelView = Backbone.View.extend({
     }
   },
 
- initMoreButton: function() { 
-   var containedChannels = this.el.find('#contained-channels');
-   if  ( containedChannels ) {
+  initMoreButton: function() { 
+    var containedChannels = this.el.find('#contained-channels');
+    if  ( containedChannels ) {
     this.el.find('.more-button').bind('click', function() { 
         var button = $(this).find(".label");
         containedChannels.slideToggle(function(e) { 
           button.text($(button).text() === 'more' ? 'less' : 'more');
         });
+      });
+    }
+  },
+
+  initSubChannelMenu: function() { 
+    if( this.model.get("followable?") ) { 
+      var addToChannelButton = this.el.find("#add_to_channel");
+      var followChannelMenu = this.el.find("#follow-channel");
+      followChannelMenu.css({"left": addToChannelButton.position().left});
+      addToChannelButton.hoverIntent(
+        function() { followChannelMenu.fadeIn("fast"); }, 
+        function() { followChannelMenu.delay(600).fadeOut("fast"); }
+      );
+      followChannelMenu.on("mouseover", function() { 
+        followChannelMenu.stop(true, true).css({"opacity": "1"});
+      });
+      followChannelMenu.on("mouseout", function() { 
+       if (!followChannelMenu.find("input#channel_title").is(":focus")) {
+          followChannelMenu.delay(500).fadeOut("fast");
+        }
       });
     }
   },
@@ -50,7 +70,7 @@ window.ChannelView = Backbone.View.extend({
         .html( $.mustache(this.tmpl, this.model.toJSON() ));
       
       this.initSubChannels();
-      
+      this.initSubChannelMenu();
       this.initAddToFact();
       this.initMoreButton(); 
         
