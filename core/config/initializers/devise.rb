11cb5d18e16f3b798c084f923516dfa64d6ecb92
Tom de Vries <tom@codigy.nl>
2011-11-28 10:50:59 +0100

    Allow longer passwords on registration
    Signed-off-by: Tom de Vries <tom@codigy.nl>

--- core/config/initializers/devise.rb
+++ core/config/initializers/devise.rb
@@ -81,7 +81,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 6..20.
-  # config.password_length = 6..20
+  config.password_length = 6..32
 
   # Regex to use to validate the email address
   # config.email_regexp = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
