cdc538efc0d925ef985b15798c65c70e40fa54ff
Mark IJbema <markijbema@gmail.com>
2011-11-28 11:34:07 +0100

    moved the doscrolling functionality to the scrollto.js

--- js-library/src/js/scripts/doscrolling.js
+++
@@ -1,8 +0,0 @@
-(function(Factlink, $, _, easyXDM, undefined) {
-  $(window).bind('factlink.factsLoaded', function(){
-    if (Factlink !== undefined && FactlinkConfig !== undefined && FactlinkConfig.scrollto) {
-      // Get al the facts on the current page
-      Factlink.scrollTo(FactlinkConfig.scrollto);
-    }
-  });
-})(window.Factlink, Factlink.$, Factlink._, Factlink.easyXDM);
--- js-library/src/js/scrollto.js
+++ js-library/src/js/scrollto.js
@@ -2,4 +2,12 @@
   Factlink.scrollTo = function(fact_id){
     $('body')._scrollable().scrollTo("span[data-factid="+fact_id+"]", 800);
   };
+
+  $(window).bind('factlink.factsLoaded', function(){
+    if (Factlink !== undefined && FactlinkConfig !== undefined && FactlinkConfig.scrollto) {
+      // Get al the facts on the current page
+      Factlink.scrollTo(FactlinkConfig.scrollto);
+    }
+  });
+  
 })(window.Factlink, Factlink.$, Factlink._, Factlink.easyXDM);
