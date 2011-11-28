299316a548d280f656ec3547863cf381bbd08908
Tom de Vries <tom@codigy.nl>
2011-11-28 10:32:50 +0100

    recalculates should work in correct environment
    Signed-off-by: Tom de Vries <tom@codigy.nl>

--- core/bin/server/start_recalculate.sh
+++ core/bin/server/start_recalculate.sh
@@ -5,7 +5,7 @@ fact_graph_count=`ps aux | grep -c 'rake fact_graph:recalculate'`
 
 if [ "$fact_graph_count" -lt "2" ]; then
    cd /applications/factlink-core/current/
-   nohup /usr/local/rvm/bin/rake fact_graph:recalculate RAILS_ENV=testserver > /applications/factlink-core/current/log/fact_graph.log 2>&1 &
+   nohup /usr/local/rvm/bin/rake fact_graph:recalculate RAILS_ENV=production > /applications/factlink-core/current/log/fact_graph.log 2>&1 &
 fi
 
 
@@ -14,7 +14,7 @@ channels_count=`ps aux | grep -c 'rake channels:recalculate'`
 
 if [ "$channels_count" -lt "2" ]; then
    cd /applications/factlink-core/current/
-   nohup /usr/local/rvm/bin/rake channels:recalculate RAILS_ENV=testserver > /applications/factlink-core/current/log/channels.log 2>&1 &
+   nohup /usr/local/rvm/bin/rake channels:recalculate RAILS_ENV=production > /applications/factlink-core/current/log/channels.log 2>&1 &
 fi
 
 exit 0
