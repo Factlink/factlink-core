f6f63523e1e34281cf0f325afd06ece8eef4f143
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-11-29 16:49:36 +0100

    Added .user to channel object

--- core/public/javascripts/backbone/models/channel.js
+++ core/public/javascripts/backbone/models/channel.js
@@ -1,7 +1,9 @@
 window.Channel = Backbone.Model.extend({
-  initialize: function() {
+  initialize: function(opts) {
     this.bind('activate', this.setActive);
     this.bind('deactivate', this.setNotActive);
+    
+    this.user = new User(opts.created_by);
   },
   
   setActive: function() {
