ec62280ff9de0f6369e5e076308b2c60e47649a0
Mark IJbema <markijbema@gmail.com>
2011-11-30 15:49:03 +0100

    added spec for ability, filled in ability

--- core/app/models/ability.rb
+++ core/app/models/ability.rb
@@ -1,16 +1,36 @@
 class Ability
   include CanCan::Ability
 
+  def user
+    @user
+  end
+
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    @user=user
     
     unless user.new?
       can :update, user
-      can :index, Channel
-      can :read, Channel
-      can :manage, Channel do |ch|
-        ch.created_by == user.graph_user
-      end
+      define_channel_abilities
+      define_fact_abilities
+    end
+    
+  end
+
+  def define_channel_abilities
+    can :index, Channel
+    can :read, Channel
+    can :manage, Channel do |ch|
+      ch.created_by == user.graph_user
     end
   end
+  
+  def define_fact_abilities
+    can :index, Fact
+    can :read, Fact
+    can :manage, Fact do |f|
+     f.created_by == user.graph_user
+    end
+  end
+  
+
 end
