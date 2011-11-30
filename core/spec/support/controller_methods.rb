18d10352950e3cc24850402ddd294f7dbb35ad1c
Mark IJbema <markijbema@gmail.com>
2011-11-30 13:56:55 +0100

    extracted out ability stuff

--- core/spec/support/controller_methods.rb
+++ core/spec/support/controller_methods.rb
@@ -2,4 +2,15 @@ module ControllerMethods
   def authenticate_user!(user)
     request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
   end
+  
+  def get_ability
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    subject.should_receive(:current_ability).any_number_of_times.and_return(@ability)
+  end
+  
+  def should_check_can(*args)
+    @ability.should_receive(:authorize!).with(*args)
+  end
+  
 end
