8055aadeaa441f05a59ab96176e76128cac65629
Mark IJbema <markijbema@gmail.com>
2011-11-30 14:33:01 +0100

    explained design decision (because i already tried to revert it ;) )

--- core/spec/support/controller_methods.rb
+++ core/spec/support/controller_methods.rb
@@ -3,6 +3,8 @@ module ControllerMethods
     request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
   end
   
+  # we call this explicitly in the before, so the default ability is empty
+  # that means we need to explicitly test that each ability is tested
   def get_ability
     @ability = Object.new
     @ability.extend(CanCan::Ability)
