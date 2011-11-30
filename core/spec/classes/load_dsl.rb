135139ff58619bc89f43b1a2ac36d064ae8dca01
Mark IJbema <markijbema@gmail.com>
2011-11-30 10:51:32 +0100

    moved the loaddsl to a more logical place; now the tests get picked up by rspec again

--- core/spec/classes/load_dsl.rb
+++ core/spec/classes/load_dsl.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require File.expand_path('../../../db/load_dsl.rb', __FILE__)
 
 describe LoadDsl do
   it "should add one channel when adding the same channel twice" do
@@ -10,11 +9,11 @@ describe LoadDsl do
   end
   
   it "should error when a user is made without passoword" do
-      expect { subject.user "merijn" }.to raise_error(UndefinedUserError)
+      expect { subject.user "merijn" }.to raise_error(LoadDsl::UndefinedUserError)
   end
 
   it "should error when a user is made without passoword" do
-      expect { subject.fact "hoi" }.to raise_error(UndefinedUserError)
+      expect { subject.fact "hoi" }.to raise_error(LoadDsl::UndefinedUserError)
   end
   
   it "creating one channel with another channel as subchannel, and redefining the second channel should only create two channels" do
@@ -34,6 +33,11 @@ describe LoadDsl do
   end
 
   it "should work" do
+    subject.run do
+      user "merijn", "merijn@gmail.com", "123hoi", "merijn481"
+      user "tom", "tom@codigy.nl", "123hoi", "tomdev"
+    end
+    subject = LoadDsl.new
     subject.run do
       user "merijn", "merijn@gmail.com", "123hoi", "merijn481"
       user "tom", "tom@codigy.nl", "123hoi", "tomdev"
