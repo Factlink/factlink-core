2f62240c196a49de50b4c334ba0dd83a7846b07a
Mark IJbema <markijbema@gmail.com>
2011-11-30 11:08:55 +0100

    added most important abilities

--- core/app/models/ability.rb
+++ core/app/models/ability.rb
@@ -6,6 +6,8 @@ class Ability
     
     unless user.new?
       can :update, user
+      can :index, Channel
+      can :read, Channel
       can :manage, Channel do |ch|
         ch.created_by == user.graph_user
       end
