1fcc191792991eab5a148c2ec3377cc9d02424fb
Mark IJbema <markijbema@gmail.com>
2011-12-01 13:06:56 +0100

    added deploy to staging

--- web-proxy/bin/ci/002_deploy.sh
+++ web-proxy/bin/ci/002_deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 source "$HOME/.rvm/scripts/rvm" 
 rvm use --default 1.9.2-p290 || exit 1
-cap -q testserver deploy || exit 1
+if [ "$GIT_BRANCH" == "release" ] ; then
+  SERVER="staging"
+else
+  SERVER="testserver"
+fi
+
+echo "deploying to $SERVER"
+cap -q $SERVER deploy || exit 1
+echo "deployed to $SERVER"
 exit
