d12c95b7218d24628b70f6a6a668a3075db77308
Mark IJbema <markijbema@gmail.com>
2011-12-01 17:03:02 +0100

    improved user spec

--- core/spec/models/user_spec.rb
+++ core/spec/models/user_spec.rb
@@ -2,37 +2,29 @@ require 'spec_helper'
 
 describe User do
 
-  subject {FactoryGirl.create(:user)}
+  subject {FactoryGirl.create :user}
 
-  before(:each) do
-    @parent = FactoryGirl.create(:fact)
-    @child1 = FactoryGirl.create(:fact)
-    @child2 = FactoryGirl.create(:fact)
+  let(:fact) {FactoryGirl.create :fact}
+  let(:child1) {FactoryGirl.create :fact}
 
-    @user1 = FactoryGirl.create(:user)
-    @user2 = FactoryGirl.create(:user)
-
-    @factlink = FactoryGirl.create(:fact)
-    @factlink2 = FactoryGirl.create(:fact)
-  end
   context "Initially" do
     it {subject.graph_user.facts.to_a.should == []}
     it {subject.graph_user.should == subject.graph_user }
   end
 
   it "should have one active factlink after adding believe" do
-    @child1.add_opinion(:beliefs, @user1.graph_user)
-    @user1.graph_user.facts.size == 1
+    child1.add_opinion(:beliefs, subject.graph_user)
+    subject.graph_user.facts.size == 1
   end
 
   it "should have a GraphUser" do
-    @user1.graph_user.should be_a(GraphUser)
+    subject.graph_user.should be_a(GraphUser)
   end
 
   [:beliefs,:doubts,:disbeliefs].each do |type|
     it "should store the Fact ID in the graph_user object when a user #{type} a fact" do
-      @factlink.add_opinion(type, @user1.graph_user )
-      @user1.graph_user.has_opinion?(type,@factlink).should == true
+      fact.add_opinion(type, subject.graph_user )
+      subject.graph_user.has_opinion?(type,fact).should == true
     end
   end
 end
