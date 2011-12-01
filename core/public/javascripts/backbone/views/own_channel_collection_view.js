e7ec6b7c8babd32a4e45bf1ca95607706fde7ba7
Mark IJbema <markijbema@gmail.com>
2011-12-01 12:52:01 +0100

    fixes add to channel functionality

--- core/public/javascripts/backbone/views/own_channel_collection_view.js
+++ core/public/javascripts/backbone/views/own_channel_collection_view.js
@@ -24,7 +24,7 @@ window.OwnChannelCollectionView = Backbone.View.extend({
         
     if ( val.length > 0 ) {
       $.ajax({
-        url: currentUser.url() + '/channels',
+        url: Router.getUsername() + '/channels',
         data: {
           title: val,
           for_channel: currentChannel.id
