3c8e320f45e4b86545fa13e6be95b0d8e7f5a600
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-12-01 16:27:16 +0100

    Cleaned up fact bubble mustache template

--- core/app/views/facts/_fact_bubble.rb
+++ core/app/views/facts/_fact_bubble.rb
@@ -1,72 +1,16 @@
 module Facts
   class FactBubble < Mustache::Railstache
-    def current_graph_user
-      @current_graph_user ||= self[:current_user].graph_user
+    def self.for_fact_and_view(fact, view, channel=nil, modal=nil)
+      fb = new(false)
+      fb.view = view
+      fb[:channel] = channel
+      fb[:fact] = fact
+      fb[:modal] = modal
+      return fb
     end
     
-    def channels
-      current_graph_user.editable_channels_for(self[:fact])
-    end
-  
-    def channel_path
-      channels_path(@current_user.username)
-    end
-    
-    def id
-      self[:fact].id
-    end
-    
-    def user_signed_in?(user=self[:current_user])
-      user
-    end
-  
-    def authority
-      self[:fact].get_opinion.as_percentages[:authority]
-    end
-
-    def opinions
-      opinions_for_user_and_fact(self[:fact])
-    end
-  
-    def opinions_for_user_and_fact(fact)
-      [
-        {
-          :type => 'believe',
-          :groupname => 'Agree',
-          :percentage => fact.get_opinion.as_percentages[:believe][:percentage],
-          :is_current_opinion => user_signed_in?(current_user) && current_graph_user.has_opinion?(:believes, fact),
-          :color => "#98d100",
-        },
-        {
-          :type => 'doubt',
-          :groupname => 'Neutral',
-          :percentage => fact.get_opinion.as_percentages[:doubt][:percentage],
-          :is_current_opinion => user_signed_in?(current_user) && current_graph_user.has_opinion?(:doubts, fact),
-          :color => "#36a9e1",
-        },
-        {
-          :type => 'disbelieve',
-          :groupname => 'Disagree',
-          :percentage => fact.get_opinion.as_percentages[:disbelieve][:percentage],
-          :is_current_opinion => user_signed_in?(current_user) && current_graph_user.has_opinion?(:disbelieves, fact),
-          :color => "#e94e1b",
-        }
-      ]
-    end
-
-    def graph_users_with_link(graph_users)
-      graph_users.map do |gu|
-        {
-          :username => gu.user.username,
-          :link => link_for(gu),
-        }
-      end
-    end
-
-    def link_for(gu)
-      imgtag = image_tag(gu.user.avatar.url(:small), :size => "24x24", :title => "#{gu.user.username} (#{gu.rounded_authority})")
-      path = view.user_profile_path(gu.user.username)
-      link_to( imgtag, path, :target => "_top" )
+    def user_signed_in?
+      self.view.user_signed_in?
     end
 
     def i_am_fact_owner
@@ -77,42 +21,72 @@ module Facts
       (user_signed_in? and i_am_fact_owner) ? " edit " : ""
     end
 
-    def pretty_url
-      if self[:fact].site
-        self[:fact].site.url.gsub(/http(s?):\/\//,'').split('/')[0]
-      else
-        ""
-      end
-    end
-
     def delete_link
       link_to(image_tag('/images/trash.gif') + "Delete", fact_path(self[:fact].id), :confirm => "You will delete this Factlink. Are you sure?", :method => :delete, :remote => true )
     end
 
-    def proxy_scroll_url
-      FactlinkUI::Application.config.proxy_url + "?url=" + URI.escape(self[:fact].site.url) + "&scrollto=" + URI.escape(self[:fact].id)
-    end
-
-    def show_links
-      not (self[:hide_links_for_site] and  self[:fact].site == self[:hide_links_for_site]) and self[:fact].site
-    end
-
     def scroll_to_link
-      show_links ? link_to(self.pretty_url, self[:fact].site.url, :target => "_blank") : self.pretty_url
+      show_links ? link_to(pretty_url, self[:fact].site.url, :target => "_blank") : pretty_url
     end
 
     def link
       displaystring = h self[:fact].data.displaystring
       show_links ? link_to(displaystring, proxy_scroll_url, :target => "_blank") : displaystring
     end
-    
-    def user_opinion
-      current_graph_user.opinion_on(self[:fact])
-    end
   
     def last_activity
       activity = self[:fact].activities(1)[0]
       activity && link_to(activity.user.user.username, user_profile_path(activity.user.user.username), target: "_top") + " " + activity.action + " " + time_ago_in_words(activity.created_at) + " ago"
     end
+    
+    def fact_title
+      self[:fact].data.title
+    end
+    
+    def fact_id
+      self[:fact].id
+    end
+    
+    def fact_wheel
+      Facts::FactWheel.for_fact_and_view(self[:fact], self.view)
+    end
+    
+    def to_hash
+      {
+        :fact_id => fact_id,
+        :editable_title_class => editable_title_class,
+        :fact_title => fact_title,
+        :link => link,
+        :scroll_to_link => scroll_to_link,
+        :last_activity => last_activity,
+        :user_signed_in? => user_signed_in?,
+        :i_am_fact_owner => i_am_fact_owner,
+        :delete_link => delete_link,
+        :fact_wheel => fact_wheel,
+      }
+    end
+    
+    private
+      def show_links
+        not (self[:hide_links_for_site] and  self[:fact].site == self[:hide_links_for_site]) and self[:fact].site
+      end
+      
+      def proxy_scroll_url
+        FactlinkUI::Application.config.proxy_url + "?url=" + URI.escape(self[:fact].site.url) + "&scrollto=" + URI.escape(self[:fact].id)
+      end
+      
+      def pretty_url
+        if self[:fact].site
+          self[:fact].site.url.gsub(/http(s?):\/\//,'').split('/')[0]
+        else
+          ""
+        end
+      end
+      
+      def link_for(gu)
+        imgtag = image_tag(gu.user.avatar.url(:small), :size => "24x24", :title => "#{gu.user.username} (#{gu.rounded_authority})")
+        path = view.user_profile_path(gu.user.username)
+        link_to( imgtag, path, :target => "_top" )
+      end
   end
 end
