34b774dc273ed73ac88e3dafdafc082f5099f06b
Mark IJbema <markijbema@gmail.com>
2011-12-01 09:51:39 +0100

    fixed ci/jshint step

--- js-library/bin/ci/001_jshint.sh
+++ js-library/bin/ci/001_jshint.sh
@@ -2,5 +2,4 @@
 npm install jshint || exit 1
 ls src/js/*.js | grep -vE '(intro|outro)' | xargs node_modules/jshint/bin/hint || exit 1
 node_modules/jshint/bin/hint src/js/lib/*.js || exit 1
-node_modules/jshint/bin/hint src/js/scripts/*.js || exit 1
 exit
