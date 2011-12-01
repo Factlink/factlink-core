c1d3771e888d1a277b9443e44c58aaeb98c23f71
Mark IJbema <markijbema@gmail.com>
2011-12-01 13:08:10 +0100

    added deploy to staging

--- js-library/bin/ci/003_deploy.sh
+++ js-library/bin/ci/003_deploy.sh
@@ -2,5 +2,13 @@
 echo "Deploying js-library to testserver"
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290
-cap -v testserver deploy
+if [ "$GIT_BRANCH" == "release" ] ; then
+  SERVER="staging"
+else
+  SERVER="testserver"
+fi
+
+echo "deploying to $SERVER"
+cap -q $SERVER deploy || exit 1
+echo "deployed to $SERVER"
 exit
