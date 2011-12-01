8a0bd4742b9ba07e5db41fb052dd56dce40c29c8
Mark IJbema <markijbema@gmail.com>
2011-12-01 14:04:40 +0100

    for now deploy master to staging

--- core/bin/ci/900_deploy.sh
+++ core/bin/ci/900_deploy.sh
@@ -3,7 +3,7 @@ banner "Deploying..."
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
 
-if [ "$GIT_BRANCH" == "release" ] ; then
+if [ "$GIT_BRANCH" == "master" ] ; then
   SERVER="staging"
 else
   SERVER="testserver"
