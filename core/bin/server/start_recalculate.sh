4ef0b6d0963ddd321a5cc341d662be3328a6ab5b
Tom de Vries <tom@codigy.nl>
2011-12-01 14:50:46 +0100

    Take param for environment

--- core/bin/server/start_recalculate.sh
+++ core/bin/server/start_recalculate.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Check if a env is given
+if [ -z $1 ] 
+then
+  echo "Please supply the environment for starting the recalculate."
+  echo "Usage:"
+  echo "start_recalculate.sh [env]"
+  exit 1
+fi
+
+
+RAILS_ENV=$1
+
 # Fact Graph
 fact_graph_count=`ps aux | grep -v grep | grep -c 'rake fact_graph:recalculate'`
 
 if [ "$fact_graph_count" -lt "1" ]; then
    cd /applications/factlink-core/current/
-   nohup /usr/local/rvm/bin/rake fact_graph:recalculate RAILS_ENV=production > /applications/factlink-core/current/log/fact_graph.log 2>&1 &
+   bundle exec rake fact_graph:recalculate RAILS_ENV=$RAILS_ENV > /applications/factlink-core/current/log/fact_graph.log 2>&1 &
 fi
 
 
@@ -14,7 +26,7 @@ channels_count=`ps aux | grep -v grep | grep -c 'rake channels:recalculate'`
 
 if [ "$channels_count" -lt "1" ]; then
    cd /applications/factlink-core/current/
-   nohup /usr/local/rvm/bin/rake channels:recalculate RAILS_ENV=production > /applications/factlink-core/current/log/channels.log 2>&1 &
+   bundle exec rake channels:recalculate RAILS_ENV=$RAILS_ENV > /applications/factlink-core/current/log/channels.log 2>&1 &
 fi
 
 exit 0
