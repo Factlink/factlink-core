c5f0220bc99bb2c32560e51d1869224d8910f200
Mark IJbema <markijbema@gmail.com>
2011-12-01 12:08:35 +0100

    added deploy to staging

--- core/bin/ci/900_deploy.sh
+++ core/bin/ci/900_deploy.sh
@@ -2,5 +2,12 @@
 banner "Deploying..."
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
-cap -q testserver deploy || exit 1
+
+if [ "$GIT_BRANCH" == "release"]; then
+  SERVER="staging"
+else
+  SERVER="testserver"
+fi
+
+cap -q $SERVER deploy || exit 1
 exit
