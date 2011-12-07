1b82aee6c37426dd9cebeb6c63e5a5cab5a88fe4
Remon Oldenbeuving <remon@oldenbeuving.net>
2011-12-07 17:32:11 +0100

    Added getOwnContainingChannel

--- core/public/javascripts/backbone/models/fact.js
+++ core/public/javascripts/backbone/models/fact.js
@@ -1,3 +1,14 @@
 window.Fact = Backbone.Model.extend({
-  
+  getOwnContainingChannels: function() {
+    var containingChannels = this.get('containing_channels');
+    var ret = [];
+    
+    currentUser.channels.each(function(ch) { 
+      if ( _.indexOf(containingChannels, ch.id ) !== -1 ) {
+        ret.push(ch);
+      }
+    });
+    
+    return ret;
+  }
 });
