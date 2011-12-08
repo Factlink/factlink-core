9b3735c4dc48457e84cfb97ab547543cf434b08b
Tom de Vries <tom@codigy.nl>
2011-12-08 10:20:21 +0100

    ADded tab

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -67,7 +67,7 @@ FactlinkUI::Application.routes.draw do
   # generate the images for the indicator used in the js-lib
   get "/images/wheel/:percentages" => "wheel#show", constraints: { percentages: /[0-9]+-[0-9]+-[0-9]+/ }
 
- ##########
+  ##########
   # Web Front-end
   root :to => "home#index"
 
