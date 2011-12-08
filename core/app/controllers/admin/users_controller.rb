b5d1966ac9c6d829edbb782e45fc96b9cff484dc
Tom de Vries <tom@codigy.nl>
2011-12-08 10:40:35 +0100

    Don't allow to destroy a User yet - needs more back-end implementation

--- core/app/controllers/admin/users_controller.rb
+++ core/app/controllers/admin/users_controller.rb
@@ -75,14 +75,14 @@ class Admin::UsersController < ApplicationController
 
   # DELETE /users/1
   # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to admin_users_url }
-      format.json { head :ok }
-    end
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to admin_users_url }
+  #     format.json { head :ok }
+  #   end
+  # end
   
 end
