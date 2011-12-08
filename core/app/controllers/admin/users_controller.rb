6040e80ad5061d3d0626604743f679fc6857fe64
Tom de Vries <tom@codigy.nl>
2011-12-08 11:31:23 +0100

    To be sure, also load the resource and authorize

--- core/app/controllers/admin/users_controller.rb
+++ core/app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   
   before_filter :authenticate_user!
-  authorize_resource
+  load_and_authorize_resource
   
   # GET /users
   # GET /users.json
