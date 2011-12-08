788d9338cbaf583c459a1ba9e5a9a87feb6e7407
Mark IJbema <markijbema@gmail.com>
2011-12-08 10:38:39 +0100

    fixed test

--- core/spec/ohm-models/our_ohm/timestamped_set_spec.rb
+++ core/spec/ohm-models/our_ohm/timestamped_set_spec.rb
@@ -6,29 +6,30 @@ class TimeContainer < OurOhm
 end
 
 describe Ohm::Model::TimestampedSet do
+  let(:c1) { TimeContainer.create }
+  let(:c2) { TimeContainer.create }
   
   let(:item1) { Item.create }
+  let(:item2) { Item.create }
+  let(:item3) { Item.create }
   
   it "should have an unread count of 0 when marked as unread" do
-    c1 = TimeContainer.create()
     c1.items.unread_count.should == 0
     
-    c1.items << Item.create
+    c1.items << item1
     c1.items.unread_count.should == 1
     
     # Prevent race condition
     sleep(0.01)
     c1.items.mark_as_read
     c1.items.unread_count.should == 0
-    c1.items << Item.create
-    c1.items << Item.create
+    c1.items << item2
+    c1.items << item3
     c1.items.unread_count.should == 2
   end
   
   it "should have a working assignment" do
-    c1 = TimeContainer.create()
-    c2 = TimeContainer.create()
-    c1.items << Item.create << Item.create
+    c1.items << item1 << item2
     c2.items = c1.items
     c2.items.count.should == 2
     TimeContainer[c2.id].items.count.should == 2
@@ -37,8 +38,13 @@ describe Ohm::Model::TimestampedSet do
   describe "#until" do
     it "should return an empty list for an empty set" do
       c1 = TimeContainer.create
+      c1.items.until(Ohm::Model::TimestampedSet.current_time).should =~ []
+    end
+    it "should return all items when no limit is given" do
+      c1 = TimeContainer.create
       c1.items << item1
-      c1.items.until(Ohm::Model::TimestampedSet.current_time).should =~ [item1]
+      c1.items << item2
+      c1.items.until(Ohm::Model::TimestampedSet.current_time).should =~ [item1,item2]
     end
   end
 end
