cad28282ba18ee6b55062fc5aa3726abbf0f12b7
Mark IJbema <markijbema@gmail.com>
2011-12-08 15:27:32 +0100

    fixed method name reference

--- core/spec/ohm-models/our_ohm/sorted_set_spec.rb
+++ core/spec/ohm-models/our_ohm/sorted_set_spec.rb
@@ -61,7 +61,7 @@ describe Ohm::Model::SortedSet do
     SortedContainer[c2.id].items.count.should == 2
   end
 
-  describe "#smaller" do
+  describe "#below" do
     it "should return an empty list for an empty set" do
       c1.items.below(3).should =~ []
     end
