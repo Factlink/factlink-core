52dac44314877ace92a5e63186b8dc56aa45066f
Tom de Vries <tom@codigy.nl>
2011-12-15 10:08:10 +0100

    Subdomain helper

--- core/app/helpers/url_helper.rb
+++ core/app/helpers/url_helper.rb
@@ -4,4 +4,10 @@ module UrlHelper
   def set_mailer_url_options
     ActionMailer::Base.default_url_options[:host] = with_subdomain(request.subdomain)
   end
+  
+  def with_subdomain(subdomain)  
+    subdomain = (subdomain || "")  
+    subdomain += "." unless subdomain.empty?  
+    [subdomain, request.domain, request.port_string].join  
+  end
 end
