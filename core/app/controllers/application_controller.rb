4a4bd8a98d65004385a1df47836e4d3e52f96007
Mark IJbema <markijbema@gmail.com>
2011-12-19 17:37:31 +0100

    removed unused method

--- core/app/controllers/application_controller.rb
+++ core/app/controllers/application_controller.rb
@@ -54,29 +54,6 @@ class ApplicationController < ActionController::Base
   def current_graph_user
     @current_graph_user ||= current_user.andand.graph_user
   end
-
-  def mustache_json(klass)
-    #ripped from mustache itself
-    mustache = klass.new
-    variables = self.instance_variable_names
-    variables -= %w[@template]
-
-    if self.respond_to?(:protected_instance_variables)
-      variables -= self.protected_instance_variables
-    end
-
-    variables.each do |name|
-      mustache.instance_variable_set(name, self.instance_variable_get(name))
-    end
-
-    # Declaring an +attr_reader+ for each instance variable in the
-    # Mustache::Rails subclass makes them available to your templates.
-    mustache.class.class_eval do
-      attr_reader *variables.map { |name| name.sub(/^@/, '').to_sym }
-    end
-
-    mustache.to_json
-  end
   
   def raise_404(message="Not Found")
     raise ActionController::RoutingError.new(message)
