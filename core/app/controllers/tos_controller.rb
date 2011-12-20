0354669cc38f4518d3d2706b3dd150dd81744104
Tom de Vries <tom@codigy.nl>
2011-12-20 13:55:14 +0100

    Controller check for Terms of Service

--- core/app/controllers/tos_controller.rb
+++ core/app/controllers/tos_controller.rb
@@ -8,9 +8,23 @@ class TosController < ApplicationController
   
   def update
     @user = current_user
+  
+    agrees_tos  = (params[:user][:agrees_tos].to_i == 1) ? true : false
+    name        = params[:user][:name]
 
-    if @user.update_with_password(params[:user])
-      redirect_to user_profile_path(@user.username), notice: 'You did it!'
+    valid = true
+    
+    unless agrees_tos
+      valid = false
+      @user.errors.add("", "You have to accept the Terms of Service to continue.")  
+    end
+    if name.blank?
+      valid = false
+      @user.errors.add("", "Please fill in your name to accept the Terms of Service.")  
+    end
+
+    if valid and @user.update_with_password(params[:user])
+      redirect_to user_profile_path(@user.username), notice: 'You did it!'      
     else
       render action: "show"
     end
