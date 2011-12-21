9cac033ff68c3fad844f38274e6d2c64c0597f5d
salvador <salvador@10.11.2.159>
2011-12-21 15:50:27 +0100

    and the last specs run!

--- core/spec/routing/jobs_routing_spec.rb
+++ core/spec/routing/jobs_routing_spec.rb
@@ -4,31 +4,11 @@ describe JobsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/jobs").should route_to("jobs#index")
-    end
-
-    it "routes to #new" do
-      get("/jobs/new").should route_to("jobs#new")
+      get("/pages/jobs").should route_to("jobs#index")
     end
 
     it "routes to #show" do
-      get("/jobs/1").should route_to("jobs#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/jobs/1/edit").should route_to("jobs#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      post("/jobs").should route_to("jobs#create")
-    end
-
-    it "routes to #update" do
-      put("/jobs/1").should route_to("jobs#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/jobs/1").should route_to("jobs#destroy", :id => "1")
+      get("/pages/jobs/1").should route_to("jobs#show", :id => "1")
     end
 
   end
--- core/spec/views/jobs/index.html.erb_spec.rb
+++ core/spec/views/jobs/index.html.erb_spec.rb
@@ -16,9 +16,5 @@ describe "jobs/index.html.erb" do
 
   it "renders a list of jobs" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
 end
