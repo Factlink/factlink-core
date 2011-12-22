8064224b526534df7e2ade599040d6cba39634db
salvador <salvador@10.11.2.159>
2011-12-22 08:43:43 +0100

    final layout of jobs

--- core/spec/views/jobs/show.html.erb_spec.rb
+++ core/spec/views/jobs/show.html.erb_spec.rb
@@ -8,11 +8,5 @@ describe "jobs/show.html.erb" do
     ))
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Title/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/MyText/)
-  end
+
 end
