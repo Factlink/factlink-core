6a38a8739855e199a4e07838533dc097e898d86c
Mark IJbema <markijbema@gmail.com>
2011-12-22 16:37:44 +0100

    removed comment

--- core/app/controllers/fact_relations_controller.rb
+++ core/app/controllers/fact_relations_controller.rb
@@ -2,8 +2,6 @@ class FactRelationsController < ApplicationController
 
   respond_to :json, :js
 
-  # fact_evidence_index
-  # /facts/:fact_id/evidence(.:format)
   def index
     @fact = Fact[params[:fact_id]]
     @evidence = @fact.sorted_fact_relations
