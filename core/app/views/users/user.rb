8a53bd74da1c06cb1441ac20a89e6acc1fbd829a
Joël Kuiper <me@joelkuiper.eu>
2012-01-02 14:20:31 +0100

    Difference between own and other channel heading

--- core/app/views/users/user.rb
+++ core/app/views/users/user.rb
@@ -28,12 +28,20 @@ module Users
       image_tag(self[:user].avatar.url(:small), :width => 32)
     end
 
+    def new_channel_button
+      link_to("Add new", new_channel_path(current_user.username), :class => "btn small add-channel")
+    end
+
     def authority
       self[:graph_user].rounded_authority
     end
 
     def all_channel_id
       self[:graph_user].stream_id
+    end
+
+    def is_current_user
+      self[:user] == current_user
     end
 
   end
