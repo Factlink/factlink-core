5f9d0e64436c20c756aba39262451c05ec0fcb7e
Joël Kuiper <me@joelkuiper.eu>
2012-01-04 10:05:49 +0100

    Changes to evidence rendering (temporarily broken, will fix asap)

--- core/app/controllers/fact_relations_controller.rb
+++ core/app/controllers/fact_relations_controller.rb
@@ -1,12 +1,14 @@
 class FactRelationsController < ApplicationController
 
-  respond_to :json, :js
+  respond_to :json
 
   def index
     @fact = Fact[params[:fact_id]]
     @evidence = @fact.sorted_fact_relations
-
-    respond_with(@fact.sorted_fact_relations)
+    
+    respond_with(@evidence.map { |evidence|
+      Facts::FactBubble.for_fact_and_view(evidence.from_fact, view_context).to_json
+    })
   end
 
 end
