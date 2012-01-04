545d9e7922d7ea9d2408b8b2dd5dc79f1f6d2160
Mark IJbema <markijbema@gmail.com>
2012-01-04 16:12:27 +0100

    fixed the lazy

--- core/app/classes/lazy.rb
+++ core/app/classes/lazy.rb
@@ -3,11 +3,19 @@ class Lazy
     @block = block
   end
   
-  def humpapa
-    @humpapa ||= @block.call
+  def original_object_container
+    @original_object_container ||= @block.call
+  end
+  
+  def to_s
+    original_object_container.to_s
+  end
+  
+  def to_json *args
+    original_object_container.to_json *args
   end
   
   def method_missing(method, *args, &block)
-    @humpapa.send method, *args, &block
+    original_object_container.send method, *args, &block
   end
 end
