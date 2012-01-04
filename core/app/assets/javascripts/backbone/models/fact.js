5f3186bf4ceab3483a225b42b29abadcb6b4dbf7
Remon Oldenbeuving <remon@MacBook-Pro-van-Remon.local>
2012-01-04 10:44:42 +0100

    Removed whitespace :D & renamed specialUrl :)

--- core/app/assets/javascripts/backbone/models/fact.js
+++ core/app/assets/javascripts/backbone/models/fact.js
@@ -2,33 +2,33 @@ window.Fact = Backbone.Model.extend({
   getOwnContainingChannels: function() {
     var containingChannels = this.get('containing_channel_ids');
     var ret = [];
-    
-    currentUser.channels.each(function(ch) { 
+
+    currentUser.channels.each(function(ch) {
       if ( _.indexOf(containingChannels, ch.id ) !== -1 ) {
         ret.push(ch);
       }
     });
-    
+
     return ret;
   },
-  
-  specialUrl: function(forChannel) {
+
+  dependentUrl: function(forChannel) {
     if ( forChannel ) {
       return this.collection.url() + '/' + this.get('id');
     } else {
       return '/facts/' + this.get('id');
     }
   },
-  
+
   sync: function(method, model, options) {
     options = options || {};
     var forChannel = options.forChannel;
-    
+
     if ( forChannel === undefined ) {
       forChannel = true;
     }
-    
-    options.url = model.specialUrl(forChannel);
+
+    options.url = model.dependentUrl(forChannel);
 
     Backbone.sync(method, model, options);
   }
