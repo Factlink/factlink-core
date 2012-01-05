c13da0377ef6c9d7c8c285b146580cd03007222e
Remon Oldenbeuving <remon@MacBook-Pro-van-Remon.local>
2012-01-05 15:36:08 +0100

    And poof it's gone, removed unused helper methods, now part of the fact_relation.rb view

--- core/app/helpers/facts_helper.rb
+++ core/app/helpers/facts_helper.rb
@@ -1,28 +1,3 @@
 module FactsHelper
 
-  def evidence_buttons_locals(fact_relation, user)
-    locals = {  :fact_relation => fact_relation,}
-    locals[:negative_active] = ''
-    locals[:positive_active] = ''
-    if current_user.graph_user.opinion_on(fact_relation) == :believes
-      locals[:positive_active] = ' active'
-    elsif current_user.graph_user.opinion_on(fact_relation) == :disbelieves
-      locals[:negative_active] = ' active'
-    end
-    locals
-  end
-
-
-  def evidence_buttons(fact_relation, user)
-    locals = evidence_buttons_locals(fact_relation,user)
-    if fact_relation.type.to_sym == :supporting
-      locals[:positive_action] = "supporting"
-      locals[:negative_action] = "not supporting"
-    elsif fact_relation.type.to_sym == :weakening
-      locals[:positive_action] = "weakening"
-      locals[:negative_action] = "not weakening"
-    end
-    render "/facts/evidence_buttons",  locals
-  end
-
 end
