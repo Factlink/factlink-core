5e34a9e95f5a35bdad4099d5c09919adc83addc3
Remon Oldenbeuving <remon@MacBook-Pro-van-Remon.local>
2012-01-05 13:56:57 +0100

    Fixed Loading/empty indicators

--- core/app/assets/javascripts/backbone/views/fact_relations_view.js
+++ core/app/assets/javascripts/backbone/views/fact_relations_view.js
@@ -2,6 +2,7 @@ window.FactRelationsView = Backbone.View.extend({
   tagName: "div",
   className: "page evidence-list fact-relations-container",
   _views: [],
+  _loading: true,
 
   initialize: function(options) {
     this.useTemplate('fact_relations','fact_relations');
@@ -9,10 +10,15 @@ window.FactRelationsView = Backbone.View.extend({
     this.collection.bind('add', this.addFactRelation, this);
     this.collection.bind('remove', this.removeFactRelation, this);
     this.collection.bind('reset', this.resetFactRelations, this);
+
+    this.showNoEvidenceMessage();
   },
 
   addFactRelation: function(factRelation) {
     var factRelationView = new FactRelationView({model: factRelation});
+
+    this.hideNoEvidenceMessage();
+
     this._views.push(factRelationView);
 
     $(this.el).find('ul').append(factRelationView.render().el);
@@ -24,6 +30,10 @@ window.FactRelationsView = Backbone.View.extend({
     this.collection.forEach(function(factRelation) {
       self.addFactRelation(factRelation);
     });
+
+    if ( this.collection.length === 0 ) {
+      this.showNoEvidenceMessage();
+    }
   },
 
   removeFactRelation: function(factRelation) {
@@ -41,12 +51,40 @@ window.FactRelationsView = Backbone.View.extend({
   },
 
   showAndFetch: function() {
+    var self = this;
+
     if ( ! this._fetched ) {
       this._fetched = true;
 
-      this.collection.reset().fetch();
+      this.collection.reset();
+
+      this.showLoadingIndicator();
+
+      this.collection.fetch({
+        success: function() {
+          self.hideLoadingIndicator();
+        }
+      });
     }
 
     $(this.el).show();
+  },
+
+  showLoadingIndicator: function() {
+    $(this.el).find('.loading-evidence').show();
+    this.hideNoEvidenceMessage();
+  },
+
+  hideLoadingIndicator: function() {
+    $(this.el).find('.loading-evidence').hide();
+  },
+
+  showNoEvidenceMessage: function() {
+    $(this.el).find('.no-evidence-message').show();
+    this.hideLoadingIndicator();
+  },
+
+  hideNoEvidenceMessage: function() {
+    $(this.el).find('.no-evidence-message').hide();
   }
 });
