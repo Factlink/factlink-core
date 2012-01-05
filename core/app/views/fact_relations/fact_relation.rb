5ff5311f30c6e8b92a8fdd0d18d904f8f5995e37
Mark IJbema <markijbema@gmail.com>
2012-01-05 14:26:34 +0100

    removed redundant code

--- core/app/views/fact_relations/fact_relation.rb
+++ core/app/views/fact_relations/fact_relation.rb
@@ -1,23 +1,5 @@
 module FactRelations
   class FactRelation < Mustache::Railstache
-    def self.for(options={})
-      self.for_fact_relation_and_view(options[:fact_relation], options[:view])
-    end
-
-    def self.for_fact_relation_and_view(fact_relation, view)
-      fr = new(false)
-      fr.view = view
-      fr[:fact_relation] = fact_relation
-      fr.init
-      return fr
-    end
-
-    def initialize(run=true)
-      init if run
-    end
-
-    def init
-    end
 
     def fact_bubble
       Facts::FactBubble.for_fact_and_view(self[:fact_relation].from_fact, self.view)
