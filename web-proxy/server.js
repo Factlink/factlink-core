2f1610b9952e2141ede06f888c946805b9a2d941
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-09 12:01:10 +0100

    Added ;

--- web-proxy/server.js
+++ web-proxy/server.js
@@ -1,13 +1,13 @@
 /*
  *	Factlink
  *	A simple proxy server which will proxy requests.
- * 
+ *
  *	Used for implementing Factlink JavaScript on client-sites.
  */
 
 /* jslint node: true */
 
-config = require('./lib/config').get(process.env)
+config = require('./lib/config').get(process.env);
 server = require('./lib/server').getServer(config);
 
 server.listen(config.INTERNAL_PROXY_PORT);
