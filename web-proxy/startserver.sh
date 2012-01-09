805fbffd220bb56fe5c71117e173c4220cd7cb59
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-09 12:01:48 +0100

    Added global supervisor

--- web-proxy/startserver.sh
+++ web-proxy/startserver.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
 npm install
-#read_config.js isn't checked because of eval is evil
-jslint server.js
+npm install supervisor -g
 
+#read_config.js isn't checked because of eval is evil
+./node_modules/jshint/bin/hint server.js
 
 supervisor ./server.js
