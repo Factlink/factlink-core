130a01213c11456dc8978eea1f2d6bacf014cba0
Mark IJbema <markijbema@gmail.com>
2012-01-12 17:08:40 +0100

    no more debugging statements ;)

--- core/app/helpers/fact_helper.rb
+++ core/app/helpers/fact_helper.rb
@@ -1,6 +1,5 @@
 module FactHelper
   def friendly_fact_path(fact)
-    #frurl_fact_path(fact.to_s.parameterize, fact.id)
-    return "/bla-bla/f/1"
+    frurl_fact_path(fact.to_s.parameterize, fact.id)
   end
 end
