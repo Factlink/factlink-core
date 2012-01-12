dba31bd183dc431b5e256eb5e3915dfc602d1b8c
Tom de Vries <tom@codigy.nl>
2012-01-12 15:50:02 +0100

    Javascript tests with jasmine-headless-webkit is not working. Requires Qt / qmake 4.7, which cannot be installed on the CI (dependency hell)

--- core/bin/ci/200_javascript_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Running javascript tests"
-source "$HOME/.rvm/scripts/rvm"
-rvm use --default 1.9.2-p290 || exit 1
-bundle exec rake template:create || exit 1
-bundle exec jasmine-headless-webkit || exit 1
-exit
