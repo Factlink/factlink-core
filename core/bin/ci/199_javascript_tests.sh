3d1fd32ca42b6e14cfd4340bc5cc95a1eec142ea
Tom de Vries <tom@codigy.nl>
2012-01-12 15:06:02 +0100

    Create templates file before testing the javascript - Should let the build succeed :)

--- core/bin/ci/199_javascript_tests.sh
+++ core/bin/ci/199_javascript_tests.sh
@@ -2,5 +2,6 @@
 echo "Running javascript tests"
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
+bundle exec rake templates:create || exit 1
 bundle exec jasmine-headless-webkit || exit 1
 exit
