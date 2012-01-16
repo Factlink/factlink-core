aca6916d22224e7e79f96eaaaeefd415b866e6a0
Tom de Vries <tom@codigy.nl>
2012-01-16 17:32:21 +0100

    Use other view for rendering the User search result

--- core/app/assets/javascripts/backbone/views/search_result_item_view.js
+++ core/app/assets/javascripts/backbone/views/search_result_item_view.js
@@ -3,7 +3,7 @@ window.SearchResultItemView = function SearchResultItemView(opts) {
   if (opts.model.get("the_class") === "FactData") {
     return new FactView({model: new Fact(opts.model.get('the_object'))});
   } else {
-    return new UserView({model: new User(opts.model.get('the_object'))});
+    return new UserSearchView({model: new User(opts.model.get('the_object'))});
   }
 }
 })();
