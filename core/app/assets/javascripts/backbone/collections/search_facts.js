e9e767086ecc3d820f4b921923e9fe6ad984e808
Tom de Vries <tom@codigy.nl>
2012-01-16 16:05:10 +0100

    Removed the old SearchFacts

--- core/app/assets/javascripts/backbone/collections/search_facts.js
+++
@@ -1,9 +0,0 @@
-window.SearchFacts = Backbone.Collection.extend({
-  model: Fact,
-  initialize: function(models, opts) {
-    this.search = opts.search;
-  },
-  url: function() {
-    return '/search/?s=' + this.search;
-  }
-});
