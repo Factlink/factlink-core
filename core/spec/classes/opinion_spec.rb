df5986fbfe5962326d2c5f4c48fe20afc92839ac
Tom de Vries <tom@codigy.nl>
2012-01-16 13:57:30 +0100

    Whitespace: done by save tool

--- core/spec/classes/opinion_spec.rb
+++ core/spec/classes/opinion_spec.rb
@@ -19,10 +19,10 @@ describe Opinion do
     its(:u) {should == 1.5}
     its(:a) {should == 1.6}
   end
-  
+
   describe "attributes" do
     subject { Opinion.tuple(0,0,0,0) }
-    
+
     it { should respond_to :b }
     it { should respond_to :d }
     it { should respond_to :u }
@@ -45,7 +45,7 @@ describe Opinion do
   it "should be equal to an opinion with the same numbers" do
     Opinion.tuple(1,2,3,4).should == Opinion.tuple(1,2,3,4)
   end
-  
+
   it "should be unequal to an opinion with different numbers" do
     a = Opinion.tuple(1,1,1,1)
 
@@ -54,13 +54,13 @@ describe Opinion do
     a.should_not == Opinion.tuple(1,2,1,1)
     a.should_not == Opinion.tuple(1,2,1,1)
   end
-  
+
   it "should have the proper values when retrieved with for_type without explicit authority" do
     Opinion.for_type(:believes).should    == Opinion.tuple(1,0,0,0)
     Opinion.for_type(:disbelieves).should == Opinion.tuple(0,1,0,0)
     Opinion.for_type(:doubts).should     == Opinion.tuple(0,0,1,0)
   end
-  
+
   it "should have the proper values when retrieved with for_type with explicit authority" do
     Opinion.for_type(:believes,1.3).should    == Opinion.tuple(1,0,0,1.3)
     Opinion.for_type(:disbelieves,1.3).should == Opinion.tuple(0,1,0,1.3)
@@ -82,21 +82,21 @@ describe Opinion do
     (@o2+@o3).should == (@o3+@o2)
     (@o1+@o3).should == (@o3+@o1)
   end
-  
+
   it "should have an associative + operation" do
     ((@o1+@o2)+@o3).should == (@o1+(@o2+@o3))
   end
-  
+
   it "should have a combine operation which does the same as the + operation" do
     Opinion.combine([@o1]).should == @o1
     Opinion.combine([@o1,@o2]).should == @o1+@o2
     Opinion.combine([@o1,@o2,@o3]).should == @o1+@o2+@o3
   end
-  
+
   it "should have zero weight when authority is zero" do
     Opinion.tuple(100,100,100,0).weight.should == 0
   end
-  
+
   it "should have weights which add up" do
     (@o1+@o2).weight.should == @o1.weight + @o2.weight
     (@o1+@o2+@o3).weight.should == @o1.weight + @o2.weight + @o3.weight
