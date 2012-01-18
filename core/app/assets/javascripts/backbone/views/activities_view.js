97b0efa00c402fdf2d6b672f8dedf2b7d3e6f0b0
Mark IJbema <markijbema@gmail.com>
2012-01-18 17:02:21 +0100

    fixed the selector (lazy copypasting)

--- core/app/assets/javascripts/backbone/views/activities_view.js
+++ core/app/assets/javascripts/backbone/views/activities_view.js
@@ -1,9 +1,8 @@
 window.ActivitiesView = Backbone.CollectionView.extend({
 
-  containerSelector: '#channel-listing',
+  containerSelector: '#facts_for_channel',
 
   initialize: function(opts) {
-    var self = this;
     this.modelView = ActivityView;
 
     this.collection.bind('add',   this.add, this);
