026a4e608ed1d0e81f52d32bc0becf73fa906a79
Tom de Vries <tom@codigy.nl>
2012-01-18 11:49:44 +0100

    Spacing

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -16,14 +16,14 @@ FactlinkUI::Application.routes.draw do
     resources :evidence
 
     resources :supporting_evidence, :weakening_evidence do
-      get     "opinion" => "evidence#opinion"
+      get     "opinion"       => "evidence#opinion"
       post    "opinion/:type" => "evidence#set_opinion", :as => "set_opinion"
-      delete  "opinion/" => "evidence#remove_opinions", :as => "delete_opinion"
+      delete  "opinion/"      => "evidence#remove_opinions", :as => "delete_opinion"
     end
 
     member do
       post    "opinion/:type" => "facts#set_opinion", :as => "set_opinion"
-      delete  "opinion/" => "facts#remove_opinions", :as => "delete_opinion"
+      delete  "opinion/"      => "facts#remove_opinions", :as => "delete_opinion"
 
       match "/evidence_search" => "facts#evidence_search"
 
