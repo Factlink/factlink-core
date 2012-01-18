0353511ea0e31d0bf4f63fc79434db9e7528e3b9
Tom de Vries <tom@codigy.nl>
2012-01-18 14:15:09 +0100

    Print all missing packages when finishing the script.

--- core/bin/server/check-installed-packages.sh
+++ core/bin/server/check-installed-packages.sh
@@ -3,17 +3,26 @@
 # Check if there is 1 occurence of the package defined in apt-requirements in
 # the dpkg -l, listing of installed packages on this system.
 
-for line in $(cat apt-requirements.txt); do
+is_missing_packages=false
+the_missing_packages=""
 
+for line in $(cat apt-requirements.txt); do
   count=`dpkg --get-selections | cut -f1 | grep -e ^$line$ -c`
 
   if [ "$count" -eq "1" ]; then
     # Installed
     echo "$line is installed"
   else
-    echo "Package '$line' is not installed. Please install."
-    exit 1
+    the_missing_packages="$the_missing_package $line"
+    is_missing_packages=true
   fi
+
 done
 
-exit 0
+if $is_missing_packages ; then
+  echo "Not all required packages are installed. The following packages are missing: $the_missing_packages"
+  exit 1
+else
+  echo "Success: All required packages from apt-packages.txt are installed."
+  exit 0
+fi
