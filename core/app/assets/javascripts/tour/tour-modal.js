8ede6f25f891fbdbdbe981c9bd2601abfb61337e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-19 13:51:32 +0100

    Fixed the double click bug in the tour

--- core/app/assets/javascripts/tour/tour-modal.js
+++ core/app/assets/javascripts/tour/tour-modal.js
@@ -1,58 +1,81 @@
-function initTourModal() {
-  $('#first-tour-modal').modal({backdrop: true});
-  $('#first-tour-modal').modal('show');
+(function(){
+  var modal = $('#first-tour-modal').modal({backdrop: true});
+  var hidden = true;
 
-  $('.hide-modal').live('click', function() {
-    $('#first-tour-modal').modal('hide');
+  modal.bind('hide', function() {
+    hidden = true;
+  }).bind('show', function() {
+    hidden = false;
   });
 
-  $('.bookmarklet').twipsy({placement:"below", offset:5});
-}
+  $('.take-the-tour').live('click', function() {
+    modal.modal('show');
 
-function sliderSize() {
-  return $("#slider > div").length - 1;
-}
+    // Load the tour
+    $.ajax({
+      url: "/tour",
+      dataType: "html",
+      success: function(data) {
+        if ( !hidden ) {
+          initTourModal(data);
+        }
+      }
+    });
+  });
 
-function setButtonsForSlide(idx) {  
-  if (idx === 0) {
-    // First page
-    $("#next").show();
-    $("#previous, #close").hide();
-  } else if (idx === sliderSize()) {
-    // Last page
-    $("#next").hide();
-    $("#previous, #close").show();    
-  } else {
-    // Middle page
-    $("#previous, #next").show();
-    $("#close").hide();
+  $('.hide-modal').live('click', function() {
+    modal.modal('hide');
+  });
+
+  function initTourModal(data) {
+    modal.html(data);
+
+    $('.bookmarklet').twipsy({placement:"below", offset:5});
   }
-}
 
-$('#slider').cycle({
-  activePagerClass: "current",
-	fx:       "scrollHorz",
-	timeout:  0, 
-	speed:    1000,
-	next: 	  "#next",
-	prev:     "#previous", 
-	easing:   "easeOutSine",
-	nowrap: 1,
-	pause: 1,
-	pager:  "#pagerNav",
-	before: function(current_slide) {
-	},
-	
-  // callback fn that creates a thumbnail to use as pager anchor 
-  pagerAnchorBuilder: function(idx, slide) {
-    var title = $(slide).data('title');
-    return '<li><a href="#">' + title + '</a></li>'; 
-  },
-  
-  onPagerEvent: function(zeroBasedSlideIndex) {
-    setButtonsForSlide(zeroBasedSlideIndex);
-  },
-  onPrevNextEvent: function(isNext, zeroBasedSlideIndex, slideElement) {
-    setButtonsForSlide(zeroBasedSlideIndex);
+  function sliderSize() {
+    return $("#slider > div").length - 1;
   }
-});
+
+  function setButtonsForSlide(idx) {
+    if (idx === 0) {
+      // First page
+      $("#next").show();
+      $("#previous, #close").hide();
+    } else if (idx === sliderSize()) {
+      // Last page
+      $("#next").hide();
+      $("#previous, #close").show();
+    } else {
+      // Middle page
+      $("#previous, #next").show();
+      $("#close").hide();
+    }
+  }
+
+  $('#slider').cycle({
+    activePagerClass: "current",
+  	fx:       "scrollHorz",
+  	timeout:  0,
+  	speed:    1000,
+  	next: 	  "#next",
+  	prev:     "#previous",
+  	easing:   "easeOutSine",
+  	nowrap: 1,
+  	pause: 1,
+  	pager:  "#pagerNav",
+
+    // callback fn that creates a thumbnail to use as pager anchor
+    pagerAnchorBuilder: function(idx, slide) {
+      var title = $(slide).data('title');
+      return '<li><a>' + title + '</a></li>';
+    },
+
+    onPagerEvent: function(zeroBasedSlideIndex) {
+      setButtonsForSlide(zeroBasedSlideIndex);
+    },
+    onPrevNextEvent: function(isNext, zeroBasedSlideIndex, slideElement) {
+      setButtonsForSlide(zeroBasedSlideIndex);
+    }
+  });
+})();
