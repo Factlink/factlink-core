69252f496c015af4ae0316790a1dc790b7f5dad1
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-19 14:11:02 +0100

    Sorry, I fucked it up. This really fixes the tour

--- core/app/assets/javascripts/tour/tour-modal.js
+++ core/app/assets/javascripts/tour/tour-modal.js
@@ -30,52 +30,56 @@
   function initTourModal(data) {
     modal.html(data);
 
+    initSlider(modal.find('.slider'));
+
     $('.bookmarklet').twipsy({placement:"below", offset:5});
   }
 
+  function initSlider(el) {
+    el.cycle({
+      activePagerClass: "current",
+    	fx:       "scrollHorz",
+    	timeout:  0,
+    	speed:    1000,
+    	next: 	  ".next",
+    	prev:     ".previous",
+    	easing:   "easeOutSine",
+    	nowrap: 1,
+    	pause: 1,
+    	pager:  "#pagerNav",
+
+      // callback fn that creates a thumbnail to use as pager anchor
+      pagerAnchorBuilder: function(idx, slide) {
+        var title = $(slide).data('title');
+        return '<li><a>' + title + '</a></li>';
+      },
+
+      onPagerEvent: function(zeroBasedSlideIndex) {
+        setButtonsForSlide(zeroBasedSlideIndex);
+      },
+      onPrevNextEvent: function(isNext, zeroBasedSlideIndex, slideElement) {
+        setButtonsForSlide(zeroBasedSlideIndex);
+      }
+    });
+  }
+
   function sliderSize() {
-    return $("#slider > div").length - 1;
+    return modal.find(".slider > div").length - 1;
   }
 
   function setButtonsForSlide(idx) {
     if (idx === 0) {
       // First page
-      $("#next").show();
-      $("#previous, #close").hide();
+      modal.find(".next").show();
+      modal.find(".previous, .closeButton").hide();
     } else if (idx === sliderSize()) {
       // Last page
-      $("#next").hide();
-      $("#previous, #close").show();
+      modal.find(".next").hide();
+      modal.find(".previous, .closeButton").show();
     } else {
       // Middle page
-      $("#previous, #next").show();
-      $("#close").hide();
+      modal.find(".previous, .next").show();
+      modal.find(".closeButton").hide();
     }
   }
-
-  $('#slider').cycle({
-    activePagerClass: "current",
-  	fx:       "scrollHorz",
-  	timeout:  0,
-  	speed:    1000,
-  	next: 	  "#next",
-  	prev:     "#previous",
-  	easing:   "easeOutSine",
-  	nowrap: 1,
-  	pause: 1,
-  	pager:  "#pagerNav",
-
-    // callback fn that creates a thumbnail to use as pager anchor
-    pagerAnchorBuilder: function(idx, slide) {
-      var title = $(slide).data('title');
-      return '<li><a>' + title + '</a></li>';
-    },
-
-    onPagerEvent: function(zeroBasedSlideIndex) {
-      setButtonsForSlide(zeroBasedSlideIndex);
-    },
-    onPrevNextEvent: function(isNext, zeroBasedSlideIndex, slideElement) {
-      setButtonsForSlide(zeroBasedSlideIndex);
-    }
-  });
 })();
