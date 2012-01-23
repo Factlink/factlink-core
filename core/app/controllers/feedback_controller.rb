5b06e8431596c4d56dfbc599c0c1a6c4d5b554d5
Joël Kuiper <me@joelkuiper.eu>
2012-01-23 15:48:12 +0100

    Test for sending emails on exception

--- core/app/controllers/feedback_controller.rb
+++ core/app/controllers/feedback_controller.rb
@@ -13,6 +13,7 @@ class FeedbackController < ApplicationController
       params = {}
       render :json =>  { msg: "Thank you for your feedback. We have registered your comments under the number #{issue[:number]}".html_safe }
     rescue
+      ExceptionNotifier::Notifier.exception_notification(request.env, $!, :data => {:message => "was doing something wrong"}).deliver
       render :json => { msg: "We're sorry, something went wrong with saving your feedback, you can still reach us at feedback@factlink.com".html_safe }, :status => 500
     end
   end
