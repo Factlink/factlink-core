12d579da046f8d3370454b6628bd4cb47c08cc67
Joël Kuiper <me@joelkuiper.eu>
2012-01-30 11:49:06 +0100

    simpified spec

--- core/spec/ohm-models/topic_specific_authority_spec.rb
+++ core/spec/ohm-models/topic_specific_authority_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 
-describe "beliefs should work as described in the google doc" do
+describe "beliefs should work"  do
   include TopicBeliefExpressions
 
   let(:u1) {FactoryGirl.create(:graph_user)}
@@ -9,23 +9,34 @@ describe "beliefs should work as described in the google doc" do
   let(:u3) {FactoryGirl.create(:graph_user)}
   let(:u4) {FactoryGirl.create(:graph_user)}
 
-  let(:f1) {FactoryGirl.create(:fact)}
-  let(:f2) {FactoryGirl.create(:fact)}
-  let(:f3) {FactoryGirl.create(:fact)}
-  let(:f4) {FactoryGirl.create(:fact)}
-  let(:f5) {FactoryGirl.create(:fact)}
-  let(:f6) {FactoryGirl.create(:fact)}
-  let(:f7) {FactoryGirl.create(:fact)}
-
-  let(:t1) {FactoryGirl.create(:topic)}
-  let(:t2) {FactoryGirl.create(:topic)}
-  let(:t3) {FactoryGirl.create(:topic)}
-
-  # Scenario A (a user without any history in Factlink):
-  # a(U1) = 1
   it "for a user without history in Factlink the authority should be 0.0" do
+    t1 = create :topic
     authority of: u1, on: t1, should_be: 0.0
   end
 
+  it "should not give authority on a topic when creating a fact in it" do
+    f = create :fact, created_by: u1
+    c = create :channel, title: "foo"
+    authority of:u1, on: Topic.by_title("foo"), should_be: 0.0
+  end
+
+
+  it "should not give authority on a topic when creating a fact in it" do
+    f = create :fact, created_by: u1
+    f2 = create :fact, created_by: u1
+    c = create :channel, title: "foo"
+    f.add_evidence(:supporting,f2,u2)
+    authority of:u1, on: Topic.by_title("foo"), should_be: 0.0
+  end
+
+  it "should not give authority on a topic when creating a fact in it" do
+    f = create :fact, created_by: u1
+    c = create :channel, title: "foo"
+    f2 = create :fact, created_by: u1
+    f3 = create :fact, created_by: u1
+    f.add_evidence(:supporting,f2,u2)
+    f.add_evidence(:supporting,f3,u2)
+    authority of:u1, on: Topic.by_title("foo"), should_be: 1.0
+  end
 end
 
