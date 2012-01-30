5684b51ff789db03f48b3664bf8a4a62a3cb6a08
Joël Kuiper <me@joelkuiper.eu>
2012-01-30 09:45:50 +0100

    Topic belief and spec

---
+++ core/spec/ohm-models/topic_specific_authority_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+
+describe "beliefs should work as described in the google doc" do
+  include TopicBeliefExpressions
+
+  let(:u1) {FactoryGirl.create(:graph_user)}
+  let(:u2) {FactoryGirl.create(:graph_user)}
+  let(:u3) {FactoryGirl.create(:graph_user)}
+  let(:u4) {FactoryGirl.create(:graph_user)}
+
+  let(:f1) {FactoryGirl.create(:fact)}
+  let(:f2) {FactoryGirl.create(:fact)}
+  let(:f3) {FactoryGirl.create(:fact)}
+  let(:f4) {FactoryGirl.create(:fact)}
+  let(:f5) {FactoryGirl.create(:fact)}
+  let(:f6) {FactoryGirl.create(:fact)}
+  let(:f7) {FactoryGirl.create(:fact)}
+
+  let(:t1) {FactoryGirl.create(:topic)}
+  let(:t2) {FactoryGirl.create(:topic)}
+  let(:t3) {FactoryGirl.create(:topic)}
+
+  # Scenario A (a user without any history in Factlink):
+  # a(U1) = 1
+  it "for a user without history in Factlink the authority should be 1.0" do
+    authority of: u1, on: t1, should_be: 1.0
+  end
+
+end
+
---
+++ core/spec/support/topic_belief_expressions.rb
@@ -0,0 +1,51 @@
+module TopicBeliefExpressions
+  def b(user,fact)
+    something_happened
+    fact.add_opinion(:believes,user.graph_user)
+  end
+
+  def d(user,fact)
+    something_happened
+    fact.add_opinion(:disbelieves,user.graph_user)
+  end
+
+  def u(user,fact)
+    something_happened
+    fact.add_opinion(:doubts,user.graph_user)
+  end
+
+  def _(b,d,u,a)
+    Opinion.new(b:b,d:d,u:u,a:a)
+  end
+
+  def possible_reset
+    unless @nothing_happened
+      FactGraph.reset_values
+      FactGraph.recalculate
+      @nothing_happened = true
+    end
+  end
+
+  def something_happened
+    @nothing_happened = false
+  end
+
+  def a(user)
+    @nothing_happened = false
+    possible_reset
+    Authority.from(GraphUser[user.graph_user.id]).to_f.should
+  end
+
+  def authority opts={}
+    @nothing_happened = false
+    possible_reset
+    Authority.from(opts[:on], for: opts[:of]).to_f.should == opts[:should_be]
+  end
+
+  def opinion?(fact)
+    possible_reset
+    # values are recalculated in Redis, so get the object fresh from Redis
+    fact = fact.class[fact.id]
+    fact.get_opinion.should
+  end
+end
