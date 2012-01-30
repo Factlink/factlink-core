ddf4efda0907242126fe11475f27e495f175dabc
Joël Kuiper <me@joelkuiper.eu>
2012-01-30 11:48:33 +0100

    added functionality to topic to retrieve by title

--- core/app/ohm-models/topic.rb
+++ core/app/ohm-models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < OurOhm
   set :channels, Channel
   attribute :title
+  index :title
+
+  def self.by_title(title)
+    find(title: title).first || new(title: title)
+  end
 end
--- core/spec/ohm-models/topic_spec.rb
+++ core/spec/ohm-models/topic_spec.rb
@@ -1,6 +1,20 @@
 require_relative '../ohm_helper.rb'
 require_relative '../../app/ohm-models/topic.rb'
 
+class Channel < OurOhm
+end
+
+
 describe Topic do
 
+  describe ".by_title" do
+    it "should return a new object when no topic with the title exists" do
+      Topic.by_title("idontexist").should be_a(Topic)
+      Topic.by_title("idontexist").title.should == "idontexist"
+   end
+    it "should return the topic with given title if the title exists" do
+      t1 = Topic.create title: "foo"
+      Topic.by_title("foo").should == t1
+    end
+  end
 end
