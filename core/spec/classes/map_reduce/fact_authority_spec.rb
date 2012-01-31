0ee2d83f92a53bc1e16a817773da6b5fe89fa283
Mark IJbema <markijbema@gmail.com>
2012-01-31 15:43:59 +0100

    created test for map, and fixed map

--- core/spec/classes/map_reduce/fact_authority_spec.rb
+++ core/spec/classes/map_reduce/fact_authority_spec.rb
@@ -2,4 +2,23 @@ require File.expand_path('../../../../app/classes/map_reduce.rb', __FILE__)
 require File.expand_path('../../../../app/classes/map_reduce/fact_authority.rb', __FILE__)
 
 describe MapReduce::FactAuthority do
+  describe :wraped_map do
+    it do
+      factrelations = [
+        stub(:FactRelation,
+          fact_id: 10,
+          created_by_id: 20,
+        ),
+        stub(:FactRelation,
+          fact_id: 10,
+          created_by_id: 15,
+        ),
+        stub(:FactRelation,
+          fact_id: 12,
+          created_by_id: 13,
+        )
+      ]
+      subject.wrapped_map(factrelations).should == {12 => [13], 10 => [20,15]}
+    end
+  end
 end
