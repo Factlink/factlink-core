b69c70c5fd7813f1f54c94dccc0ff305cabb7884
Joël Kuiper <me@joelkuiper.eu>
2012-01-31 15:04:37 +0100

    FactAuthority MapReduce (it still needs tests)

--- core/app/classes/map_reduce/fact_authority.rb
+++ core/app/classes/map_reduce/fact_authority.rb
@@ -1,11 +1,27 @@
-class MapReduce 
+class MapReduce
   class FactAuthority < MapReduce
-    def internal_map
-
+    def all_set
+      FactRelation.all
     end
 
-    def internal_reduce
+    def set_for_one(f)
+      FactRelation.all.find(from_fact_id: f.id)
+    end
 
+    def map iterator
+      iterator.each do |fr|
+        yield fr.from_fact. fr.created_by
+      end
+    end
+
+    def reduce bucket, partials
+      result = 0
+      partials.each do |i|
+        if i != bucket.created_by
+          results += 1
+        end
+      end
+      [1, result].max
     end
   end
 end
