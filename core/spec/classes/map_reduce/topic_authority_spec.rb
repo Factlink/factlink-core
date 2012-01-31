2dc51cf03674b38ab5726318f86bafe76eaa4004
Mark IJbema <markijbema@gmail.com>
2012-01-31 15:05:45 +0100

    added tests for mapreducers

---
+++ core/spec/classes/map_reduce/topic_authority_spec.rb
@@ -0,0 +1,5 @@
+require File.expand_path('../../../../app/classes/map_reduce.rb', __FILE__)
+require File.expand_path('../../../../app/classes/map_reduce/topic_authority.rb', __FILE__)
+
+describe MapReduce::TopicAuthority do
+end
