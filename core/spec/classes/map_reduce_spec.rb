fd48fb2c9a917109d80a291b82a8c76e2ac53f84
Joël Kuiper <me@joelkuiper.eu>
2012-01-31 14:25:11 +0100

    spec for naming changes

--- core/spec/classes/map_reduce_spec.rb
+++ core/spec/classes/map_reduce_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../../app/classes/map_reduce.rb', __FILE__)
 
 class ExampleMapReduce < MapReduce
-  def internal_map(iterator)
+  def map(iterator)
     iterator.each do |i|
       i.each_pair do |k,v|
         yield k, v*v
@@ -9,7 +9,7 @@ class ExampleMapReduce < MapReduce
     end
   end
 
-  def internal_reduce(bucket, partials)
+  def reduce(bucket, partials)
       partials.inject(:+)
   end
 end
@@ -18,12 +18,12 @@ end
 describe ExampleMapReduce do
   it do
     list = [{a: 1}, {b: 2}, {c: 3}, {a: 4}]
-    subject.map(list).should == {a: [1, 16], b: [4], c: [9]}
+    subject.wrapped_map(list).should == {a: [1, 16], b: [4], c: [9]}
   end
 
   it do
     list = {a: [1, 16], b: [4], c: [9]}
-    subject.reduce(list).should == {:a=>17, :b=>4, :c=>9}
+    subject.wrapped_reduce(list).should == {:a=>17, :b=>4, :c=>9}
   end
 
   it do
@@ -33,10 +33,10 @@ describe ExampleMapReduce do
 end
 
 describe MapReduce do
-  describe :reduce do
+  describe :wrapped_reduce do
     it do
-      subject.should_receive(:internal_reduce).with(:a, [1,2]).and_return(3)
-      subject.reduce({a: [1,2]}).should == {:a=>3}
+      subject.should_receive(:reduce).with(:a, [1,2]).and_return(3)
+      subject.wrapped_reduce({a: [1,2]}).should == {:a=>3}
     end
   end
 end
