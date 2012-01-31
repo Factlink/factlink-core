7515bc8b5745d0ccf1bad3ef143da59a754cee0c
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-31 10:46:29 +0100

    Created backend Mixpanel tracking for signing in

--- core/app/controllers/users/sessions_controller.rb
+++ core/app/controllers/users/sessions_controller.rb
@@ -1,3 +1,19 @@
 class Users::SessionsController < Devise::SessionsController
   layout "frontend"
+
+  after_filter :track_sign_in, :only => [:create]
+  before_filter :track_sign_out, :only => [:destroy]
+
+  def track_sign_in
+    @mixpanel.track_event("Sign in", mixpanel_user_identity_opts)
+  end
+
+  def track_sign_out
+    @mixpanel.track_event("Sign out", mixpanel_user_identity_opts)
+  end
+
+  def mixpanel_user_identity_opts
+    { :mp_name_tag => current_user.username,
+      :distinct_id => current_user.id } if current_user
+  end
 end
