2cda8f490dfcde2818948b2d226b8f4eeb646eb4
Mark IJbema <markijbema@gmail.com>
2012-01-31 13:12:14 +0100

    apparently only and a block don't combine

--- core/app/controllers/users/sessions_controller.rb
+++ core/app/controllers/users/sessions_controller.rb
@@ -1,6 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   layout "frontend"
 
-  after_filter only: :create { track "Sign out" }
-  before_filter :only => [:destroy] { track "Sign in" }
+  after_filter  :track_sign_in, only: :create 
+  before_filter :track_sign_in, only: :destroy
+
+  def track_sign_out; track "Sign out"; end
+  def track_sign_in; track "Sign in"; end
 end
