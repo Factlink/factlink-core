ec41f2ba4e35dc6fb4241e93154c5aa746a3c52e
Joël Kuiper <me@joelkuiper.eu>
2012-01-31 14:23:06 +0100

    Changed naming of internal wrappers

--- core/app/classes/map_reduce.rb
+++ core/app/classes/map_reduce.rb
@@ -1,23 +1,23 @@
 class MapReduce
-  def map iterator
+  def wrapped_map iterator
     result_hash = {}
-    internal_map iterator do |bucket, value|
+    map iterator do |bucket, value|
       result_hash[bucket] ||= []
       result_hash[bucket] << value
     end
     result_hash
   end
 
-  def reduce iterator
+  def wrapped_reduce iterator
     results_hash = {}
     iterator.each_pair do |bucket, partials|
-      results_hash[bucket] = (internal_reduce bucket, partials)
+      results_hash[bucket] = (reduce bucket, partials)
     end
     results_hash
   end
 
   def map_reduce iterator
-    reduce(map(iterator))
+    wrapped_reduce(wrapped_map(iterator))
   end
 
 end
