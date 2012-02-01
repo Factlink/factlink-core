64d9207b094f081cd4fcb6a10872012ec1d50d98
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-02-01 17:29:36 +0100

    Changed start/stop annotating functionality
    I do think we need to fix this, but there are cases that Factlink is
    not yet entirely setup, so we wait for that.

--- js-library/src/js/chrome/start_annotating.js
+++ js-library/src/js/chrome/start_annotating.js
@@ -1,3 +1,19 @@
-(function(Factlink){
-  Factlink.startAnnotating();
-})(window.Factlink);
+(function(Factlink, waitingCount) {
+  // Store arguments object so we can use from the setTimeout and loadFactlink
+  var arg = arguments;
+
+  // If it takes longer then 5 seconds we just stop
+  // TODO maybe some error message here?
+  if (waitingCount >= 50) {
+    return;
+  }
+
+  try {
+    // Start annotating
+    Factlink.startAnnotating();
+  } catch (e) {
+    setTimeout(function() {
+      arg.callee(Factlink, ++waitingCount);
+    }, 100);
+  }
+})(window.Factlink, 0);
--- js-library/src/js/chrome/start_highlighting.js
+++ js-library/src/js/chrome/start_highlighting.js
@@ -1,3 +1,19 @@
-(function(Factlink){
-  Factlink.startHighlighting();
-})(window.Factlink);
+(function(Factlink, waitingCount) {
+  // Store arguments object so we can use from the setTimeout and loadFactlink
+  var arg = arguments;
+
+  // If it takes longer then 5 seconds we just stop
+  // TODO maybe some error message here?
+  if (waitingCount >= 50) {
+    return;
+  }
+
+  try {
+    // Start highlighting
+    Factlink.startHighlighting();
+  } catch (e) {
+    setTimeout(function() {
+      arg.callee(Factlink, ++waitingCount);
+    }, 100);
+  }
+})(window.Factlink, 0);
