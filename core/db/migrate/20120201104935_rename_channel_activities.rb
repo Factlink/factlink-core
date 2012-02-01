6828eb5cce9f52ad02f24cd43eb554fdd9378fb7
Mark IJbema <markijbema@gmail.com>
2012-02-01 14:20:26 +0100

    added migration for renamed activities

---
+++ core/db/migrate/20120201104935_rename_channel_activities.rb
@@ -0,0 +1,20 @@
+class RenameChannelActivities < Mongoid::Migration
+  def self.up
+    say_with_time "migrating activities to match new names/structure" do
+      Activity.find(subject_class: FactRelation, action: 'created').each do |a|
+        fr = a.subject
+        a.subject = fr.from_fact
+        a.object = fr.fact
+        a.action = :"added_#{fr.type}_evidence"
+        a.save
+      end
+      Activity.find(subject_class: Channel, action: 'added').each do |a|
+        a.action = :added_subchannel
+        a.save
+      end
+    end
+  end
+
+  def self.down
+  end
+end
