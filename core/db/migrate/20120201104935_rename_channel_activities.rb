0d37983e1311160e6f84815c4bd9825bd826c5a2
Mark IJbema <markijbema@gmail.com>
2012-02-01 17:47:32 +0100

    made migration more robust

--- core/db/migrate/20120201104935_rename_channel_activities.rb
+++ core/db/migrate/20120201104935_rename_channel_activities.rb
@@ -3,10 +3,14 @@ class RenameChannelActivities < Mongoid::Migration
     say_with_time "migrating activities to match new names/structure" do
       Activity.find(subject_class: FactRelation, action: 'created').each do |a|
         fr = a.subject
-        a.subject = fr.from_fact
-        a.object = fr.fact
-        a.action = :"added_#{fr.type}_evidence"
-        a.save
+        if fr
+          a.subject = fr.from_fact
+          a.object = fr.fact
+          a.action = :"added_#{fr.type}_evidence"
+          a.save
+        else
+          a.delete
+        end
       end
       Activity.find(subject_class: Channel, action: 'added').each do |a|
         a.action = :added_subchannel
