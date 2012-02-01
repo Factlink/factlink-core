fe35c7f019e15ac7b676bc244987800cd829a926
Mark IJbema <markijbema@gmail.com>
2012-02-01 10:53:57 +0100

    improved query for channel

--- core/spec/ohm-models/activity/queries_spec.rb
+++ core/spec/ohm-models/activity/queries_spec.rb
@@ -21,6 +21,29 @@ describe Activity::For do
         {user: gu1, action: :created, subject: f1}
       ]
     end
-
+  end
+  
+  describe ".channel" do
+    it "should return activity for when a channel followed this channel" do
+      ch1 = create :channel
+      ch2 = create :channel
+      ch1.add_channel(ch2)
+      Activity::For.channel(ch2).map(&:to_hash_without_time).should == [
+        {user: ch1.created_by, action: :added_subchannel, subject: ch2, object: ch1}
+      ]
+    end
+    
+    [:supporting, :weakening].each do |type|
+      it "should return activities about facts which have received extra #{type} evidence" do
+        ch1 = create :channel
+        f1 = create :fact
+        f2 = create :fact
+        ch1.add_fact f1
+        f1.add_evidence type, f2, gu1
+        Activity::For.channel(ch1).map(&:to_hash_without_time).should == [
+          {user: gu1, action: :"added_#{type}_evidence", subject: f2, object: f1}
+        ]
+      end
+    end
   end
 end
