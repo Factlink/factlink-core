27373d7db752ca265ef750dfced16bd6cd75aa83
Mark IJbema <markijbema@gmail.com>
2012-02-06 16:58:31 +0100

    start of implementation of channel authority

--- core/app/classes/map_reduce/channel_authority.rb
+++ core/app/classes/map_reduce/channel_authority.rb
@@ -1,11 +1,16 @@
 class MapReduce
   class ChannelAuthority < MapReduce
-    def map
-
+    def map iterator
+      iterator.each do |fact|
+        fact.channel_ids.each do |ch_id|
+          authority = Authority.from fact
+          yield({user_id: fact.created_by_id, channel_id: ch_id }, authority)
+        end
+      end
     end
 
-    def reduce
-
+    def reduce bucket, values
+      return 0
     end
   end
 end
--- core/spec/classes/map_reduce/channel_authority_spec.rb
+++ core/spec/classes/map_reduce/channel_authority_spec.rb
@@ -1,5 +1,23 @@
 require File.expand_path('../../../../app/classes/map_reduce.rb', __FILE__)
 require File.expand_path('../../../../app/classes/map_reduce/channel_authority.rb', __FILE__)
 
+unless defined?(Authority)
+  class Authority
+  end
+end
+
 describe MapReduce::ChannelAuthority do
+  describe :wrapped_map do
+    it do
+      facts = [
+        stub(:Fact,
+          id: 10,
+          created_by_id: 20,
+          channel_ids: [10],
+        )
+      ]
+      Authority.stub!(:from).and_return(18)
+      subject.wrapped_map(facts).should == {{user_id: 20, channel_id:10} => [18] }
+    end
+  end
 end
