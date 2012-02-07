cad847cc6af1342dc75e07cf93c14fc0462fbe86
Tom de Vries <tom@codigy.nl>
2012-02-07 10:48:35 +0100

    Added tests and agreed_tos_on to user model

--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -13,6 +13,7 @@ class User
 
   field :admin,       type: Boolean, default: false
   field :agrees_tos,  type: Boolean, default: false
+  field :agreed_tos_on, type: DateTime
 
   field :seen_the_tour,  type: Boolean, default: false
 
@@ -75,7 +76,7 @@ class User
       self.errors.add("", "Please fill in your name to accept the Terms of Service.")
     end
 
-    valid and self.update_without_password(agrees_tos: agrees_tos, name: name)
+    valid and self.update_without_password(agrees_tos: agrees_tos, name: name, agreed_tos_on: DateTime.now)
   end
 
   private :create_graph_user #WARING!!! is called by the database reset function to recreate graph_users after they were wiped, while users were preserved
--- core/spec/models/user_spec.rb
+++ core/spec/models/user_spec.rb
@@ -57,9 +57,12 @@ describe User do
     end
     describe "when agreeing with a name" do
       it "should be allowed" do
+        t = DateTime.now
+        DateTime.stub!(:now).and_return(t)
         nonnda_subject.sign_tos(true, 'Sjaak').should == true
         nonnda_subject.name.should == 'Sjaak'
         nonnda_subject.agrees_tos.should == true
+        nonnda_subject.agreed_tos_on.to_i.should == t.to_i
         nonnda_subject.errors.keys.length.should == 0
       end
     end
