e8f959d5c0e584865ccac1263dc7d56a6ff934ac
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-02-08 11:38:38 +0100

    Use grunt for JSHinting

--- js-library/bin/ci/001_jshint.sh
+++ js-library/bin/ci/001_jshint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-npm install jshint || exit 1
-ls src/js/*.js | grep -vE '(intro|outro)' | xargs node_modules/jshint/bin/hint || exit 1
-node_modules/jshint/bin/hint src/js/lib/*.js || exit 1
+grunt lint || exit 1
 exit
