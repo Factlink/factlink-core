abd135cd8d89177c1ce582fa58be010005277557
Mark IJbema <markijbema@gmail.com>
2012-02-13 14:43:53 +0100

    simplified testhelper

--- core/spec/support/topic_belief_expressions.rb
+++ core/spec/support/topic_belief_expressions.rb
@@ -1,17 +1,10 @@
 module TopicBeliefExpressions
-  def possible_reset
-    unless @nothing_happened
-      Authority.run_calculation
-      @nothing_happened = true
-    end
-  end
-
-  def something_happened
-    @nothing_happened = false
+  def reset
+    Authority.run_calculation
   end
 
   def authority opts={}
-    possible_reset
+    reset
     Authority.from(opts[:from], opts.slice(:of)).to_f.should == opts[:should_be]
   end
 end
