ed29847fadc0db78aa1486bdfb7d343fc19d8884
Mark IJbema <markijbema@gmail.com>
2012-02-13 16:33:38 +0100

    removed all references to global authority (at least on the site, probably not all the tests yet)

--- core/app/views/channels/_single_menu_item.rb
+++ core/app/views/channels/_single_menu_item.rb
@@ -41,8 +41,7 @@ module Channels
       {
         id: self[:user].id,
         username: self[:user].username,
-        avatar: image_tag(self[:user].avatar_url(size: 32), :width => 32),
-        authority: Authority.from(self[:user].graph_user).to_f
+        avatar: image_tag(self[:user].avatar_url(size: 32), :width => 32)
       }
     end
 
