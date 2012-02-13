0f92833fe76e72e8febff4695926aac217460785
Mark IJbema <markijbema@gmail.com>
2012-02-13 14:44:17 +0100

    extended tests up until topic authority

--- core/spec/ohm-models/topic_specific_authority_spec.rb
+++ core/spec/ohm-models/topic_specific_authority_spec.rb
@@ -4,10 +4,23 @@ require 'spec_helper'
 describe "beliefs should work"  do
   include TopicBeliefExpressions
 
-  let(:u1) {FactoryGirl.create(:graph_user)}
-  let(:u2) {FactoryGirl.create(:graph_user)}
-  let(:u3) {FactoryGirl.create(:graph_user)}
-  let(:u4) {FactoryGirl.create(:graph_user)}
+  let(:u1) { create :graph_user }
+  let(:u2) { create :graph_user }
+  let(:u3) { create :graph_user }
+  let(:u4) { create :graph_user }
+
+  let(:ch1) { create :channel }
+
+  let(:fact_of_u1_with_two_supporting) do
+    f   = create :fact, created_by: u1
+    sf1 = create :fact, created_by: u2
+    sf2 = create :fact, created_by: u2
+
+    f.add_evidence(:supporting,sf1,u2)
+    f.add_evidence(:supporting,sf2,u2)
+
+    f
+  end
 
   before :all do
     load_topic_specific_authority
@@ -19,19 +32,26 @@ describe "beliefs should work"  do
 
 
   context "for facts" do
+    it "should give zero authority when a fact is only created" do
+      f = create :fact, created_by: u1
+
+      authority from: f, should_be: 0.0
+    end
 
     it "should give authority for a fact with supporting evidence" do
-      f = create :fact, created_by: u1
-      f2 = create :fact, created_by: u1
-      f3= create :fact, created_by: u1
-
-      f.add_evidence(:supporting,f2,u2)
-      f.add_evidence(:supporting,f3,u2)
-      authority from: f, should_be: 1.0
+      authority from: fact_of_u1_with_two_supporting, should_be: 1.0
     end
   end
 
   context "for channels" do
+    it "should give zero authority to a user who hasn't done anything" do
+      authority from: ch1, for: u1, should_be: 0.0
+    end
+    it "should forward the authority from the facts it contains" do
+      ch1.add_fact(fact_of_u1_with_two_supporting)
+
+      authority from: ch1, should_be: 1.0
+    end
   end
 
   context "for topics" do
@@ -42,27 +62,34 @@ describe "beliefs should work"  do
 
     it "should not give authority on a topic when creating a fact in it" do
       f = create :fact, created_by: u1
+
       c = create :channel, title: "foo"
-      authority of:u1, from: Topic.by_title("foo"), should_be: 0.0
+      c.add_fact f
+
+      foo_t = create :topic, title: "foo"
+
+      authority of:u1, from: foo_t, should_be: 0.0
     end
 
 
     it "should not give authority on a topic with a fact in it with only one supporting fact" do
       f = create :fact, created_by: u1
       f2 = create :fact, created_by: u1
-      c = create :channel, title: "foo"
       f.add_evidence(:supporting,f2,u2)
-      authority of:u1, from: Topic.by_title("foo"), should_be: 0.0
+
+      c = create :channel, title: "foo"
+      c.add_fact(f)
+
+      foo_t = create :topic, title: "foo"
+
+      authority of:u1, from: foo_t, should_be: 0.0
     end
 
-    pending "should not give authority on a topic when creating a fact in it" do
-      f = create :fact, created_by: u1
-      c = create :channel, title: "foo"
-      f2 = create :fact, created_by: u1
-      f3 = create :fact, created_by: u1
-      f.add_evidence(:supporting,f2,u2)
-      f.add_evidence(:supporting,f3,u2)
-      authority of:u1, from: Topic.by_title("foo"), should_be: 1.0
+    it "should not give authority on a topic when creating a fact in it" do
+      foo_ch = create :channel, title: "foo"
+      foo_ch.add_fact(fact_of_u1_with_two_supporting)
+      foo_t = create :topic, title: "foo"
+      authority of:u1, from: foo_t, should_be: 1.0
     end
   end
 end
