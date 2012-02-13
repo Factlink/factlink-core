7ec41a4ab1ac86c8846743de195aeaa5831bb045
Mark IJbema <markijbema@gmail.com>
2012-02-13 13:23:56 +0100

    simplified helper, improved authority retrieval to support from without a user

--- core/spec/support/topic_belief_expressions.rb
+++ core/spec/support/topic_belief_expressions.rb
@@ -1,23 +1,4 @@
 module TopicBeliefExpressions
-  def b(user,fact)
-    something_happened
-    fact.add_opinion(:believes,user.graph_user)
-  end
-
-  def d(user,fact)
-    something_happened
-    fact.add_opinion(:disbelieves,user.graph_user)
-  end
-
-  def u(user,fact)
-    something_happened
-    fact.add_opinion(:doubts,user.graph_user)
-  end
-
-  def _(b,d,u,a)
-    Opinion.new(b:b,d:d,u:u,a:a)
-  end
-
   def possible_reset
     unless @nothing_happened
       FactGraph.reset_values
@@ -30,22 +11,8 @@ module TopicBeliefExpressions
     @nothing_happened = false
   end
 
-  def a(user)
-    @nothing_happened = false
-    possible_reset
-    Authority.from(GraphUser[user.graph_user.id]).to_f.should
-  end
-
   def authority opts={}
-    @nothing_happened = false
     possible_reset
-    Authority.from(opts[:on], for: opts[:of]).to_f.should == opts[:should_be]
-  end
-
-  def opinion?(fact)
-    possible_reset
-    # values are recalculated in Redis, so get the object fresh from Redis
-    fact = fact.class[fact.id]
-    fact.get_opinion.should
+    Authority.from(opts[:on], opts.slice(:of)).to_f.should == opts[:should_be]
   end
 end
