31ed1964a0eac7b0382df544717576d39bd2eb3e
Tom de Vries <tom@codigy.nl>
2012-02-14 15:35:39 +0100

    Only kill the Resque process if it is running

--- core/bin/server/stop_resque.sh
+++ core/bin/server/stop_resque.sh
@@ -1,1 +1,8 @@
-cat ~/resque.pid | xargs kill
+PID=`cat ~/resque.pid`
+
+# Check if the process id is in the running processes list
+count=`ps x --no-header -eo pid | grep -c $PID`
+
+if [ "$count" -eq "1" ]; then
+    cat ~/resque.pid | xargs kill
+fi
