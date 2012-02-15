892391bdf80c5d768ff28d7010dd5e041b46d529
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-02-15 11:31:59 +0100

    Created a contained_channels list sorted by authority

--- core/app/ohm-models/channel.rb
+++ core/app/ohm-models/channel.rb
@@ -101,6 +101,28 @@ class Channel < OurOhm
     res
   end
 
+  def contained_channels_by_authority(gu)
+    auth = {}
+
+    self.contained_channels.to_a.sort do |a, b|
+      topic_a = Topic.by_title(a.title)
+      topic_b = Topic.by_title(b.title)
+
+      if topic_a.new?
+        topic_a.save
+      end
+
+      if topic_b.new?
+        topic_b.save
+      end
+
+      auth[topic_a.id] ||= Authority.from(topic_a, for: gu).to_f
+      auth[topic_b.id] ||= Authority.from(topic_b, for: gu).to_f
+
+      authority_b <=> authority_a
+    end
+  end
+
   def validate
     execute_callback(:before, :validate) # needed because of ugly ohm contrib callbacks
     super
