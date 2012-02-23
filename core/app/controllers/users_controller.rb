572feb2577707906f2f2f430b7d78cf887022a42
Mark IJbema <markijbema@gmail.com>
2012-02-23 14:27:05 +0100

    disable notifications

--- core/app/controllers/users_controller.rb
+++ core/app/controllers/users_controller.rb
@@ -11,28 +11,18 @@ class UsersController < ApplicationController
   end
 
   def activities
-    # TODO: This needs to become much more efficient. Now all activities are
-    # returned and sliced.
-    activities = Activity::For.user(@user.graph_user).sort(order: "DESC").slice(0..6)
-
     authorize! :index, Activity
 
     respond_to do |format|
-      format.json { render json: activities.map { |activity| Notifications::Activity.for(activity: activity, view: view_context) } }
+      format.json { render json: [] }
     end
   end
 
   def mark_activities_as_read
     authorize! :mark_activities_as_read, @user
 
-    @user.last_read_activities_on = DateTime.now
-
     respond_to do |format|
-      if @user.save
-        format.json { head :no_content }
-      else
-        format.json { render json: { :status => :unprocessable_entity } }
-      end
+      format.json { head :no_content }
     end
   end
 
