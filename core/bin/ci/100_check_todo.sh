5a21266add044cda1ede55bef0b669f5420d8690
Mark IJbema <markijbema@gmail.com>
2012-02-23 10:37:38 +0100

    more debugging

--- core/bin/ci/100_check_todo.sh
+++ core/bin/ci/100_check_todo.sh
@@ -2,6 +2,8 @@
 echo "TODO check"
 pwd
 
+ack-grep -c --ignore-dir=coverage  --ignore-dir=bin --ignore-dir=tmp '(TODO|HACK)'
+
 TODO=`ack-grep -c --ignore-dir=coverage  --ignore-dir=bin --ignore-dir=tmp '(TODO|HACK)' | perl -pe 's/.*://' | perl -pe 's/\n/+/smg' | perl -pe 's/$/0\n/'  | bc`
 TOO_MUCH_TODO=50
 echo "$TODO TODO's"
