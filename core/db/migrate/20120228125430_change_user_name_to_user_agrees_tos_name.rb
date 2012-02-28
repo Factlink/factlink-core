a76abee6a55a9617dff836d7c5d7945038c42ab6
Tom de Vries <tom@codigy.nl>
2012-02-28 14:05:29 +0100

    Added migration from the user.name to user.agrees_tos_name

---
+++ core/db/migrate/20120228125430_change_user_name_to_user_agrees_tos_name.rb
@@ -0,0 +1,13 @@
+class ChangeUserNameToUserAgreesTosName < Mongoid::Migration
+  def self.up
+    say_with_time "Changing user.name to user.agrees_tos_name" do
+      User.all.each do |user|
+        user.agrees_tos_name = u.name
+        user.save
+      end
+    end
+  end
+
+  def self.down
+  end
+end
