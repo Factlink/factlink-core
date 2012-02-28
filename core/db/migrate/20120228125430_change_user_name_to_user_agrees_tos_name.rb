a795f7468b472526961bc916567b9c891bffd594
Tom de Vries <tom@codigy.nl>
2012-02-28 14:37:11 +0100

    Unset the old name

--- core/db/migrate/20120228125430_change_user_name_to_user_agrees_tos_name.rb
+++ core/db/migrate/20120228125430_change_user_name_to_user_agrees_tos_name.rb
@@ -3,6 +3,7 @@ class ChangeUserNameToUserAgreesTosName < Mongoid::Migration
     say_with_time "Changing user.name to user.agrees_tos_name" do
       User.all.each do |user|
         user.agrees_tos_name = u.name
+        user.unset(:name)
         user.save
       end
     end
