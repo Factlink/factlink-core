17f021ee86c481042e131ae89ee14320d96f0002
Tom de Vries <tom@codigy.nl>
2012-02-28 15:44:19 +0100

    Added space

--- core/app/controllers/tos_controller.rb
+++ core/app/controllers/tos_controller.rb
@@ -13,7 +13,7 @@ class TosController < ApplicationController
     agrees_tos      = (params[:user][:agrees_tos].to_i == 1) ? true : false
     agrees_tos_name = params[:user][:agrees_tos_name]
 
-    if current_user.sign_tos(agrees_tos,agrees_tos_name)
+    if current_user.sign_tos(agrees_tos, agrees_tos_name)
       redirect_to user_profile_path(current_user)
     else
       render :show
