7f4a98d85922f05e059f94b76b5ed80472ae885d
Tom de Vries <tom@codigy.nl>
2012-02-29 18:29:47 +0100

    Reverse can be done as an option with a sorted set

--- core/app/controllers/users_controller.rb
+++ core/app/controllers/users_controller.rb
@@ -26,8 +26,7 @@ class UsersController < ApplicationController
 
   def activities
     authorize! :see_activities, @user
-    activities = @user.graph_user.notifications.below('inf', count: 7)
-    activities.reverse!
+    activities = @user.graph_user.notifications.below('inf', count: 7, reversed: true )
 
     respond_to do |format|
       format.json { render json: activities.map { |activity| Notifications::Activity.for(activity: activity, view: view_context) } }
