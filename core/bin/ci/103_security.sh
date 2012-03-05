cd1c42d4c5bd7f17b56beaf4311d6375bb039913
Mark IJbema <markijbema@gmail.com>
2012-03-05 16:00:22 +0100

    break quick if something fails

--- core/bin/ci/103_security.sh
+++ core/bin/ci/103_security.sh
@@ -2,6 +2,6 @@
 echo "Running security check"
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
-bundle exec brakeman -o /tmp/brakeman_tabs
+bundle exec brakeman -o /tmp/brakeman_tabs || exit 1
 cat bin/ci/103_security_ignore.txt | grep -E '.' | perl -e '$_=join"|",<>;s/\n//;chomp;print"grep -vE \"".$_."\"  /tmp/brakeman_tabs\n"' | sh
 exit
