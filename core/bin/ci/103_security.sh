1a212dc00ccd79c9e544c19c2e2a8dfbfb6ee9c7
Mark IJbema <markijbema@gmail.com>
2012-03-05 16:45:47 +0100

    improved securitycheck (still with intentional error)

--- core/bin/ci/103_security.sh
+++ core/bin/ci/103_security.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running security check"
 source "$HOME/.rvm/scripts/rvm"
-rvm use --default 1.9.2-p290 || exit 1
-bundle exec brakeman -o /tmp/brakeman_tabs || exit 1
-cat bin/ci/103_security_ignore.txt | grep -E '.' | perl -e '$_=join"|",<>;s/\n//;chomp;print"grep -vE \"".$_."\"  /tmp/brakeman_tabs\n"' | sh
-exit
+rvm use 1.9.2-p290 || exit 1
+bundle || exit 1
+bundle exec brakeman -o /tmp/brakeman.tabs || exit 1
+exit `cat bin/ci/103_security_ignore.txt | grep -E '.' | perl -e '$_=join"|",<>;s/\n//;chomp;print"grep -vE \"".$_."\"  /tmp/brakeman.tabs\n"' | sh | grep -E . | wc -l`
