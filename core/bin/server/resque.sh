f378266aaf370f46688b329c6aa939e484280ed6
Tom de Vries <tom@codigy.nl>
2012-03-06 14:04:09 +0100

    Added resque script

--- core/bin/server/resque.sh
+++ core/bin/server/resque.sh
@@ -4,8 +4,12 @@ case $1 in
       sh /applications/factlink-core/current/bin/server/start_resque.sh
       ;;
     stop)
+<<<<<<< HEAD
       sh /applications/factlink-core/current/bin/server/stop_resque.sh
       ;;
+=======
+      sh /applications/factlink-core/current/bin/server/stop_resque.sh ;;
+>>>>>>> 024485a... Wrapper script to start and stop Resque
     *)
       echo "usage: xyz {start|stop}" ;;
 esac
