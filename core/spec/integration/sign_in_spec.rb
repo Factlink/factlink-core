1ac84c7253fcfd2cf0b2ee5fb2152531b8ec3f03
Tom de Vries <tom@codigy.nl>
2012-03-12 19:20:34 +0100

    Integration test for signing in

--- core/spec/integration/sign_in_spec.rb
+++ core/spec/integration/sign_in_spec.rb
@@ -2,17 +2,22 @@ require 'spec_helper'
 require 'integration_helper'
 
 describe "the sign in process", :type => :request do
-  before :each do
-    @user = Factory.create(:user, email: "user@example.com")
-    @user.confirm!
 
+  before :each do
+    @user = int_user
     visit "/"
   end
 
-  it "signs me in" do
+  it "signs me in", js: true do
     fill_in 'user_email', :with => @user.email
     fill_in 'user_password', :with => @user.password
 
     click_button 'Sign in'
+
+    within(:css, "h1") do
+      page.should have_content("All")
+    end
+
+    page.should have_content("Sign out")
   end
 end
