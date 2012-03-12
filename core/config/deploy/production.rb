75eb74e6a8fe42dcdf711f715d9231618f38ac2d
Mark IJbema <markijbema@gmail.com>
2012-03-12 11:34:04 +0100

    also fix branches for staging and beta

--- core/config/deploy/production.rb
+++ core/config/deploy/production.rb
@@ -5,8 +5,6 @@ set :full_url, 'https://beta.factlink.com'
 set :deploy_env, 'production'
 set :rails_env,  'production' # Also used by capistrano for some specific tasks
 
-set :branch, "master"
-
 role :web, "beta.factlink.com"                          # Your HTTP server, Apache/etc
 role :app, "beta.factlink.com"                          # This may be the same as your `Web` server
 
