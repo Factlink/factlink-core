f68e5db0e5b4a33a435fc911c992719f24ccff5e
Tom de Vries <tom@codigy.nl>
2012-03-12 18:37:33 +0100

    No monkeypatching password_digest, and please stretch the configu at least one time (as stated in the Devise documentation)

--- core/spec/support/devise.rb
+++ core/spec/support/devise.rb
@@ -3,17 +3,11 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 end
 
-module Devise
-  module Models
-    module DatabaseAuthenticatable
-      protected
-        def password_digest(password)
-          password
-        end
-    end
-  end
-end
-
 Devise.setup do |config|
-  config.stretches = 0
+  # Set the number of stretches to 1 for your test environment to speed up
+  # unit tests: https://github.com/plataformatec/devise/wiki/Speed-up-your-unit-tests
+
+  # Using less stretches will increase performance dramatically if you use
+  # bcrypt and create a lot of users (i.e. you use FactoryGirl or Machinist).
+  config.stretches = 1
 end
