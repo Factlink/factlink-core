d33debf0133084c1565e849f6bb2d0210c8f9495
Mark IJbema <markijbema@gmail.com>
2012-03-12 12:59:29 +0100

    fixed deploying racecondition

--- js-library/config/deploy/testserver.rb
+++ js-library/config/deploy/testserver.rb
@@ -2,8 +2,6 @@ server 'testserver.factlink.com', :app, :web, :primary => true
 
 set :deploy_env, 'testserver'
 
-set :branch, "develop"
-
 role :web, "testserver.factlink.com"                          # Your HTTP server, Apache/etc
 role :app, "testserver.factlink.com"                          # This may be the same as your `Web` server
 role :db,  "testserver.factlink.com", :primary => true # This is where Rails migrations will run
