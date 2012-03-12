18c174f753a3a5d55396d182c1cffe5d50a426d1
Tom de Vries <tom@codigy.nl>
2012-03-12 19:20:23 +0100

    Added helper functions

--- core/spec/integration_helper.rb
+++ core/spec/integration_helper.rb
@@ -1,1 +1,16 @@
 require 'capybara/rspec'
+
+Capybara.javascript_driver = :webkit
+
+def int_user
+  user = Factory.create(:user, email: "user@example.com")
+  user.confirm!
+  user
+end
+
+def handle_js_confirm(accept=true)
+  page.evaluate_script "window.original_confirm_function = window.confirm"
+  page.evaluate_script "window.confirm = function(msg) { return #{!!accept}; }"
+  yield
+  page.evaluate_script "window.confirm = window.original_confirm_function"
+end
