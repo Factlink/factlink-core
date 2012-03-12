8af63f07fc4af5eefcc31fef276fa440d8c3d3d5
Mark IJbema <markijbema@gmail.com>
2012-03-12 17:29:36 +0100

    fixed the tests

--- core/spec/routing/jobs_routing_spec.rb
+++ core/spec/routing/jobs_routing_spec.rb
@@ -4,11 +4,11 @@ describe JobsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/pages/jobs").should route_to("jobs#index")
+      get("/p/jobs").should route_to("jobs#index")
     end
 
     it "routes to #show" do
-      get("/pages/jobs/1").should route_to("jobs#show", :id => "1")
+      get("/p/jobs/1").should route_to("jobs#show", :id => "1")
     end
 
   end
