31783fccb0eba54dadac2b3e64decb3939120643
Mark IJbema <markijbema@gmail.com>
2012-03-14 14:32:13 +0100

    now hopefully with outputfile

--- core/bin/ci/201_soundcheck.sh
+++ core/bin/ci/201_soundcheck.sh
@@ -3,5 +3,7 @@ echo "Running tests"
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
 export RUN_METRICS=TRUE
-bundle exec rspec spec | grep ', 0 failures' || exit 1
+OUTPUTFILE=(tempfile)
+bundle exec rspec spec | tee "$OUTPUTFILE"
+cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
 exit 
