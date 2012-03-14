7090cd62b51c2fd36d8438db19889bc1ff6dcd99
Mark IJbema <markijbema@gmail.com>
2012-03-14 14:05:53 +0100

    whitespace

--- core/spec/controllers/channels_controller_spec.rb
+++ core/spec/controllers/channels_controller_spec.rb
@@ -12,19 +12,19 @@ describe ChannelsController do
 
   let (:ch1) do
     ch1 = FactoryGirl.create :channel, created_by: user.graph_user
-    
+
     [f1,f2,f3].each do |f|
       f.created_by.user = FactoryGirl.create(:user)
       f.created_by.save
     end
-    
+
     ch1.add_fact f1
     ch1.add_fact f2
     ch1.add_fact f3
     ch1.save
     ch1
   end
-  
+
   describe "#new" do
     it "should be succesful" do
       authenticate_user!(user)
@@ -41,14 +41,14 @@ describe ChannelsController do
       get :index, username: user.username, format: 'json'
       response.should be_succes
     end
-    
+
     it "as bogus user should redirect to Terms of Service page" do
       authenticate_user!(nonnda_user)
       get :index, username: user.username
       response.should redirect_to(tos_path)
     end
   end
-  
+
   describe "#related_users" do
     it "should render" do
       authenticate_user!(user)
@@ -66,7 +66,7 @@ describe ChannelsController do
       response.should be_succes
     end
   end
-  
+
   describe "#facts" do
     it "should render" do
       authenticate_user!(user)
@@ -75,7 +75,7 @@ describe ChannelsController do
       response.should be_succes
     end
   end
-  
+
 
   describe "#show" do
     it "a channel should be succesful" do
