3ba69afb50b75454a7da9268dfec7e1e1ee90ec8
Tom de Vries <tom@codigy.nl>
2012-03-15 11:26:59 +0100

    Integration tests should stop the CI when test fails. Use correct tempfile

--- core/bin/ci/200_integration_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Running integration tests"
-source "$HOME/.rvm/scripts/rvm"
-rvm use --default 1.9.2-p290 || exit 1
-bundle exec  rspec spec/integration/integration_spec.rb
-exit
---
+++ core/bin/ci/202_integration_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running integration tests"
+source "$HOME/.rvm/scripts/rvm"
+rvm use --default 1.9.2-p290 || exit 1
+
+OUTPUTFILE=$(tempfile)
+bundle exec rspec spec/integration/integration_spec.rb | tee "$OUTPUTFILE"
+cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
+exit
