c6c92140be23d13b88f068098db9ef087c79c971
Tom de Vries <tom@codigy.nl>
2012-03-15 11:07:00 +0100

    Improved the message when running integration tests

--- core/bin/ci/200_integration_tests.sh
+++ core/bin/ci/200_integration_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Running tests"
+echo "Running integration tests"
 source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
 bundle exec  rspec spec/integration/integration_spec.rb
