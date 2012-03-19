0aab0d176519fce618a661d05f1ece749993a3e8
Tom de Vries <tom@codigy.nl>
2012-03-19 17:31:31 +0100

    Fix tooltip bug. Don't appear on the right side anymore but appear on the right side.

--- core/app/assets/javascripts/backbone/views/channel_view.js
+++ core/app/assets/javascripts/backbone/views/channel_view.js
@@ -134,8 +134,8 @@ window.ChannelView = Backbone.View.extend({
       self.model.trigger('loaded')
                   .trigger('activate', self.model);
     }
-    twipsy.toElement(this.$el.find('header .authority'))
-      .addHint('Authority of ' + self.model.attributes.created_by.username + ' on "' + self.model.attributes.title + '"');
+    this.$el.find('header .authority')
+      .tooltip({title: 'Authority of ' + self.model.attributes.created_by.username + ' on "' + self.model.attributes.title + '"'});
 
 
     return this;
