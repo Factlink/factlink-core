aa55b515808b16a94c0730f898d6364c5e064eee
Mark IJbema <markijbema@gmail.com>
2012-03-19 16:30:08 +0100

    syntax fix

--- core/app/ohm-models/channel.rb
+++ core/app/ohm-models/channel.rb
@@ -106,7 +106,7 @@ class Channel < OurOhm
     super
     assert_present :title
     assert_present :created_by
-    assert_unique([:slug_title:,:created_by_id])
+    assert_unique([:slug_title,:created_by_id])
     execute_callback(:after, :validate) # needed because of ugly ohm contrib callbacks
   end
 
