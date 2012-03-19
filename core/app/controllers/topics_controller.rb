71674fc0013168625cabfb17f9f5d9666fbea1f1
Mark IJbema <markijbema@gmail.com>
2012-03-19 16:29:58 +0100

    renamed controller

--- core/app/controllers/topics_controller.rb
+++ core/app/controllers/topics_controller.rb
@@ -1,4 +1,4 @@
-class TopicController < ApplicationController
+class TopicsController < ApplicationController
   before_filter :load_topic
 
   def related_users
@@ -7,14 +7,14 @@ class TopicController < ApplicationController
      render layout: false, partial: "channels/related_users",
        locals: {
             related_users: GraphUser.all,
-            topic: Topic.by_title(@channel.title),
+            topic: @topic,
             excluded_users: [@channel.created_by]
        }
   end
 
   private
     def load_topic
-      @topic  = Topic[params[:id]]
+      @topic  = Topic.by_slug(params[:id])
       @topic || raise_404("Topic not found")
     end
 end
