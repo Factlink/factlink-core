eb2fd5b0dffe2931ba3923e46b9336e7b1397e3f
Tom de Vries <tom@codigy.nl>
2012-03-22 11:13:04 +0100

    Markje pullen. Broken commit pushen.

--- core/spec/classes/lazy_spec.rb
+++ core/spec/classes/lazy_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path('../../../app/classes/lazy.rb', __FILE__)
 
+describe "Fukushima" do
+  it "should fail" do
+    res = 1 + 1
+    res.should == 3
+  end
+end
+
 describe Lazy do
-  subject { 
+  subject {
     Lazy.new { 1 }
   }
 
