3a2ea33a568b1734332a489ed39360566ab1310b
Tom de Vries <tom@codigy.nl>
2012-03-26 16:54:37 +0200

    Added :approved fiel to User migration. All current users will get approved

---
+++ core/db/migrate/20120326121026_add_approved_to_user.rb
@@ -0,0 +1,18 @@
+class AddApprovedToUser < Mongoid::Migration
+  def self.up
+    say_with_time "approving current users" do
+      User.all.each do |user|
+        user.approve = true
+        user.save
+      end
+    end
+  end
+
+  def self.down
+    say_with_time "removing :approved field from User" do
+      User.all.each do |user|
+        user.unset(:approved)
+      end
+    end
+  end
+end
