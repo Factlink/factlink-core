0a3ce02d4f692b282a62a2b3036b021f48a7c80e
Tom de Vries <tom@codigy.nl>
2012-03-26 16:14:17 +0200

    Register a new user

--- core/app/controllers/users/registrations_controller.rb
+++ core/app/controllers/users/registrations_controller.rb
@@ -4,6 +4,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :load_user, only: [:edit_password, :update_password]
   before_filter :authenticate_user!
 
+  def create
+    build_resource
+
+    # Set random password
+    generated_password = Devise.friendly_token
+
+    # Assign field
+    resource.email    = params[:user][:email]
+    resource.password = generated_password
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      redirect_to root_path
+    end
+  end
+
   def edit_password
     authorize! :update, @user
 
