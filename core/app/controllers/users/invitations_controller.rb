e7645119243d3e2ee3eb7d3ca50fc692807931d5
Mark IJbema <markijbema@gmail.com>
2012-03-28 15:59:31 +0200

    added option to add message to invitation

--- core/app/controllers/users/invitations_controller.rb
+++ core/app/controllers/users/invitations_controller.rb
@@ -1,4 +1,22 @@
 class Users::InvitationsController < Devise::InvitationsController
+  def create
+    self.resource = resource_class.invite! params[resource_name], current_inviter do |invitee|
+      invitee.instance_variable_set :@invitation_message, params[:invite][:message]
+      class << invitee
+        def invitation_message
+          @invitation_message
+        end
+      end
+    end
+
+    if resource.errors.empty?
+      set_flash_message :notice, :send_instructions, :email => self.resource.email
+      respond_with resource, :location => after_invite_path_for(resource)
+    else
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+
   def after_invite_path_for(resource)
     after_sign_in_path_for(current_user)
   end
