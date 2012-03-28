7ac176d0d85b5028a2a92d2fd59fc1ce3838007d
Mark IJbema <markijbema@gmail.com>
2012-03-28 17:29:54 +0200

    added basic tests and test for issue #140

--- core/spec/classes/url_normalizer_spec.rb
+++ core/spec/classes/url_normalizer_spec.rb
@@ -5,8 +5,17 @@ describe UrlNormalizer do
     def should_normalize_to(url, expected)
       UrlNormalizer.normalize(url).should == expected
     end
-    
-    it { should_normalize_to 'http://www.google.com/foo', 'http://www.google.com/foo'}
+
+    it { should_normalize_to 'http://www.google.com/foo', 'http://www.google.com/foo' }
+    it { should_normalize_to 'http://www.google.com/foo#bar', 'http://www.google.com/foo' }
+
+    pending "fixing issue #140" do
+      ['utm_source', 'utm_medium', 'utm_source', 'utm_content', 'utm_campaign'].each do |strip_param|
+        it { should_normalize_to "http://www.google.com/foo?#{strip_param}=bar",'http://www.google.com/foo'}
+        it { should_normalize_to "http://www.google.com/foo?#{strip_param}=bar&x=y",'http://www.google.com/foo?x=y'}
+      end
+    end
+
   end
-  
+
 end
