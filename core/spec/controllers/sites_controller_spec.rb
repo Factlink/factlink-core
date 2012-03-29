15c53b957fee1a20e66a6f903409bfd96d95e636
Mark IJbema <markijbema@gmail.com>
2012-03-29 10:32:22 +0200

    moved site controller actions used by chrome extension out of authenticated section. Should fix #330, if that was not already fixed.

--- core/spec/controllers/sites_controller_spec.rb
+++ core/spec/controllers/sites_controller_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe SitesController do
-  describe "gets count for page" do
-    
+  describe :facts_count_for_url do
+
     it "should return 0 for site with no facts" do
+      should_check_can :check, Blacklist
       should_check_can :index, Fact
       get "facts_count_for_url", { :url => "http://baronie.nl" }
       parsed_content = JSON.parse(response.body)
       parsed_content.should have_key("count")
       parsed_content['count'].should == 0
     end
-    
+
   end
 
-  describe :factlinks_for_url do
+  describe :facts_for_url do
     it "should work with an existing site" do
+      should_check_can :check, Blacklist
       should_check_can :index, Fact
       @site = FactoryGirl.create(:site, :url => "http://batman.org")
       get :facts_for_url, :url => @site.url
@@ -22,9 +24,23 @@ describe SitesController do
     end
 
     it "should work with an non-existing site" do
+      should_check_can :check, Blacklist
       should_check_can :index, Fact
       get :facts_for_url, :url => "http://www.thebaronisinthebuilding.com/"
       response.body.should eq("[]")
     end
-  end 
+  end
+
+  describe :blacklisted do
+    it "should work with a non-blocked site" do
+      should_check_can :check, Blacklist
+      get :blacklisted, :url => 'http://batman.org/'
+      response.body.should eq("{}")
+    end
+    it "should work with a blocked site" do
+      should_check_can :check, Blacklist
+      get :blacklisted, :url => 'http://factlink.com/'
+      response.body.should eq("{\"blacklisted\":true}")
+    end
+  end
 end
