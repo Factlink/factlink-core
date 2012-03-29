23f76fe5232d735b6c290a3d0f3718c34c661437
Mark IJbema <markijbema@gmail.com>
2012-03-29 10:32:42 +0200

    don't throw exceptions when a template cannot be found, throw a 404 (normal exceptions still raised)

--- core/app/controllers/templates_controller.rb
+++ core/app/controllers/templates_controller.rb
@@ -2,7 +2,13 @@ class TemplatesController < ApplicationController
 
   def show
     respond_to do |format|
-      format.html {render "templates/" + params[:name], :layout => "templates", :content_type => "text/javascript"}
+      format.html do
+        begin
+          render "templates/" + params[:name], :layout => "templates", :content_type => "text/javascript"
+        rescue ActionView::MissingTemplate
+          raise_404
+        end
+      end
     end
   end
 end
