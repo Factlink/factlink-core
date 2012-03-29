182f296e6fc86facb39eb86f1670323d4335a11c
Jan Salvador van der Ven <salvador@MacBookPro481.local>
2012-03-29 14:12:04 +0200

    integration tests for the static part of the site

--- core/bin/ci/202_integration_tests.sh
+++ core/bin/ci/202_integration_tests.sh
@@ -4,6 +4,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm use --default 1.9.2-p290 || exit 1
 
 OUTPUTFILE=$(tempfile)
-bundle exec rspec spec/integration/integration_spec.rb | tee "$OUTPUTFILE"
+bundle exec rspec spec/integration/ | tee "$OUTPUTFILE"
 cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
 exit
--- core/spec/integration/home_page_spec.rb
+++ core/spec/integration/home_page_spec.rb
@@ -7,11 +7,26 @@ describe "When visiting the home page", type: :request do
   end
 
   context "as a logged out user" do
+    it "should be able to view the Home page" do
+      click_link "Home"
+
+      within(:css, "h2") do
+        page.should have_content("Building a collectively reviewed perspective on the world's information")
+      end
+    end
+   
     it "should be able to view the About page" do
       click_link "About"
 
       within(:css, "h1") do
-        page.should have_content("About Factlink")
+        page.should have_content("Building a collective perspective")
+      end
+    end
+
+    it "should be able to view the Team page" do
+      click_link "Team"
+      within(:css, "h1") do
+        page.should have_content("The people behind Factlink")
       end
     end
 
@@ -21,6 +36,36 @@ describe "When visiting the home page", type: :request do
         page.should have_content("Jobs")
       end
     end
+
+    it "should be able to view the Blog page" do
+      click_link "Blog"
+      within(:css, "h2") do
+        page.should have_content("Pages")
+      end
+    end
+
+    it "should be able to view the Contact page" do
+      click_link "Contact"
+      within(:css, "h1") do
+        page.should have_content("Contact us")
+      end
+    end
+
+    it "should be able to view the TOS page" do
+      click_link "Terms of Service"
+      within(:css, "h1") do
+        page.should have_content("Terms of Service")
+      end
+    end
+
+    it "should be able to view the Privacy Policy page" do
+      click_link "Privacy Policy"
+      within(:css, "h1") do
+        page.should have_content("Privacy Policy")
+      end
+    end
+
+
   end
 
 end
