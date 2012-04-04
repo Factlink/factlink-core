0bb94d34975eeedf90a827e3f73e4c4a66a618d2
Tom de Vries <tom@codigy.nl>
2012-04-04 14:23:05 +0200

    Redirect unapproved users to the information page

--- core/app/controllers/users/confirmations_controller.rb
+++ core/app/controllers/users/confirmations_controller.rb
@@ -1,12 +1,19 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
 
+  layout "frontend"
+
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_navigational_format?
       # sign_in(resource_name, resource) #Don't sign in, we don't allow that
-      respond_with_navigational(resource){ redirect_to "/" } # Redirect to the root
+
+      unless resource.approved?
+        respond_with_navigational(resource){ render "awaiting_approval" }
+      else
+        respond_with_navigational(resource){ render "/" }
+      end
     else
       respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
     end
