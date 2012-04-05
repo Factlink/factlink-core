f4d4fa2c6e21963d72e0d35985b4713f2be8b693
Mark IJbema <markijbema@gmail.com>
2012-04-05 17:00:16 +0200

    remove discontinued channels

---
+++ core/db/migrate/20120405145855_remove_discontinued_channels.rb
@@ -0,0 +1,10 @@
+class RemoveDiscontinuedChannels < Mongoid::Migration
+  def self.up
+    Channel.find(discontinued: 'true').each do |ch|
+      ch.delete
+    end
+  end
+
+  def self.down
+  end
+end
