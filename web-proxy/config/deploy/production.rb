e61ac1b9d4edbc9d44ef8b14852a9d776dce64d0
Mark IJbema <markijbema@gmail.com>
2012-04-05 16:49:29 +0200

    Revert "use beta.fct.li to deploy the new server"
    This reverts commit b0979501d7488bd22509e4f8038d7d0794915747.

--- web-proxy/config/deploy/production.rb
+++ web-proxy/config/deploy/production.rb
@@ -1,7 +1,7 @@
-server 'beta.fct.li', :app, :web, :primary => true
+server 'fct.li', :app, :web, :primary => true
 
 set :deploy_env, 'production'
 
-role :web, "beta.fct.li"                          # Your HTTP server, Apache/etc
-role :app, "beta.fct.li"                          # This may be the same as your `Web` server
-role :db,  "beta.fct.li", :primary => true # This is where Rails migrations will run
+role :web, "fct.li"                          # Your HTTP server, Apache/etc
+role :app, "fct.li"                          # This may be the same as your `Web` server
+role :db,  "fct.li", :primary => true # This is where Rails migrations will run
