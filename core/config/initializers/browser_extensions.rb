648bd42269ce960aaf069667df794c82784df73c
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-04-17 10:08:52 +0200

    Fixed small naming issue

--- core/config/initializers/browser_extensions.rb
+++ core/config/initializers/browser_extensions.rb
@@ -1,3 +1,2 @@
-extensions_conf = YAML::load_file(Rails.root.join('config/browser_extensions.yml'))[Rails.env]['chrome']
-FactlinkUI::Application.config.chrome_extension_id = extensions_conf['id']
- 
+extensions_conf = YAML::load_file(Rails.root.join('config/browser_extensions.yml'))[Rails.env]
+FactlinkUI::Application.config.chrome_extension_id = extensions_conf['chrome']['id']
