6f62fcf19bea4980821c452fdffbade04fa982a4
tomdev <tom@codigy.nl>
2012-04-18 15:28:39 +0200

    Defined a maximum length for the friendly_fath_path slug
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/spec/helpers/fact_helper_spec.rb
+++ core/spec/helpers/fact_helper_spec.rb
@@ -15,5 +15,14 @@ describe FactHelper do
       f.should_receive(:id).any_number_of_times().and_return(1)
       helper.friendly_fact_path(f).should eq("/1/f/1")
     end
+
+    it "should have a maximum length" do
+      max_slug_length = 5
+
+      f = mock(:fact)
+      f.should_receive(:to_s).any_number_of_times().and_return("Bla Bla Bla Bla")
+      f.should_receive(:id).any_number_of_times().and_return(1)
+      helper.friendly_fact_path(f, max_slug_length).should eq("/bla-b/f/1")
+    end
   end
 end
