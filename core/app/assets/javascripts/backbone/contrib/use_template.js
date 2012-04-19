aafe996dead696c2cdf4fed472626a6857547859
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-04-19 15:25:51 +0200

    Added caching to our client side templates. This should speed things up drastically

--- core/app/assets/javascripts/backbone/contrib/use_template.js
+++ core/app/assets/javascripts/backbone/contrib/use_template.js
@@ -3,16 +3,44 @@
 
     call this in the initialize()
 */
-Backbone.View.prototype.useTemplate = function(dir,file) {
-  if (this.tmpl !== undefined) {
-    return;
-  }
+(function() {
+  var TemplateCache = {
+    get: function(dir, file) {
+      var self = this;
 
-  var self = this;
-  this.tmpl = $('#mustache-templates .mustache-dir-'+dir+".mustache-file-"+file).html();
-  this.partials = {};
-  $('#mustache-templates .mustache-dir-'+dir).each(function(index, element){
-    var filename = $(element).data('filename').replace(/^_/,'');
-    self.partials[filename] = $(element).html();
-  });
-};
+      if ( !self.templates ) { self.templates = {}; }
+
+      if ( !self.templates[dir] ) {
+        self.templates[dir] = {};
+
+        $('#mustache-templates>.mustache-dir-' + dir ).each(function(index, element) {
+          var $element = $( element );
+          var filename = $element.data('filename');
+
+          self.templates[dir][filename] = $element.html();
+        });
+      }
+
+      var template = self.templates[dir][file];
+
+      if ( !template ) {
+        template = self.templates[dir][ file.indexOf("_") === 0 ? file.substr(1) : "_" + file ]
+      }
+
+      return template;
+    }
+  };
+
+  Backbone.View.prototype.useTemplate = function(dir,file) {
+    if (this.tmpl !== undefined) { return; }
+
+    var self = this;
+
+    this.tmpl = TemplateCache.get(dir, file);
+    this.partials = {};
+
+    _.each(TemplateCache.templates[dir], function(value, key) {
+      self.partials[ key.replace(/^_/,'') ] = TemplateCache.get(dir, key);
+    });
+  };
+}());
