7a2d6e57531b0d402a9363911a52a45d77a3c9e0
tomdev <tom@codigy.nl>
2012-04-19 16:35:39 +0200

    Fixed the tests, use array in stead of hash
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/spec/classes/fake_fact_spec.rb
+++ core/spec/classes/fake_fact_spec.rb
@@ -1,14 +1,13 @@
-require File.expand_path('../../../app/classes/fake_fact.rb', __FILE__)
-require 'hashie'
-require 'date'
+require "spec_helper"
+
 describe FakeFact do
   it "should be possible to create one" do
-    f = FakeFact.new({believes: 100, disbelieves: 3, doubts: 50}, [], "banna")
+    f = FakeFact.new([100, 3, 50], [], "banna")
     f.interactions.below(10).should == []
   end
 
   it "should be possible to create one" do
-    f = FakeFact.new({believes: 100, disbelieves: 3, doubts: 50}, [['remon','http://foo']], "banna")
+    f = FakeFact.new([100, 3, 50], [['remon','http://foo']], "banna")
     f.interactions.below(10).length.should == 1
     puts f.interactions.below(10)[0].user
 
