04cec1526eed8276b509f25bdde9c6ddc5e37ea8
Tom de Vries <tom@codigy.nl>
2012-04-19 20:55:34 +0200

    Forgot extension for the fake_facts.css file. 00ps.

--- core/config/application.rb
+++ core/config/application.rb
@@ -140,6 +140,7 @@ module FactlinkUI
       /\w+\.(?!js|css|less).+/,
       'admin.css',
       'bubble.css',
+      'fake_facts.css',
       'frontend.css',
       'general.css',
       'landing.css',
@@ -152,8 +153,7 @@ module FactlinkUI
       'landing.js',
       'intermediate.js',
       'modernizr-loader.js',
-      'admin.js',
-      'fake_facts'
+      'admin.js'
     ]
 
     # we only cache very little, so memory_store is fine for now
