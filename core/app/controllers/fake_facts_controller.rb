90881bca395ba8c655ec4fd2d03edb0aa06f325d
Mark IJbema <markijbema@gmail.com>
2012-04-19 12:30:01 +0200

    correct type switching, plus fixed vars

--- core/app/controllers/fake_facts_controller.rb
+++ core/app/controllers/fake_facts_controller.rb
@@ -2,39 +2,41 @@ class FakeFactsController < ApplicationController
   layout "fake_facts"
 
   def show
-    if params[:type] == 1
-      @interacting_users_aaa = []
+    type = params[:cid].to_i
+    if type == 0
+      interacting_users = []
 
-      @opinions_aaa = {
-        believes: 100,
-        disbelieves: 5,
-        doubts: 10
+      opinions = {
+        believes: 0,
+        disbelieves: 0,
+        doubts: 0
       }
-    elsif params[:type] == 2
-      @interacting_users_aaa = [
+    elsif type == 2
+      interacting_users = [
         ["remon", "https://secure.gravatar.com/avatar/cd0ce9385b21c5015f66854e760fdfe2?rating=PG&size=20&default=retro"],
         ["tom", "https://secure.gravatar.com/avatar/cd0ce9385b21c5015f66854e760fdfe2?rating=PG&size=20&default=retro"]
       ]
 
-      @opinions_aaa = {
+      opinions = {
         believes: 100,
         disbelieves: 5,
         doubts: 10
       }
     else
-      @interacting_users_aaa = [
+      interacting_users = [
+        ["remon", "https://secure.gravatar.com/avatar/cd0ce9385b21c5015f66854e760fdfe2?rating=PG&size=20&default=retro"],
         ["remon", "https://secure.gravatar.com/avatar/cd0ce9385b21c5015f66854e760fdfe2?rating=PG&size=20&default=retro"],
         ["tom", "https://secure.gravatar.com/avatar/cd0ce9385b21c5015f66854e760fdfe2?rating=PG&size=20&default=retro"]
       ]
 
-      @opinions_aaa = {
+      opinions = {
         believes: 100,
         disbelieves: 5,
         doubts: 10
       }
     end
 
-    @fact = FakeFact.new @opinions_aaa, @interacting_users_aaa, "Bananen zijn krom"
+    @fact = FakeFact.new opinions, interacting_users, "Bananen zijn krom"
     render "facts/extended_show"
   end
 
