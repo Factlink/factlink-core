5c585a4ca10fd389d6f5a3d6c1b8b0bf3821aa25
Mark IJbema <markijbema@gmail.com>
2012-04-24 15:48:37 +0200

    use airbrake as error backend for resque

--- core/config/initializers/resque.rb
+++ core/config/initializers/resque.rb
@@ -8,3 +8,11 @@ Resque.redis = resque_config[rails_env]
 if defined?(Rails)
   Resque.inline = Rails.env.test?
 end
+
+
+require 'resque/failure/airbrake'
+Resque::Failure::Airbrake.configure do |config|
+  config.api_key = '03abfb3c20150b13e470fdd4d21d03d3'
+  config.secure = false # only set this to true if you are on a paid Airbrake plan
+end
+Resque::Failure.backend = Resque::Failure::Airbrake
