795d94f76a1e41a6204ab0049210448d6f82a8a1
Mark IJbema <markijbema@gmail.com>
2012-04-24 10:13:28 +0200

    more hacks and monkeypatching

--- core/config/initializers/resque_mailer.rb
+++ core/config/initializers/resque_mailer.rb
@@ -1,2 +1,16 @@
 # config/initializers/resque_mailer.rb
-Resque::Mailer.excluded_environments = [:test, :develop]
+Resque::Mailer.excluded_environments = []#[:test, :develop]
+
+module Devise
+  module Mailers
+    module Helpers
+      def hack_record(record)
+        record.kind_of?(Hash) ? kmodel(record).where(:email=>record['email']).first : record
+      end
+
+      def kmodel(record)
+        User
+      end
+    end
+  end
+end
