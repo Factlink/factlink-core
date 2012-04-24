f4648d038638469c52d6dafa3313247d813e94cb
Mark IJbema <markijbema@gmail.com>
2012-04-24 11:17:41 +0200

    some more output

--- core/bin/server/resque.sh
+++ core/bin/server/resque.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 case $1 in
    start)
+      echo "Starting Resque"
       sh /applications/core/current/bin/server/start_resque.sh || exit 1
       ;;
     stop)
+      echo "Stopping Resque"
       sh /applications/core/current/bin/server/stop_resque.sh || exit 1
       ;;
     *)
