e7857ec4cb2f012e4ff00d81763fbdef3b66842c
Mark IJbema <markijbema@gmail.com>
2012-04-24 10:37:48 +0200

    give me errors when something goes wrong with starting/stopping resque

--- core/bin/server/resque.sh
+++ core/bin/server/resque.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 case $1 in
    start)
-      sh /applications/core/current/bin/server/start_resque.sh
+      sh /applications/core/current/bin/server/start_resque.sh || exit 1
       ;;
     stop)
-      sh /applications/core/current/bin/server/stop_resque.sh
+      sh /applications/core/current/bin/server/stop_resque.sh || exit 1
       ;;
     *)
       echo "usage: xyz {start|stop}" ;;
