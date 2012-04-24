3f1b8c1d70a747f6b6562691c7400d9ee75fef70
tomdev <tom@codigy.nl>
2012-04-24 14:11:55 +0200

    Cleaner method in UserMailer
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/mailers/user_mailer.rb
+++ core/app/mailers/user_mailer.rb
@@ -2,11 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@factlink.com"
 
   def welcome_instructions(user)
-  	# @set_password_url = edit_password_url(user, :reset_password_token => user.reset_password_token)
-
   	@user = user
-  	mail to: user.email, subject: 'Start using Factlink'
-
+  	mail to: @user.email, subject: 'Start using Factlink'
   end
 
 end
