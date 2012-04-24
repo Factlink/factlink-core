8b3ce81fafd3ed4b1c834a0b33d8fa55d1cbc711
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-04-24 10:20:18 +0200

    Introduced listenFor() method in the popup

--- core/app/assets/javascripts/popup/chrome.js
+++ core/app/assets/javascripts/popup/chrome.js
@@ -1,55 +1,70 @@
 (function() {
 var top = window.top;
 
+var listenFor = (function() {
+  var boundEvents = {};
+
+  window.addEventListener("message", function(messageObject) {
+    var messageKey = messageObject.data.message;
+
+    if ( typeof boundEvents[messageKey] === "function" ) {
+      boundEvents[messageKey].apply(this, arguments);
+    }
+  }, false);
+
+  return function(message, handler) {
+    boundEvents[message] = handler;
+  };
+})();
+
 function passThroughClick(elem, messageKey) {
   elem.bind('click', function() {
-    top.postMessage(messageKey, "*");
+    top.postMessage({message: messageKey}, "*");
   });
 }
-
-window.addEventListener("message", function(messageObject) {
-  var data = messageObject.data;
-
-  if ( data === "highlight" ) {
-    $('#highlight').prop('checked', true );
-  } else if ( data === "annotate" ) {
-    $('#annotate_button>input').prop('checked',true);
-  } else if ( typeof data.title !== "undefined" ) {
-    if ( data.text.length <= 0 ) {
-      $('#new_fact_form').hide(0, updateHeight);
-    }
-
-    $('#title').val(data.title);
-    $('#fact').val(data.text);
-    $('#url').val(data.url);
-  } else if ( data === "blacklist" ) {
-    $('#highlight').prop('disabled', true);
-    $('#blacklisted').show(0, updateHeight);
-    $('#annotate_button').hide();
-  } else if ( data === "error") {
-    $('#highlight').prop('disabled', true);
-    $('#error_loading_factlink').show(0, updateHeight);
-    $('#annotate_button>input').prop('disabled', true);
-  }
-  
-  
-}, false);
 
 function updateHeight(){
-  top.postMessage({message: "changeHeight", height: $(document).height()}, "*");
+  var height = document.body.scrollHeight;
+
+  top.postMessage({message: "changeHeight", height: height}, "*");
 }
+
+listenFor("annotate", function(messageObject) {
+  $('#annotate_button>input').prop('checked',true);
+});
+
+listenFor("setFormData", function(messageObject) {
+  if ( messageObject.data.text.length > 0 ) {
+    $('#new_fact_form').show();
+
+    updateHeight();
+  }
+
+  $('#title').val(messageObject.data.title);
+  $('#fact').val(messageObject.data.text);
+  $('#url').val(messageObject.data.url);
+});
+
+listenFor("blacklist", function(messageObject) {
+  $('#highlight').prop('disabled', true);
+  $('#blacklisted').show();
+  $('#annotate_button').hide();
+
+  console.info( "Blacklisting" );
+  updateHeight();
+});
+
+listenFor("error", function(messageObject) {
+  $('#highlight').prop('disabled', true);
+  $('#error_loading_factlink').show();
+  $('#annotate_button>input').prop('disabled', true);
+
+  updateHeight();
+});
 
 passThroughClick( $('#annotate_button>input'), "annotateToggle");
-passThroughClick( $('#highlight'), "highlightToggle");
 
-if ( $('.alert').length === 1 ) {
-  top.postMessage("showNotification", "*");
-
-  $('.alert').bind('closed', function (e) {
-    top.postMessage('hideNotification', "*");
-  });
-}
-
+// When the cancel button is clicked, close the popup
 $('#cancel').on('click', function (e) {
   e.preventDefault();
 
@@ -57,7 +72,6 @@ $('#cancel').on('click', function (e) {
 
   top.window.close();
 });
-
 
 $(window).on('resize', function(e) {
   console.info( "Resize" );
