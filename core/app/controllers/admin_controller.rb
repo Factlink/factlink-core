118ad6d7e7c4f6832a00bba0882d551ec902d259
Mark IJbema <markijbema@gmail.com>
2012-04-25 13:48:31 +0200

    added cachebusting to admincontroller

--- core/app/controllers/admin_controller.rb
+++ core/app/controllers/admin_controller.rb
@@ -1,6 +1,13 @@
 class AdminController < ApplicationController
+  before_filter :require_admin_access
   def require_admin_access
     authorize! :access, Ability::AdminArea
   end
-  before_filter :require_admin_access
+
+  before_filter :set_cache_buster
+  def set_cache_buster
+     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+     response.headers["Pragma"] = "no-cache"
+     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 end
