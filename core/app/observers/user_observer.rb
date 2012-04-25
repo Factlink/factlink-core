193c3ff6516214664f19534917759f0a86149731
tomdev <tom@codigy.nl>
2012-04-25 15:25:59 +0200

    More solid check on sending welcome instructions email
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/observers/user_observer.rb
+++ core/app/observers/user_observer.rb
@@ -1,9 +1,14 @@
 class UserObserver < Mongoid::Observer
   def after_update(user)
-    if user.approved_changed? and user.approved?
-      
+  	@sending ||= {}
+
+  	puts "baron UserObserver"
+    if user.approved_changed? and user.approved? and not @sending[user.id]
+    	@sending[user.id] = true
+
       # Send mail
       user.send_welcome_instructions
+    	@sending[user.id] = false
     end
   end
 end
