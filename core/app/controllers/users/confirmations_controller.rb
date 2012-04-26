bf66a68fdd89c8471d523e9aebe15c5b49226fcb
tomdev <tom@codigy.nl>
2012-04-26 13:17:41 +0200

    Don't show weird error on opening the page with an inactive token
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/controllers/users/confirmations_controller.rb
+++ core/app/controllers/users/confirmations_controller.rb
@@ -5,6 +5,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
+    if resource.new?
+      respond_with_navigational(resource){ render "awaiting_approval" }
+      return
+    end
+
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_navigational_format?
       # sign_in(resource_name, resource) #Don't sign in, we don't allow that
