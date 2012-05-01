32bff6f17ef5442a7c97ce0ab5bf3676f27a7b8b
tomdev <tom@codigy.nl>
2012-05-01 10:17:17 +0200

    Only compile assets when files have changed
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/config/deploy.rb
+++ core/config/deploy.rb
@@ -74,6 +74,22 @@ namespace :deploy do
       curl --user deploy:sdU35-YGGdv1tv21jnen3 #{full_url}
     CMD
   end
+
+  #
+  #  Only precompile if files have changed
+  #
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      # Only precompile if files have changed: http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
 end
 
 before 'deploy:all',      'deploy'
