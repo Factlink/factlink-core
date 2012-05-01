9d8631d5accb05454954aa0762b74934bf974a2d
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-01 15:30:23 +0200

    Created PassiveWheel for landing

---
+++ core/app/assets/javascripts/backbone/views/passive_wheel_view.js
@@ -0,0 +1,3 @@
+window.PassiveWheelView = BaseFactWheelView.extend({
+
+});
--- core/app/assets/javascripts/bubble-landing.js
+++ core/app/assets/javascripts/bubble-landing.js
@@ -1,8 +1,6 @@
 //= require_self
 //= require twitter/bootstrap
 //= require raphael-min
-//= require ./bubble/wheel
-//= require ./bubble/jquery.infiniScroll
-//= require jquery.scrollTo
+//= require bubble
+//= require backbone
 //= require jquery.hoverIntent
-//= require_directory ./bubble
