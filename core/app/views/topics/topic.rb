fde73119042d57b3ac6274b8ba730333a6132f66
Martijn Russchen <martijn@factlink.com>
2012-05-02 13:59:17 +0200

    show topics in search results

--- core/app/views/topics/topic.rb
+++ core/app/views/topics/topic.rb
@@ -3,5 +3,19 @@ module Topics
     def title
       self[:topic].title
     end
+
+    def channels
+    	self[:topic].top_users(3).map do |u|
+    		ch = u.graph_user.channels.find(slug_title: self[:topic].slug_title).first
+    		{channel: ch, user: u}
+    	end.keep_if { |h| h[:channel] }.map do |h|
+    		{
+    			user_name: h[:user].username,
+    			channel_url: channel_path(h[:user],h[:channel]),
+    			avatar_url: h[:user].avatar_url,
+    			authority: Authority.from(self[:topic],for: h[:user].graph_user).to_f	
+    		}
+    	end
+    end
   end
 end
