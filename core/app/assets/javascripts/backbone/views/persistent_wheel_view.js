a317d96c4d4800b0b90cc4f22246f60ccabf937b
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-02 14:25:46 +0200

    Created PersistentWheelView which stores it's click in the radio-button

---
+++ core/app/assets/javascripts/backbone/views/persistent_wheel_view.js
@@ -0,0 +1,33 @@
+(function() {
+window.PersistentWheelView = BaseFactWheelView.extend({
+  clickOpinionType: function (opinionType, e) {
+    this.updateTo("1.0", {
+      believe: getHash(opinionType.get('type'), "believe"),
+      disbelieve: getHash(opinionType.get('type'), "disbelieve"),
+      doubt: getHash(opinionType.get('type'), "doubt")
+    });
+
+    $('input[name=opinion][value=' + opinionType.get('type') + 's]')
+      .prop('checked', true);
+  }
+});
+
+function getHash(selectedType, type) {
+  if ( selectedType == type ) {
+    return selected;
+  } else {
+    return notSelected;
+  }
+}
+
+var selected = {
+  percentage: 100,
+  is_user_opinion: true
+};
+
+var notSelected = {
+  percentage: 0,
+  is_user_opinion: false
+};
+
+}());
