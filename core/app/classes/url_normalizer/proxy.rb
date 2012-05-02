0b3583c193227b5ca86924dc3599571413b34712
Mark IJbema <markijbema@gmail.com>
2012-05-02 16:52:53 +0200

    improved handling of proxy pages, also supporting non ?parse pages

--- core/app/classes/url_normalizer/proxy.rb
+++ core/app/classes/url_normalizer/proxy.rb
@@ -1,3 +1,5 @@
+require 'andand'
+
 class UrlNormalizer
   class Proxy < UrlNormalizer
     normalize_for 'testserver.fct.li'
@@ -5,8 +7,12 @@ class UrlNormalizer
     normalize_for 'fct.li'
 
     def normalize
-      url = CGI.parse(@uri.query)['url'][0]
-      UrlNormalizer.normalize url
+      url = @uri.query && CGI.parse(@uri.query)['url'].andand[0]
+      if url
+        UrlNormalizer.normalize url
+      else
+        super
+      end
     end
   end
 end
