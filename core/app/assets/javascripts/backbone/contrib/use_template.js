653c10571df9b6dc840b3e2a3c181b35970392e0
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-08 17:45:39 +0200

    Sorry, misclicked, updated use_template to use HoganTemplates

--- core/app/assets/javascripts/backbone/contrib/use_template.js
+++ core/app/assets/javascripts/backbone/contrib/use_template.js
@@ -4,42 +4,15 @@
     call this in the initialize()
 */
 (function() {
-  var TemplateCache = {
-    get: function(dir, file) {
-      var self = this;
-
-      if ( !self.templates ) { self.templates = {}; }
-
-        self.templates[dir] = {};
-
-        $('#mustache-templates>.mustache-dir-' + dir ).each(function(index, element) {
-          var $element = $( element );
-          var filename = $element.data('filename');
-
-          self.templates[dir][filename] = $element.html();
-        });
-      }
-
-      var template = self.templates[dir][file];
-
-      if ( !template ) {
-        template = self.templates[dir][ file.indexOf("_") === 0 ? file.substr(1) : "_" + file ]
-      }
-
-      return template;
-    }
-  };
-
   Backbone.View.prototype.useTemplate = function(dir,file) {
     if (this.tmpl !== undefined) { return; }
 
-    var self = this;
+    this.tmpl = Template.use(dir, file);
+  };
 
-    this.tmpl = TemplateCache.get(dir, file);
-    this.partials = {};
-
-    _.each(TemplateCache.templates[dir], function(value, key) {
-      self.partials[ key.replace(/^_/,'') ] = TemplateCache.get(dir, key);
-    });
+  window.Template = {
+    use: function (dir, file) {
+      return HoganTemplates[dir + "/" + file];
+    }
   };
 }());
