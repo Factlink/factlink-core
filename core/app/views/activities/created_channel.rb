226fa440c1ecff33b3fc1361a6db5664f87734d0
tomdev <tom@codigy.nl>
2012-05-09 17:36:22 +0200

    Use correct CreatedChannel class name and add methods
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/views/activities/created_channel.rb
+++ core/app/views/activities/created_channel.rb
@@ -1,5 +1,25 @@
 module Activities
-  class CreatedChannelView < Mustache::Railstache
+  class CreatedChannel < Mustache::Railstache
+
+    def channel_title
+      self[:activity].subject.title
+    end
+
+    def channel_url
+      channel_path(self[:activity].subject.created_by.user, self[:activity].subject)
+    end
+
+    def icon
+      image_tag('activities/icon-channel.png')
+    end
+
+    def channel_definition
+      t(:channel)
+    end
+
+    def channels_definition
+      t(:channels)
+    end
 
   end
 end
