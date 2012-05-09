d5b5044681db7ac5db3cb520566d4ebd8eb69a95
Mark IJbema <markijbema@gmail.com>
2012-05-09 16:18:08 +0200

    because tom ocd-ish types a trailing slash

--- core/config/application.rb
+++ core/config/application.rb
@@ -108,6 +108,11 @@ module FactlinkUI
     # - /facts/new              : Loaded in iframe through Chrome Extension (popup)
     # config.middleware.use Rack::XFrameOptions, "SAMEORIGIN", ["/factlink/intermediate", "/facts/new"]
 
+
+    config.middleware.insert_before("Rack::Lock", "Rack::Rewrite") do
+      r301 %r{^\/(.+)\/(\?.*)?$}, '/$1$2'
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
