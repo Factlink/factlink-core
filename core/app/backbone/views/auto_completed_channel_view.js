e49dc623e02ccd02c590ec8e1040ff5450542725
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-10 11:34:24 +0200

    Added client side highlighting

--- core/app/backbone/views/auto_completed_channel_view.js
+++ core/app/backbone/views/auto_completed_channel_view.js
@@ -3,12 +3,16 @@ window.AutoCompletedChannelView = Backbone.View.extend({
 
   tmpl: HoganTemplates["channels/_auto_completed_channel"],
 
+  initialize: function () {
+    this.queryRegex = new RegExp(this.options.query, "gi");
+  },
+
   render: function () {
-    console.info( this.options.query );
-    var highlightedTitle = this.model.get('title');
+    var title = this.model.get('title');
+    var highlightedTitle = title.replace(this.queryRegex, "<em>$&</em>");
 
     var context = _.extend(this.model.toJSON(), {
-
+      highlightedTitle: highlightedTitle
     });
 
     this.$el.html( this.tmpl.render( context ) );
--- core/app/views/channels/_auto_completed_channel.rb
+++ core/app/views/channels/_auto_completed_channel.rb
@@ -1,7 +1,7 @@
 module Channels
   class AutoCompletedChannel < Mustache::Railstache
     def title
-      self[:topic].title
+      h self[:topic].title
     end
   end
 end
