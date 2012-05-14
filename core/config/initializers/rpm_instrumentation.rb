a45bac55da38646212eddba135248a4599262c7a
tomdev <tom@codigy.nl>
2012-05-14 15:51:16 +0200

    Added benchmarking for Ability#initialize
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/config/initializers/rpm_instrumentation.rb
+++ core/config/initializers/rpm_instrumentation.rb
@@ -1,16 +1,20 @@
-# require 'new_relic/agent/method_tracer'
+require 'new_relic/agent/method_tracer'
 # Mustache::Railstache.class_eval do
 #   include NewRelic::Agent::MethodTracer
 #   add_method_tracer :to_json
 # end
-# 
+#
 # #require mustache partial views (the autoloader does not find them)
 # Dir["#{Rails.root}/app/views/**/_*.rb"].each do |path|
 #   require_dependency path
 # end
-# 
-# 
+#
+#
 # Facts::Fact.class_eval do
 #   include NewRelic::Agent::MethodTracer
 #   add_method_tracer :interacting_users
 # end
+Ability.class_eval do
+  include NewRelic::Agent::MethodTracer
+  add_method_tracer :initialize
+end
