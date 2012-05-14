f29fb26e32c3b909c2fd866d37dde047a58cf676
tomdev <tom@codigy.nl>
2012-05-14 16:09:29 +0200

    Profile Mustache::Railstache[]
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/config/initializers/rpm_instrumentation.rb
+++ core/config/initializers/rpm_instrumentation.rb
@@ -5,9 +5,9 @@ require 'new_relic/agent/method_tracer'
 # end
 #
 # #require mustache partial views (the autoloader does not find them)
-# Dir["#{Rails.root}/app/views/**/_*.rb"].each do |path|
-#   require_dependency path
-# end
+Dir["#{Rails.root}/app/views/**/_*.rb"].each do |path|
+  require_dependency path
+end
 #
 #
 # Facts::Fact.class_eval do
@@ -17,4 +17,16 @@ require 'new_relic/agent/method_tracer'
 Ability.class_eval do
   include NewRelic::Agent::MethodTracer
   add_method_tracer :initialize
+  add_method_tracer :can?
+  add_method_tracer :cannot?
+end
+
+# Channels::Channel.class_eval do
+#   include NewRelic::Agent::MethodTracer
+#   add_method_tracer :for
+# end
+
+Mustache::Railstache.class_eval do
+  include NewRelic::Agent::MethodTracer
+  add_method_tracer :[]
 end
