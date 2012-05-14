153e2794ff97193f4a6d9ba5fa75f265d1a88195
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-14 11:13:22 +0200

    Check if the topic is an existing channel

--- core/app/views/channels/_auto_completed_channel.rb
+++ core/app/views/channels/_auto_completed_channel.rb
@@ -3,5 +3,15 @@ module Channels
     def title
       h self[:topic].title
     end
+
+    def user_channel
+      ch = ::Channel.find(created_by_id: current_graph_user.id, slug_title: title.to_url).first
+
+      if ch
+        Channel.for(:channel => ch, view: self).to_hash
+      else
+        false
+      end
+    end
   end
 end
