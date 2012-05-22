53c838007180d01fcf01b769c22b0cf5281f6a08
tomdev <tom@codigy.nl>
2012-05-22 17:08:12 +0200

    Error reporting bangging to Errbit!!
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/config/initializers/airbrake.rb
+++
@@ -1,3 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = '03abfb3c20150b13e470fdd4d21d03d3'
-end
---
+++ core/config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+Airbrake.configure do |config|
+   config.api_key     = '15ac8a10c7b9e8939aa5608e186d3fd8'
+   config.host        = 'factlink-errbit.herokuapp.com'
+   config.port        = 80
+   config.secure      = config.port == 443
+end
