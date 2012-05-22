275a9ccdec1bdefb985fd604681e63a84c3a9549
Mark IJbema <markijbema@gmail.com>
2012-05-22 14:42:38 +0200

    added message when loaded (bookmarklet)

--- js-library/src/js/loaded_message.js
+++ js-library/src/js/loaded_message.js
@@ -1,6 +1,11 @@
 (function(Factlink, $, _, easyXDM, undefined) {
   Factlink.loadedMessage = function(fact_id){
     console.info('Factlink is fully loaded; this message will appear on screen some day soon.');
+    $('#fl').append("<div class='fl-message' style='display:none'> Factlink is loaded</div>");
+    $('#fl .fl-message').fadeIn('slow');
+    setTimeout(function() {
+      $('#fl .fl-message').fadeOut('slow');
+    }, 2545);
   };
 
   $(window).bind('factlink.factsLoaded', function(){
