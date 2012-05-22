8767c65a8f28632505d036f4e90d8ad07bed028e
Mark IJbema <markijbema@gmail.com>
2012-05-22 15:24:48 +0200

    Unfortunately, this breaks shit
    Revert "make rails behave like Remon assumed it did ;)"
    
    This reverts commit dced9bac299d9ec45e993e1ef2717a1f979dd10b.

--- core/app/controllers/application_controller.rb
+++ core/app/controllers/application_controller.rb
@@ -41,11 +41,6 @@ class ApplicationController < ActionController::Base
     return return_to || stored_location_for(user) || channel_path(user, user.graph_user.stream)
   end
 
-  before_filter :return_json_if_xhr
-  def return_json_if_xhr
-    request.format = :json if request.xhr?
-  end
-
   ##########
   # Set the Access Control, so XHR request from other domains are allowed.
   def set_access_control
