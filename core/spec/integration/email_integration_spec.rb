ff748216e549fe95e19bc7a089b03228bba895e9
tomdev <tom@codigy.nl>
2012-05-23 10:58:26 +0200

    Automated the "Password recovery" flow, including the email
    added capybabra-email to make it possible to integration test emails
    
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/spec/integration/email_integration_spec.rb
+++ core/spec/integration/email_integration_spec.rb
@@ -1,19 +1,5 @@
 require 'integration_helper'
 
-# describe 'Reserving a username', type: :request do
-
-#   it 'should be reservable form the frontpage' do
-#     visit '/'
-
-#     fill_in 'user_username', with: 'teh_user'
-#     fill_in 'user_email',    with: 'teh_user@factlink.com'
-
-#     click_button 'Reserve'
-
-#     page.should have_content('You have successfully reserved your username!')
-#   end
-
-# end
 describe 'Password recovery', type: :request do
 
   before do
@@ -21,9 +7,31 @@ describe 'Password recovery', type: :request do
   end
 
   it 'should inform the user the email has been send' do
+    clear_emails
+
+     @user.username = "Barones"
+
     visit '/'
     click_link "Can't access?"
     page.should have_content('Forgot your password?')
+
+    fill_in 'user_email', with: @user.email
+    click_button "Send me reset password instructions"
+    page.should have_content("You will receive an email with instructions about how to reset your password in a few minutes")
+
+    open_email(@user.email)
+    current_email.should have_content('Change my password')
+
+    @user.reload
+
+    visit edit_user_password_path(reset_password_token: @user.reset_password_token)
+
+    fill_in "user_password", with: "our_house"
+    fill_in "user_password_confirmation", with: "our_house"
+    click_button "Change my password"
+
+    page.should have_content("Your password was changed successfully. You are now signed in.")
+
   end
 
 end
