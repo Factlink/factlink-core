df673adf6c231581e301a29b4711d7d3de10bc13
Mark IJbema <markijbema@gmail.com>
2012-05-24 11:58:58 +0200

    added pictures of the belgium student and the professors

--- core/app/controllers/fake_facts_controller.rb
+++ core/app/controllers/fake_facts_controller.rb
@@ -3,6 +3,7 @@
 
 class FakeFactsController < ApplicationController
   layout "fake_facts"
+  include Gravatar
 
   def facts
     [
@@ -91,13 +92,13 @@ class FakeFactsController < ApplicationController
       pos_opinions = [340,10,30]
       neg_opinions = [34,10,303]
     elsif type == 2
-      interacting_users = [["fam expert","https://secure.gravatar.com/avatar/2fbbe1c53162b9e2417bb85aab1726a1?rating=PG&size=20&default=retro", beltype]]
+      interacting_users = [["fam expert",gravatar_url('mark+prof@factlink.com', size: 20), beltype]]
     elsif type == 3
-      interacting_users = [["unfam expert","https://secure.gravatar.com/avatar/2fbbe1c53162b9e2417bb85aab1726a1?rating=PG&size=20&default=retro", beltype ]]
+      interacting_users = [["unfam expert",gravatar_url('mark+prof2@factlink.com', size: 20), beltype ]]
     elsif type == 4
       interacting_users = [["fam non-expert","https://secure.gravatar.com/avatar/2fbbe1c53162b9e2417bb85aab1726a1?rating=PG&size=20&default=retro", beltype]]
     elsif type == 5
-      interacting_users = [["unfam nonexp","https://secure.gravatar.com/avatar/2fbbe1c53162b9e2417bb85aab1726a1?rating=PG&size=20&default=retro", beltype]]
+      interacting_users = [["unfam nonexp",gravatar_url('mark+belg@factlink.com', size: 20), beltype]]
     else
       interacting_users = []
     end
