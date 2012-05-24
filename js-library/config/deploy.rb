cb8b748abbf1c1cb6140eb625d7f5f7318df1a7c
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-24 13:46:37 +0200

    Trying to solve a bug, http://stackoverflow.com/questions/5253153/rvm-error-with-deploying-rails-app-via-capistrano

--- js-library/config/deploy.rb
+++ js-library/config/deploy.rb
@@ -15,6 +15,7 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 # Load RVM's capistrano plugin.
 require "rvm/capistrano"
 set :rvm_ruby_string, '1.9.2'
+set :rvm_type, :system
 
 set :user, "deploy"
 set :use_sudo,    false
