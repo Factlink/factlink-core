49eab5ffad2d2fbe6ae45f34890a131f4f446be5
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-05-24 17:14:36 +0200

    Fixed typo

--- core/bin/ci/103_security.sh
+++ core/bin/ci/103_security.sh
@@ -5,4 +5,4 @@ bundle exec brakeman -o /tmp/brakeman.tabs || exit 1
 
 cat /tmp/brakeman.tabs
 
-exit `cat bin/ci2/103_security_ignore.txt | perl -pe 's/#.*$//'|grep -E '.' | perl -e '$_=join"|",<>;s/\n//;chomp;print"grep -vE \"".$_."\"  /tmp/brakeman.tabs\n"' | sh | grep -E . | wc -l`
+exit `cat bin/ci/103_security_ignore.txt | perl -pe 's/#.*$//'|grep -E '.' | perl -e '$_=join"|",<>;s/\n//;chomp;print"grep -vE \"".$_."\"  /tmp/brakeman.tabs\n"' | sh | grep -E . | wc -l`
