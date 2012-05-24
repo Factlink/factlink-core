a713cf049f9085a3ad07ef1d839bac240c66eeea
Mark IJbema <markijbema@gmail.com>
2012-05-24 11:58:44 +0200

    moved gravatar functionality to its own file

---
+++ core/app/classes/gravatar.rb
@@ -0,0 +1,15 @@
+module Gravatar
+  # from: http://douglasfshearer.com/blog/gravatar-for-ruby-and-ruby-on-rails
+  # Returns a Gravatar URL associated with the email parameter.
+  #
+  # Gravatar Options:
+  # - rating: Can be one of G, PG, R or X. Default is X.
+  # - size: Size of the image. Default is 80px.
+  # - default: URL for fallback image if none is found or image exceeds rating.
+  def gravatar_url(email,gravatar_options={})
+    grav_url = 'https://secure.gravatar.com/avatar/'
+    grav_url << "#{Digest::MD5.new.update(email)}?"
+    grav_url << (gravatar_options.slice(:rating,:size,:default).map{|k,v| "#{k}=#{v}"}.join "&")
+    grav_url
+  end
+end
