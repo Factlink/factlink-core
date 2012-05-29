2a651b93b6a1652b6d5305fc4fe5cbd21b4989dc
Mark IJbema <markijbema@gmail.com>
2012-05-29 12:35:42 +0200

    nu gebruiken we coffeescript! zijn we hip of wat

--- core/app/backbone/views/auto_completed_added_channel_view.js
+++
@@ -1,9 +0,0 @@
-window.AutoCompletedAddedChannelView = Backbone.Marionette.ItemView.extend({
-  tagName: "li",
-
-  triggers: {
-    "click a": "remove"
-  },
-
-  template: "channels/_auto_completed_added"
-});
