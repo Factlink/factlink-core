c9b291965bccb836da6571151ccacf4da8e89452
Mark IJbema <markijbema@gmail.com>
2012-05-30 21:58:18 +0200

    don't show stream of other users

--- core/app/backbone/views/channels_view.js
+++ core/app/backbone/views/channels_view.js
@@ -3,6 +3,9 @@ window.ChannelsView = Backbone.Marionette.CompositeView.extend({
   itemView: ChannelItemView,
 
   appendHtml: function(collectionView, itemView) {
-    collectionView.$('#channel-listing').append(itemView.el);
+    var ch = itemView.model;
+    if (ch.get('type') !== 'stream' || ch.get('created_by').username === window.currentUser.get('username')){
+      collectionView.$('#channel-listing').append(itemView.el);
+    }
   }
 });
