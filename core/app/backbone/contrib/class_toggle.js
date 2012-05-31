ebda415a05d1a2ccee114a15ac4353cb56be7a94
Mark IJbema <markijbema@gmail.com>
2012-05-31 12:35:17 +0200

    class toggles reverted to first design

--- core/app/backbone/contrib/class_toggle.js
+++ core/app/backbone/contrib/class_toggle.js
@@ -1,17 +1,11 @@
 /* easy event handler which just toggles a class on the element of the view */
 
 
-_.extend(Backbone.View.prototype,{
-  addClassToggle: function(togglename) {
-    this[togglename+'On'] = function(){
-      console.info(this.model.get('title'));
-      console.info(this.$el);
-      this.$el.addClass(togglename);
-    };
-    this[togglename+'Off'] = function(){
-      console.info(this.model.get('title'));
-      console.info(this.$el);
-      this.$el.removeClass(togglename);
-    };
-  }
-})
+Backbone.View.prototype.addClassToggle = function(togglename) {
+  this[togglename+'On'] = function(){
+    this.$el.addClass(togglename);
+  };
+  this[togglename+'Off'] = function(){
+    this.$el.removeClass(togglename);
+  };
+};
