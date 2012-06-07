1d8af71b70f9726deb48ae9c808a3370e4ea7c33
tomdev <tom@codigy.nl>
2012-06-07 10:49:06 +0200

    Removed console.info
    Signed-off-by: tomdev <tom@codigy.nl>

--- js-library/src/js/loaded_message.js
+++ js-library/src/js/loaded_message.js
@@ -1,6 +1,5 @@
 (function(Factlink, $, _, easyXDM, undefined) {
   Factlink.loadedMessage = function(fact_id){
-    console.info('Factlink is fully loaded; this message will appear on screen some day soon.');
     $('#fl').append("<div class='fl-message' style='display:none'> Factlink is loaded</div>");
     $('#fl .fl-message').fadeIn('slow');
     setTimeout(function() {
@@ -13,5 +12,5 @@
       Factlink.loadedMessage();
     }
   });
-  
+
 })(window.Factlink, Factlink.$, Factlink._, Factlink.easyXDM);
