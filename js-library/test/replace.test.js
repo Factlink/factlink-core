56f31614e759c642028577709dd8a70c5ac07846
tomdev <tom@codigy.nl>
2012-06-11 09:53:22 +0200

    Added test to check if the fact span has the attribute data-factid
    Signed-off-by: tomdev <tom@codigy.nl>

--- js-library/test/replace.test.js
+++ js-library/test/replace.test.js
@@ -1,5 +1,9 @@
 /*global test:true, equal:true, notEqual:true, _:true */
 
+Factlink.getTemplate = function (str, callback) {
+  callback(Factlink._.template(""));
+};
+
 test("Simple search/replace", function(){
 
 });
@@ -7,14 +11,23 @@ test("Simple search/replace", function(){
 test("Search/replace should work with newlines", function () {
   equal($('p.newline')[0].childNodes.length, 1);
 
-  Factlink.getTemplate = function (str, callback) {
-    callback(Factlink._.template(""));
-  };
-
   Factlink.selectRanges(
     Factlink.search("This text has multiple newlines"),
     1,
     {});
 
   equal($('span.factlink')[0].childNodes.length, 1);
+});
+
+test("fact-span should have the data-factid attribute", function(){
+
+  Factlink.selectRanges(
+    Factlink.search("This text has multiple newlines"),
+    1,
+    {});
+
+  $('span.factlink').each(function(key, span) {
+    equal($(span).is("[data-factid]"), true);
+  });
+
 });
