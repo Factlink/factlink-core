01fc510308bdd9d49d10d0df98c4cf0272785f19
tomdev <tom@codigy.nl>
2012-06-12 13:41:46 +0200

    Show user grouped activities
    - added new template containing the user and list of activities
    
    Signed-off-by: tomdev <tom@codigy.nl>

---
+++ core/app/backbone/views/activity_item_view.js
@@ -0,0 +1,4 @@
+window.ActivityItemView = Backbone.Marionette.ItemView.extend({
+  template: "activities/_generic_activity",
+  tagName:  "div"
+});
---
+++ core/app/backbone/views/user_activities_view.js
@@ -0,0 +1,22 @@
+window.UserActivitiesView = Backbone.Marionette.CompositeView.extend({
+
+  template: 'activities/user-activities',
+  className: 'activity-block',
+
+  beforeRender: function() {
+    console.info( this );
+  },
+
+  initialize: function(opts) {
+    this.itemView = ActivityItemView;
+  },
+
+  appendable: function(model) {
+    return this.model.get('username') === model.get('username');
+  },
+
+  appendHtml: function(collectionView, itemView){
+    collectionView.$(".the-activities").append(itemView.el);
+  }
+
+});
