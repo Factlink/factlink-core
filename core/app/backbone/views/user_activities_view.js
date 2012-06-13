fbd8209a4fdd333f35744e2700da5c9f517c0c9d
tomdev <tom@codigy.nl>
2012-06-13 11:35:52 +0200

    Sorry for not publishing my feature branch earlier and that you have to read a lot of commits now. Keep on forgetting that. KFC is the best I know you are thinking of chicken right now.
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/backbone/views/user_activities_view.js
+++ core/app/backbone/views/user_activities_view.js
@@ -11,7 +11,6 @@ window.UserActivitiesView = Backbone.Marionette.CompositeView.extend({
     var same_user                  = this.model.get('username') === new_model.get('username');
     var new_subject_is_channel     =  new_model.get('subject_class') === "Channel";
     var current_subject_is_channel = this.model.get('subject_class') === "Channel";
-
     return same_user && new_subject_is_channel && current_subject_is_channel;
   },
 
