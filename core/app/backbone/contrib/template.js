1a5f9cd542515fd000634382dd1c9ed3536f1791
Mark IJbema <markijbema@gmail.com>
2012-06-18 14:26:22 +0200

    updated the way templates are retrieved

--- core/app/backbone/contrib/template.js
+++ core/app/backbone/contrib/template.js
@@ -9,11 +9,17 @@
   };
 
 
-  Backbone.Marionette.TemplateCache.loadTemplate = function(template, callback) {
+  Backbone.Marionette.TemplateCache.prototype.loadTemplate = function(template) {
     console.info("TEMPLATE:", template)
     compiledTemplate = HoganTemplates[template];
-    callback.call(this, compiledTemplate);
+    //callback.call(this, compiledTemplate);
+    return compiledTemplate;
   };
+
+  Backbone.Marionette.TemplateCache.prototype.loadTemplate = function(template) {
+    return template;
+  };
+
   Backbone.Marionette.Renderer.renderTemplate = function(template, data) {
     return template.render(data);
   };
