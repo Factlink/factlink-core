6ef33a4b19098c1c2cbe03340c868121c4168a1d
Mark IJbema <markijbema@gmail.com>
2012-06-18 15:33:35 +0200

    fixed indentation

--- core/app/backbone/contrib/auto_loading_composite_view.js
+++ core/app/backbone/contrib/auto_loading_composite_view.js
@@ -1,60 +1,70 @@
-window.AutoloadingCompositeView = Backbone.Marionette.CompositeView.extend({
-  constructor: function(options){
-    Backbone.Marionette.CompositeView.prototype.constructor.apply(this, arguments);
-    this.collection.loadMore();
-    this.on('add', _.bind(function(){
-      this.hideEmpty();
-    },this));
-  },
+function extendWithAutoloading(superclass) {
+  return superclass.extend({
+    constructor: function(options){
+      superclass.prototype.constructor.apply(this, arguments);
+      this.collection.loadMore();
+      this.on('add', _.bind(function(){
+        this.hideEmpty();
+      },this));
+      this.collection.on('stopLoading', this.loadMore, this);
+      
+    },
 
 
-  bottomOfViewReached: function() {
-    var bottomOfTheViewport = window.pageYOffset + window.innerHeight;
-    var bottomOfEl = this.$el.offset().top + this.$el.outerHeight();
+    bottomOfViewReached: function() {
+      var bottomOfTheViewport = window.pageYOffset + window.innerHeight;
+      var bottomOfEl = this.$el.offset().top + this.$el.outerHeight();
 
-    if ( bottomOfEl < bottomOfTheViewport ) {
-      return true;
-    } else if ($(document).height() - ($(window).scrollTop() + $(window).height()) < 700) {
-      return true;
-    } else {
-      return false;
-    }
-  },
+      if ( bottomOfEl < bottomOfTheViewport ) {
+        return true;
+      } else if ($(document).height() - ($(window).scrollTop() + $(window).height()) < 700) {
+        return true;
+      } else {
+        return false;
+      }
+    },
 
-  bindScroll: function() {
-    this.unbindScroll();
-    $(window).on('scroll.' + this.cid, _.bind(function MCBiggah() {
+    loadMore: function() {
       if ( this.bottomOfViewReached()) {
         this.collection.loadMore();
       }
-    }, this));
-  },
+    },
 
-  unbindScroll: function() {
-    $(window).off('scroll.' + this.cid);
-  },
+    bindScroll: function() {
+      this.unbindScroll();
+      $(window).on('scroll.' + this.cid, _.bind(function MCBiggah() {
+        this.loadMore();
+      }, this));
+    },
 
-  close: function() {
-    Backbone.Marionette.CompositeView.prototype.close.apply(this, arguments);
-    this.unbindScroll();
-  },
+    unbindScroll: function() {
+      $(window).off('scroll.' + this.cid);
+    },
 
-  render: function() {
-    Backbone.Marionette.CompositeView.prototype.render.apply(this, arguments);
-    this.bindScroll();
+    close: function() {
+      superclass.prototype.close.apply(this, arguments);
+      this.unbindScroll();
+    },
 
-    if (this.collection.length === 0 && !this.collection._loading) {
-      this.showEmpty();
-    }
-  },
+    render: function() {
+      superclass.prototype.render.apply(this, arguments);
+      this.bindScroll();
+
+      if (this.collection.length === 0 && !this.collection._loading) {
+        this.showEmpty();
+      }
+    },
 
 
-  reset: function(e){
-    this.collection.stopLoading();
-    Backbone.Marionette.CompositeView.prototype.reset.apply(this, arguments);
-  },
+    reset: function(e){
+      this.collection.stopLoading();
+      superclass.prototype.reset.apply(this, arguments);
+    },
 
-  showEmpty: function(){},
-  hideEmpty: function(){}
+    showEmpty: function(){},
+    hideEmpty: function(){}
 
-});
+  });
+}
+window.AutoloadingCompositeView = extendWithAutoloading(Backbone.Marionette.CompositeView);
+window.AutoloadingView = extendWithAutoloading(Backbone.Marionette.View);
