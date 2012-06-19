82e3ab6b67a5eaff14b24a14808141bb617e1414
tomdev <tom@codigy.nl>
2012-06-19 10:17:51 +0200

    Added SendMail configuration
    Signed-off-by: tomdev <tom@codigy.nl>

---
+++ core/config/email_configuration.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  user_name: "factlink",
+  password: '4q"_NdREV89[s0zq',
+  domain: "factlink.com",
+  address: "smtp.sendgrid.com",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
