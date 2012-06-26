3895ccf99b486b4c056f5608d367350c0321bfbf
Mark IJbema <markijbema@gmail.com>
2012-06-26 11:02:47 +0200

    init in the right place

--- core/app/backbone/views/add_to_channel_view.js
+++ core/app/backbone/views/add_to_channel_view.js
@@ -1,8 +1,6 @@
 window.AddToChannelView = Backbone.View.extend({
   tagName: "div",
 
-  _views: [],
-
   events: {
     'keyup .channel-title': 'submitFormIfNeeded',
     'click .submit': 'addChannel'
@@ -11,6 +9,8 @@ window.AddToChannelView = Backbone.View.extend({
   template: "channels/_own_channel_listing",
 
   initialize: function(opts) {
+    this._views = [];
+
     this.collection.bind('add',   this.render, this);
     this.collection.bind('reset', this.render, this);
 
