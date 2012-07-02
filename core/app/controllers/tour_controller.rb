a9a0eb09a19d61280b990a72113c7e429191c02e
tomdev <tom@codigy.nl>
2012-07-02 17:25:14 +0200

    Show the "Almost Done" page in the signup flow
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/controllers/tour_controller.rb
+++ core/app/controllers/tour_controller.rb
@@ -1,7 +1,7 @@
 class TourController < ApplicationController
 
 	before_filter :authenticate_user!
-	layout "general"
+	layout "one_column"
 
 	def almost_done
 		@step_in_signup_process = :almost_done
