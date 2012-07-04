4966dfcf5fdbe47079ce19d952b9384c7b8649cb
Mark IJbema <markijbema@gmail.com>
2012-07-04 10:28:11 +0200

    simplified subchannel list rendering logic

--- core/app/backbone/views/subchannels_view.js
+++ core/app/backbone/views/subchannels_view.js
@@ -8,32 +8,28 @@ window.SubchannelsView = Backbone.Factlink.CollectionView.extend({
     this.views = {};
   },
 
-  addOneSubchannel: function(subchannel, count) {
+  addOneSubchannel: function(subchannel) {
     var view = new SubchannelItemView({model: subchannel});
     this.views[subchannel.id] = view;
     view.render();
-    if(count < 3) {
-      this.$el.prepend(view.el);
-    } else {
-      this.$el.find('.overflow').append(view.el);
-    }
+    this.appendHtml(this, view)
+  },
 
+  appendHtml: function(collectView, itemView) {
+    $(container).find('.contained-channel-description').show();
+    if(this.$el.children().length < 3 + 1) { // real children + overflow
+      this.$el.prepend(itemView.el);
+    } else {
+      this.$el.find('.overflow').append(itemView.el);
+      $("#more-button").show();
+    }
   },
 
   resetSubchannels: function() {
     _.each(this._views,function(view) { view.close(); });
 
-    var count = 0;
-
-    var subchannelSize = this.collection.size();
-    if(subchannelSize !== 0) { $(container).find('.contained-channel-description').show(); }
-    if(subchannelSize > 3) {
-      $("#more-button").show();
-    }
-
     this.collection.each(function(subchannel) {
-      this.addOneSubchannel.call(this, subchannel, count);
-      count++;
+      this.addOneSubchannel(subchannel);
     },this);
   }
 });
