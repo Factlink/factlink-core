c807c259978cd8e54e8be6a8948b020d7aa9ff00
Mark IJbema <markijbema@gmail.com>
2012-07-09 10:14:41 +0200

    factored out rendering logic

--- core/app/controllers/subchannels_controller.rb
+++ core/app/controllers/subchannels_controller.rb
@@ -1,25 +1,21 @@
 class SubchannelsController < ApplicationController
   def index
     authorize! :show, channel
-    contained_channels = channel.contained_channels
-
-    render :json => contained_channels.map {|ch| Channels::Channel.for(channel: ch,view: view_context)}
+    render_subchannels
   end
 
   def create
     authorize! :update, channel
-    channel.add_channel(subchannel)
-
-    render :json => channel.contained_channels.map {|ch| Channels::Channel.for(channel: ch,view: view_context)}, :location => channel
+    channel.add_channel subchannel
+    render_subchannels
   end
 
   alias :update :create
 
   def destroy
     authorize! :update, channel
-    channel.remove_channel(subchannel)
-
-    render :json => channel.contained_channels.map {|ch| Channels::Channel.for(channel: ch,view: view_context)}, :location => channel
+    channel.remove_channel subchannel
+    render_subchannels
   end
 
   private
@@ -27,8 +23,12 @@ class SubchannelsController < ApplicationController
     def channel
       @channel ||= Channel[params[:channel_id]] || raise_404("Channel not found")
     end
-    
+
     def subchannel
       @subchannel ||= Channel[params[:subchannel_id]||params[:id]] || raise_404("Subchannel not found")
+    end
+
+    def render_subchannels
+      render json: channel.contained_channels.map {|ch| Channels::Channel.for(channel: ch,view: view_context)}, location: channel
     end
 end
