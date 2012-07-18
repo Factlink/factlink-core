abcf3e7b1a525287f419a75f6371140c605bcd28
tomdev <tom@codigy.nl>
2012-07-18 15:30:25 +0200

    InteractiveTour showStep
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/assets/javascripts/tour.js
+++ core/app/assets/javascripts/tour.js
@@ -1,33 +1,35 @@
 (function() {
 
-  function InteractiveTour (opts) {
-    var self = this,
-        opts = $.merge(opts, {}),
-        lastStep;
+  function InteractiveTour () {
 
+    var self = this;
     var SHOW_HELP_TEXT_DELAY = 450;
 
     initialize();
 
-    self.hoi = function hoi() {
-      console.info("hoi");
-    }
-
     function initialize () {
-      showHelpText('step1');
+      self.lastStep    = 0;
+      self.currentStep = 0;
+
+      showStep(1);
     }
 
-    function showHelpText (id) {
+    function showStep(id) {
+      self.lastStep = self.currentStep;
+      self.currentStep = id;
+
+      showHelpText(id);
+    }
+
+    function showHelpText () {
       $('.help-text-container > div').fadeOut('slow', function () {
         setTimeout(function () {
-          $('#' + id).fadeIn('slow');
+          $('#step' + self.currentStep).fadeIn('slow');
         }, SHOW_HELP_TEXT_DELAY);
       });
     }
 
-    self.showHelpText = function aap(id) {
-      showHelpText(id);
-    }
+    self.showStep = showStep;
 
     return self;
   }
