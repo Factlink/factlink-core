ad46a61dfb7db48987ff2b293fc828c600c01d0e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-07-24 14:07:14 +0200

    Rewrote && -> and

--- core/db/migrate/20120724080635_add_created_factlinks_count_to_mixpanel.rb
+++ core/db/migrate/20120724080635_add_created_factlinks_count_to_mixpanel.rb
@@ -5,7 +5,7 @@ class AddCreatedFactlinksCountToMixpanel < Mongoid::Migration
     User.active.each do |user|
       gu = user.graph_user
 
-      facts = gu.real_created_facts.to_a.keep_if { |f| f.site && f.site.url && not f.site.url.blank? }
+      facts = gu.real_created_facts.to_a.keep_if { |f| f.site and f.site.url and not f.site.url.blank? }
 
       mixpanel.set_person_event user.id.to_s, factlinks_created_with_url: facts.length
     end
