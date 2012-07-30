e5ce968e2fd60e046e0bd6f10ae2134bca73fcae
tomdev <tom@codigy.nl>
2012-07-30 10:07:48 +0200

    Don't use Throttled resque
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/workers/set_last_interaction_for_user.rb
+++ core/app/workers/set_last_interaction_for_user.rb
@@ -1,15 +1,6 @@
-class SetLastInteractionForUser < Resque::ThrottledJob
-
-  throttle can_run_every: 1.minutes
+class SetLastInteractionForUser
 
   @queue = :user_operations
-
-  def self.identifier(*args)
-    args = *args
-    user_id = args[0]
-
-    "set_last_interaction:user_id:#{user_id}" # unique identifier for job/user
-  end
 
   def self.perform(user_id, timestamp)
     user = User.find(user_id)
