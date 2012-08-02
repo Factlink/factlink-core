b7121c7a874bed98b0e0b9fe151f7be454b41e39
Mark IJbema <markijbema@gmail.com>
2012-08-02 12:12:34 +0200

    no more shoulds

--- core/spec/controllers/js_lib_controller_spec.rb
+++ core/spec/controllers/js_lib_controller_spec.rb
@@ -4,22 +4,23 @@ describe JsLibController do
   render_views
 
   describe :show_template do
-    it "should be routed to for valid templates" do
+    it "routes to valid templates" do
       {get: "/templates/_channel_li"}.should route_to controller: 'js_lib', action: 'show_template', name: '_channel_li'
     end
 
-    it "should not be routed with directory traversal" do
+    it "does not route with directory traversal" do
       {get: "/templates/../../../../etc/passwd"}.should_not be_routable
     end
 
-    it "should be able to retrieve a valid template" do
+    it "retrieves a valid template" do
       get :show_template, name: '_channel_li'
       response.should be_success
     end
 
-    it "should render the indicator template when not logged in without raising errors" do
+    it "renders the indicator template when not logged in without raising errors" do
       get :show_template, name: 'indicator'
       response.should be_success
     end
   end
+
 end
