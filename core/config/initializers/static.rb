77971c6d2560226ff588c6ca56b56275f7355038
Mark IJbema <markijbema@gmail.com>
2012-08-02 14:01:39 +0200

    initialize JsLibUrl properly

--- core/config/initializers/static.rb
+++ core/config/initializers/static.rb
@@ -1,2 +1,7 @@
 static_conf = YAML::load_file(Rails.root.join('config/static.yml'))[Rails.env]['static']
-FactlinkUI::Application.config.static_url = static_conf['protocol'] + static_conf['hostname'] + ':' + static_conf['port'].to_s
+FactlinkUI::Application.config.static_url =
+    static_conf['protocol'] + static_conf['hostname'] +
+       ':' + static_conf['port'].to_s
+
+JsLibUrl.base_url = FactlinkUI::Application.config.static_url
+JsLibUrl.salt     = 'TOP_SEKRIT'
