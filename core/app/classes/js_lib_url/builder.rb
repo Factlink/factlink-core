2594641805b267483d3d9a164c93cb096f9b37c0
Mark IJbema <markijbema@gmail.com>
2012-08-07 14:47:51 +0200

    readded it, but does not seem to matter

--- core/app/classes/js_lib_url/builder.rb
+++ core/app/classes/js_lib_url/builder.rb
@@ -9,10 +9,10 @@ class JsLibUrl
       @cipher = Gibberish::AES.new(opts[:secret])
     end
     def url_for username
-      JsLibUrl.new Username.new(username, @cipher), @opts[:salt], @opts[:base_url]
+      ::JsLibUrl.new Username.new(username, @cipher), @opts[:salt], @opts[:base_url]
     end
     def url_from_string url
-      JsLibUrl.new username_from_string(url), @opts[:salt], @opts[:base_url]
+      ::JsLibUrl.new username_from_string(url), @opts[:salt], @opts[:base_url]
     end
 
     def username_from_string url
