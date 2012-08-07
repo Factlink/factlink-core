94ef17c93d7b0bc4e9422080fd07f5f5a87e75a1
Mark IJbema <markijbema@gmail.com>
2012-08-07 14:39:30 +0200

    removed AES from the salt

--- core/config/initializers/static.rb
+++ core/config/initializers/static.rb
@@ -3,11 +3,8 @@ FactlinkUI::Application.config.static_url =
     static_conf['protocol'] + static_conf['hostname'] +
        ':' + static_conf['port'].to_s
 
-gibber_cipher = Gibberish::AES.new(static_conf['secret'])
-
-
 FactlinkUI::Application.config.jslib_url_builder = JsLibUrl::Builder.new({
   base_url: FactlinkUI::Application.config.static_url + '/jslib/',
-  salt: Base64.urlsafe_encode64(gibber_cipher.enc(static_conf['salt'])).gsub(/=/,''),
+  salt: Base64.urlsafe_encode64(static_conf['salt']).gsub(/=/,''),                    #/# silly highlighting
   secret: static_conf['secret']
 })
