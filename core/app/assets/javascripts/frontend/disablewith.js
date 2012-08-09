28937ad83c20e47517d4010e13a2fe62e2dc4537
tomdev <tom@codigy.nl>
2012-08-09 11:09:56 +0200

    Remove unused code for disable_with
    Signed-off-by: tomdev <tom@codigy.nl>

--- core/app/assets/javascripts/frontend/disablewith.js
+++ core/app/assets/javascripts/frontend/disablewith.js
@@ -1,26 +1,9 @@
 (function () {
-  $('a[data-disable-with][data-onloadingtext]')
-    .live("ajax:before", setLoadingText);
-
-  $("a[data-disable-with][data-oncompletetext]")
-    .live("ajax:complete", setCompletedText);
-
-  $('a[data-disable-with][data-onloadingtext]:not([data-remote])')
+  $('a[data-disable-with]:not([data-remote])')
     .live('click', setLoadingText);
 
   function setLoadingText() {
-    var el = $(this);
-    var isDataRemote = el.data('remote');
-
-    el.text( $(this).attr('data-onloadingtext') )
+    $(this).text( $(this).attr('data-disable-with') )
       .addClass('disabled');
-
-    if (isDataRemote) {
-      el.attr('onclick','return false;');
-    }
-  }
-
-  function setCompletedText() {
-    $(this).text( $(this).attr('data-oncompletetext') );
   }
 }());
