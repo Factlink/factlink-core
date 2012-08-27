fa0ef1bcc0e438ca48cfb0e850776cd1a1c54855
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-08-27 15:31:12 +0200

    Created falsy login integration spec

--- core/spec/integration/sign_in_user_spec.rb
+++ core/spec/integration/sign_in_user_spec.rb
@@ -8,4 +8,15 @@ describe 'When a user signs in', type: :request do
 
     page.should have_content "Your account has not yet been approved"
   end
+
+  it 'he should not be able to sign in with false credentials' do
+    user = FactoryGirl.create :approved_confirmed_user
+
+    visit "/"
+    fill_in "user_login", :with => user.email
+    fill_in "user_login_password", :with => user.password + "1"
+    click_button "Sign in"
+
+    page.should have_content "Your login credentials were incorrect. Please check and try again"
+  end
 end
