d0cf2f33540267de428687737d95bee2aace4e3d
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-08-27 14:38:03 +0200

    Simplified confirmation spec

--- core/spec/integration/confirm_user_spec.rb
+++ core/spec/integration/confirm_user_spec.rb
@@ -1,24 +1,7 @@
 require 'integration_helper'
 
 describe 'When a username is reserved', type: :request do
-  it 'the user should receive a confirmation email' do
-    email = random_email
-
-    clear_emails
-
-    visit '/'
-
-    fill_in 'user_username', with: random_username
-    fill_in 'user_email',    with: email
-
-    click_button 'Reserve'
-
-    open_email email
-
-    current_email.have_content 'Confirm my email address'
-  end
-
-  it 'the user should be able to confirm his email address' do
+  it 'the user should receive a confirmation email and should be able to confirm its e-mail address' do
     email = random_email
 
     clear_emails
