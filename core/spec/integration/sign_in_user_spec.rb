59969903238947dfad59dd1dba4cd9e284dc507b
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-08-27 15:45:36 +0200

    Created sign out integration spec

--- core/spec/integration/sign_in_user_spec.rb
+++ core/spec/integration/sign_in_user_spec.rb
@@ -9,6 +9,20 @@ describe 'When a user signs in', type: :request do
     page.should have_content "Your account has not yet been approved"
   end
 
+  it 'he should be able to sign out' do
+    user = FactoryGirl.create :approved_confirmed_user
+
+    sign_in_user(user)
+
+    page.should_not have_content "Your login credentials were incorrect. Please check and try again"
+
+    visit '/users/sign_out'
+
+    visit '/'
+
+    find "sign-in-form"
+  end
+
   it 'he should not be able to sign in with false credentials' do
     user = FactoryGirl.create :approved_confirmed_user
 
