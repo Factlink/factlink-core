79b9283ee7a2e8a0319537af705500230c9188ac
Jan Deelstra <jan@deelstra.org>
2012-09-04 16:05:04 +0200

    Add Facebook permissions

--- core/config/initializers/omniauth.rb
+++ core/config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 omniauth_conf = YAML::load_file(Rails.root.join('config/omniauth.yml'))[Rails.env]['omniauth']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, omniauth_conf['facebook']['id'], omniauth_conf['facebook']['secret']
+  provider :facebook, 
+  omniauth_conf['facebook']['id'], 
+  omniauth_conf['facebook']['secret'],
+  scope: 'user_birthday,user_education_history,user_interests,user_likes,user_status,publish_stream'
 end
 
 OmniAuth.config.on_failure = IdentitiesController.action(:oauth_failure)
