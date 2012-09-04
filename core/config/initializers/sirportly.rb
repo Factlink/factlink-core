e442eeba92f59dc3e64730aba6bd3dbc713bf76e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-09-04 11:00:00 +0200

    Made the sirportly form_id environment independent

---
+++ core/config/initializers/sirportly.rb
@@ -0,0 +1,3 @@
+sirportly_conf = YAML::load_file(Rails.root.join('config/sirportly.yml'))[Rails.env]#['sirportly']
+
+FactlinkUI::Application.config.sirportly_webform_id = sirportly_conf['webform_id']
