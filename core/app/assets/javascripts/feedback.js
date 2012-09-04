6133c90991ae0c97459484736f9950f6cae171fa
Mark IJbema <markijbema@gmail.com>
2012-09-04 15:40:32 +0200

    close the alert in the feedback modal (and reindented everything with spaces)

--- core/app/assets/javascripts/feedback.js
+++ core/app/assets/javascripts/feedback.js
@@ -2,27 +2,32 @@
 //= require twitter/bootstrap
 
 $(function () {
-	updateHeight();
+  updateHeight();
 
-	$(window).on('resize', updateHeight);
+  $(window).on('resize', updateHeight);
 
-	$('a.close').on('click', function () {
-		// Wait till it's closed, ugly?
-		setTimeout(updateHeight, 10);
-	});
+  $('a.close').on('click', function () {
+    // Wait till it's closed, ugly?
+    setTimeout(updateHeight, 10);
+  });
 
-	$('.cancel').on('click', function () {
-		console.info( "TEST" );
-	});
+  $('.cancel').on('click', function () {
+    console.info( "TEST" );
+  });
 
-	$('.cancel').on('click', window.parent.closeFeedbackFrame);
+  $('.cancel').on('click', window.parent.closeFeedbackFrame);
+
 });
 
 function updateHeight() {
-	var $iframe = window.parent.$('#feedback_frame>iframe');
+  var $iframe = window.parent.$('#feedback_frame>iframe');
 
-	$iframe.css({
-		height: $('body').outerHeight() + "px",
-		marginTop: - $('body').outerHeight()/2 + "px"
-	});
+  $iframe.css({
+    height: $('body').outerHeight() + "px",
+    marginTop: - $('body').outerHeight()/2 + "px"
+  });
+}
+
+function onCloseFrame() {
+  $('.alert').remove();
 }
