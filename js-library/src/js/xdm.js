bc674bc5bf3c7b6c4282d1dc748870600a3f1c92
Mark IJbema <markijbema@gmail.com>
2012-09-05 15:48:48 +0200

    removed the ajax tunnel from xdm

--- js-library/src/js/xdm.js
+++ js-library/src/js/xdm.js
@@ -11,30 +11,9 @@
     remote: {
       showFactlink: {},
       prepareNewFactlink: {},
-      position: {},
-      ajax: {}
+      position: {}
     }
   });
-
-  Factlink.ajax = function(path, options) {
-    function globalErrorFunction(xhr, status, err) {
-      console.info('ERROR : ' + path );
-      if (xhr.message.status === 401) {
-        alert("Please login to Factlink.");
-      }
-    }
-
-    function globalSuccessFunction(data) {
-      console.info('SUCCESS : ' + path );
-    }
-
-    var success = ( $.isFunction(options.success) ? options.success : globalSuccessFunction );
-    var error = ( $.isFunction(options.error) ? options.error : globalErrorFunction );
-    delete options.success;
-    delete options.error;
-
-    Factlink.remote.ajax(path, options, success, error);
-  };
 
   $(window).trigger("factlink.loaded");
 })(window.Factlink, Factlink.$, Factlink._, Factlink.easyXDM, Factlink.global);
