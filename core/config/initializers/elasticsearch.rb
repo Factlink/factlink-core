ad6ad72cb00d17d70967d0cbcb27690ba48dffe0
tomdev <tom@north26.nl>
2012-09-20 11:18:11 +0200

    ElasticSearch 'cluster' should be used in the url to namespace our search index
    Signed-off-by: tomdev <tom@north26.nl>

--- core/config/initializers/elasticsearch.rb
+++ core/config/initializers/elasticsearch.rb
@@ -1,9 +1,9 @@
 elasticsearch_conf = YAML::load_file(Rails.root.join('config/elasticsearch.yml'))[Rails.env]['elasticsearch']
 
 connection_hash = {
-  host: elasticsearch_conf['host'],
+  hostname: elasticsearch_conf['hostname'],
   port: elasticsearch_conf['port'],
   cluster: elasticsearch_conf['cluster']
 }
 
-FactlinkUI::Application.config.elasticsearch_url = "#{connection_hash[:host]}:#{connection_hash[:port]}"
+FactlinkUI::Application.config.elasticsearch_url = "#{connection_hash[:hostname]}:#{connection_hash[:port]}/#{connection_hash[:cluster]}"
