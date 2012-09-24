6c6dad2c73cf231df8def1a9695632e4a030b3ec
Mark IJbema <markijbema@gmail.com>
2012-09-24 14:57:09 +0200

    don't prefetch my own list of channels, improves speed by factor 5 (facts/new)

--- core/app/backbone/collections/own_channel_collection.js
+++ core/app/backbone/collections/own_channel_collection.js
@@ -1,6 +1,6 @@
 window.OwnChannelCollection = Backbone.Collection.extend({
   model: Channel,
   url: function() {
-    return '/' + currentUser.get('username') + '/channels/';
+    return '/' + currentUser.get('username') + '/channels';
   }
 });
