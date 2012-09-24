aee74edf97216e1bd0bf681973ddf9aa3d09f023
Jan Deelstra <jan@deelstra.org>
2012-09-24 15:06:48 +0200

    Return actual objects as result of ElasticSearch query
    Currently using a simple implementation, a single query for each hit.

--- core/app/interactors/queries/elastic_search_all_query.rb
+++ core/app/interactors/queries/elastic_search_all_query.rb
@@ -8,8 +8,29 @@ class ElasticSearchAllQuery
   def execute
     from = (@page - 1) * @row_count
     url = "http://#{FactlinkUI::Application.config.elasticsearch_url}/_search?q=#{@keywords}&from=#{from}&size=#{@row_count}"
-    puts url
+
     results = HTTParty.get url
-    puts results
+    hits = results.parsed_response['hits']['hits']
+
+    result_objects = []
+
+    hits.each do |record|
+      result_objects << get_object(record['_id'], record['_type'])
+    end
+    result_objects
+  end
+
+  private
+  def get_object id, type
+    if(type == 'factdata')
+      return FactData.find(id)
+    end
+    if (type == 'topic')
+      return Topic.find(id)
+    end
+    if (type == 'user')
+      return User.find(id)
+    end
+    raise 'Object type unknown.'
   end
 end
--- core/spec/interactors/queries/elastic_search_all_query_spec.rb
+++ core/spec/interactors/queries/elastic_search_all_query_spec.rb
@@ -8,6 +8,7 @@ describe 'ElasticSearchAllQuery' do
     stub_const 'FactData', fake_class
     stub_const 'User', fake_class
     stub_const 'Topic', fake_class
+    stub_const 'FactData', fake_class
     stub_const 'FactlinkUI::Application', fake_class
   end
 
@@ -17,22 +18,47 @@ describe 'ElasticSearchAllQuery' do
     query.should_not be_nil
   end
 
-  it 'executes correcly' do
-    config = mock()
-    base_url = "1.0.0.0:4000/index"
-    config.stub elasticsearch_url: base_url
-    FactlinkUI::Application.stub config: config
-    keywords = "searching for this channel"
-    interactor = ElasticSearchAllQuery.new keywords, 1, 20
-    internal_result = mock()
-    results = ['a','b','c']
-    internal_result.stub hits: results
-    internal_result.stub results: results
-    url =
-    HTTParty.should_receive(:get).
-      with("http://#{base_url}/_search?q=#{keywords}&from=0&size=20").
-      and_return(results)
 
-    interactor.execute.should eq results
+  ['user', 'topic', 'factdata'].each do |type|
+
+    it "executes correctly with return value of #{type} class" do
+      config = mock()
+      base_url = "1.0.0.0:4000/index"
+      config.stub elasticsearch_url: base_url
+      FactlinkUI::Application.stub config: config
+      keywords = "searching for this channel"
+      interactor = ElasticSearchAllQuery.new keywords, 1, 20
+
+      hit = mock()
+      hit.should_receive(:[]).with('_id').and_return(1)
+      hit.should_receive(:[]).with('_type').and_return(type)
+
+      results = mock()
+      results.stub parsed_response: { 'hits' => { 'hits' => [ hit ] } }
+
+      HTTParty.should_receive(:get).
+        with("http://#{base_url}/_search?q=#{keywords}&from=0&size=20").
+        and_return(results)
+
+      return_object = mock()
+
+      case type
+      when 'user'
+        User.should_receive(:find).
+          with(1).
+          and_return(return_object)
+      when 'topic'
+        Topic.should_receive(:find).
+          with(1).
+          and_return(return_object)
+      when 'factdata'
+        User.should_receive(:find).
+          with(1).
+          and_return(return_object)
+      end
+
+      interactor.execute.should eq [return_object]
+    end
+
   end
 end
