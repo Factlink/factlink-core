2b7c6a977d257f68541b85ecc51105f232d0b0b5
Mark IJbema <markijbema@gmail.com>
2012-09-24 14:33:27 +0200

    corrected for a user with channels

--- core/spec/benchmark/facts_new.rb
+++ core/spec/benchmark/facts_new.rb
@@ -1,19 +1,31 @@
 require 'spec_helper'
 
+def add_channel usser, name
+  channel = Channel.new title: name
+  channel.created_by =  user.graph_user
+  channel.save
+end
+
 describe FactsController, type: :controller do
   render_views
 
   let(:user) { create(:user) }
   let(:fact) { create(:fact) }
 
+
   describe :new do
     it "should work" do
       authenticate_user!(user)
+
+      20.times do |i|
+        add_channel user, "Channel #{i}"
+      end
+
       Benchmark.bmbm do |bm|
         bm.report do
           # this number is chosen so that it takes about 10 seconds on marks mac (total)
-          # initially it was 200 -> ~10 total
-          200.times do |i|
+          # initially it was with 20 channels 42 runs -> ~10 seconds total
+          45.times do |i|
             post 'new', :url => "http://example.org/#{i}",  :displaystring => "Facity Fact (#{i})", :title => "Title nr. #{i}"
           end
         end
