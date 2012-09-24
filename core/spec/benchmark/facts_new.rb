ffdbd395c04b8aea5c640678cd669726378b1159
Mark IJbema <markijbema@gmail.com>
2012-09-24 14:16:22 +0200

    cache busting, just to be sure

--- core/spec/benchmark/facts_new.rb
+++ core/spec/benchmark/facts_new.rb
@@ -13,8 +13,8 @@ describe FactsController, type: :controller do
         bm.report do
           # this number is chosen so that it takes about 10 seconds on marks mac (total)
           # initially it was 200 -> ~10 total
-          200.times do
-            post 'new', :url => "http://example.org/",  :displaystring => "Facity Fact", :title => "Title"
+          200.times do |i|
+            post 'new', :url => "http://example.org/#{i}",  :displaystring => "Facity Fact (#{i})", :title => "Title nr. #{i}"
           end
         end
       end
