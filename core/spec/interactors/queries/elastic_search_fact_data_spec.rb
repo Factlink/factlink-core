77d356a5ae79750c643d6b27cdbf8795eadd3125
tomdev <tom@north26.nl>
2012-09-25 10:35:09 +0200

    Added logger test for ElasticSearchFactDataQuery
    Signed-off-by: tomdev <tom@north26.nl>

--- core/spec/interactors/queries/elastic_search_fact_data_spec.rb
+++ core/spec/interactors/queries/elastic_search_fact_data_spec.rb
@@ -19,30 +19,51 @@ describe 'ElasticSearchFactData' do
   end
 
   it "executes correctly with return value of FactData class" do
+    config = mock()
+    base_url = "1.0.0.0:4000/index"
+    config.stub elasticsearch_url: base_url
+    FactlinkUI::Application.stub config: config
+    keywords = "searching for evidence"
+    wildcard_keywords = "*searching* *for* *evidence*"
+    interactor = ElasticSearchFactDataQuery.new keywords, 1, 20
+
+    hit = mock()
+    hit.should_receive(:[]).with('_id').and_return(1)
+
+    results = mock()
+    results.stub parsed_response: { 'hits' => { 'hits' => [ hit ] } }
+    results.stub code: 200
+
+    HTTParty.should_receive(:get).
+      with("http://#{base_url}/factdata/_search?q=#{wildcard_keywords}&from=0&size=20").
+      and_return(results)
+
+    return_object = mock()
+
+    FactData.should_receive(:find).with(1).and_return(return_object)
+
+    interactor.execute.should eq [return_object]
+  end
+
+  it 'logs and raises an error when HTTParty returns a non 2xx status code.' do
       config = mock()
       base_url = "1.0.0.0:4000/index"
       config.stub elasticsearch_url: base_url
       FactlinkUI::Application.stub config: config
-      keywords = "searching for evidence"
-      wildcard_keywords = "*searching* *for* *evidence*"
-      interactor = ElasticSearchFactDataQuery.new keywords, 1, 20
-
-      hit = mock()
-      hit.should_receive(:[]).with('_id').and_return(1)
-
+      keywords = "searching for this channel"
       results = mock()
-      results.stub parsed_response: { 'hits' => { 'hits' => [ hit ] } }
-
+      error_response = 'error has happened server side'
+      results.stub response: error_response
+      results.stub code: 501
       HTTParty.should_receive(:get).
-        with("http://#{base_url}/factdata/_search?q=#{wildcard_keywords}&from=0&size=20").
         and_return(results)
+      logger = mock()
+      error_message = "Server error, status code: 501, response: '#{error_response}'."
+      logger.should_receive(:error).with(error_message)
+      query = ElasticSearchFactDataQuery.new keywords, 1, 20, logger: logger
 
-      return_object = mock()
-
-      FactData.should_receive(:find).with(1).and_return(return_object)
-
-      interactor.execute.should eq [return_object]
-  end
+      expect { query.execute }.to raise_error(RuntimeError, error_message)
+    end
 
 end
 
