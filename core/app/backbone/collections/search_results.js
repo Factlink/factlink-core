c7233eab426ee6301d553552f7dad433493bc864
Mark IJbema <markijbema@gmail.com>
2012-09-25 13:21:27 +0200

    converted search results to coffeescript

--- core/app/backbone/collections/search_results.js
+++
@@ -1,9 +0,0 @@
-window.SearchResults = Backbone.Collection.extend({
-  model: SearchResultItem,
-  initialize: function(models, opts) {
-    this.search = opts.search;
-  },
-  url: function() {
-    return '/search/?s=' + this.search;
-  }
-});
