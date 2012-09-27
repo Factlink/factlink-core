8c6944784e1e6a4f5e32c8310e878a2a7c461076
tomdev <tom@north26.nl>
2012-09-27 12:00:59 +0200

    Factored registration controller, responds to xhr requests now
    Signed-off-by: tomdev <tom@north26.nl>

--- core/app/controllers/users/registrations_controller.rb
+++ core/app/controllers/users/registrations_controller.rb
@@ -7,27 +7,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource
 
-    # Set random password
-    generated_password = Devise.friendly_token
-
-    # Assign field
     resource.email    = params[:user][:email]
+    resource.password = Devise.friendly_token # Random password
+
     if ( /\A([-a-zA-Z0-9_]+)\Z/.match(params[:user][:registration_code]))
-      resource.registration_code    = params[:user][:registration_code]
+      resource.registration_code = params[:user][:registration_code]
     end
-    resource.password = generated_password
 
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
+
+        location = after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
 
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        location = after_inactive_sign_up_path_for(resource)
       end
+
+      respond_with resource, location: location
     else
       clean_up_passwords resource
 
@@ -36,7 +36,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
         the_errors << "#{value.to_s}<br>"
       end
 
-      redirect_to root_path, alert: the_errors.html_safe
+      if request.xhr?
+        render status: 400, json: the_errors
+        return
+      else
+        redirect_to root_path, alert: the_errors.html_safe
+      end
+
     end
   end
 
