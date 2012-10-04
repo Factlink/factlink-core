86b941fd8b97e1cfe40ce0d52d6f8a09cb602808
Jan Deelstra <jan@deelstra.org>
2012-10-04 15:10:46 +0200

    Added more detailed description of problem

--- core/spec/integration/capybara_and_devise_are_broken.rb
+++ core/spec/integration/capybara_and_devise_are_broken.rb
@@ -9,6 +9,9 @@ require 'integration_helper'
 
 describe 'capybara and devise are broken', type: :request do
   it 'action_mailer default url should change when visiting a page' do
+    # Something changes the config option, so that the e-mails don't contain correct links.
+    # Depending on which capybara driver is used, it changes in an example.org or :56781.
+    # This error is triggered when enabling javascript either by js: true (rack test) or using webkit.
     initialValue = FactlinkUI::Application.config.action_mailer.default_url_options[:host]
 
 		visit "/"
