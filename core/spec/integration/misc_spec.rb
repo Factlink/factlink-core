315166e78dce31bcb874a7a5b06f7951724edcbb
Jan Paul Posma <me@janpaulposma.nl>
2012-10-09 17:49:05 +0200

    Sign in screenshot

--- core/spec/integration/misc_spec.rb
+++ core/spec/integration/misc_spec.rb
@@ -1,23 +1,9 @@
 require 'integration_helper'
 
 describe "Compare screens", type: :request do
-
+  
   it "should render the frontpage as expected" do
-    u = create :user
-    displaystrings = [
-      "hoi",
-      "hoi hoih ohiaetnoi htneoahtniahtn aeionhteoahtnaeo hni tnaeohtin haeotni haeotin heoatni haeotni hoeatni haeotni heoatnih aetnoih aetnoih aetnoi haetonih tnoaei htaenoih aetnoih aetnoihaetno hiaetnoaih aetnoi haeotni haeotnih aetnoih aeotni heoatni haeotnih tnoeahi aetnohi aetnohi aetnohi tnaohieo atnhaeiotn",
-      "moi"
-    ]
-    3.times do |i|
-      f1 = create :fact, created_by: u.graph_user
-      f1.add_opinion :believes, u.graph_user
-      f1.data.displaystring = displaystrings[i]
-      f1.data.save
-      f1.reposition_in_top
-    end
-
-    visit "/"
+    visit "/?show_sign_in=true"
     assume_unchanged_screenshot "homepage"
   end
 
