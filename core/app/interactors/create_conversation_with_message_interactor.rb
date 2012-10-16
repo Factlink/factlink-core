ad157fb3aada5bd9b04cfc681fe4c697d4b55295
tomdev <tom@north26.nl>
2012-10-16 11:27:28 +0200

    Fixed merge
    Signed-off-by: tomdev <tom@north26.nl>

--- core/app/interactors/create_conversation_with_message_interactor.rb
+++ core/app/interactors/create_conversation_with_message_interactor.rb
@@ -1,5 +1,7 @@
 class CreateConversationWithMessageInteractor
   include Interactor
+  include Activity::Subject
+
   def initialize(recipients, sender, content) # add subject_type and subject_id
     @recipient_usernames = recipients
     @sender_username = sender
@@ -14,9 +16,11 @@ class CreateConversationWithMessageInteractor
     m = created_message
     m.conversation = c
     m.save
+
     # TODO: create activity :created_conversation,
     #       and add it to notifications of (receivers-[sender]) via
     #       create_listeners.rb
+    activity user_for(@sender).graph_user, :created_conversation, c
   end
 
   def created_message
