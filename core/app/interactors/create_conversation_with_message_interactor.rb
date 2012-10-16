4a756f8fa073a0cb6f1092c4087841af18d0e825
Mark IJbema <markijbema@gmail.com>
2012-10-16 12:14:35 +0200

    factored out functionality to multiple classes (commands, and support classes)

--- core/app/interactors/create_conversation_with_message_interactor.rb
+++ core/app/interactors/create_conversation_with_message_interactor.rb
@@ -1,33 +1,69 @@
-class CreateConversationWithMessageInteractor
-  include Interactor
-  def initialize(recipients, sender, content) # add subject_type and subject_id
-    @recipient_usernames = recipients
-    @sender_username = sender
-    @content = content
+module SmartInit
+  extend ActiveSupport::Concern
+  module ClassMethods
+    # arguments :foo, :bar
+    #
+    # results in
+    #
+    # def initialize(foo, bar)
+    #   @foo = foo
+    #   @bar = bar
+    # end
+    def arguments *keys
+      define_method :initialize do |*names|
+        (keys.zip names).each do |pair|
+          name = "@" + pair[0].to_s
+          value = pair[1]
+          instance_variable_set(name, value)
+        end
+      end
+    end
   end
+end
+
+module Command
+  extend ActiveSupport::Concern
+  include SmartInit
+end
+
+class CreateMessageCommand
+  include Command
+  arguments :sender_username, :content, :conversation_id
+
+  def execute
+    m = Message.create
+    m.sender = User.where(username: @sender_username).first
+    m.content = @content
+    m.conversation_id = @conversation_id
+    m.save
+    m
+  end
+end
+
+class CreateConversationCommand
+  include Command
+  arguments :recipient_usernames
   def execute
     c = Conversation.new
-    @recipient_usernames.each do |name|
-      c.recipients << user_for(name)
+    @recipient_usernames.each do |username|
+      c.recipients << User.where(username: username).first
     end
     c.save
-    m = created_message
-    m.conversation = c
-    m.save
+    c
+  end
+end
+
+class CreateConversationWithMessageInteractor
+  include Interactor
+  include SmartInit
+
+  arguments :recipient_usernames, :sender_username, :content
+
+  def execute
+    c = command :create_conversation, @recipient_usernames
+    command :create_message, @sender_username, @content, c.id
     # TODO: create activity :created_conversation,
     #       and add it to notifications of (receivers-[sender]) via
     #       create_listeners.rb
-  end
-
-  def created_message
-    m = Message.create
-    m.sender = user_for(@sender_username)
-    m.content = @content
-    m.save or raise 'hell'
-    m
-  end
-
-  def user_for(username)
-    User.where(username: username).first
   end
 end
