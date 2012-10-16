7f9d7d260e02f606a5b367853f6bb8ca7a52a866
Jan Paul Posma <me@janpaulposma.nl>
2012-10-16 16:52:36 +0200

    Added JSON testing for conversations with messages

--- core/spec/controllers/conversations_controller_spec.rb
+++ core/spec/controllers/conversations_controller_spec.rb
@@ -3,19 +3,34 @@ require 'spec_helper'
 describe ConversationsController do
   render_views
 
-  let(:conv) {create(:conversation_with_messages, message_count: 10, user_count: 3)}
+  let(:conv) {create(:conversation_with_messages, message_count: 4, user_count: 3)}
 
   describe :show do
-    it "should show" do 
-      get :show, id: conv.id.to_s, format: 'json'
-      # require 'debugger'; debugger
-      # puts 'bla'
-      # puts 'bla'
-      # puts 'bla'
-      # puts 'bla'
-      # puts response.inspect
+    describe "json" do
+      it "should contain conversation fields" do 
+        get :show, id: conv.id.to_s, format: 'json'
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+
+        expect(json["id"]).to eq(conv.id.to_s)
+        expect(json["fact_data_id"]).to eq(conv.fact_data.id.to_s)
+      end
+
+      it "should contain messages" do
+        get :show, id: conv.id.to_s, format: 'json'
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json["messages"][0]["id"]).to eq(conv.messages[0].id.to_s)
+        expect(json["messages"][1]["sender_id"]).to eq(conv.messages[1].sender.id.to_s)
+        expect(json["messages"][2]["content"]).to eq(conv.messages[2].content)
+      end
+    end
+    
+    describe "html" do
+      it "should be successful" do
+        get :show, id: 0
+        expect(response).to be_success
+      end
     end
   end
 end
-
-# expect(parsed_content["fact_bubble"]["displaystring"]).to eq(displaystring)
