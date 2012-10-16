170bf36ec8b8d143fa870aade2087c48cf2f2e10
Mark IJbema <markijbema@gmail.com>
2012-10-16 09:43:33 +0200

    implemented very very first version of create for conversation+message
    interactor isn't working yet, please fix test + interactor

---
+++ core/app/interactors/create_conversation_with_message_interactor.rb
@@ -0,0 +1,31 @@
+class CreateConversationWithMessageInteractor
+  include Interactor
+  def initialize(recipients, sender, content) # add subject_type and subject_id
+    @recipient_usernames = recipients
+    @sender_username = sender
+    @content = content
+  end
+  def execute
+    c = Conversation.new
+    @recipient_usernames.each do |name|
+      c.recipients << user_for(name)
+    end
+    c.messages << created_message
+    c.save
+    # TODO: create activity :created_conversation,
+    #       and add it to notifications of (receivers-[sender]) via
+    #       create_listeners.rb
+  end
+
+  def created_message
+    m = Message.create
+    m.sender = user_for(@sender_username)
+    m.content = @content
+    m.save or raise 'hell'
+    m
+  end
+
+  def user_for(username)
+    User.where(username: username).first
+  end
+end
---
+++ core/spec/interactors/create_conversation_with_message_interactor_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe CreateConversationWithMessageInteractor do
+  it "creates a conversation" do
+    jan = create :user, username: 'jan'
+    frank = create :user, username: 'frank'
+
+    CreateConversationWithMessageInteractor.perform [jan, frank], jan, 'geert'
+    expect(Conversation.all.length.should).to eq(1)
+
+    conversation = Conversation.all.first
+
+    expect(conversation.messages.length).to eq(1)
+    expect(conversation.messages[0].content).to eq('geert')
+  end
+end
