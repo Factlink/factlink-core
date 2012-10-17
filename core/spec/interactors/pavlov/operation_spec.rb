6aea54c3e3b75e8a700371c7e37e7d3b66fddf34
Mark IJbema <markijbema@gmail.com>
2012-10-17 11:51:42 +0200

    restructured test

--- core/spec/interactors/pavlov/operation_spec.rb
+++ core/spec/interactors/pavlov/operation_spec.rb
@@ -5,54 +5,56 @@ describe Pavlov::Operation do
   end
 
   describe '#arguments' do
-    it "should ensure that the initializer saves arguments passed to instance variables" do
-      dummy_class = Class.new do
-        include Pavlov::Operation
-        arguments :geert
+    describe "creates an initializer which" do
+      it "saves arguments passed to instance variables" do
+        dummy_class = Class.new do
+          include Pavlov::Operation
+          arguments :geert
+        end
+
+        x = dummy_class.new 'argument'
+        expect(x.send(:instance_variable_get,'@geert')).to eq('argument')
       end
 
-      x = dummy_class.new 'argument'
-      expect(x.send(:instance_variable_get,'@geert')).to eq('argument')
-    end
+      it "saves arguments passed to instance variables in order" do
+        dummy_class = Class.new do
+          include Pavlov::Operation
+          arguments :var1, :var2
+        end
 
-    it "should ensure that the initializer saves arguments passed to instance variables in order" do
-      dummy_class = Class.new do
-        include Pavlov::Operation
-        arguments :var1, :var2
+        x = dummy_class.new 'VAR1', 'VAR2'
+        expect(x.send(:instance_variable_get,'@var1')).to eq('VAR1')
+        expect(x.send(:instance_variable_get,'@var2')).to eq('VAR2')
       end
 
-      x = dummy_class.new 'VAR1', 'VAR2'
-      expect(x.send(:instance_variable_get,'@var1')).to eq('VAR1')
-      expect(x.send(:instance_variable_get,'@var2')).to eq('VAR2')
-    end
-
-    it "should create an initializer which calls validate if it exists" do
-      dummy_class = Class.new do
-        include Pavlov::Operation
-        arguments
-        def validate
-          @x_val = :validate_was_called
+      it "calls validate if it exists" do
+        dummy_class = Class.new do
+          include Pavlov::Operation
+          arguments
+          def validate
+            @x_val = :validate_was_called
+          end
+          def validate_was_called
+            @x_val
+          end
         end
-        def validate_was_called
-          @x_val
-        end
+        x = dummy_class.new
+        expect(x.validate_was_called).to eq(:validate_was_called)
       end
-      x = dummy_class.new
-      expect(x.validate_was_called).to eq(:validate_was_called)
-    end
-    it "should create an initializer which call check_authority" do
-      dummy_class = Class.new do
-        include Pavlov::Operation
-        arguments
-        def check_authority
-          @x_val = :check_authority_was_called
+      it "calls check_authority" do
+        dummy_class = Class.new do
+          include Pavlov::Operation
+          arguments
+          def check_authority
+            @x_val = :check_authority_was_called
+          end
+          def check_authority_was_called
+            @x_val
+          end
         end
-        def check_authority_was_called
-          @x_val
-        end
+        x = dummy_class.new
+        expect(x.check_authority_was_called).to eq(:check_authority_was_called)
       end
-      x = dummy_class.new
-      expect(x.check_authority_was_called).to eq(:check_authority_was_called)
     end
   end
 end
