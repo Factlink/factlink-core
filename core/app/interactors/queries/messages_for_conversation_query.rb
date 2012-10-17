b6b5d5728ca07aed921a7237ff686241b8a9f2b5
Jan Paul Posma <me@janpaulposma.nl>
2012-10-17 13:06:30 +0200

    Minor fix in messages query

--- core/app/interactors/queries/messages_for_conversation_query.rb
+++ core/app/interactors/queries/messages_for_conversation_query.rb
@@ -8,13 +8,13 @@ class MessagesForConversationQuery
 
   def execute
     Message.where(conversation_id: @conversation.id).map do |message|
-      Hashie::Mash.new({
+      Hashie::Mash.new(
         id: message.id,
         created_at: message.created_at,
         updated_at: message.updated_at,
         content: message.content,
         sender_id: message.sender_id
-      })
+      )
     end
   end
 end
