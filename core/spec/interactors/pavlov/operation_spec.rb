abffdca693e4573de713bab7587c98ebcc2bad08
Mark IJbema <markijbema@gmail.com>
2012-10-17 11:33:52 +0200

    added test for the arguments function

--- core/spec/interactors/pavlov/operation_spec.rb
+++ core/spec/interactors/pavlov/operation_spec.rb
@@ -1,4 +1,29 @@
 require_relative '../../../app/interactors/pavlov.rb'
 
 describe Pavlov::Operation do
+  let :dummy_class do
+  end
+
+  describe '#arguments' do
+    it "should ensure that the initializer saves arguments passed to instance variables" do
+      dummy_class = Class.new do
+        include Pavlov::Operation
+        arguments :geert
+      end
+
+      x = dummy_class.new 'argument'
+      expect(x.send(:instance_variable_get,'@geert')).to eq('argument')
+    end
+
+    it "should ensure that the initializer saves arguments passed to instance variables in order" do
+      dummy_class = Class.new do
+        include Pavlov::Operation
+        arguments :var1, :var2
+      end
+
+      x = dummy_class.new 'VAR1', 'VAR2'
+      expect(x.send(:instance_variable_get,'@var1')).to eq('VAR1')
+      expect(x.send(:instance_variable_get,'@var2')).to eq('VAR2')
+    end
+  end
 end
