eb130c5bf2c9d16d4279666602a6da8d1ffb4fee
Mark IJbema <markijbema@gmail.com>
2012-10-17 11:22:06 +0200

    used require_relative

--- core/app/interactors/pavlov.rb
+++ core/app/interactors/pavlov.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../pavlov/operation', __FILE__)
-require File.expand_path('../pavlov/command', __FILE__)
-require File.expand_path('../pavlov/query', __FILE__)
-require File.expand_path('../pavlov/interactor', __FILE__)
+require_relative 'pavlov/operation.rb'
+require_relative 'pavlov/command.rb'
+require_relative 'pavlov/query.rb'
+require_relative 'pavlov/interactor.rb'
