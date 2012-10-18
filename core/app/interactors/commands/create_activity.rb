63593a7182cf1118035ec9e94e8db9be77600d5c
tomdev <tom@north26.nl>
2012-10-18 15:25:23 +0200

    And compare string with a string
    Signed-off-by: tomdev <tom@north26.nl>

--- core/app/interactors/commands/create_activity.rb
+++ core/app/interactors/commands/create_activity.rb
@@ -11,7 +11,7 @@ module Commands
     end
 
     def authorized?
-      @options[:current_user].id == @graph_user.user_id
+      @options[:current_user].id.to_s == @graph_user.user_id
     end
   end
 end
