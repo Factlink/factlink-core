fbea643e0f105eea690143393bd8f91ff75d2f31
Jan Paul Posma <me@janpaulposma.nl>
2012-10-18 11:28:22 +0200

    Added Queries::ElasticSearchUser
    * Query for users
    * SearchUser interactor
    * Tests for both

---
+++ core/app/interactors/queries/elastic_search_user.rb
@@ -0,0 +1,10 @@
+require 'logger'
+require_relative "elastic_search.rb"
+
+module Queries
+  class ElasticSearchUser < ElasticSearch
+    def define_query
+      type :user
+    end
+  end
+end
---
+++ core/app/interactors/search_user_interactor.rb
@@ -0,0 +1,36 @@
+class SearchUserInteractor
+  def initialize keywords, user, options={}
+    raise 'Keywords should be a string.' unless keywords.kind_of? String
+    raise 'Keywords must not be empty.'  unless keywords.length > 0
+    raise 'User should be of User type.' unless user.kind_of? User
+
+    @user = user
+    @keywords = keywords
+    @ability = options[:ability]
+    @page = options[:page] || 1
+    @row_count = options[:row_count] || 20
+  end
+
+  def execute
+    raise CanCan::AccessDenied unless authorized?
+
+    if filter_keywords.length == 0
+      return []
+    end
+
+    query = Queries::ElasticSearchUser.new filter_keywords, @page, @row_count
+
+    results = query.execute
+
+    results
+  end
+
+  private
+  def filter_keywords
+    @keywords.split(/\s+/).select{|x|x.length > 1}.join(" ")
+  end
+
+  def authorized?
+    (@ability.can? :index, Topic) and (@ability.can? :show, @user)
+  end
+end
