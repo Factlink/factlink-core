156455ac642d70bf4e9c9bf8ab77d38360090140
Jan Paul Posma <me@janpaulposma.nl>
2012-10-22 15:08:29 +0200

    Updated Gravatar module

--- core/app/classes/gravatar.rb
+++ core/app/classes/gravatar.rb
@@ -8,8 +8,12 @@ module Gravatar
   # - default: URL for fallback image if none is found or image exceeds rating.
   def gravatar_url(email,gravatar_options={})
     grav_url = 'https://secure.gravatar.com/avatar/'
-    grav_url << "#{Digest::MD5.new.update(email)}?"
+    grav_url << gravatar_hash(email) << "?"
     grav_url << (gravatar_options.slice(:rating,:size,:default).map{|k,v| "#{k}=#{v}"}.join "&")
     grav_url
+  end
+
+  def gravatar_hash(email)
+    "#{Digest::MD5.new.update(email.downcase)}"
   end
 end
