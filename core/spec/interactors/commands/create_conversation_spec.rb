f317b79acae7030a162e8f133caf61875dbbee14
Mark IJbema <markijbema@gmail.com>
2012-10-22 14:06:08 +0200

    added fact to a message

--- core/spec/interactors/commands/create_conversation_spec.rb
+++ core/spec/interactors/commands/create_conversation_spec.rb
@@ -2,34 +2,46 @@ require File.expand_path('../../../../app/interactors/commands/create_conversati
 
 describe Commands::CreateConversation do
 
+  let(:fact_id) {10}
+
   before do
     stub_const('Conversation', Class.new)
     stub_const('User', Class.new)
+    stub_const('Fact', Class.new)
   end
 
   it 'initializes correctly' do
-    command = Commands::CreateConversation.new ['username']
+    command = Commands::CreateConversation.new fact_id, ['username']
     command.should_not be_nil
   end
 
   it 'throws an error when recipient_usernames is not a list' do
-    expect { Commands::CreateConversation.new nil }.
+    expect { Commands::CreateConversation.new fact_id, nil }.
       to raise_error(RuntimeError, 'recipient_usernames should be a list')
   end
 
   it 'throws an error when recipient_usernames is an empty list' do
-    expect { Commands::CreateConversation.new [] }.
+    expect { Commands::CreateConversation.new fact_id, [] }.
       to raise_error(RuntimeError, 'recipient_usernames should not be empty')
   end
 
   describe '.execute' do
     it 'correctly' do
       username = 'username'
-      command = Commands::CreateConversation.new [username]
-      conversation = mock('conversation', :recipients => [])
+      command = Commands::CreateConversation.new fact_id, [username]
+      recipients = mock('recipients')
+      conversation = mock('conversation', :recipients => recipients)
       Conversation.should_receive(:new).and_return(conversation)
       user = mock()
       User.should_receive(:where).with(username: username).and_return([user])
+
+      fact_data_id = 'abc'
+      fact = mock('fact', data_id: fact_data_id)
+      Fact.should_receive(:[]).with(fact_id).and_return(fact)
+
+      recipients.should_receive(:<<).with(user)
+
+      conversation.should_receive(:fact_data_id=).with(fact_data_id)
       conversation.should_receive(:save)
 
       command.execute
