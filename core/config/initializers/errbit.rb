ae96dd3cce8078fe78f56500ae40e99a520a9134
Mark IJbema <markijbema@gmail.com>
2012-10-23 12:03:44 +0200

    some more gem file sanitation

--- core/config/initializers/errbit.rb
+++ core/config/initializers/errbit.rb
@@ -1,8 +1,10 @@
-Airbrake.configure do |config|
-   config.api_key     = '15ac8a10c7b9e8939aa5608e186d3fd8'
-   config.host        = 'factlink-errbit.herokuapp.com'
-   config.port        = 80
-   config.secure      = config.port == 443
+if defined? Airbrake
+  Airbrake.configure do |config|
+    config.api_key     = '15ac8a10c7b9e8939aa5608e186d3fd8'
+    config.host        = 'factlink-errbit.herokuapp.com'
+    config.port        = 80
+    config.secure      = config.port == 443
 
-   config.rescue_rake_exceptions = true # report exceptions that happen inside a rake task
+    config.rescue_rake_exceptions = true # report exceptions that happen inside a rake task
+  end
 end
--- core/config/initializers/resque.rb
+++ core/config/initializers/resque.rb
@@ -9,10 +9,11 @@ if defined?(Rails)
   Resque.inline = Rails.env.test?
 end
 
-
-require 'resque/failure/airbrake'
-Resque::Failure::Airbrake.configure do |config|
-  config.api_key = '15ac8a10c7b9e8939aa5608e186d3fd8'
-  config.secure = false # only set this to true if you are on a paid Airbrake plan
+if defined?(Airbrake)
+  require 'resque/failure/airbrake'
+  Resque::Failure::Airbrake.configure do |config|
+    config.api_key = '15ac8a10c7b9e8939aa5608e186d3fd8'
+    config.secure = false # only set this to true if you are on a paid Airbrake plan
+  end
+  Resque::Failure.backend = Resque::Failure::Airbrake
 end
-Resque::Failure.backend = Resque::Failure::Airbrake
