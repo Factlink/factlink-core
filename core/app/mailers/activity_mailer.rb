361c7ad23ae95d1431b621aca35487280de5e13d
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-10-30 17:19:19 +0100

    Set the correct layout for the email and set the correct globals

--- core/app/mailers/activity_mailer.rb
+++ core/app/mailers/activity_mailer.rb
@@ -1,9 +1,14 @@
 class ActivityMailer < ActionMailer::Base
   include Resque::Mailer
 
+  layout "email"
+
   default from: "Factlink <support@factlink.com>"
 
   def new_activity(user, activity)
+    @user = user
+    @activity = activity
+
     mail to: user.email, subject: 'New notification!'
   end
 end
