3f2418f7a672852337a68817ae608b087e3a22ff
Martijn Russchen <martijn@factlink.com>
2012-10-30 15:19:49 +0100

    Enabled messaging for all users

--- core/app/models/ability.rb
+++ core/app/models/ability.rb
@@ -120,7 +120,7 @@ class Ability
   end
 
   FEATURES = %w(pink_feedback_button authority_calculation_details notification_settings social_connect firefox_extension messaging)
-  GLOBAL_ENABLED_FEATURES = [:firefox_extension]
+  GLOBAL_ENABLED_FEATURES = [:firefox_extension, :messaging]
 
   def enable_features list
     @features ||= []
