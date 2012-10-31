a4c09f6222ac3137ae187a3da7eb1f1426d4e0f9
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-10-31 16:33:30 +0100

    Some gems require the port number to be stated in the :port key

--- core/app/helpers/url_helper.rb
+++ core/app/helpers/url_helper.rb
@@ -3,11 +3,14 @@ module UrlHelper
   # https://github.com/plataformatec/devise/wiki/How-To:-Send-emails-from-subdomains
   def set_mailer_url_options
     ActionMailer::Base.default_url_options[:host] = with_subdomain(request.subdomain)
+    ActionMailer::Base.default_url_options[:port] = request.port_string
+
+    puts ActionMailer::Base.default_url_options
   end
 
   def with_subdomain(subdomain)
     subdomain = (subdomain || "")
     subdomain += "." unless subdomain.empty?
-    [subdomain, request.domain, request.port_string].join
+    [subdomain, request.domain].join
   end
 end
