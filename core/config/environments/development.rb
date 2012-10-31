b5bb5f19fa0525688967fa19e101bf79453389fa
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-10-31 17:58:34 +0100

    IMPORTANT

--- core/config/environments/development.rb
+++ core/config/environments/development.rb
@@ -22,7 +22,8 @@ FactlinkUI::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # These options don't belong here since they are overwritten in the url_helper
+  # config.action_mailer.default_url_options = { :host => '', :port => '' }
 
   # Do not compress assets
   config.assets.compress = false
