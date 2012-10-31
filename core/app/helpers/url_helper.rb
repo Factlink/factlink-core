a9d4b7cbddedb91a88c977c4a8456994d8cd054b
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-10-31 16:46:21 +0100

    Debug statement should be removed

--- core/app/helpers/url_helper.rb
+++ core/app/helpers/url_helper.rb
@@ -4,8 +4,6 @@ module UrlHelper
   def set_mailer_url_options
     ActionMailer::Base.default_url_options[:host] = with_subdomain(request.subdomain)
     ActionMailer::Base.default_url_options[:port] = request.port_string
-
-    puts ActionMailer::Base.default_url_options
   end
 
   def with_subdomain(subdomain)
