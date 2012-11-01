e2057918ca90f3ea67f3a33dc8943b35a0dc3037
Mark IJbema <markijbema@gmail.com>
2012-11-01 14:21:55 +0100

    Test whether the query actually returns something

--- core/spec/interactors/queries/users_by_graph_user_ids_spec.rb
+++ core/spec/interactors/queries/users_by_graph_user_ids_spec.rb
@@ -27,16 +27,21 @@ describe Queries::UsersByGraphUserIds do
       graph_users = [mock(user_id: 4), mock(user_id: 5), mock(user_id: 6)]
       gu_ids = [1, 2, 3]
 
+      users = mock()
+
       gu_ids.each_with_index do |graph_user_id, index|
         GraphUser.should_receive(:[]).with(graph_user_id).and_return(graph_users[index])
       end
 
       should_receive_new_with_and_receive_execute(
-          Queries::UsersByIds, graph_users.map {|gu| gu.user_id }, {})
+          Queries::UsersByIds, graph_users.map {|gu| gu.user_id }, {}).
+          and_return(users)
+
 
       Queries::UsersByGraphUserIds.any_instance.stub(authorized?: true)
 
       result = Queries::UsersByGraphUserIds.execute(gu_ids)
+      expect(result).to eq(users)
     end
   end
 end
