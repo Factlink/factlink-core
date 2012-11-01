7f3d966e1cc25844717530cdf293c7069f7440c5
Jan Paul Posma <me@janpaulposma.nl>
2012-11-01 10:45:21 +0100

    Rewritten recalculate scripts to mimick rescue functionality with monit.

---
+++ core/bin/server/start_recalculate_using_monit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##
+# This script starts fact_graph:recalculate using Monit.
+
+for i in {1..3}
+do
+
+  echo "Starting fact_graph:recalculate, try #$i:"
+
+  sudo /usr/sbin/monit start recalculate
+
+  # Check the exit status
+  if [ "$?" -eq "0" ]; then
+
+    # Previous command was a success. I can safely exit
+    echo "Succes. Exiting now"
+    exit 0
+
+  else
+    # Failed to start
+    if [ "$i" -eq "3" ]; then
+      # Failed 3 times. Probabaly better to stop trying
+      echo "Failed 3 times. Exiting..."
+      exit 1
+    else
+      echo "Failed"
+    fi
+  fi
+done
+
+exit 0
