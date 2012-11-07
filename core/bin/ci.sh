333f6e4d5b508909b7154172a6f31d6f93421959
Mark IJbema <markijbema@gmail.com>
2012-11-07 08:25:25 +0100

    added timing to the ci parts

--- core/bin/ci.sh
+++ core/bin/ci.sh
@@ -6,6 +6,8 @@ cd ..
 
 for action in bin/ci/*.sh; do
   banner $action;
-  /bin/bash $action
+  time=`time /bin/bash $action`
+  echo "Action $action timing:"
+  echo "$time"
   if [ "$?" -gt "0" ] ; then exit 1; fi
 done
