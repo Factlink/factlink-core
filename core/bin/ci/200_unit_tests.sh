cbafb4e2d641f811a01f82806a9ba4912e62fb37
Mark IJbema <markijbema@gmail.com>
2012-11-08 13:52:18 +0100

    unit-specs are so fast, no need for running them separately

--- core/bin/ci/200_unit_tests.sh
+++ core/bin/ci/200_unit_tests.sh
@@ -4,9 +4,7 @@ echo "Running unit tests"
 OUTPUTFILE=$(tempfile)
 
 
-for dir in `ls spec-unit`; do
-  rspec "spec-unit/$dir" | tee "$OUTPUTFILE"
-  cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
-done
+rspec spec-unit | tee "$OUTPUTFILE"
+cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
 
 exit
