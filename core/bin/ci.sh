2a86b10161ad79d3e6fbd6f03ab3e576e2a98869
Mark IJbema <markijbema@gmail.com>
2012-11-08 10:56:03 +0100

    don't try to be to smart mark ;)

--- core/bin/ci.sh
+++ core/bin/ci.sh
@@ -6,8 +6,6 @@ cd ..
 
 for action in bin/ci/*.sh; do
   banner $action;
-  time=`time /bin/bash $action`
-  echo "Action $action timing:"
-  echo "$time"
+  time /bin/bash "$action"
   if [ "$?" -gt "0" ] ; then exit 1; fi
 done
