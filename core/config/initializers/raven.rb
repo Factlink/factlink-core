c884cd2824bd241d970da91b90fd17bd81300794
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-08 14:37:36 +0100

    Enabled raven client for sentry

---
+++ core/config/initializers/raven.rb
@@ -0,0 +1,8 @@
+require 'raven'
+
+sentry_conf = YAML::load_file(Rails.root.join('config/sentry.yml'))[Rails.env]['sentry']
+
+Raven.configure do |config|
+  config.dsn = "http://#{sentry_conf[:public]}:#{sentry_conf[:secret]}@example.com/#{sentry_conf[:project_id]}"
+  config.processors = [Raven::Processor::SanitizeData]
+end
