dc554f13767b622166093275fdce3a4e5fd27551
Jan Paul Posma <me@janpaulposma.nl>
2012-11-12 11:04:18 +0100

    Quick fix for stopping recalculate and resque.

--- core/bin/server/stop_resque.sh
+++ core/bin/server/stop_resque.sh
@@ -1,1 +1,5 @@
-/bin/sh -c 'kill -9 `cat /home/deploy/resque.pid` && rm -f /home/deploy/resque.pid'
+#!/bin/bash
+
+if [ -f /home/deploy/resque.pid ]; then
+  kill -9 `cat /home/deploy/resque.pid` && rm -f /home/deploy/resque.pid
+fi
