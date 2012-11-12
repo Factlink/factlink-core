2f4308865dc5379b7834183ce9e26a676bd56058
Mark IJbema <markijbema@gmail.com>
2012-11-12 07:50:30 +0100

    started moving from mustache rails to jbuilder, so we can more easily remove implicit queries and extract out the explicit queries

--- core/app/views/channels/_channel.rb
+++ core/app/views/channels/_channel.rb
@@ -5,22 +5,6 @@ module Channels
       self[:user]||= self[:channel].created_by.user
     end
 
-    def link
-      if discover_stream?
-        @link||="/#{self[:user].username}/channels/#{id}/activities"
-      else
-        @link||="/#{self[:user].username}/channels/#{id}"
-      end
-    end
-
-    def edit_link
-      link + "/edit"
-    end
-
-    def created_by_authority
-      topic and (Authority.from(topic , for: self[:channel].created_by ).to_s.to_f + 1.0).to_s
-    end
-
     def add_channel_url
       '/' + self[:user].username + '/channels/new'
     end
@@ -70,63 +54,61 @@ module Channels
       self[:user] == current_user
     end
 
-    def discover_stream?
-      is_all && is_mine
-    end
 
-    def created_by
-      json = Jbuilder.new
-      json.id self[:user].id
-      json.username self[:user].username
-      json.avatar image_tag(self[:user].avatar_url(size: 32), title: self[:user].username, alt: self[:user].username, width: 32)
-      json.all_channel_id self[:user].graph_user.stream_id
-      json.attributes!
-    end
-
-    def new_facts
-      (unread_count != 0) && self[:user] == current_user
-    end
 
     def id
       self[:channel].id
     end
 
-    def slug_title
-      self[:channel].slug_title
-    end
 
-    def containing_channel_ids
-      self[:channel].containing_channels_for(current_graph_user).ids
-    end
 
-    def created_by_id
-      self[:channel].created_by_id
-    end
+    def to_hash
+      channel = self[:channel]
+      user = self[:user]
 
-    def editable?
-      current_graph_user.id == created_by_id && self[:channel].editable?
-    end
+      json = Jbuilder.new
 
-    def followable?
-      current_graph_user.id != created_by_id && is_normal
-    end
-
-    def inspectable?
-       self[:channel].inspectable?
-    end
-
-    def unread_count
-      @unread_count ||= is_normal ? self[:channel].unread_count : 0
-    end
-
-    private
-      def topic
-        @topic ||= if is_normal
-                     self[:channel].topic
-                   else
-                     nil
-                   end
+      if is_normal
+        topic = channel.topic
+        json.created_by_authority(Authority.from(topic , for: channel.created_by ).to_s.to_f + 1.0).to_s
       end
+
+      json.created_by do |j|
+        j.id self[:user].id
+        j.username self[:user].username
+        j.avatar image_tag(self[:user].avatar_url(size: 32), title: self[:user].username, alt: self[:user].username, width: 32)
+        j.all_channel_id self[:user].graph_user.stream_id
+      end
+
+      json.slug_title channel.slug_title
+
+      is_discover_stream = is_all && is_mine
+      json.discover_stream? is_discover_stream
+
+      link = if is_discover_stream
+              "/#{user.username}/channels/#{id}/activities"
+             else
+              "/#{user.username}/channels/#{id}"
+             end
+      json.link link
+      json.edit_link link + "/edit"
+
+
+      created_by_id = channel.created_by_id
+      json.created_by_id created_by_id
+
+      json.inspectable? channel.inspectable?
+      json.followable?  current_graph_user.id != created_by_id && is_normal
+      json.editable?    current_graph_user.id == created_by_id && channel.editable?
+
+      unread_count = is_normal ? channel.unread_count : 0
+      json.unread_count unread_count
+      json.new_facts( (unread_count != 0) && user == current_user )
+
+      json.containing_channel_ids channel.containing_channels_for(current_graph_user).ids
+
+      super.merge json.attributes!
+    end
 
   end
 end
