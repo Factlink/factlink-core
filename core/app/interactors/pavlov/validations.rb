3329eeb8e94898c8b50f83c0a4d58f1feb9b5251
Jan Deelstra <jan@deelstra.org>
2012-11-13 13:37:11 +0100

    Added some validations to pavlov
    Added validate integer
    Added validate in set

--- core/app/interactors/pavlov/validations.rb
+++ core/app/interactors/pavlov/validations.rb
@@ -7,5 +7,13 @@ module Pavlov
     def validate_regex param_name, param, regex, message
       raise Pavlov::ValidationError, "#{param_name.to_s} #{message}" unless regex.match param
     end
+
+    def validate_integer param_name, param
+      raise Pavlov::ValidationError, "#{param_name.to_s} should be a integer." unless param.is_a? Integer
+    end
+
+    def validate_in_set param_name, param, set
+      raise Pavlov::ValidationError, "#{param_name.to_s} should be on of these values: #{set.inspect}." unless set.include? param
+    end
   end
 end
