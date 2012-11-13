d83f981465e72d8ba48e878646f6a11422386456
Jan Deelstra <jan@deelstra.org>
2012-11-13 14:27:19 +0100

    Added fact believers interactor

---
+++ core/app/interactors/fact_believers.rb
@@ -0,0 +1,17 @@
+require_relative 'pavlov'
+
+module Interactors
+  class FactBelievers
+    include Pavlov::Interactor
+
+    arguments :fact_id, :skip, :take
+
+    def execute
+      query :fact_interacting_users, @fact_id, @skip, @take, 'believes'
+    end
+
+    def authorized?
+      @options[:current_user]
+    end
+  end
+end
---
+++ core/spec-unit/unit/interactors/fact_believers_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../../../../app/interactors/fact_believers.rb', __FILE__)
+
+describe Interactors::FactBelievers do
+  it 'initializes correctly' do
+    interactor = Interactors::FactBelievers.new 1, 0, 3, current_user: mock
+    interactor.should_not be_nil
+  end
+
+  it 'gives an authorized error when there isn''t a logged in user' do
+    expect { Interactors::FactBelievers.new 1, 0, 3 }.
+      to raise_error(Pavlov::AccessDenied, 'Unauthorized')
+  end
+
+  describe '.execute' do
+    before do
+      stub_const('Queries', Class.new)
+      stub_const('Queries::FactInteractingUsers', Class.new)
+    end
+
+    it 'correctly' do
+      fact_id = 1
+      skip = 0
+      take = 0
+      user = mock()
+      query = mock()
+      u1 = mock()
+      Queries::FactInteractingUsers.should_receive(:new).with(fact_id, skip, take, 'believes', current_user: user).and_return(query)
+      query.should_receive(:execute).and_return({users: [u1], total: 1})
+
+      results = Interactors::FactBelievers.perform fact_id, skip, take, current_user: user
+
+      results[:total].should eq 1
+      results[:users].should eq [u1]
+    end
+  end
+end
