4ad844606189ffce8f45549f04fa456e9c8e1e46
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-13 11:54:16 +0100

    Updated deploy script to only deploy when a DEPLOY_SERVER is set

--- core/bin/ci/900_deploy.sh
+++ core/bin/ci/900_deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "Deploying..."
+if [[ -z "$DEPLOY_SERVER" ]]; then
+  echo "Deploying..."
 
-echo "deploying to $DEPLOY_SERVER"
-bundle exec cap -S "branch=$GIT_COMMIT" $DEPLOY_SERVER deploy || exit 1
-echo "deployed to $DEPLOY_SERVER"
+  echo "deploying to $DEPLOY_SERVER"
+  bundle exec cap -S "branch=$GIT_COMMIT" $DEPLOY_SERVER deploy || exit 1
+  echo "deployed to $DEPLOY_SERVER"
+else
+  echo "No DEPLOY_SERVER found, not deploying."
+fi
+
 exit
