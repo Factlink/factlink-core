b5aa911aea37feefa4d9703710cb5030b8e10bac
Mark IJbema <markijbema@gmail.com>
2012-11-13 15:11:45 +0100

    optimised topics for channels

--- core/app/interactors/queries/topics_for_channels.rb
+++ core/app/interactors/queries/topics_for_channels.rb
@@ -5,9 +5,11 @@ module Queries
     arguments :channels
 
     def execute
-      @channels.map do |ch|
-        Topic.by_slug ch.slug_title
-      end
+      Topic.any_in(slug_title: slug_titles)
+    end
+
+    def slug_titles
+      @channels.map {|ch| ch.slug_title}
     end
 
     def authorized?
