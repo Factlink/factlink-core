a15050a0d151339fdb3d690fc090453d2e3bb692
Mark IJbema <markijbema@gmail.com>
2012-11-14 12:09:08 +0100

    added some comments on performance of the query

--- core/app/interactors/queries/creator_authorities_for_channels.rb
+++ core/app/interactors/queries/creator_authorities_for_channels.rb
@@ -1,4 +1,14 @@
 module Queries
+  # Given a list of channels this query returns a list of
+  # authorities, which match in position
+  #
+  # Performance
+  #
+  # We make sure to retrieve all the topics needed for this
+  # query at once, but don't cache the authorities. In the usecase
+  # which demanded this query, it never happens that we have
+  # multiple channels with the same topic, so optimization wouldn't
+  # actually optimize anything
   class CreatorAuthoritiesForChannels
     include Pavlov::Query
     arguments :channels
