f19268ba9c20f0f755054d5e5346907472bd404b
Mark IJbema <markijbema@gmail.com>
2012-11-15 13:23:25 +0100

    added some more profiling for new relic

--- core/app/interactors/queries/containing_channel_ids_for_channel_and_user.rb
+++ core/app/interactors/queries/containing_channel_ids_for_channel_and_user.rb
@@ -4,10 +4,19 @@ module Queries
     arguments :channel_id, :graph_user_id
 
     def execute
-      current_graph_user  = GraphUser[@graph_user_id]
-      graph_user_channels = Channel.active_channels_for(current_graph_user)
-      containing_channels = Channel[@channel_id].containing_channels
+      union_ids graph_user_channels, containing_channels
+    end
 
+    def graph_user_channels
+      current_graph_user  = GraphUser[@graph_user_id]
+      Channel.active_channels_for(current_graph_user)
+    end
+
+    def containing_channels
+      Channel[@channel_id].containing_channels
+    end
+
+    def union_ids channels1, channels2
       (graph_user_channels & containing_channels).ids
     end
   end
