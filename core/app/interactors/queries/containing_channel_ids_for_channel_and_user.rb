7e455f0a5c7c1717e72611d4a3d55d8361f27425
Mark IJbema <markijbema@gmail.com>
2012-11-15 17:23:58 +0100

    optimised query for a specific, but very often happening, case

--- core/app/interactors/queries/containing_channel_ids_for_channel_and_user.rb
+++ core/app/interactors/queries/containing_channel_ids_for_channel_and_user.rb
@@ -1,22 +1,35 @@
 module Queries
+  # This query returns the list of channels for the
+  # graph_user with graph_user_id which contain/follow
+  # the channel with channel_id
+  #
+  # Since we cannot follow channels of ourselves in our own
+  # channel we can directly return an empty list if we call
+  # this for a channel of the graph_user
   class ContainingChannelIdsForChannelAndUser
     include Pavlov::Query
     arguments :channel_id, :graph_user_id
 
     def execute
-      union_ids graph_user_channels, containing_channels
+      return [] if channel.created_by_id == @graph_user_id
+
+      intersect_ids graph_user_channels, containing_channels
     end
 
     def graph_user_channels
-      current_graph_user  = GraphUser[@graph_user_id]
-      Channel.active_channels_for(current_graph_user)
+      graph_user = GraphUser[@graph_user_id]
+      Channel.active_channels_for(graph_user)
     end
 
     def containing_channels
-      Channel[@channel_id].containing_channels
+      channel.containing_channels
     end
 
-    def union_ids channels1, channels2
+    def channel
+      @channel ||= Channel[@channel_id]
+    end
+
+    def intersect_ids channels1, channels2
       (channels1 & channels2).ids
     end
   end
