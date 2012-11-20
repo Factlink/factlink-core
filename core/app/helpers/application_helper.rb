bd0108f7b1aaa69492f780a5ce086da787d2ae73
Mark IJbema <markijbema@gmail.com>
2012-11-20 08:24:51 +0100

    cleaned up return logic a little (ruby does implicit returns)

--- core/app/helpers/application_helper.rb
+++ core/app/helpers/application_helper.rb
@@ -38,31 +38,27 @@ module ApplicationHelper
   end
 
   def linkedin_icon_for_team_member handle, team_member
-    if handle
-      url = handle.match(/^http/) ? handle : "http://www.linkedin.com/in/#{handle}"
-      return link_to content_tag(:i, "Linkedin"), url, class: "ico linkedin", target: "_blank", title: "#{team_member}'s LinkedIn"
-    end
+    url = handle.match(/^http/) ? handle : "http://www.linkedin.com/in/#{handle}"
+    link_to content_tag(:i, "Linkedin"), url, class: "ico linkedin", target: "_blank", title: "#{team_member}'s LinkedIn"
   end
 
   def twitter_icon_for_team_member handle, team_member
-    if handle
-      twitter_url = handle.match(/^http/) ? handle : "http://www.twitter.com/#{handle}"
-      return link_to content_tag(:i, "Twitter"), twitter_url, class: "ico twitter", target: "_blank", title: "#{team_member}'s Twitter"
-    end
+    twitter_url = handle.match(/^http/) ? handle : "http://www.twitter.com/#{handle}"
+    link_to content_tag(:i, "Twitter"), twitter_url, class: "ico twitter", target: "_blank", title: "#{team_member}'s Twitter"
   end
 
   def team_photo_tag photo, name, linkedin=nil, twitter=nil
     html = image_tag "team/#{photo}.jpg", alt: name, class: "img-circle team-photo", rel: "tooltip", title: name, width: 135, height: 135
-    html += linkedin_icon_for_team_member linkedin, name
-    html += twitter_icon_for_team_member twitter, name
-    html = content_tag "div", html, class: "span3 team-member"
-    return html
+    html += linkedin_icon_for_team_member linkedin, name if linkedin
+    html += twitter_icon_for_team_member twitter, name   if twitter
+
+    content_tag "div", html, class: "span3 team-member"
   end
 
   def advisor_photo_tag photo, name, linkedin=nil
     html = image_tag "team/#{photo}.jpg", alt: name, class: "img-circle team-photo", rel: "tooltip", title: name, width: 80, height: 80
-    html = content_tag "div", html, class: "span2 team-member"
-    return html
+
+    content_tag "div", html, class: "span2 team-member"
   end
 
   def show_active_step step_in_signup_proces, step
