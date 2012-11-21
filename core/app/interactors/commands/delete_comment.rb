c0945193e469cbdf04ca053aef333e7f98547e33
Jan Deelstra <jan@deelstra.org>
2012-11-21 15:18:21 +0100

    Bugfix all commands and queries

--- core/app/interactors/commands/delete_comment.rb
+++ core/app/interactors/commands/delete_comment.rb
@@ -9,13 +9,13 @@ module Commands
     def execute
       comment = Comment.find(@comment_id)
       user = User.find(@user_id)
-      raise_unauthorized unless comment.created_by_id == @user_id
+      raise_unauthorized unless comment.created_by_id.to_s == @user_id
       comment.delete
     end
 
     def validate
       validate_hexadecimal_string :comment_id, @comment_id
-      validate_integer :user_id, @user_id
+      validate_hexadecimal_string :user_id, @user_id
     end
   end
 end
--- core/app/interactors/queries/get_comments.rb
+++ core/app/interactors/queries/get_comments.rb
@@ -7,12 +7,17 @@ module Queries
     arguments :fact_id, :opinion
 
     def validate
-      validate_hexadecimal_string :fact_id, @fact_id
+      validate_integer :fact_id, @fact_id
       validate_in_set  :opinion, @opinion, ['believes', 'disbelieves', 'doubts']
     end
 
     def execute
-      Comment.find({fact_data_id: @fact_id, opinion: @opinion})
+      fact_data_id = Fact[@fact_id].data_id
+      comments = Comment.where({fact_data_id: fact_data_id, opinion: @opinion}).to_a
+
+      comments.map do |comment|
+        KillObject.comment comment
+      end
     end
   end
 end
