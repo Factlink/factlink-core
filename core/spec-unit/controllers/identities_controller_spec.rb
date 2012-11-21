4fbf73024a0c3fe759c98f01680a6d8faa3efe0f
Jan Deelstra <jan@deelstra.org>
2012-11-21 10:48:28 +0100

    Moved controller unit test to spec-unit
    This is a unit test, maybe it's not that fast, but it runs on my
    machine around 0.01 secs. And that's fine by me.

--- core/spec-unit/controllers/identities_controller_spec.rb
+++ core/spec-unit/controllers/identities_controller_spec.rb
@@ -1,5 +1,3 @@
-# this test isn't in unit-spec because the identitiescontroller actually
-# requires the applicationcontroller, making this test kind-of slow
 require_relative '../../app/controllers/identities_controller.rb'
 
 describe IdentitiesController do
