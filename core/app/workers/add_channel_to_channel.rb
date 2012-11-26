fc682e417d90abf42ceb1f9ae576d103782cd9ee
Jan Paul Posma <me@janpaulposma.nl>
2012-11-26 13:35:30 +0100

    Adds no more than 13 facts to a channel when starting to follow another channel. Should bring luck.

--- core/app/workers/add_channel_to_channel.rb
+++ core/app/workers/add_channel_to_channel.rb
@@ -1,10 +1,14 @@
 class AddChannelToChannel
+  NUM_FACTS = 13
+
   @queue = :channel_operations
 
   def self.perform(subchannel_id, channel_id)
     subchannel = Channel[subchannel_id]
     channel = Channel[channel_id]
-    subchannel.sorted_cached_facts.each do |fact|
+
+    latest_facts = subchannel.sorted_cached_facts.below('inf', count: NUM_FACTS)
+    latest_facts.each do |fact|
       Resque.enqueue(AddFactToChannel, fact.id, channel.id)
     end
   end
--- core/spec/workers/add_channel_to_channel_spec.rb
+++ core/spec/workers/add_channel_to_channel_spec.rb
@@ -5,13 +5,31 @@ describe AddChannelToChannel do
     before do
       @ch = create :channel
       @sub_ch = create :channel
-      @f = create :fact
-
-      @sub_ch.sorted_cached_facts << @f
     end
 
     it "should add the fact to the cached facts of the super channel using Resque" do
-      Resque.should_receive(:enqueue).with(AddFactToChannel, @f.id, @ch.id)
+      fact = create :fact
+      @sub_ch.sorted_cached_facts << fact
+
+      Resque.should_receive(:enqueue).with(AddFactToChannel, fact.id, @ch.id)
+      AddChannelToChannel.perform @sub_ch.id, @ch.id
+    end
+
+    it "should only add NUM_FACTS facts to the super channel" do
+      oldest_fact = create :fact
+      @sub_ch.sorted_cached_facts << oldest_fact
+
+      facts = []
+
+      for i in 1..AddChannelToChannel::NUM_FACTS
+        facts[i] = create :fact
+        @sub_ch.sorted_cached_facts << facts[i]
+      end
+
+      for i in 1..AddChannelToChannel::NUM_FACTS
+        Resque.should_receive(:enqueue).with(AddFactToChannel, facts[i].id, @ch.id)
+      end
+
       AddChannelToChannel.perform @sub_ch.id, @ch.id
     end
   end
