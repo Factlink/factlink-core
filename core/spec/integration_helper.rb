0d405996de86401052a365e391a9818adad2e9e0
tomdev <tom@north26.nl>
2012-11-26 16:05:25 +0100

    Include Acceptance module in integration tests

--- core/spec/integration_helper.rb
+++ core/spec/integration_helper.rb
@@ -8,6 +8,9 @@ require 'capybara/email/rspec'
 require 'headless'
 require 'database_cleaner'
 
+require File.expand_path("../support/acceptance", __FILE__)
+include Acceptance
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
