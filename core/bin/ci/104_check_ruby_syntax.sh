09841a62dad57310d3da4e9dabf26e043965503e
Mark IJbema <markijbema@gmail.com>
2012-11-27 10:25:03 +0100

    hopefully fixed ruby lint check

--- core/bin/ci/104_check_ruby_syntax.sh
+++ core/bin/ci/104_check_ruby_syntax.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc | grep -v 'Syntax OK' | wc -l
+find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK' | wc -l
 
 
-TODO=`find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc | grep -v 'Syntax OK' | wc -l`
+TODO=`find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK' | wc -l`
 
 if [ "$TODO" -gt "0" ]; then
   echo "There are warnings in the code"
