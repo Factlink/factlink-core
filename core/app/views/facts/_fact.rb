61dfb842d4c1a31b839e7678e4b8104e901a419e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-27 16:20:30 +0100

    Fixed syntax issue

--- core/app/views/facts/_fact.rb
+++ core/app/views/facts/_fact.rb
@@ -72,15 +72,15 @@ module Facts
     def created_by
       user = self[:fact].created_by.user
       authority = Authority.on(self[:fact], for: user.graph_user).to_f + 1.0
+      authority_for_subject = {
+        authority: NumberFormatter.new(authority).as_authority,
+        id: self[:fact].id
+      }
 
       json = Jbuilder.new
       json.username user.username
       json.avatar image_tag(user.avatar_url(size: 32), title: user.username, alt: user.username, width: 32)
-      json.authority_for_subject
-        {
-          authority: NumberFormatter.new(authority).as_authority,
-          id: self[:fact].id,
-        }
+      json.authority_for_subject authority_for_subject
       json.url user_profile_path(user)
 
       json.attributes!
