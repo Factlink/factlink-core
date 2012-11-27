1b5c6b27f5418688738dac9c5e5869c9181d0430
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-27 11:46:13 +0100

    This should work better according to @markijbema

--- core/spec/integration_helper.rb
+++ core/spec/integration_helper.rb
@@ -8,9 +8,6 @@ require 'capybara/email/rspec'
 require 'headless'
 require 'database_cleaner'
 
-require File.expand_path("../support/acceptance", __FILE__)
-include Acceptance
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -23,6 +20,7 @@ RSpec.configure do |config|
   config.pattern = "**/*_spec.rb"
   config.mock_with :rspec
 
+  config.include Acceptance
   config.include FactoryGirl::Syntax::Methods
   config.include ControllerMethods, type: :controller
   config.include ScreenshotTest
