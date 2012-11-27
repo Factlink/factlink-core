8a49141557daa85c38fd316543e75394a2b33887
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-27 15:10:21 +0100

    Made Opinion.friendly_authority use NumberFormatter and synced the tests

--- core/app/ohm-models/opinion.rb
+++ core/app/ohm-models/opinion.rb
@@ -133,13 +133,7 @@ class Opinion < OurOhm
   end
 
   def friendly_authority
-    if a < 10
-      sprintf('%.1f', a)
-    elsif a >= 1000
-      sprintf('%.0fk', a.div(1000))
-    else
-      sprintf('%.0f', a)
-    end
+    NumberFormatter.new(a).as_authority
   end
 
   def self.types
--- core/spec-unit/classes/number_formatter_spec.rb
+++ core/spec-unit/classes/number_formatter_spec.rb
@@ -18,9 +18,12 @@ describe NumberFormatter do
         expect(NumberFormatter.new(number).as_authority).to eq "3.3"
       end
 
-      it 'should round digits above 999 to 1k' do
+      it 'should round digits above 999 and below 2000 to 1k' do
         number = 1000
         expect(NumberFormatter.new(number).as_authority).to eq "1k"
+
+        other_number = 1999
+        expect(NumberFormatter.new(other_number).as_authority).to eq "1k"
       end
 
       it 'should not show decimals when the number is > 10' do
@@ -40,9 +43,12 @@ describe NumberFormatter do
         expect(NumberFormatter.new(number).as_authority).to eq "-3.3"
       end
 
-      it "should round digits below -999 to -1k" do
+      it "should round digits below -999 and above to -1k" do
         number = -1000
         expect(NumberFormatter.new(number).as_authority).to eq "-1k"
+
+        other_number = -1999
+        expect(NumberFormatter.new(other_number).as_authority).to eq "-1k"
       end
 
       it "should not show decimals when the number is < -10" do
