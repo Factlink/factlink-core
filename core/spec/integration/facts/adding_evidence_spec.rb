10d60cdb0e48d2c0db9a7032a2f3fe413a048be2
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-11-28 17:33:05 +0100

    Let's see if this works..

--- core/spec/integration/facts/adding_evidence_spec.rb
+++ core/spec/integration/facts/adding_evidence_spec.rb
@@ -32,7 +32,6 @@ feature "adding evidence to a fact", type: :request do
   end
 
   scenario "we can click on evidence to go to the page of that factlink" do
-    pending "Only breaks on the CI"
     go_to_discussion_page_of factlink
 
     supporting_factlink = backend_create_fact
@@ -44,6 +43,8 @@ feature "adding evidence to a fact", type: :request do
         page.find('span', text: supporting_factlink.to_s).click
       end
     end
+
+    sleep 2
 
     page.find('.fact-view .fact-body .js-displaystring', text: supporting_factlink.to_s)
   end
