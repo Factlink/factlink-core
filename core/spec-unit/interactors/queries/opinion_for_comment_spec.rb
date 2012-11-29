b1f45c952cef5005cc5629b88f9228f9349fd2cf
Mark IJbema <markijbema@gmail.com>
2012-11-29 14:19:00 +0100

    Implemented OpinionForComment

--- core/spec-unit/interactors/queries/opinion_for_comment_spec.rb
+++ core/spec-unit/interactors/queries/opinion_for_comment_spec.rb
@@ -5,10 +5,14 @@ require_relative '../../../app/interactors/queries/opinion_for_comment.rb'
 describe Queries::OpinionForComment do
   include PavlovSupport
 
+  before do
+    stub_classes 'Believable::Comment', 'UserOpinionCalculation',
+                 'Authority'
+  end
+
   it 'initializes' do
     Queries::OpinionForComment.new '1', mock
   end
-
 
   describe 'validation' do
     let(:subject_class) {Queries::OpinionForComment}
@@ -17,4 +21,66 @@ describe Queries::OpinionForComment do
         to fail_validation 'comment_id should be an hexadecimal string.'
     end
   end
+
+  describe '.execute' do
+    it "returns the opinion the calculator calculates" do
+      opinion = mock
+      calculator = mock(:calculator, opinion: opinion)
+      query = Queries::OpinionForComment.new 'a1', mock
+      query.stub calculator: calculator
+
+      expect(query.execute).to eq opinion
+    end
+  end
+
+  describe 'calculator' do
+    it "calls the UserOpinionCalculation with the authority_for" do
+      believable = mock
+      authority_block = mock
+      calculator = mock
+
+      query = Queries::OpinionForComment.new '1', mock
+
+      query.stub believable: believable,
+                 authority_for: authority_block
+
+      UserOpinionCalculation.should_receive(:new)
+                            .with(believable, authority_block)
+                            .and_return(calculator)
+
+      expect(query.calculator).to eq calculator
+    end
+  end
+
+  describe 'authority_for' do
+    it "returns a block which calculates authority for the user" do
+      fact = mock
+      graph_user = mock
+      authority = mock
+
+      query = Queries::OpinionForComment.new '1', fact
+      auth = query.authority_for
+
+      Authority.should_receive(:on)
+               .with(fact, for: graph_user)
+               .and_return(authority)
+
+      expect(auth.call(graph_user)).to eq authority
+    end
+  end
+
+  describe '.believable' do
+    it "returns the Believable::Comment for this comment" do
+      id = 'a1'
+      believable = mock
+      query = Queries::OpinionForComment.new id, mock
+
+      Believable::Comment.should_receive(:new)
+                       .with(id)
+                       .and_return(believable)
+
+      expect(query.believable).to eq believable
+    end
+  end
+
 end
