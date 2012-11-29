68ce888cd151b2bcead9c09ee1d1731067cb0250
Mark IJbema <markijbema@gmail.com>
2012-11-29 10:13:48 +0100

    fixed indenting (2 spaces)

--- core/app/interactors/pavlov.rb
+++ core/app/interactors/pavlov.rb
@@ -1,26 +1,24 @@
 module Pavlov
-    # this method is also available as constantize in Rails,
-    # but we want to be able to write classes and/or tests without Rails
-    def self.get_class_by_string classname
-      classname.split('::').inject(Kernel) {|x,y|x.const_get(y)}
-    end
+  # this method is also available as constantize in Rails,
+  # but we want to be able to write classes and/or tests without Rails
+  def self.get_class_by_string classname
+    classname.split('::').inject(Kernel) {|x,y|x.const_get(y)}
+  end
 
-    def self.command command_name, *args
-      klass = get_class_by_string("Commands::"+command_name.to_s.camelize)
-      klass.new(*args).execute
-    end
+  def self.command command_name, *args
+    klass = get_class_by_string("Commands::"+command_name.to_s.camelize)
+    klass.new(*args).execute
+  end
 
-    def self.interactor command_name, *args
-      klass = get_class_by_string(command_name.to_s.camelize + "Interactor")
-      klass.new(*args).execute
-    end
+  def self.interactor command_name, *args
+    klass = get_class_by_string(command_name.to_s.camelize + "Interactor")
+    klass.new(*args).execute
+  end
 
-    def self.query command_name, *args
-      klass = get_class_by_string("Queries::"+command_name.to_s.camelize)
-      klass.new(*args).execute
-    end
-
-
+  def self.query command_name, *args
+    klass = get_class_by_string("Queries::"+command_name.to_s.camelize)
+    klass.new(*args).execute
+  end
 end
 
 require_relative 'pavlov/helpers.rb'
