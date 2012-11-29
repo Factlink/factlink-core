f81c8a47b2f7aa9ba0608657628263752aae40a7
tomdev <tom@north26.nl>
2012-11-29 12:04:20 +0100

    Implemented `Interactors::Comments::SetOpinion`

--- core/app/interactors/interactors/comments/set_opinion.rb
+++ core/app/interactors/interactors/comments/set_opinion.rb
@@ -8,12 +8,25 @@ module Interactors
       arguments :comment_id, :opinion
 
       def execute
-        command :set_opinion, @comment_id, @opinion
+        command 'comment/set_opinion', @comment_id, @opinion
+
+        comment.authority = authority_of comment
+        comment
       end
 
       def validate
         validate_hexadecimal_string :comment_id, @comment_id
         validate_in_set :opinion, @opinion, ['believes', 'disbelieves', 'doubts']
+      end
+
+      def comment
+        @comment ||= Comment.find(@comment_id)
+      end
+
+      def authority_of comment
+        fact = comment.fact_data.fact
+        graph_user = comment.created_by.graph_user
+        query :authority_on_fact_for, fact, graph_user
       end
 
       def authorized?
--- core/spec-unit/interactors/interactors/comments/set_opinion_spec.rb
+++ core/spec-unit/interactors/interactors/comments/set_opinion_spec.rb
@@ -3,6 +3,8 @@ require_relative '../../../../app/interactors/interactors/comments/set_opinion.r
 
 
 describe Interactors::Comments::SetOpinion do
+  include PavlovSupport
+
   it 'initializes correctly' do
     user = mock()
     interactor = Interactors::Comments::SetOpinion.new '1', 'believes', current_user: user
@@ -25,5 +27,59 @@ describe Interactors::Comments::SetOpinion do
   end
 
   describe '.execute' do
+    before do
+      stub_classes 'Commands::Comments::SetOpinion'
+    end
+
+    it 'does stuff' do
+      opinion = 'believes'
+      user = mock()
+      comment = mock(id: '123abc456def')
+      authority_string = '1.0'
+
+      interactor = Interactors::Comments::SetOpinion.new comment.id, opinion, current_user: user
+
+      interactor.should_receive(:command).with('comment/set_opinion', comment.id, opinion)
+
+      interactor.should_receive(:comment).any_number_of_times.and_return(comment)
+      interactor.should_receive(:authority_of).with(comment).and_return(authority_string)
+      comment.should_receive(:authority=).with(authority_string)
+
+      interactor.execute
+    end
+  end
+
+  describe '.comment' do
+    before do
+      stub_classes 'Comment'
+    end
+
+    it 'returns a comment' do
+      comment = mock(id: '123abc')
+      opinion = 'believes'
+      user    = mock()
+
+      Comment.should_receive(:find).with(comment.id).and_return(comment)
+
+      interactor = Interactors::Comments::SetOpinion.new comment.id, opinion, current_user: user
+      interactor.comment.should eq comment
+      interactor.comment.should eq comment # must cache
+    end
+  end
+
+  describe '.authority_of' do
+    it 'retrieves the authority' do
+      fact_data = mock( fact: mock() )
+      user      = mock( graph_user: mock() )
+      comment   = mock(id: '123abc', fact_data: fact_data, created_by: user )
+      opinion   = 'believes'
+      authority = '2.0'
+
+      interactor = Interactors::Comments::SetOpinion.new comment.id, opinion, current_user: user
+
+      interactor.should_receive(:query).with(:authority_on_fact_for, comment.fact_data.fact, comment.created_by.graph_user).and_return(authority)
+
+      interactor.authority_of(comment).should eq authority
+    end
   end
 end
