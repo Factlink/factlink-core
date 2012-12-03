1a026e2be9f261570dc4fa1982c577c42e3f0304
tomdev <tom@north26.nl>
2012-12-03 13:12:40 +0100

    Added migration to change :opinion to :type on Comment

---
+++ core/db/migrate/20121203120621_change_opinion_to_type_on_comment.rb
@@ -0,0 +1,19 @@
+class ChangeOpinionToTypeOnComment < Mongoid::Migration
+  def self.up
+
+    Comment.all.each do |comment|
+      comment.type = comment.opinion
+      # comment.unset :opinion
+      comment.save
+    end
+
+  end
+
+  def self.down
+    Comment.all.each do |comment|
+      comment.opinion = comment.type
+      comment.unset :type
+      comment.save
+    end
+  end
+end
