6cfd640c5103da90279cd3475ad38d027e71a3f4
Jan Deelstra <jan@deelstra.org>
2012-12-03 16:59:51 +0100

    Removing old functionality
    Removed most of it excluding css.

--- core/app/backbone/views/channel_view.js
+++ core/app/backbone/views/channel_view.js
@@ -20,8 +20,6 @@ window.ChannelViewLayout = Backbone.Marionette.Layout.extend({
     this.initSubChannels();
     this.on('render', _.bind(function(){
       this.renderSubChannels();
-      this.initSubChannelMenu();
-      this.initAddToChannel();
       this.$('header .authority').tooltip({title: 'Authority of ' + this.model.attributes.created_by.username + ' on "' + this.model.attributes.title + '"'});
     },this));
   },
@@ -42,40 +40,6 @@ window.ChannelViewLayout = Backbone.Marionette.Layout.extend({
     }
   },
 
-  initSubChannelMenu: function() {
-    if( this.model.get("followable?") ) {
-      var addToChannelButton = this.$("#add-to-channel");
-      var followChannelMenu =this.$("#follow-channel");
-
-      followChannelMenu.css({"left": addToChannelButton.position().left});
-
-      addToChannelButton.hoverIntent(
-        function() { followChannelMenu.fadeIn("fast"); },
-        function() { followChannelMenu.delay(600).fadeOut("fast"); }
-      );
-
-      followChannelMenu.on("mouseover", function() {
-        followChannelMenu.stop(true, true).show();
-      });
-
-      followChannelMenu.on("mouseout", function() {
-       if (!followChannelMenu.find("input#channel_title").is(":focus")) {
-          followChannelMenu.delay(500).fadeOut("fast");
-        }
-      });
-    }
-  },
-
-  initAddToChannel: function() {
-    if ( this.$('#add-to-channel') && typeof currentUser !== "undefined" && typeof currentChannel !== "undefined" ) {
-      this.addToChannelView = new AddToChannelView({
-        collection: currentUser.channels,
-        el: this.$('#follow-channel'),
-        model: currentChannel,
-        containingChannels: currentChannel.getOwnContainingChannels()
-      }).render();
-    }
-  },
   onClose: function() {
     if ( this.addToChannelView ) {
       this.addToChannelView.close();
