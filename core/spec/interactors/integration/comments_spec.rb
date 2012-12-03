228c911fe5dc6f4f08709587f6d5aed4a8871d8d
Mark IJbema <markijbema@gmail.com>
2012-12-03 15:41:53 +0100

    added an interactor integration spec

--- core/spec/interactors/integration/comments_spec.rb
+++ core/spec/interactors/integration/comments_spec.rb
@@ -1,1 +1,41 @@
-# TODO-0312 create test
+require 'spec_helper'
+
+describe 'comments' do
+  include Pavlov::Helpers
+
+  let(:current_user) {
+    user = create :user
+    puts user.graph_user.id
+    user
+  }
+
+  def pavlov_options
+    {current_user: current_user}
+  end
+
+  before do
+    ElasticSearch.stub synchronous: true
+  end
+
+  describe 'initially' do
+    it 'a fact has no comments' do
+      f = create :fact
+      comments = interactor :'comments/index', f.id.to_i, 'believes'
+      expect(comments).to eq []
+    end
+  end
+
+  describe 'adding a few comments' do
+    it 'the fact should get the comments we add' do
+      f = create :fact, created_by: (create :user).graph_user
+
+      interactor :'comments/create', f.id.to_i, 'believes', 'Gekke Gerrit'
+      interactor :'comments/create', f.id.to_i, 'believes', 'Handige Harrie'
+
+      comments = interactor :'comments/index', f.id.to_i, 'believes'
+      expect(comments.length).to eq 2
+
+      expect(comments.map(&:content)).to eq ['Gekke Gerrit', 'Handige Harrie']
+    end
+  end
+end
