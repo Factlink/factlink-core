e778ffc41ab0309d506c9b846d1d3721bf377e31
tomdev <tom@north26.nl>
2012-12-03 13:40:40 +0100

    Made migration safer, using hash syntax makes it possible to set the field, even if it is not defined as field in the model

--- core/db/migrate/20121203120621_change_opinion_to_type_on_comment.rb
+++ core/db/migrate/20121203120621_change_opinion_to_type_on_comment.rb
@@ -1,7 +1,7 @@
 class ChangeOpinionToTypeOnComment < Mongoid::Migration
   def self.up
     Comment.all.each do |comment|
-      comment.type = comment.opinion
+      comment[:type] = comment[:opinion]
       comment.unset :opinion
       comment.save
     end
@@ -9,7 +9,7 @@ class ChangeOpinionToTypeOnComment < Mongoid::Migration
 
   def self.down
     Comment.all.each do |comment|
-      comment.opinion = comment.type
+      comment[:opinion] = comment[:type]
       comment.unset :type
       comment.save
     end
