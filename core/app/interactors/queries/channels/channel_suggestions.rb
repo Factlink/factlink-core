01ea384a46a4a8e62ce1903fe19d254ad05f5b05
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-12-04 11:04:28 +0100

    Added simple implementation of channel suggestions query, which only returns the 3 highest authority channels for a user

---
+++ core/app/interactors/queries/channels/channel_suggestions.rb
@@ -0,0 +1,21 @@
+require_relative '../../pavlov'
+
+module Queries
+  module Channels
+    class ChannelSuggestions
+      include Pavlov::Query
+
+      def execute
+        current_graph_user.editable_channels_by_authority(suggestion_count)
+      end
+
+      def suggestion_count
+        3
+      end
+
+      def current_graph_user
+        @options[:current_user].graph_user
+      end
+    end
+  end
+end
---
+++ core/spec-unit/interactors/queries/channels/channel_suggestions_spec.rb
@@ -0,0 +1,26 @@
+require 'pavlov_helper'
+require_relative '../../../../app/interactors/queries/channels/channel_suggestions'
+
+describe Queries::Channels::ChannelSuggestions do
+  include PavlovSupport
+
+  describe '.execute' do
+    it 'returns the three channels on which the current_user has the highest authority' do
+      current_graph_user = mock
+      channels = mock
+      suggestion_count = mock
+
+      query = Queries::Channels::ChannelSuggestions.new
+
+      current_graph_user.should_receive(:editable_channels_by_authority)
+                        .with(suggestion_count)
+                        .and_return(channels)
+
+      query.should_receive(:suggestion_count).and_return suggestion_count
+      query.should_receive(:current_graph_user).and_return current_graph_user
+
+
+      expect(query.execute).to eq channels
+    end
+  end
+end
