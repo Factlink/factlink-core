d5def2d8b7a443e2b81a5b05e81af528dc14fffc
tomdev <tom@north26.nl>
2012-12-04 09:01:49 +0100

    Extracted comment helpers to Acceptance::CommentHelper module

---
+++ core/spec/support/acceptance/comment_helper.rb
@@ -0,0 +1,30 @@
+module Acceptance
+  module CommentHelper
+
+      def add_comment_with_toggle comment
+        evidence_input = page.find_field 'add_factrelation'
+
+        evidence_input.trigger 'focus'
+        evidence_input.set comment
+        evidence_input.trigger 'blur'
+
+        page.find('.js-switch').set(true)
+
+        click_button 'Post comment'
+        wait_for_ajax
+      end
+
+
+      def add_comment comment
+        comment_input = page.find_field 'add_comment'
+
+        comment_input.trigger 'focus'
+        comment_input.set comment
+        comment_input.trigger 'blur'
+
+        click_button 'Post comment'
+        wait_for_ajax
+      end
+
+  end
+end
