922b857259c507e8ac5396b1f75d771aeb363ef7
Mark IJbema <markijbema@gmail.com>
2012-12-04 09:43:49 +0100

    only retrieve once (like the test wants)

--- core/app/interactors/queries/comments/graph_user_opinion.rb
+++ core/app/interactors/queries/comments/graph_user_opinion.rb
@@ -30,7 +30,7 @@ module Queries
       end
 
       def believable
-        Believable::Commentje.new @comment_id
+        @believable ||= Believable::Commentje.new @comment_id
       end
     end
   end
