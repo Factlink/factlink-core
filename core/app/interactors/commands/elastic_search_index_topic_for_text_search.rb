da4a31b9f9ff33d1b8051c0363426a578634506a
Mark IJbema <markijbema@gmail.com>
2012-12-13 15:47:43 +0100

    Removed no longer needed requires

--- core/app/interactors/commands/elastic_search_index_topic_for_text_search.rb
+++ core/app/interactors/commands/elastic_search_index_topic_for_text_search.rb
@@ -1,5 +1,3 @@
-require_relative 'elastic_search_index_for_text_search_command.rb'
-
 module Commands
   class ElasticSearchIndexTopicForTextSearch < ElasticSearchIndexForTextSearchCommand
     def define_index
