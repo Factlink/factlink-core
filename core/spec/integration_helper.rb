d2b426e0b2a70f4f6fe85e8349f0e5b3fc142c77
Jan Deelstra <jan@deelstra.org>
2012-12-17 16:58:47 +0100

    Adding debugging and enabling javascript errors.

--- core/spec/integration_helper.rb
+++ core/spec/integration_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
     Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, {:js_errors => false})
+      Capybara::Poltergeist::Driver.new(app, {:debug => true})
     end
 
   end
