3576762d02fc5b8e99637722f1b15e3ced87f3c7
Mark IJbema <markijbema@gmail.com>
2012-12-18 13:53:10 +0100

    make add_comment idempotent

--- core/spec/support/acceptance/comment_helper.rb
+++ core/spec/support/acceptance/comment_helper.rb
@@ -1,29 +1,28 @@
 module Acceptance
   module CommentHelper
 
-      def add_comment_with_toggle comment
-        evidence_input = page.find_field 'text_input_view'
-
-        evidence_input.trigger 'focus'
-        evidence_input.set comment
-        evidence_input.trigger 'blur'
-
-        page.find('.js-switch').set(true)
-
-        click_button 'Post comment'
-        wait_for_ajax
+      def toggle_to_comment
+        within('.fact-relation-search') do
+          evidence_input = page.find_field 'text_input_view'
+          evidence_input.trigger 'focus'
+          page.find('.js-switch').set(true)
+        end
       end
 
-
       def add_comment comment
-        comment_input = page.find_field 'add_comment'
+        if find('button').has_content? 'Post Factlink'
+          toggle_to_comment
+        end
+        within('.fact-relation-search') do
+          comment_input = page.find_field 'add_comment'
 
-        comment_input.trigger 'focus'
-        comment_input.set comment
-        comment_input.trigger 'blur'
+          comment_input.trigger 'focus'
+          comment_input.set comment
+          comment_input.trigger 'blur'
 
-        click_button 'Post comment'
-        wait_for_ajax
+          click_button 'Post comment'
+          wait_for_ajax
+        end
       end
 
   end
