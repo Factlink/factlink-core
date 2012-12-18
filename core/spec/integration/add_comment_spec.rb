4e728a76782cf297e552ee89744aad5849b7fa4d
Mark IJbema <markijbema@gmail.com>
2012-12-18 13:18:43 +0100

    renamed to make purpose clearer

--- core/spec/integration/add_comment_spec.rb
+++ core/spec/integration/add_comment_spec.rb
@@ -21,11 +21,11 @@ feature "adding comments to a fact", type: :request do
     # Input should be empty
     find_field('add_comment').value.blank?.should be_true
 
-    comment_listing.should have_content comment
+    evidence_listing.should have_content comment
 
     go_to_discussion_page_of factlink # Reload the page
 
-    comment_listing.should have_content comment
+    evidence_listing.should have_content comment
 
   end
 
@@ -41,7 +41,7 @@ feature "adding comments to a fact", type: :request do
 
     go_to_discussion_page_of factlink
 
-    within comment_listing do
+    within evidence_listing do
       find('.total-authority-evidence').should have_content user_authority_on_fact + 1
     end
   end
@@ -55,14 +55,14 @@ feature "adding comments to a fact", type: :request do
     comment = 'Buffels zijn niet klein te krijgen joh'
     add_comment_with_toggle comment
 
-    within comment_listing do
+    within evidence_listing do
       evidence_item(comment).find('.supporting').click
       evidence_item(comment).find('.total-authority-evidence', text: "0.0")
     end
 
     go_to_discussion_page_of factlink
 
-    within comment_listing do
+    within evidence_listing do
       find('.total-authority-evidence', text: "0.0")
     end
   end
@@ -76,13 +76,13 @@ feature "adding comments to a fact", type: :request do
     add_comment_with_toggle comment1
     add_comment comment2
 
-    comment_listing.should have_content comment1
-    comment_listing.should have_content comment2
+    evidence_listing.should have_content comment1
+    evidence_listing.should have_content comment2
 
     go_to_discussion_page_of factlink # Reload the page
 
-    comment_listing.should have_content comment1
-    comment_listing.should have_content comment2
+    evidence_listing.should have_content comment1
+    evidence_listing.should have_content comment2
   end
 
 
@@ -93,7 +93,7 @@ feature "adding comments to a fact", type: :request do
 
     add_comment_with_toggle comment
 
-    within comment_listing do
+    within evidence_listing do
       find('.evidence-popover-arrow').click
       find('.delete').click
       wait_for_ajax
@@ -110,7 +110,7 @@ feature "adding comments to a fact", type: :request do
     find '.evidence-item', text: text
   end
 
-  def comment_listing
+  def evidence_listing
     find '.fact-relation-listing'
   end
 end
