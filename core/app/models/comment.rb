d58c8c8d1ec7d4cb645416110d4d06fe0cd52f9c
Jan Paul Posma <me@janpaulposma.nl>
2012-12-19 13:30:54 +0100

    Added deletable? to comment, and wrote a test.

--- core/app/models/comment.rb
+++ core/app/models/comment.rb
@@ -11,4 +11,18 @@ class Comment
   belongs_to :created_by, class_name: 'User', inverse_of: :comments
 
   index({ fact_data: 1, opinion: 1, created_at: 1})
+
+  def people_believes
+    Believable::Commentje.new(self).people_believes
+  end
+
+  def deletable?
+    has_no_believers = people_believes.ids == []
+    creator_is_only_believer = people_believes.ids.map {|i| i.to_i} == [created_by.graph_user_id.to_i]
+
+    sub_comment_count = Queries::SubComments::Count.new(id.to_s, 'Comment').execute
+    has_no_sub_comments = sub_comment_count == 0
+
+    (has_no_believers or creator_is_only_believer) and has_no_sub_comments
+  end
 end
