8ff4515bb709a7a88573a664fc579d734689fb30
Jan Paul Posma <me@janpaulposma.nl>
2012-12-20 10:03:34 +0100

    Moved the current comment spec to a evidence_deletable spec with a "context 'Comment'"

--- core/spec/models/evidence_deletable_spec.rb
+++ core/spec/models/evidence_deletable_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Comment do
+describe EvidenceDeletable do
 
-  describe :deletable? do
+  context 'Comment' do
     let(:created_by_user) {create :approved_confirmed_user}
     let(:other_user)      {create :approved_confirmed_user}
     let(:fact)            {create :fact}
@@ -13,33 +13,42 @@ describe Comment do
       Comment.find(dead_comment.id)
     end
 
+    def deletable comment
+      EvidenceDeletable.new(comment, 'Comment', believable_of(comment),
+        comment.created_by.graph_user_id).deletable?
+    end
+
     def add_opinion comment, opinion, graph_user
-      Believable::Commentje.new(comment).add_opiniated(opinion, graph_user)
+      believable_of(comment).add_opiniated(opinion, graph_user)
+    end
+
+    def believable_of comment
+      Believable::Commentje.new(comment)
     end
 
     it "should be true initially" do
-      comment.deletable?.should be_true
+      deletable(comment).should be_true
     end
     it "should be true if only the creator believes it" do
       add_opinion(comment, :believes, comment.created_by.graph_user)
-      comment.deletable?.should be_true
+      deletable(comment).should be_true
     end
     it "should be false after someone else believes the relation" do
       add_opinion(comment, :believes, other_user.graph_user)
-      comment.deletable?.should be_false
+      deletable(comment).should be_false
     end
     it "should be true if only the creator believes it" do
       add_opinion(comment, :disbelieves, other_user.graph_user)
-      comment.deletable?.should be_true
+      deletable(comment).should be_true
     end
     it "should be true if only the creator believes it" do
       add_opinion(comment, :doubts, other_user.graph_user)
-      comment.deletable?.should be_true
+      deletable(comment).should be_true
     end
     it "should be false after someone comments on it" do
       interactor = Interactors::SubComments::CreateForComment.new comment.id.to_s, 'hoi', current_user: comment.created_by
       interactor.execute
-      comment.deletable?.should be_false
+      deletable(comment).should be_false
     end
   end
 
