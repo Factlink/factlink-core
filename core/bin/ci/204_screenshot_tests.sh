d75142ac14104978cae98edf99cbea1673aa13d4
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-03 15:04:23 +0100

    Created CI task for screenshot tests

---
+++ core/bin/ci/204_screenshot_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Running screenshot tests"
+
+OUTPUTFILE=$(tempfile)
+bundle exec rspec spec/screenshots/ | tee "$OUTPUTFILE"
+cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
+exit
