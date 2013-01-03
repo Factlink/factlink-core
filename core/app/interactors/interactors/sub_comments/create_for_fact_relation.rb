191c544ae193e91276154b3bd1e18f39b56757bc
Jan Paul Posma <me@janpaulposma.nl>
2013-01-03 16:37:09 +0100

    Extracted some common functionality from create_for_comment and create_for_fact_relation into create_for_generic.

--- core/app/interactors/interactors/sub_comments/create_for_fact_relation.rb
+++ core/app/interactors/interactors/sub_comments/create_for_fact_relation.rb
@@ -1,8 +1,9 @@
 require_relative '../../pavlov'
+require_relative 'create_for_generic'
 
 module Interactors
   module SubComments
-    class CreateForFactRelation
+    class CreateForFactRelation < CreateForGeneric
       include Pavlov::Interactor
 
       arguments :fact_relation_id, :content
@@ -13,31 +14,12 @@ module Interactors
           "should not be empty."
       end
 
-      def authorized?
-        @options[:current_user]
-      end
-
-      def execute
-        sub_comment = command :'sub_comments/create_xxx', @fact_relation_id, 'FactRelation', @content, @options[:current_user]
-
-        create_activity sub_comment
-
-        KillObject.sub_comment sub_comment,
-          authority: authority_of_user_who_created(sub_comment)
-      end
-
-      def create_activity sub_comment
-        command :create_activity,
-          @options[:current_user].graph_user, :created_sub_comment,
-          sub_comment, top_fact
+      def create_sub_comment
+        command :'sub_comments/create_xxx', @fact_relation_id, 'FactRelation', @content, @options[:current_user]
       end
 
       def top_fact
         @top_fact ||= FactRelation[@fact_relation_id].fact
-      end
-
-      def authority_of_user_who_created sub_comment
-        query :authority_on_fact_for, top_fact, sub_comment.created_by.graph_user
       end
     end
   end
