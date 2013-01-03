2c6fa71695b5e5f4e272281d379b1397b206ed50
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-03 15:03:34 +0100

    Removed old screenshot tag for rspec

--- core/bin/ci/203_integration_tests.sh
+++ core/bin/ci/203_integration_tests.sh
@@ -2,6 +2,6 @@
 echo "Running integration tests"
 
 OUTPUTFILE=$(tempfile)
-bundle exec rspec spec/integration/ --tag type:request --tag screenshot | tee "$OUTPUTFILE"
+bundle exec rspec spec/integration/ | tee "$OUTPUTFILE"
 cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
 exit
