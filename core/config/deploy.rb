a8022da60bfca27e2c4a9b471d8339229f57e1e8
tomdev <tom@north26.nl>
2013-01-07 14:22:21 +0100

    Precompile assets when locales have change

--- core/config/deploy.rb
+++ core/config/deploy.rb
@@ -83,7 +83,7 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       # Only precompile if files have changed: http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
       from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ app/backbone/ app/templates/ Gemfile.lock config/application.rb config/deploy.rb config/deploy/ | wc -l").to_i > 0
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ app/backbone/ app/templates/ Gemfile.lock config/application.rb config/deploy.rb config/deploy/ config/locales | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile:primary}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
