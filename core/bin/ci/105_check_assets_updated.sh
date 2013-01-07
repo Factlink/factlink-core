c4e609fec107998966f9a63ce18f1f35f69ac732
Mark IJbema <markijbema@gmail.com>
2013-01-07 15:07:16 +0100

    actually show the expected value -.-;; gonna get me some caffeine

--- core/bin/ci/105_check_assets_updated.sh
+++ core/bin/ci/105_check_assets_updated.sh
@@ -4,7 +4,7 @@ CURRENT_MD5=`cat config/locales/* | md5sum | perl -pe 's/\s*-\s*//'`
 RECORDED_MD5=`cat app/assets/javascripts/globals/globals.coffee.erb  | grep '# MD5' | perl -pe 's/.*:\s*//'`
 
 if [ "$CURRENT_MD5" != "$RECORDED_MD5" ]; then
-  echo "Please update the MD5 in globals.coffee.erb to '$RECORDED_MD5'"
+  echo "Please update the MD5 in globals.coffee.erb to '$CURRENT_MD5'"
   echo "You can calculate this beforehand by running 'cat config/locales/* | md5'"
   exit 1
 else
