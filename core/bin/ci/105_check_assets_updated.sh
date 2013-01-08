c0aec43dc6574ee4573eb8779496cecda5644a8f
Jan Paul Posma <me@janpaulposma.nl>
2013-01-08 11:06:55 +0100

    Attempt to fix ci check 105.

--- core/bin/ci/105_check_assets_updated.sh
+++ core/bin/ci/105_check_assets_updated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # LC_ALL=C with the "ls config/locales/*" is to make sure the file order is always the same
-CURRENT_SHASUM=`LC_ALL=C cat $(ls config/locales/*) | shasum | perl -pe 's/\s*-\s*//'`
+CURRENT_SHASUM=`LC_ALL=C shasum -b $(ls config/locales/*) | shasum -b | perl -pe 's/\s*\*-\s*//'`
 RECORDED_SHASUM=`cat app/assets/javascripts/globals/globals.coffee.erb  | grep '# SHASUM' | perl -pe 's/.*:\s*//'`
 
 echo "This script checks if locales have been updated, as Rails doesn't check these files"
@@ -18,6 +18,10 @@ echo "SHASUM in globals.coffee.erb: $RECORDED_SHASUM"
 
 if [ "$CURRENT_SHASUM" != "$RECORDED_SHASUM" ]; then
   echo "Please update the SHASUM in globals.coffee.erb to '$CURRENT_SHASUM'"
+
+  echo "For debugging purposes:"
+  LC_ALL=C shasum -b $(ls config/locales/*)
+
   exit 1
 else
   echo "SHASUM matches!"
