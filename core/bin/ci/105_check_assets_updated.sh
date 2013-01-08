a74be8e2230805ccfbb9c63baee68b9f486ddc73
Jan Paul Posma <me@janpaulposma.nl>
2013-01-08 10:50:50 +0100

    Added some more information to the 105 script.

--- core/bin/ci/105_check_assets_updated.sh
+++ core/bin/ci/105_check_assets_updated.sh
@@ -4,11 +4,17 @@
 CURRENT_SHASUM=`LC_ALL=C cat $(ls config/locales/*) | shasum | perl -pe 's/\s*-\s*//'`
 RECORDED_SHASUM=`cat app/assets/javascripts/globals/globals.coffee.erb  | grep '# SHASUM' | perl -pe 's/.*:\s*//'`
 
-echo "Using the following files for SHASUM:"
-ls config/locales/*
+echo "This script checks if locales have been updated, as Rails doesn't check these files"
+echo "You can also run this script locally using 'bin/ci/105_check_assets_updated.sh'"
+echo ""
 
+echo "Using the following files for SHASUM (note the order, especially files with underscores..):"
+LC_ALL=C ls -la config/locales/*
+
+echo ""
 echo "SHASUM of these files: $CURRENT_SHASUM"
 echo "SHASUM in globals.coffee.erb: $RECORDED_SHASUM"
+
 
 if [ "$CURRENT_SHASUM" != "$RECORDED_SHASUM" ]; then
   echo "Please update the SHASUM in globals.coffee.erb to '$CURRENT_SHASUM'"
