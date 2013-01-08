887cb7be80e9a742db10a0d6cd2c3d47ab1d9ce7
Jan Paul Posma <me@janpaulposma.nl>
2013-01-08 11:18:08 +0100

    Final attempt, otherwise I'm giving up on this shit.

--- core/bin/ci/105_check_assets_updated.sh
+++ core/bin/ci/105_check_assets_updated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sort is to make sure the file order is always the same
-CURRENT_SHASUM=`shasum -b $(ls config/locales/* | sort) | shasum -b | perl -pe 's/\s*\*-\s*//'`
+# sort -d is to make sure the file order is always the same
+CURRENT_SHASUM=`shasum -b $(ls config/locales/* | sort -d) | shasum -b | perl -pe 's/\s*\*-\s*//'`
 RECORDED_SHASUM=`cat app/assets/javascripts/globals/globals.coffee.erb  | grep '# SHASUM' | perl -pe 's/.*:\s*//'`
 
 echo "This script checks if locales have been updated, as Rails doesn't check these files"
@@ -9,7 +9,7 @@ echo "You can also run this script locally using 'bin/ci/105_check_assets_update
 echo ""
 
 echo "Using the following files for SHASUM (note the order, especially files with underscores..):"
-ls -la config/locales/* | sort
+ls -la config/locales/* | sort -d
 
 echo ""
 echo "SHASUM of these files: $CURRENT_SHASUM"
@@ -20,7 +20,7 @@ if [ "$CURRENT_SHASUM" != "$RECORDED_SHASUM" ]; then
   echo "Please update the SHASUM in globals.coffee.erb to '$CURRENT_SHASUM'"
 
   echo "For debugging purposes:"
-  LC_ALL=C shasum -b $(ls config/locales/* | sort)
+  LC_ALL=C shasum -b $(ls config/locales/* | sort -d)
 
   exit 1
 else
