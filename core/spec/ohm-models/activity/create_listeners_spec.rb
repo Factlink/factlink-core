962a728c70accedd449b08155f9cba99d7f0af91
Jan Paul Posma <me@janpaulposma.nl>
2013-01-08 17:16:26 +0100

    Added specs for checking that adding a sub comment means following a factrelation/comment.

--- core/spec/ohm-models/activity/create_listeners_spec.rb
+++ core/spec/ohm-models/activity/create_listeners_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe 'activity queries' do
   include AddFactToChannelSupport
   include RedisSupport
-  let(:gu1) { create :graph_user }
-  let(:gu2) { create :graph_user }
+  let(:gu1) { create(:active_user).graph_user }
+  let(:gu2) { create(:active_user).graph_user }
 
   before do
     # TODO: remove this once creating an activity does not cause an email to be sent
@@ -358,6 +358,36 @@ describe 'activity queries' do
           ]
         end
       end
+
+      context "gu1 has added a subcomment to the comment" do
+        it "creates a stream activity" do
+          fact = create :fact, created_by: current_user.graph_user
+
+          comment = interactor :'comments/create', fact.id.to_i, 'disbelieves', 'content'
+
+          Interactors::SubComments::CreateForComment.new(comment.id.to_s, 'content', current_user: gu1.user).call
+
+          sub_comment = interactor :'sub_comments/create_for_comment', comment.id.to_s, 'content'
+
+          gu1.stream_activities.map(&:to_hash_without_time).should == [
+            {user: current_user.graph_user, action: :created_sub_comment, subject: SubComment.find(sub_comment.id), object: fact }
+          ]
+        end
+        
+        it "creates a notification" do
+          fact = create :fact, created_by: current_user.graph_user
+
+          comment = interactor :'comments/create', fact.id.to_i, 'disbelieves', 'content'
+
+          Interactors::SubComments::CreateForComment.new(comment.id.to_s, 'content', current_user: gu1.user).call
+
+          sub_comment = interactor :'sub_comments/create_for_comment', comment.id.to_s, 'content'
+
+          gu1.notifications.map(&:to_hash_without_time).should == [
+            {user: current_user.graph_user, action: :created_sub_comment, subject: SubComment.find(sub_comment.id), object: fact }
+          ]
+        end
+      end
     end
 
     context "creating a sub comment on a fact relation" do
@@ -417,6 +447,37 @@ describe 'activity queries' do
           gu1.notifications.map(&:to_hash_without_time).should == [
             {user: current_user.graph_user, action: :created_sub_comment, subject: SubComment.find(sub_comment.id), object: fact }
           ]
+        end
+      end
+
+      context "gu1 has added a subcomment to the fact relation" do
+        it "creates a stream activity" do
+          fact = create :fact, created_by: current_user.graph_user
+
+          fact_relation = fact.add_evidence :supporting, create(:fact), current_user
+
+          Interactors::SubComments::CreateForFactRelation.new(fact_relation.id.to_i, 'content', current_user: gu1.user).call
+
+          sub_comment = interactor :'sub_comments/create_for_fact_relation', fact_relation.id.to_i, 'content'
+
+          gu1.stream_activities.map(&:to_hash_without_time).should == [
+            {user: current_user.graph_user, action: :created_sub_comment, subject: SubComment.find(sub_comment.id), object: fact }
+          ]
+        end
+
+        it "creates a notification" do
+          fact = create :fact, created_by: current_user.graph_user
+
+          fact_relation = fact.add_evidence :supporting, create(:fact), current_user
+
+          Interactors::SubComments::CreateForFactRelation.new(fact_relation.id.to_i, 'content', current_user: gu1.user).call
+
+          sub_comment = interactor :'sub_comments/create_for_fact_relation', fact_relation.id.to_i, 'content'
+
+          gu1.notifications.map(&:to_hash_without_time).should == [
+            {user: current_user.graph_user, action: :created_sub_comment, subject: SubComment.find(sub_comment.id), object: fact }
+          ]
+
         end
       end
     end
