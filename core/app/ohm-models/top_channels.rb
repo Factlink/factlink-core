184896346b0448bbd633c356532bd8671330277e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-09 13:53:43 +0100

    Added "add_id" method to TopChannels

--- core/app/ohm-models/top_channels.rb
+++ core/app/ohm-models/top_channels.rb
@@ -4,6 +4,14 @@ class TopChannels
   include Redis::Aid::Ns(:top_channels)
 
   def ids
-    redis[:handpicked_channels].smembers
+    handpicked_channels_interface.smembers
+  end
+
+  def add_id id
+    handpicked_channels_interface.sadd id
+  end
+
+  def handpicked_channels_interface
+    redis[:handpicked_channels]
   end
 end
