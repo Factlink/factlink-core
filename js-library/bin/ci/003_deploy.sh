a842f3e70102f879eea335d5909c76693d1902a8
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-15 11:53:15 +0100

    Updated deploy script so it wont deploy when there is no deploy server set

--- js-library/bin/ci/003_deploy.sh
+++ js-library/bin/ci/003_deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo "deploying to $DEPLOY_SERVER"
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if [[ ! -z $DEPLOY_SERVER ]]; then
+  echo "deploying to $DEPLOY_SERVER"
 
-cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 
-echo "deployed to $DEPLOY_SERVER"
+  cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
+
+  echo "deployed to $DEPLOY_SERVER"
+else
+  echo "No DEPLOY_SERVER found, not deploying."
+fi
+
 exit
