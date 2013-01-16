806d0d7baed646c65c7a87079042c1aba3397031
tomdev <tom@north26.nl>
2013-01-16 18:14:44 +0100

    Added helper to link to friendly_fact with correct tab opened

--- core/app/helpers/fact_helper.rb
+++ core/app/helpers/fact_helper.rb
@@ -2,6 +2,10 @@ module FactHelper
 
   # IE 8 max url length is 2083 characters according to
   # http://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url
+  def friendly_fact_with_opened_tab_path(fact, tab_type)
+    "#{friendly_fact_path(fact)}?tab=#{tab_type}"
+  end
+
   def friendly_fact_path(fact, max_slug_length = 1024)
     slug = friendly_fact_slug(fact, max_slug_length)
     frurl_fact_path(slug, fact.id)
