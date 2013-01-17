aff82606b33d9e4823cc508d38cb4b7263fb2964
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-17 15:23:05 +0100

    Tour/choose-channels shouldn't be fixed
    Conflicts:
    	app/controllers/tour_controller.rb

--- core/app/controllers/tour_controller.rb
+++ core/app/controllers/tour_controller.rb
@@ -15,7 +15,7 @@ class TourController < ApplicationController
   def choose_channels
     @step_in_signup_process = :almost_done
     @user = current_user
-    render inline:'', layout: "channels"
+    render inline:'', layout: "channels", locals: {not_fixed: true}
   end
 
 end
