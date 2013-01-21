224fabc12ae441a1b2e993bb102e303e0dfb89a6
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-21 14:17:17 +0100

    Turned all screenshot tests pending

--- core/spec/screenshots/frontpage_spec.rb
+++ core/spec/screenshots/frontpage_spec.rb
@@ -1,7 +1,7 @@
 require 'screenshot_helper'
 
 describe "Compare screens", type: :request do
-  it "should render the frontpage as expected" do
+  pending "should render the frontpage as expected" do
     visit "/?show_sign_in=true"
     assume_unchanged_screenshot "homepage"
   end
