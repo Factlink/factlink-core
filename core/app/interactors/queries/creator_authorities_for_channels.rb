d24891e6a1f3d10ad7e9dd0240feac65ba34bd9e
Mark IJbema <markijbema@gmail.com>
2013-01-21 12:56:26 +0100

    cache graph_users per query since the most common use case is to get the channels of one user, this will typically result in O(constant) instead of O(N) retrievals of the user.

--- core/app/interactors/queries/creator_authorities_for_channels.rb
+++ core/app/interactors/queries/creator_authorities_for_channels.rb
@@ -10,10 +10,11 @@ module Queries
   # multiple channels with the same topic, so optimization wouldn't
   # actually optimize anything
   #
-  # TODO do some caching of the creator here. For instance, make a
-  #      method graph_user which caches the graph_user on id. the
-  #      .created_by now does two redis calls each time authority_for
-  #      is called
+  # We cache the graph_users after they're retrieved, since
+  # the most common use case is to get the channels of one
+  # user, which means that we will now typically get
+  # O(constant) graph_user retrievals (2 redis commands)
+  # instead of O(N).
   class CreatorAuthoritiesForChannels
     include Pavlov::Query
     arguments :channels
@@ -24,12 +25,22 @@ module Queries
     def authority_for(channel)
       if channel.type == 'channel'
         topic = topic_for(channel)
-        query :authority_on_topic_for,topic, channel.created_by
+        graph_user = graph_user_for(channel)
+        query :authority_on_topic_for, topic, graph_user
       else
         # channel is a userstream or a created facts channel,
         # so there is no topic, therefor we define authority as 0
         0
       end
+    end
+
+    def graph_user_for channel
+      graph_user channel.created_by_id
+    end
+
+    def graph_user id
+      @graph_users     ||= {}
+      @graph_users[id] ||= GraphUser[id]
     end
 
     def topics
