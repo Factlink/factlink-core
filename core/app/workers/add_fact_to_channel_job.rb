c95c7b88dfb52ae03aca1ba3200bb425bbe108a3
Mark IJbema <markijbema@gmail.com>
2013-01-21 14:53:05 +0100

    boyscouting, transformed conditional to guard, and removed attr_readers

--- core/app/workers/add_fact_to_channel_job.rb
+++ core/app/workers/add_fact_to_channel_job.rb
@@ -1,8 +1,6 @@
 class AddFactToChannelJob
   @queue = :channel_operations
 
-  attr_reader :fact_id, :channel_id, :options
-
   def initialize(fact_id, channel_id, options={})
     @fact_id = fact_id
     @channel_id = channel_id
@@ -10,26 +8,29 @@ class AddFactToChannelJob
   end
 
   def perform
-    if fact and channel
-      unless already_propagated or already_deleted
-        add_to_channel
-        add_to_unread
-        propagate_to_channels
-        propagate_to_stream
-      end
-    end
+    return unless fact and channel
+    return if already_propagated or already_deleted
+
+    add_to_channel
+    add_to_unread
+    propagate_to_channels
+    propagate_to_stream
   end
 
   def fact
-    @fact ||= Fact[fact_id]
+    @fact ||= Fact[@fact_id]
   end
 
   def score
-    @score ||= options['score']
+    @options['score']
+  end
+
+  def initiated_by_id
+    @options['initiated_by_id']
   end
 
   def channel
-    @channel ||= Channel[channel_id]
+    @channel ||= Channel[@channel_id]
   end
 
   def already_propagated
@@ -46,7 +47,7 @@ class AddFactToChannelJob
   end
 
   def add_to_unread
-    if (not channel.created_by_id == options['initiated_by_id']) and
+    if (not channel.created_by_id == initiated_by_id) and
        (not channel.created_by_id == fact.created_by_id)
       channel.unread_facts.add(fact)
     end
@@ -54,13 +55,13 @@ class AddFactToChannelJob
 
   def propagate_to_channels
     channel.containing_channels.each do |ch|
-      Resque.enqueue(AddFactToChannelJob, fact_id, ch.id, options)
+      Resque.enqueue(AddFactToChannelJob, fact.id, ch.id, @options)
     end
   end
 
   def propagate_to_stream
     # DEPRECATED: we should tear out the old stream
-    Resque.enqueue(AddFactToChannelJob, fact_id, channel.created_by.stream.id, options) unless channel.type == 'stream'
+    Resque.enqueue(AddFactToChannelJob, fact.id, channel.created_by.stream.id, @options) unless channel.type == 'stream'
   end
 
   def self.perform(fact_id, channel_id, options={})
