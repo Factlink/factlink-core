06b795af7beedd148eaa70bef3bd182573de7f07
tomdev <tom@north26.nl>
2013-01-22 15:56:08 +0100

    Send browser and version to backend to register in MixPanel
    When signing the ToS. We cannot (easily?) track MixPanel people events in the frontend so we are handling this in the backend.

--- core/app/controllers/tos_controller.rb
+++ core/app/controllers/tos_controller.rb
@@ -18,10 +18,18 @@ class TosController < ApplicationController
     last_name  = params[:user][:tos_last_name]
 
     if current_user.sign_tos(agrees_tos, first_name, last_name)
+      track_tos_browser
       redirect_to almost_done_path
     else
       render :show
     end
+  end
+
+  private
+  def track_tos_browser
+    tos_browser_name    = params[:tos_browser_name]
+    tos_browser_version = params[:tos_browser_version]
+    track_people_event tos_browser_name: tos_browser_name, tos_browser_version: tos_browser_version
   end
 
 end
