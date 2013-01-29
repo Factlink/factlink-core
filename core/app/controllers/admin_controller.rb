5693404b658811a4d413a3e8ff90aaee94ccc075
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-29 10:48:22 +0100

    Created /a/info action with basic rails/factlink info!

--- core/app/controllers/admin_controller.rb
+++ core/app/controllers/admin_controller.rb
@@ -1,7 +1,14 @@
 class AdminController < ApplicationController
+  layout "admin"
+
   before_filter :require_admin_access
   def require_admin_access
     authorize! :access, Ability::AdminArea
+  end
+
+  # GET /a/info
+  def info
+    @info = Rails::Info.to_html
   end
 
   before_filter :set_cache_buster
