9022a8828b71a856458424abd0e890abd9e01216
Mark IJbema <markijbema@gmail.com>
2013-01-29 14:23:41 +0100

    use 1.9.3 when deploying

--- core/config/deploy.rb
+++ core/config/deploy.rb
@@ -20,7 +20,7 @@ require "bundler/capistrano"
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 # Load RVM's capistrano plugin.
 require "rvm/capistrano"
-set :rvm_ruby_string, '1.9.2'
+set :rvm_ruby_string, '1.9.3'
 set :rvm_bin_path, "/usr/local/rvm/bin"
 set :rvm_type, :system
 
