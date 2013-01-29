31dfa281b16fb6d498a2d80d167bc1333818e5c1
Jan Paul Posma <me@janpaulposma.nl>
2013-01-29 12:16:44 +0100

    Added test for discussion link below FactRelations.

--- core/spec/acceptance/add_comment_spec.rb
+++ core/spec/acceptance/add_comment_spec.rb
@@ -177,4 +177,20 @@ feature "adding comments to a fact", type: :request do
 
     page.find('.fact-view .fact-body .js-displaystring', text: supporting_factlink.to_s)
   end
+
+  scenario "we can click on the discussion link to go to the page of that factlink" do
+    go_to_discussion_page_of factlink
+
+    supporting_factlink = backend_create_fact
+
+    within ".relation-tabs-view" do
+      add_existing_factlink supporting_factlink
+
+      within "li.evidence-item" do
+        click_link 'Discussion'
+      end
+    end
+
+    page.find('.fact-view .fact-body .js-displaystring', text: supporting_factlink.to_s)
+  end
 end
