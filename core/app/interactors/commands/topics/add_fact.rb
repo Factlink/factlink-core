993a5d5e4f56ac3501f1ccda6d9600233323f394
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-01-30 11:11:43 +0100

    Made the topic/add_fact receive score as an argument
    this way, the AddFactToChannelJob is in job of keeping track of the
    score

--- core/app/interactors/commands/topics/add_fact.rb
+++ core/app/interactors/commands/topics/add_fact.rb
@@ -3,23 +3,20 @@ module Commands
     class AddFact
       include Pavlov::Command
 
-      arguments :fact_id, :topic_slug_title
+      arguments :fact_id, :topic_slug_title, :score
 
       def execute
-        redis_key.zadd score, @fact_id
+        redis_key.zadd @score, @fact_id
       end
 
       def redis_key
         Nest.new(:new_topic)[@topic_slug_title][:facts]
       end
 
-      def score
-        (DateTime.now.to_time.to_f*1000).to_i
-      end
-
       def validate
         validate_integer  :fact_id, @fact_id
         validate_string   :topic_slug_title, @topic_slug_title
+        validate_integer  :score, @score
       end
     end
   end
--- core/spec-unit/interactors/commands/topics/add_fact_spec.rb
+++ core/spec-unit/interactors/commands/topics/add_fact_spec.rb
@@ -11,33 +11,22 @@ describe Commands::Topics::AddFact do
   describe '#execute' do
     it 'correctly' do
       fact_id = 1
-      command = described_class.new fact_id, '1e'
+      score = 1
+      command = described_class.new fact_id, '1e', score
       key = mock
-      score = mock
 
       command.should_receive(:redis_key).and_return(key)
-      command.should_receive(:score).and_return(score)
       key.should_receive(:zadd).with(score, fact_id)
 
       command.execute
     end
   end
 
-  describe '#score' do
-    it 'last generated score is the highest' do
-      command = described_class.new 1, '1e'
-
-      old_score = command.score
-      sleep 0.001
-
-      expect(command.score).to be > old_score
-    end
-  end
-
   describe '#redis_key' do
     it 'calls nest correcly' do
       topic_slug_title = '2a'
-      command = described_class.new 1, topic_slug_title
+      score = 1
+      command = described_class.new 1, topic_slug_title, score
       key = mock
       final_key = mock
 
@@ -52,12 +41,12 @@ describe Commands::Topics::AddFact do
   describe '#validation' do
     let(:subject_class) { described_class }
     it 'requires arguments' do
-      expect_validating('a', '2e').
+      expect_validating('a', '2e', 1).
         to fail_validation('fact_id should be an integer.')
     end
 
     it 'requires arguments' do
-      expect_validating(1, 1).
+      expect_validating(1, 1, 1).
         to fail_validation('topic_slug_title should be a string.')
     end
   end
