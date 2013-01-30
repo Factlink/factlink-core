bb670c649976e34e8012e5011b5c4b6ad2169efc
Remon Oldenbeuving <remon@oldenbeuving.net>
2013-01-30 13:32:45 +0100

    Changed secret_token
    This commit makes all sessions/cookies invalid, forcing all users to sign in to Factlink again.
    
    This was needed because of a change in the way Devise stores users in the session.

--- core/config/initializers/secret_token.rb
+++ core/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FactlinkUI::Application.config.secret_token = '4fc7e64e09fe5644f4e74a64b04ef89fc2f3bf56e81dc46d53f60f45325917a35713273c0fc6f29cf53ff5b9e4e145bf50dcc39745d9ac64028b1d93a4016a80'
+FactlinkUI::Application.config.secret_token = '5fc7e64e09fe5644f4e74a64b04ef89fc2f3bf56e81dc46d53f60f45325917a35713273c0fc6f29cf53ff5b9e4e145bf50dcc39745d9ac64028b1d93a4016a81'
