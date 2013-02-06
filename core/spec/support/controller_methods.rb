120e7b130cd092691dcc9114075a86de8ee04c40
tomdev <tom@north26.nl>
2013-02-06 14:03:09 +0100

    Fix ability mock to respond to signed_in?

--- core/spec/support/controller_methods.rb
+++ core/spec/support/controller_methods.rb
@@ -1,6 +1,7 @@
 module ControllerMethods
   def authenticate_user!(user)
     request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
+    request.env['test_logged_in'] = true
   end
 
   def ability
@@ -8,6 +9,7 @@ module ControllerMethods
       @ability = Object.new
       @ability.extend(CanCan::Ability)
       @ability.should_receive(:feature_toggles).any_number_of_times.and_return([])
+      @ability.should_receive(:signed_in?).any_number_of_times.and_return(request.env['test_logged_in'] || false)
       subject.should_receive(:current_ability).any_number_of_times.and_return(@ability)
     end
     @ability
