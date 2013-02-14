4517141377ade5d69f1d66bfdce7dc0116583f75
Jan Paul Posma <me@janpaulposma.nl>
2013-02-14 11:36:11 +0100

    Implemented pending test and fixed other test, per @jjoos's comment.

--- core/spec/ohm-models/channel_list_spec.rb
+++ core/spec/ohm-models/channel_list_spec.rb
@@ -87,7 +87,7 @@ describe ChannelList do
       ch2 = create :channel, created_by: gu1, title: 'b'
       ch3 = create :channel, created_by: gu1, title: 'c'
 
-      f = create :fact
+      f = create :fact, created_by: gu1
 
       interactor :"channels/add_fact", f, ch1
       interactor :"channels/add_fact", f, ch3
@@ -95,12 +95,33 @@ describe ChannelList do
       list = ChannelList.new(gu1)
 
       expect(list.containing_channel_ids_for_fact(f)).
-        to eq [ch1.id, ch3.id]
+        to eq [gu1.created_facts_channel_id, ch1.id, ch3.id]
     end
   end
 
   describe '.containing_real_channel_ids_for_fact' do
-    pending 'should not include created_facts_channel and stream'
+    include Pavlov::Helpers
+    let(:current_user) {create :graph_user}
+    def pavlov_options
+      {current_user: current_user}
+    end
+    it "returns the channels of the graphuser which contain the fact except created_facts_channel and stream" do
+      gu1 = current_user.graph_user
+
+      ch1 = create :channel, created_by: gu1, title: 'a'
+      ch2 = create :channel, created_by: gu1, title: 'b'
+      ch3 = create :channel, created_by: gu1, title: 'c'
+
+      f = create :fact, created_by: gu1
+
+      interactor :"channels/add_fact", f, ch1
+      interactor :"channels/add_fact", f, ch3
+
+      list = ChannelList.new(gu1)
+
+      expect(list.containing_real_channel_ids_for_fact(f)).
+        to eq [ch1.id, ch3.id]
+    end
   end
 
   describe ".containing_channel_ids_for_channel" do
