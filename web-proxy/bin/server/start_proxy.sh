368d36f1dbe93bb08794d2fdfaa72c30ce958932
Mark IJbema <markijbema@gmail.com>
2013-02-19 10:43:22 +0100

    moved config into this repo, removing dependency on core

--- web-proxy/bin/server/start_proxy.sh
+++ web-proxy/bin/server/start_proxy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 ENVIRONMENT=`cat /etc/factlink-environment`;
 
-CONFIG_PATH=/applications/config/;
 PROXY_PATH=/applications/web-proxy/current/;
 
 echo "starting node proxy using forever";
 
 cd $PROXY_PATH;
 
-export CONFIG_PATH=/applications/config/;
 export NODE_ENV=$ENVIRONMENT;
 
 npm install;
