13dc0a7d4e8ee1e3b909a8930f685b4715a8e838
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-02-20 11:33:40 +0100

    Oops, minor fuckup

--- web-proxy/bin/ci/200_deploy.sh
+++ web-proxy/bin/ci/200_deploy.sh
@@ -3,7 +3,7 @@ if [[ ! -z $DEPLOY_SERVER ]]; then
   echo "Deploying..."
 
   echo "deploying to $DEPLOY_SERVER"
-  bundle exec cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
+  cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
   echo "deployed to $DEPLOY_SERVER"
 else
   echo "No DEPLOY_SERVER found, not deploying."
