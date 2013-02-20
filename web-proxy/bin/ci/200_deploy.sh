8ca2f5e305a788664dadcc2f02500f5d572b5c89
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-02-20 11:04:08 +0100

    Updated deploy script for the CI so it doesn't try to deploy when no deploy server is set

--- web-proxy/bin/ci/200_deploy.sh
+++ web-proxy/bin/ci/200_deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+if [[ ! -z $DEPLOY_SERVER ]]; then
+  echo "Deploying..."
 
-echo "deploying to $DEPLOY_SERVER"
+  echo "deploying to $DEPLOY_SERVER"
+  bundle exec cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
+  echo "deployed to $DEPLOY_SERVER"
+else
+  echo "No DEPLOY_SERVER found, not deploying."
+fi
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-cap -S "branch=$GIT_COMMIT" -q $DEPLOY_SERVER deploy || exit 1
-
-echo "deployed to $DEPLOY_SERVER"
 exit
