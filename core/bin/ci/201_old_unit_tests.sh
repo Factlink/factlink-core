e44231d764bd1b4e7a11e6de2899365a5f229b48
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-02-21 10:08:38 +0100

    Also fail when there are no tests found to run

--- core/bin/ci/201_old_unit_tests.sh
+++ core/bin/ci/201_old_unit_tests.sh
@@ -6,4 +6,5 @@ OUTPUTFILE=$(mktemp /tmp/integration.XXXX)
 bundle exec rspec spec | tee "$OUTPUTFILE"
 
 cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
+cat "$OUTPUTFILE" | grep '0 examples, 0 failures' && exit 1
 exit
--- core/bin/ci/203_acceptance_tests.sh
+++ core/bin/ci/203_acceptance_tests.sh
@@ -4,4 +4,5 @@ echo "Running acceptance tests"
 OUTPUTFILE=$(mktemp /tmp/acceptance.XXXX)
 bundle exec rspec spec/acceptance/ | tee "$OUTPUTFILE"
 cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
+cat "$OUTPUTFILE" | grep '0 examples, 0 failures' && exit 1
 exit
