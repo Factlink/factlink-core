3b2d4a90d4b76d6e271c73cbd465e6dbdf1187a1
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-02-21 10:30:53 +0100

    Oops, seems like we have ourselves 770 examples, which didn't pass the 0 examples check ;-)

--- core/bin/ci/204_screenshot_tests.sh
+++ core/bin/ci/204_screenshot_tests.sh
@@ -4,5 +4,5 @@ echo "Running screenshot tests"
 OUTPUTFILE=$(mktemp /tmp/screenshot.XXXX)
 bundle exec rspec spec/screenshots/ | tee "$OUTPUTFILE"
 cat "$OUTPUTFILE" | grep ', 0 failures' || exit 1
-cat "$OUTPUTFILE" | grep '0 examples, 0 failures' && exit 1
+cat "$OUTPUTFILE" | grep "^0\sexamples,\s0\sfailures" && exit 1
 exit
