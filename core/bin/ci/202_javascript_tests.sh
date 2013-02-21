d15d78cd7bb7972236e85ea2360f6b3016faa2f4
Mark IJbema <markijbema@gmail.com>
2013-02-21 14:11:35 +0100

    Suppress unneeded output by konacha

--- core/bin/ci/202_javascript_tests.sh
+++ core/bin/ci/202_javascript_tests.sh
@@ -3,7 +3,9 @@ echo "Running Javascript tests"
 
 OUTPUTFILE=$(mktemp /tmp/javascript.XXXX)
 
-bundle exec rake konacha:run | tee "$OUTPUTFILE"
+bundle exec rake konacha:run \
+ | grep -vE '^(Compiled|method=GET|Served asset)' \
+ | tee "$OUTPUTFILE"
 
 if ! grep ', 0 failed' $OUTPUTFILE > /dev/null
 then
