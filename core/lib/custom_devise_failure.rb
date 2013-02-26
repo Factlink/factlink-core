83ee0672d1fb9eab3852bb1e04d68248170869d8
Mark IJbema <markijbema@gmail.com>
2013-02-26 09:33:43 +0100

    Factored out the two separate redirect_url methods

--- core/lib/custom_devise_failure.rb
+++ core/lib/custom_devise_failure.rb
@@ -9,20 +9,28 @@ class CustomDeviseFailure < Devise::FailureApp
 
   def redirect_url
     if warden_options[:attempted_path] == "/users/sign_in"
-      # We are logging in in this request, but we failed.
-
-      session[:redirect_after_failed_login_path] || '/?show_sign_in=1'
+      redirect_url_on_failed_login
     else
-      # we requested a page, but we aren't authorized to see this page,
-      # because we are not authenticated
+      redirect_url_on_authentication_required
+    end
+  end
 
-      options = { return_to: request.original_url }
 
-      if client_page
-        user_session_path(options.merge(layout: 'client'))
-      else
-        root_path(options.merge(show_sign_in: 1))
-      end
+  # We are logging in in this request, but we failed.
+  def redirect_url_on_failed_login
+      session[:redirect_after_failed_login_path] ||
+      '/?show_sign_in=13'
+  end
+
+  # we requested a page, but we aren't authorized to see this page,
+  # because we are not authenticated
+  def redirect_url_on_authentication_required
+    options = { return_to: request.original_url }
+
+    if client_page
+      user_session_path(options.merge(layout: 'client'))
+    else
+      root_path(options.merge(show_sign_in: 1))
     end
   end
 
