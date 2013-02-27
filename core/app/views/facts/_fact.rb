df7396735508467ea5a1cb9198f596f704496f39
Mark IJbema <markijbema@gmail.com>
2013-02-27 16:00:17 +0100

    Inlined the last methods so we can easily move to properly using jbuilder

--- core/app/views/facts/_fact.rb
+++ core/app/views/facts/_fact.rb
@@ -8,42 +8,25 @@ module Facts
       @timestamp = options[:timestamp] || 0
     end
 
-    def containing_channel_ids
-      return [] unless @view.current_graph_user
-
-      channel_list = ChannelList.new(@view.current_graph_user)
-      channel_list.containing_real_channel_ids_for_fact @fact
-    end
-
-    def deletable_from_channel?
-      @view.user_signed_in? and @channel and @channel.is_real_channel? and @channel.created_by == @view.current_graph_user
-    end
-
-    def url
-      @view.friendly_fact_path(@fact)
-    end
-
-    def created_by_ago
-      "Created #{TimeFormatter.as_time_ago @fact.data.created_at} ago"
-    end
-
-    def proxy_scroll_url
-      FactlinkUI::Application.config.proxy_url + "/?url=" + CGI.escape(@fact.site.url) + "&scrollto=" + URI.escape(@fact.id)
-    rescue
-      nil
-    end
-
     def to_hash
       json = JbuilderTemplate.new(@view)
 
       json.displaystring @fact.data.displaystring
       json.id @fact.id
       json.site_id @fact.site_id
-      json.containing_channel_ids containing_channel_ids
-      json.url url
+
+      if @view.current_graph_user
+        channel_list = ChannelList.new(@view.current_graph_user)
+        containing_channel_ids = channel_list.containing_real_channel_ids_for_fact @fact
+        json.containing_channel_ids containing_channel_ids
+      end
+      json.url @view.friendly_fact_path(@fact)
 
       if @channel
-        json.deletable_from_channel? deletable_from_channel?
+        deletable_from_channel = @view.user_signed_in? and
+                                 @channel.is_real_channel? and
+                                 @channel.created_by == @view.current_graph_user
+        json.deletable_from_channel? deletable_from_channel
         # TODO : this should be moved to view logic in the frontend,
         #        however, because of the strong coupling with channel
         #        this isn't trivial, so I decided to postpone this for
@@ -67,7 +50,7 @@ module Facts
                       }
       end
 
-      json.created_by_ago created_by_ago
+      json.created_by_ago "Created #{TimeFormatter.as_time_ago @fact.data.created_at} ago"
 
       json.fact_title @fact.data.title
       json.fact_wheel do |j|
@@ -76,7 +59,13 @@ module Facts
                       locals: { fact: @fact }
       end
       json.fact_url(@fact.has_site? ? @fact.site.url : nil)
-      json.proxy_scroll_url proxy_scroll_url
+
+      begin
+        proxy_scroll_url = FactlinkUI::Application.config.proxy_url + "/?url=" + CGI.escape(@fact.site.url) + "&scrollto=" + URI.escape(@fact.id)
+        json.proxy_scroll_url proxy_scroll_url
+      rescue
+        nil
+      end
 
       json.timestamp @timestamp
 
