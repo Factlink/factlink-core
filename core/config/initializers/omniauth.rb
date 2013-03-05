150e37dd1e8fae4a4ed81ca4723229d9ccb99577
Jan Deelstra <jan@deelstra.org>
2013-03-05 17:59:29 +0100

    Display facebook popup style pages and change format to 800x600

--- core/config/initializers/omniauth.rb
+++ core/config/initializers/omniauth.rb
@@ -5,7 +5,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
     omniauth_conf['facebook']['id'],
     omniauth_conf['facebook']['secret'],
-    scope: 'user_birthday,user_education_history,user_interests,user_likes,user_status,publish_stream'
+    scope: 'user_birthday,user_education_history,user_interests,user_likes,user_status,publish_stream',
+    display: 'popup'
+
 end
 
 OmniAuth.config.on_failure = IdentitiesController.action(:oauth_failure)
