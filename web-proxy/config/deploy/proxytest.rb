3c7a3097a1ca2b109e53efe262fea8865f6e0a3b
tomdev <tom@north26.nl>
2013-03-05 13:46:49 +0100

    Add proxytest environment

---
+++ web-proxy/config/deploy/proxytest.rb
@@ -0,0 +1,7 @@
+server '89.188.27.175', :app, :web, :primary => true
+
+set :deploy_env, 'testserver'
+
+role :web, "89.188.27.175"                          # Your HTTP server, Apache/etc
+role :app, "89.188.27.175"                          # This may be the same as your `Web` server
+role :db,  "89.188.27.175", :primary => true # This is where Rails migrations will run
