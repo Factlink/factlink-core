d7c733d334b9ded5c056d449b50296941e6aae22
Jan Deelstra <jan@deelstra.org>
2013-03-05 12:03:04 +0100

    Boyscout remove spaces.

--- core/config/initializers/omniauth.rb
+++ core/config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ omniauth_conf = YAML::load_file(Rails.root.join('config/omniauth.yml'))[Rails.en
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, omniauth_conf['twitter']['id'], omniauth_conf['twitter']['secret']
-  provider :facebook, 
-    omniauth_conf['facebook']['id'], 
+  provider :facebook,
+    omniauth_conf['facebook']['id'],
     omniauth_conf['facebook']['secret'],
     scope: 'user_birthday,user_education_history,user_interests,user_likes,user_status,publish_stream'
 end
