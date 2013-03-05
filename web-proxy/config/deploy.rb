d4cc200be4b3d1b8e02dc102d4780db2af498f1e
tomdev <tom@north26.nl>
2013-03-05 11:15:50 +0100

    Removed rvm in capistrano

--- web-proxy/config/deploy.rb
+++ web-proxy/config/deploy.rb
@@ -9,15 +9,6 @@ set :stages, %w(testserver staging production)
 set :default_stage, "testserver"
 require 'capistrano/ext/multistage'
 
-#############
-# RVM support
-# Add RVM's lib directory to the load path.
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-# Load RVM's capistrano plugin.
-require "rvm/capistrano"
-set :rvm_ruby_string, '1.9.2'
-set :rvm_type, :system
-
 set :user, "deploy"
 set :use_sudo,    false
 
