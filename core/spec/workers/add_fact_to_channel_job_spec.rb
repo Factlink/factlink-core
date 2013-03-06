3e4a178b06f7bc1d478f97e236e1d8af3557f8a9
Mark IJbema <markijbema@gmail.com>
2013-03-06 13:09:08 +0100

    used readable variable names

--- core/spec/workers/add_fact_to_channel_job_spec.rb
+++ core/spec/workers/add_fact_to_channel_job_spec.rb
@@ -2,61 +2,59 @@ require 'spec_helper'
 
 describe AddFactToChannelJob do
   describe ".perform" do
-    before do
-      @ch = create :channel
-      @f = create :fact
-    end
+    let(:channel) { create :channel }
+    let(:fact)    { create :fact    }
 
     it "should add the fact to the cached facts" do
-      AddFactToChannelJob.perform @f.id, @ch.id
-      @ch.sorted_cached_facts.should include(@f)
-      @f.channels.all.should =~ [@ch]
+      AddFactToChannelJob.perform fact.id, channel.id
+      channel.sorted_cached_facts.should include(fact)
+      fact.channels.all.should =~ [channel]
     end
     it "should call resque on all its containing channels" do
       sup_ch = create :channel
-      @ch.containing_channels << sup_ch
-      @ch.sorted_internal_facts <<@f
+      channel.containing_channels << sup_ch
+      channel.sorted_internal_facts << fact
 
-      Resque.should_receive(:enqueue).with(AddFactToChannelJob, @f.id, sup_ch.id, {})
+      Resque.should_receive(:enqueue).with(AddFactToChannelJob, fact.id, sup_ch.id, {})
 
-      AddFactToChannelJob.perform @f.id, @ch.id
+      AddFactToChannelJob.perform fact.id, channel.id
     end
 
     context "when the channel explicitely deletes the fact" do
       before do
-        @ch.sorted_delete_facts << @f
+        channel.sorted_delete_facts << fact
       end
 
       it "should not add the fact" do
-        AddFactToChannelJob.perform @f.id, @ch.id
-        @ch.sorted_cached_facts.should_not include(@f)
-        @f.channels.should_not include(@ch)
+        AddFactToChannelJob.perform fact.id, channel.id
+        channel.sorted_cached_facts.should_not include(fact)
+        fact.channels.should_not include(channel)
       end
 
       it "should not call resque on its containing channels if the channel explicitely deletes the fact" do
         sup_ch = create :channel
-        @ch.containing_channels << sup_ch
+        channel.containing_channels << sup_ch
 
 
         Resque.should_not_receive(:enqueue)
 
-        AddFactToChannelJob.perform @f.id, @ch.id
+        AddFactToChannelJob.perform fact.id, channel.id
       end
     end
 
     context "when the channel cached facts already included the fact" do
       before do
-        @ch.sorted_cached_facts << @f
+        channel.sorted_cached_facts << fact
       end
 
       it "should not call resque on its containing channels if the channel explicitely deletes the fact" do
         sup_ch = create :channel
-        @ch.containing_channels << sup_ch
+        channel.containing_channels << sup_ch
 
 
         Resque.should_not_receive(:enqueue)
 
-        AddFactToChannelJob.perform @f.id, @ch.id
+        AddFactToChannelJob.perform fact.id, channel.id
       end
     end
 
