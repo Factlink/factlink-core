1e511e31c5f218a05439586fc9cf7bea477f834e
Mark IJbema <markijbema@gmail.com>
2013-03-06 16:51:57 +0100

    extracted out interactor to add one fact to a channel

--- core/app/workers/add_fact_to_channel_job.rb
+++ core/app/workers/add_fact_to_channel_job.rb
@@ -12,29 +12,11 @@ class AddFactToChannelJob
   end
 
   def perform
-    return unless can_perform and should_perform
+    return unless fact and channel
 
-    to_be_extracted_add_command
+    executed = interactor :"channels/add_fact_without_propagation", fact, channel, score, not(channel.created_by_id == initiated_by_id)
 
-    propagate_to_channels
-  end
-
-  def to_be_extracted_add_command
-    interactor :"channels/add_fact_without_propagation", fact, channel, score
-    command :"topics/add_fact", fact.id, channel.slug_title, score.to_s
-    unless posted_by_channel_owner?
-      unless fact_created_by_channel_owner?
-        channel.unread_facts.add(fact)
-      end
-    end
-  end
-
-  def can_perform
-    fact and channel
-  end
-
-  def should_perform
-    not(already_propagated or already_deleted)
+    propagate_to_channels if executed
   end
 
   def score
@@ -45,31 +27,13 @@ class AddFactToChannelJob
     @options['initiated_by_id']
   end
 
-  def already_propagated
-    channel.sorted_cached_facts.include?(fact)
-  end
-
-  def already_deleted
-    channel.sorted_delete_facts.include?(fact)
-  end
-
-  def channel_facts
-    ChannelFacts.new(channel)
-  end
-
-  def posted_by_channel_owner?
-    channel.created_by_id == initiated_by_id
-  end
-
-  def fact_created_by_channel_owner?
-    channel.created_by_id == fact.created_by_id
-  end
-
   def propagate_to_channels
     return unless should_propagate?
 
     channel.containing_channels.ids.each do |ch_id|
-      Resque.enqueue(AddFactToChannelJob, fact.id, ch_id, @options)
+      if ch = Channel[ch_id]
+        interactor :"channels/add_fact_without_propagation", fact, ch, score, true
+      end
     end
   end
 
--- core/spec-unit/interactors/interactors/channels/add_fact_without_propagation_spec.rb
+++ core/spec-unit/interactors/interactors/channels/add_fact_without_propagation_spec.rb
@@ -8,17 +8,126 @@ describe Interactors::Channels::AddFactWithoutPropagation do
       stub_classes
     end
 
-    it 'correctly' do
-      fact = mock(:fact, channels: mock)
-      channel = mock(:channel, sorted_cached_facts: mock, type: 'channel')
-      score = mock
+    it 'adds the fact to the topic and the channel' do
+      fact = mock(:fact, channels: mock, id: mock, created_by_id: 14)
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    type: 'channel',
+                    slug_title: mock,
+                    created_by_id: 14
+      score = mock(:score, to_s: mock)
 
-      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, false
+      interactor.stub(should_execute?: true)
 
       channel.sorted_cached_facts.should_receive(:add).with(fact, score)
       fact.channels.should_receive(:add).with(channel)
 
+      interactor.should_receive(:command).with(:"topics/add_fact",
+          fact.id, channel.slug_title, score.to_s)
+
+      expect(interactor.call).to be_true
+    end
+
+    it 'adds the fact to the unread facts if it is indicated and it makes sense' do
+      fact = mock(:fact, channels: mock, id: mock, created_by_id: 14)
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    unread_facts: mock,
+                    type: 'channel',
+                    slug_title: mock,
+                    created_by_id: 56 # not 14
+      score = mock(:score, to_s: mock)
+      channel.sorted_cached_facts.stub!(:add)
+      fact.channels.stub!(:add)
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, true
+      interactor.stub(should_execute?: true)
+      interactor.stub!(:command)
+
+      channel.unread_facts.should_receive(:add).with(fact)
+
       interactor.call
+    end
+
+    it "doesn't add the fact to the unread facts if the user has seen the fact because he made it" do
+      fact = mock(:fact, channels: mock, id: mock, created_by_id: 14)
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    unread_facts: mock,
+                    type: 'channel',
+                    slug_title: mock,
+                    created_by_id: 14 # not 14
+      score = mock(:score, to_s: mock)
+      channel.sorted_cached_facts.stub!(:add)
+      fact.channels.stub!(:add)
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, true
+      interactor.stub(should_execute?: true)
+      interactor.stub!(:command)
+
+      interactor.call
+    end
+
+    it "returns false if the fact did not need to be added, or wasn't added " do
+      fact = mock
+      channel = mock
+      score = mock
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, false
+      interactor.stub(should_execute?: false)
+
+      expect(interactor.call).to be_false
+    end
+  end
+  describe 'should_execute?' do
+    it "should return false if the fact is already added" do
+      fact = mock
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    sorted_delete_facts: mock
+      score = mock
+
+      channel.sorted_cached_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(true)
+      channel.sorted_delete_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(false)
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, false
+
+      expect(interactor.should_execute?).to be_false
+    end
+    it "should return false if the fact is already deleted" do
+      fact = mock
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    sorted_delete_facts: mock
+      score = mock
+
+      channel.sorted_cached_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(false)
+      channel.sorted_delete_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(true)
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, false
+
+      expect(interactor.should_execute?).to be_false
+    end
+    it "should return true in other cases" do
+      fact = mock
+      channel = mock :channel,
+                    sorted_cached_facts: mock,
+                    sorted_delete_facts: mock
+      score = mock
+
+      channel.sorted_cached_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(false)
+      channel.sorted_delete_facts.should_receive(:include?).any_number_of_times
+                                 .with(fact).and_return(false)
+
+      interactor = Interactors::Channels::AddFactWithoutPropagation.new fact, channel, score, false
+
+      expect(interactor.should_execute?).to be_true
     end
   end
 end
