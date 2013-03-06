051660ef926b7ea03ec3ff8f6568017a70ffeb85
tomdev <tom@north26.nl>
2013-03-06 10:23:22 +0100

    Deploy to proxytest environment

--- web-proxy/config/deploy/proxytest.rb
+++ web-proxy/config/deploy/proxytest.rb
@@ -1,6 +1,6 @@
 server '89.188.27.175', :app, :web, :primary => true
 
-set :deploy_env, 'testserver'
+set :deploy_env, 'proxytest'
 
 role :web, "89.188.27.175"                          # Your HTTP server, Apache/etc
 role :app, "89.188.27.175"                          # This may be the same as your `Web` server
