d5f2e7356b4aa61603e9ed5ffde4acf101ba283d
Mark IJbema <markijbema@gmail.com>
2013-03-07 09:40:05 +0100

    Removed broken tests

--- core/spec/workers/add_fact_to_channel_job_spec.rb
+++ core/spec/workers/add_fact_to_channel_job_spec.rb
@@ -21,33 +21,6 @@ describe AddFactToChannelJob do
         channel.sorted_cached_facts.should_not include(fact)
         fact.channels.should_not include(channel)
       end
-
-      it "should not call resque on its containing channels if the channel explicitely deletes the fact" do
-        sup_ch = create :channel
-        channel.containing_channels << sup_ch
-
-
-        Resque.should_not_receive(:enqueue)
-
-        AddFactToChannelJob.perform fact.id, channel.id
-      end
     end
-
-    context "when the channel cached facts already included the fact" do
-      before do
-        channel.sorted_cached_facts << fact
-      end
-
-      it "should not call resque on its containing channels if the channel explicitely deletes the fact" do
-        sup_ch = create :channel
-        channel.containing_channels << sup_ch
-
-
-        Resque.should_not_receive(:enqueue)
-
-        AddFactToChannelJob.perform fact.id, channel.id
-      end
-    end
-
   end
 end
