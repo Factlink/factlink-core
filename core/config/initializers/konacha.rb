7c656e3cd6f77531e2b557b97abe05b0945284c2
Mark IJbema <markijbema@gmail.com>
2013-03-11 11:30:15 +0100

    Added reminder to fix konacha/poltergeist loading

--- core/config/initializers/konacha.rb
+++ core/config/initializers/konacha.rb
@@ -1,4 +1,9 @@
 if defined?(Konacha)
+
+  puts 'Hello there, I am loading capybara poltergeist'
+  puts 'maybe you are surprised to see me. I get loaded a few'
+  puts 'times too much. Maybe you should fix me, thx!'
+
   require 'capybara/poltergeist'
   Konacha.configure do |config|
     config.spec_dir     = "spec/javascripts"
