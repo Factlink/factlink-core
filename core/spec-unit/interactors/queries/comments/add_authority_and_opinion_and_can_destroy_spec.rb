c02fa01dfe118eccf1a722246125a670ff5585c7
Mark IJbema <markijbema@gmail.com>
2013-03-13 16:22:03 +0100

    Removed eternally pending spec

--- core/spec-unit/interactors/queries/comments/add_authority_and_opinion_and_can_destroy_spec.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../../../../app/interactors/queries/comments/add_authority_and_opinion_and_can_destroy'
-
-describe Queries::Comments::AddAuthorityAndOpinionAndCanDestroy do
-  pending "we should add some tests here"
-end
