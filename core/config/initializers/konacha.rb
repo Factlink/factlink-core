6f97d01948bd7a87e1114505003c157b2be69d7f
Mark IJbema <markijbema@gmail.com>
2013-03-14 16:59:18 +0100

    only load poltergeist for konacha when we are going to run konacha

--- core/config/initializers/konacha.rb
+++ core/config/initializers/konacha.rb
@@ -1,10 +1,4 @@
 if defined?(Konacha)
-
-  puts 'Hello there, I am loading capybara poltergeist'
-  puts 'maybe you are surprised to see me. I get loaded a few'
-  puts 'times too much. Maybe you should fix me, thx!'
-
-  require 'capybara/poltergeist'
   Konacha.configure do |config|
     config.spec_dir     = "spec/javascripts"
     config.spec_matcher = /_spec\./
