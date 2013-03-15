8b1644416c9a8e0c5301ae472e67707479e4f95d
Remon Oldenbeuving <remon@oldenbeuving.net>
2013-03-15 15:42:58 +0100

    Enable UDP for Sentry2

--- core/config/initializers/raven.rb
+++ core/config/initializers/raven.rb
@@ -4,7 +4,7 @@ sentry_conf = YAML::load_file(Rails.root.join('config/sentry.yml'))[Rails.env].a
 
 if sentry_conf
   Raven.configure do |config|
-    config.dsn = "https://#{sentry_conf["public"]}:#{sentry_conf["secret"]}@sentry2.factlink.com/#{sentry_conf["project_id"]}"
+    config.dsn = "udp://#{sentry_conf["public"]}:#{sentry_conf["secret"]}@sentry2.factlink.com:9001/#{sentry_conf["project_id"]}"
     config.environments = %w[ testserver staging production ]
     config.excluded_exceptions = ['ActionController::RoutingError']
     config.processors = [Raven::Processor::SanitizeData]
