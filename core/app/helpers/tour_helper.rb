184f081788df5c7fac3f884ce461cbf29a7decf4
Mark IJbema <markijbema@gmail.com>
2013-03-19 10:00:53 +0100

    Cleaning up names and logic in tour_controller and associated templates and routes

--- core/app/helpers/tour_helper.rb
+++ core/app/helpers/tour_helper.rb
@@ -7,14 +7,14 @@ module TourHelper
 
   def tour_steps
     [
-      'create_your_first_factlink',
-      'almost_done',
-      'choose_channels'
+      :create_your_first_factlink,
+      :install_extension,
+      :choose_channels
     ]
   end
 
   def next_tourstep
-    current_step = controller.action_name.to_s
+    current_step = controller.action_name.to_sym
     current_index = tour_steps.index(current_step)
     tour_steps[current_index + 1]
   end
