cc17ce02a1756164357d48b98b0b636bb5c18369
Mark IJbema <markijbema@gmail.com>
2013-03-21 16:19:58 +0100

    Switching to mandrill because of big dongle issues

--- core/config/email_configuration.rb
+++ core/config/email_configuration.rb
@@ -1,4 +1,5 @@
-ActionMailer::Base.smtp_settings = {
+
+sendgrid_settings = {
   user_name: "factlink",
   password: '4q"_NdREV89[s0zq',
   domain: "factlink.com",
@@ -6,5 +7,16 @@ ActionMailer::Base.smtp_settings = {
   port: 465,
   authentication: :plain,
   enable_starttls_auto: true,
-  openssl_verify_mode: "none" # secure connection, but not verifying
 }
+
+mandrill_settings = {
+  user_name: "mark@factlink.com",
+  password: 'YKJLeC_CBFnr1sxc6dtjXw',
+  domain: "factlink.com",
+  address: "smtp.mandrillapp.com",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
+ActionMailer::Base.smtp_settings = mandrill_settings
