0c32dd639b445ffde991dd2fa493acc9ecaa5b2a
Mark IJbema <markijbema@gmail.com>
2013-04-01 18:47:13 +0200

    Trying with global better settings

--- core/bin/ci/201_old_unit_tests.sh
+++ core/bin/ci/201_old_unit_tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "Running unit tests"
 
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=100000000
-export RUBY_HEAP_FREE_MIN=500000
-
 OUTPUTFILE=$(mktemp /tmp/integration.XXXX)
 
 bundle exec rspec spec | tee "$OUTPUTFILE"
