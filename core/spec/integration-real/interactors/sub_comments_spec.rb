144c3ae6b89d6bea93691b852d5fd939e287b698
Mark IJbema <markijbema@gmail.com>
2013-04-02 16:30:33 +0200

    Simplified structure of test

--- core/spec/integration-real/interactors/sub_comments_spec.rb
+++ core/spec/integration-real/interactors/sub_comments_spec.rb
@@ -5,66 +5,54 @@ describe 'subcomments' do
 
   let(:current_user) { create :user }
 
-  describe 'commenting on a comment on a fact' do
-    before do
-      @fact = create :fact, created_by: create(:user).graph_user
+  describe 'initially' do
+    it 'has no subcomments and can be deleted' do
       as(current_user) do |pavlov|
-        @comment = pavlov.interactor :'comments/create', @fact.id.to_i, 'believes', 'Gekke Gerrit'
+        fact = pavlov.interactor :'facts/create', 'a fact', '', ''
+        comment = pavlov.interactor :'comments/create', fact.id.to_i, 'believes', 'Gekke Gerrit'
+
+        sub_comments = pavlov.interactor :'sub_comments/index_for_comment', comment.id.to_s
+        comments = pavlov.interactor :'evidence/for_fact_id', fact.id.to_s, :supporting
+
+        expect(sub_comments).to eq []
+        expect(comments.map(&:can_destroy?)).to eq [true]
+      end
+    end
+  end
+
+  describe 'after adding a few subcomments' do
+    it 'should have the subcomments we added and cannot be deleted' do
+      as(current_user) do |pavlov|
+        fact = pavlov.interactor :'facts/create', 'a fact', '', ''
+        comment = pavlov.interactor :'comments/create', fact.id.to_i, 'believes', 'Gekke Gerrit'
+
+        sub_comment1 = pavlov.interactor :'sub_comments/create_for_comment', comment.id.to_s, 'Gekke Gerrit'
+        sub_comment2 = pavlov.interactor :'sub_comments/create_for_comment', comment.id.to_s, 'Handige Harrie'
+
+        sub_comments = pavlov.interactor :'sub_comments/index_for_comment', comment.id.to_s
+        comments = pavlov.interactor :'evidence/for_fact_id', fact.id.to_s, :supporting
+
+        expect(sub_comments.map(&:content)).to eq ['Gekke Gerrit', 'Handige Harrie']
+        expect(comments.map(&:can_destroy?)).to eq [false]
       end
     end
 
-    describe 'initially' do
-      it 'has no subcomments' do
+    describe "after removing one subcomment again" do
+      it "should only contain the other comment" do
         as(current_user) do |pavlov|
-          sub_comments = pavlov.interactor :'sub_comments/index_for_comment', @comment.id.to_s
+          fact = pavlov.interactor :'facts/create', 'a fact', '', ''
+          comment = pavlov.interactor :'comments/create', fact.id.to_i, 'believes', 'Gekke Gerrit'
 
-          expect(sub_comments).to eq []
-        end
-      end
+          sub_comment1 = pavlov.interactor :'sub_comments/create_for_comment', comment.id.to_s, 'Gekke Gerrit'
+          sub_comment2 = pavlov.interactor :'sub_comments/create_for_comment', comment.id.to_s, 'Handige Harrie'
 
-      it 'can be deleted' do
-        as(current_user) do |pavlov|
-          comments = pavlov.interactor :'evidence/for_fact_id', @fact.id.to_s, :supporting
+          pavlov.interactor :'sub_comments/destroy', sub_comment1.id.to_s
 
-          expect(comments.map(&:can_destroy?)).to eq [true]
+          sub_comments = pavlov.interactor :'sub_comments/index_for_comment', comment.id.to_s
+          expect(sub_comments.map(&:content)).to eq ['Handige Harrie']
         end
       end
     end
 
-    describe 'after adding a few subcomments' do
-      before do
-        as(current_user) do |pavlov|
-          @sub_comment1 = pavlov.interactor :'sub_comments/create_for_comment', @comment.id.to_s, 'Gekke Gerrit'
-          @sub_comment2 = pavlov.interactor :'sub_comments/create_for_comment', @comment.id.to_s, 'Handige Harrie'
-        end
-      end
-
-      it 'should have the subcomments we added' do
-        as(current_user) do |pavlov|
-          sub_comments = pavlov.interactor :'sub_comments/index_for_comment', @comment.id.to_s
-
-          expect(sub_comments.map(&:content)).to eq ['Gekke Gerrit', 'Handige Harrie']
-        end
-      end
-
-      describe "after removing on subcomment again" do
-        it "should only contain the other comment" do
-          as(current_user) do |pavlov|
-            pavlov.interactor :'sub_comments/destroy', @sub_comment1.id.to_s
-
-            sub_comments = pavlov.interactor :'sub_comments/index_for_comment', @comment.id.to_s
-            expect(sub_comments.map(&:content)).to eq ['Handige Harrie']
-          end
-        end
-      end
-
-      it 's parent cannot be deleted any more' do
-        as(current_user) do |pavlov|
-          comments = pavlov.interactor :'evidence/for_fact_id', @fact.id.to_s, :supporting
-
-          expect(comments.map(&:can_destroy?)).to eq [false]
-        end
-      end
-    end
   end
 end
