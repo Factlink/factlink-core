55e1e15998f6ae78cf22d28af40e01385684a957
Jan Paul Posma <me@janpaulposma.nl>
2013-04-02 16:02:57 +0200

    Added spec for migration to check that facts are moved between channels but topics are left untouched.

--- core/spec/integration-real/workers/move_away_subchannels_with_other_title_spec.rb
+++ core/spec/integration-real/workers/move_away_subchannels_with_other_title_spec.rb
@@ -8,6 +8,8 @@ describe MoveAwaySubchannelsWithOtherTitle do
   let(:user3) {create :user}
   let(:user4) {create :user}
   let(:channel_user) {create :user}
+  let(:fact1) {create :fact}
+  let(:fact2) {create :fact}
 
   describe '#subchannels_with_other_title' do
     it "returns all subchannels which don't have the same title as the top channel" do
@@ -70,10 +72,75 @@ describe MoveAwaySubchannelsWithOtherTitle do
         expect(channel.contained_channels.ids).to match_array [ch1.id]
       end
     end
+
+    it "moves facts" do
+      old_channel, new_channel, subchannel = ()
+
+      as(user1) do |pavlov|
+        subchannel = pavlov.command :'channels/create', 'bar'
+      end
+
+      as(channel_user) do |pavlov|
+        old_channel = pavlov.command :'channels/create', 'foo'
+        new_channel = pavlov.command :'channels/create', 'bar'
+        pavlov.command :'channels/add_subchannel', old_channel, subchannel
+      end
+
+      as(user1) do |pavlov|
+        pavlov.interactor :'channels/add_fact', fact1, subchannel
+      end
+
+      worker = MoveAwaySubchannelsWithOtherTitle.new(old_channel.id)
+      worker.reassign_subchannel subchannel
+
+      as(channel_user) do |pavlov|
+        old_facts = pavlov.interactor :'channels/facts', old_channel.id, nil, nil
+        expect(old_facts).to match_array []
+
+        new_facts = pavlov.interactor :'channels/facts', new_channel.id, nil, nil
+        expect(new_facts[0][:item]).to   eq fact1
+        expect(new_facts.size).to eq 1
+      end
+    end
+
+    it "does not touch topics" do
+      old_channel, new_channel, subchannel, old_foo_topics, old_bar_topics = ()
+
+      as(user1) do |pavlov|
+        subchannel = pavlov.command :'channels/create', 'bar'
+      end
+
+      as(channel_user) do |pavlov|
+        old_channel = pavlov.command :'channels/create', 'foo'
+        new_channel = pavlov.command :'channels/create', 'bar'
+        pavlov.command :'channels/add_subchannel', old_channel, subchannel
+      end
+
+      as(user1) do |pavlov|
+        pavlov.interactor :'channels/add_fact', fact1, subchannel
+      end
+
+      as(channel_user) do |pavlov|
+        pavlov.interactor :'channels/add_fact', fact2, new_channel
+        old_foo_topics = pavlov.interactor :'topics/facts', 'foo', nil, nil
+        old_bar_topics = pavlov.interactor :'topics/facts', 'bar', nil, nil
+      end
+
+      worker = MoveAwaySubchannelsWithOtherTitle.new(old_channel.id)
+      worker.reassign_subchannel subchannel
+
+      as(channel_user) do |pavlov|
+        new_foo_topics = pavlov.interactor :'topics/facts', 'foo', nil, nil
+        expect(old_foo_topics).to match_array new_foo_topics
+
+        new_bar_topics = pavlov.interactor :'topics/facts', 'bar', nil, nil
+        expect(old_bar_topics).to match_array new_bar_topics
+      end
+    end
   end
   describe '#perform' do
     it "moves away all subchannels which don't belong here" do
-            channel, ch1, ch2, ch3, ch4 = ()
+      channel, ch1, ch2, ch3, ch4 = ()
 
       as(user1) do |pavlov|
         ch1 = pavlov.command :'channels/create', 'Foo'
@@ -110,7 +177,6 @@ describe MoveAwaySubchannelsWithOtherTitle do
 
         expect(channel.contained_channels.ids).to match_array [ch1.id, ch4.id]
       end
-
     end
   end
 end
