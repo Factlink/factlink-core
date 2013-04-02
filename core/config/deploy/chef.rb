815938b0830a83d73ad0fe14a1fea2b82607c4a4
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-02 14:07:56 +0200

    Made capistrano/chef deploy the correct branch

--- core/config/deploy/chef.rb
+++ core/config/deploy/chef.rb
@@ -5,6 +5,8 @@ set :full_url, 'https://10.253.0.101'
 set :deploy_env, 'chef'
 set :rails_env,  'chef' # Also used by capistrano for some specific tasks
 
+set :branch, 'feature/chef'
+
 role :web, '10.253.0.101'                          # Your HTTP server, Apache/etc
 role :app, '10.253.0.101'                          # This may be the same as your `Web` server
 role :db,  '10.253.0.101', :primary => true # This is where Rails migrations will run
