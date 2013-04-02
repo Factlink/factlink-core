364b6798f8be877df09371688249f6ad6516d2f2
Jan Paul Posma <me@janpaulposma.nl>
2013-04-02 11:36:35 +0200

    AddChannelToChannel spec should also use sorted_internal_facts since we don't propagate facts anymore.

--- core/spec/workers/add_channel_to_channel_spec.rb
+++ core/spec/workers/add_channel_to_channel_spec.rb
@@ -7,7 +7,7 @@ describe AddChannelToChannel do
 
     it "should add the fact to the cached facts of the super channel using Resque" do
       fact = create :fact
-      sub_channel.sorted_cached_facts << fact
+      sub_channel.sorted_internal_facts << fact
 
       Fact.should_receive(:[]).with(fact.id)
            .and_return(fact)
@@ -22,13 +22,13 @@ describe AddChannelToChannel do
 
     it "should only add NUMBER_OF_INITIAL_FACTS facts to the super channel" do
       oldest_fact = create :fact
-      sub_channel.sorted_cached_facts << oldest_fact
+      sub_channel.sorted_internal_facts << oldest_fact
 
       facts = []
 
       AddChannelToChannel::NUMBER_OF_INITIAL_FACTS.times do |i|
         facts[i] = create :fact
-        sub_channel.sorted_cached_facts << facts[i]
+        sub_channel.sorted_internal_facts << facts[i]
       end
 
       Interactors::Channels::AddFactWithoutPropagation.should_receive(:new)
