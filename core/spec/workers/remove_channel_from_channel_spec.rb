c99024ddb28cf2386f71149baee570ede84f40eb
Mark IJbema <markijbema@gmail.com>
2013-04-02 13:12:41 +0200

    Cleaned up spec

--- core/spec/workers/remove_channel_from_channel_spec.rb
+++ core/spec/workers/remove_channel_from_channel_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
-describe AddChannelToChannel do
+describe RemoveChannelFromChannel do
   describe ".perform" do
-    before do
-      @ch = create :channel
-      @sub_ch = create :channel
-      @f = create :fact
-
-      @sub_ch.sorted_cached_facts << @f
-    end
-
     it "should add the fact to the cached facts of the super channel using Resque" do
-      Resque.should_receive(:enqueue).with(RemoveFactFromChannel, @f.id, @ch.id)
-      RemoveChannelFromChannel.perform @sub_ch.id, @ch.id
+      ch = create :channel
+      sub_ch = create :channel
+      fact = create :fact
+
+      sub_ch.sorted_cached_facts << fact
+
+      Resque.should_receive(:enqueue).with(RemoveFactFromChannel, fact.id, ch.id)
+      RemoveChannelFromChannel.perform sub_ch.id, ch.id
     end
   end
 end
