37da6d5f5782bae2955a07b7c8a0afb53d1ee172
Mark IJbema <markijbema@gmail.com>
2013-04-02 14:31:59 +0200

    use new 'as' syntax in integrationtests

--- core/spec/integration-real/interactors/channels/adding_facts_spec.rb
+++ core/spec/integration-real/interactors/channels/adding_facts_spec.rb
@@ -1,60 +1,72 @@
 require 'spec_helper'
 
 describe 'when adding a fact to a channel' do
-  include Pavlov::Helpers
-
-  let(:fact) {create :fact}
-
-  def pavlov_options
-    {current_user: (create :user), ability: stub(:ability, can?: true)}
-  end
+  include PavlovSupport
+  let(:current_user) {create :user}
 
   context 'with no followers' do
     it "adds the fact to the channel" do
-      channel = command :'channels/create', 'something'
-      interactor :"channels/add_fact", fact, channel
+      fact = create :fact
 
-      facts = interactor :'channels/facts', channel.id, nil, nil
-      latest_fact = facts.map{|i| i[:item]}[0]
-      expect(latest_fact).to eq fact
+      as(current_user) do |pavlov|
+        channel = pavlov.command :'channels/create', 'something'
+        pavlov.interactor :"channels/add_fact", fact, channel
+
+        facts = pavlov.interactor :'channels/facts', channel.id, nil, nil
+        latest_fact = facts.map{|i| i[:item]}[0]
+        expect(latest_fact).to eq fact
+      end
     end
 
     it "adds the fact to the channels topic" do
-      channel = command :'channels/create', 'something'
-      interactor :"channels/add_fact", fact, channel
+      fact = create :fact
 
-      Topic.for_channel(channel)
-      facts = interactor :'topics/facts', channel.slug_title, nil, nil
+      as(current_user) do |pavlov|
+        channel = pavlov.command :'channels/create', 'something'
+        pavlov.interactor :"channels/add_fact", fact, channel
+
+        Topic.for_channel(channel)
+        facts = pavlov.interactor :'topics/facts', channel.slug_title, nil, nil
+      end
     end
   end
 
   context 'with a follower' do
     it "should add the fact to the follower" do
-      channel = command :'channels/create', 'something'
-      sub_channel = command :'channels/create', 'something2'
+      fact = create :fact
 
-      command :"channels/add_subchannel", channel, sub_channel
-      interactor :"channels/add_fact", fact, sub_channel
+      as(current_user) do |pavlov|
+        channel = pavlov.command :'channels/create', 'something'
+        sub_channel = pavlov.command :'channels/create', 'something2'
 
-      facts = interactor :'channels/facts', channel.id, nil, nil
-      latest_fact = facts.map{|i| i[:item]}[0]
-      expect(latest_fact).to eq fact
+        pavlov.command :"channels/add_subchannel", channel, sub_channel
+        pavlov.interactor :"channels/add_fact", fact, sub_channel
+
+        facts = pavlov.interactor :'channels/facts', channel.id, nil, nil
+        latest_fact = facts.map{|i| i[:item]}[0]
+        expect(latest_fact).to eq fact
+      end
     end
   end
 
   context 'with a followed follower' do
     it "should not add the fact to the indirect follower" do
-      channel = command :'channels/create', 'something'
-      sub_channel = command :'channels/create', 'something2'
-      sub_sub_channel = command :'channels/create', 'something3'
+      fact = create :fact
 
-      command :"channels/add_subchannel", channel, sub_channel
-      command :"channels/add_subchannel", sub_channel, sub_sub_channel
+      as(current_user) do |pavlov|
 
-      interactor :"channels/add_fact", fact, sub_sub_channel
+        channel = pavlov.command :'channels/create', 'something'
+        sub_channel = pavlov.command :'channels/create', 'something2'
+        sub_sub_channel = pavlov.command :'channels/create', 'something3'
 
-      facts = interactor :'channels/facts', channel.id, nil, nil
-      expect(facts).to eq []
+        pavlov.command :"channels/add_subchannel", channel, sub_channel
+        pavlov.command :"channels/add_subchannel", sub_channel, sub_sub_channel
+
+        pavlov.interactor :"channels/add_fact", fact, sub_sub_channel
+
+        facts = pavlov.interactor :'channels/facts', channel.id, nil, nil
+        expect(facts).to eq []
+      end
     end
   end
 end
