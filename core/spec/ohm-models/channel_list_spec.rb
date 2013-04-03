411cac627017d1b32ad10ab0a66775fd0d72af5f
Mark IJbema <markijbema@gmail.com>
2013-04-03 10:17:40 +0200

    Removed unneeded ivars

--- core/spec/ohm-models/channel_list_spec.rb
+++ core/spec/ohm-models/channel_list_spec.rb
@@ -20,46 +20,46 @@ describe ChannelList do
     end
     describe "after creating a channel" do
       it "contains the channel and the expected channels" do
-        @ch1 = Channel.create created_by: @graph_user, title: 'foo'
-        ChannelList.new(@graph_user).channels.to_a.should =~ [@ch1]+@expected_channels
+        ch1 = Channel.create created_by: @graph_user, title: 'foo'
+        ChannelList.new(@graph_user).channels.to_a.should =~ [ch1]+@expected_channels
       end
     end
     describe "after creating two channels" do
       it "contains the two channels and the expected channels" do
-        @ch1 = Channel.create created_by: @graph_user, title: 'foo'
-        @ch2 = Channel.create created_by: @graph_user, title: 'foo2'
-        ChannelList.new(@graph_user).channels.to_a.should =~ [@ch1,@ch2]+@expected_channels
+        ch1 = Channel.create created_by: @graph_user, title: 'foo'
+        ch2 = Channel.create created_by: @graph_user, title: 'foo2'
+        ChannelList.new(@graph_user).channels.to_a.should =~ [ch1,ch2]+@expected_channels
       end
     end
     describe "after creating two channels and deleting the first" do
       it "should contain the second channel and the expected channels" do
-        @ch1 = Channel.create created_by: @graph_user, title: 'foo'
-        @ch2 = Channel.create created_by: @graph_user, title: 'foo2'
-        @ch1.delete
-        ChannelList.new(@graph_user).channels.to_a.should =~ [@ch2]+@expected_channels
+        ch1 = Channel.create created_by: @graph_user, title: 'foo'
+        ch2 = Channel.create created_by: @graph_user, title: 'foo2'
+        ch1.delete
+        ChannelList.new(@graph_user).channels.to_a.should =~ [ch2]+@expected_channels
       end
     end
     describe "after creating a channel, and someone else creates a channel" do
       it "only contains our channel, and the expected channels" do
-        @ch1 = Channel.create created_by: @graph_user, title: 'foo'
-        @ch2 = Channel.create created_by: (create :graph_user), title: 'foo2'
+        ch1 = Channel.create created_by: @graph_user, title: 'foo'
+        ch2 = Channel.create created_by: (create :graph_user), title: 'foo2'
 
-        ChannelList.new(@graph_user).channels.to_a.should =~ [@ch1]+@expected_channels
+        ChannelList.new(@graph_user).channels.to_a.should =~ [ch1]+@expected_channels
       end
     end
   end
 
   describe 'get_by_topic_slug' do
     it "returns nil if the list does not contain said channel" do
-      @graph_user = create :graph_user
-      ch = ChannelList.new(@graph_user).get_by_slug_title 'henk'
+      graph_user = create :graph_user
+      ch = ChannelList.new(graph_user).get_by_slug_title 'henk'
       expect(ch).to be_nil
     end
     it "returns one channel by topic slug" do
-      @graph_user = create :graph_user
-      @ch1 = Channel.create created_by: @graph_user, title: 'foo'
-      ch = ChannelList.new(@graph_user).get_by_slug_title 'foo'
-      expect(ch).to eq @ch1
+      graph_user = create :graph_user
+      channel1 = Channel.create created_by: graph_user, title: 'foo'
+      ch = ChannelList.new(graph_user).get_by_slug_title 'foo'
+      expect(ch).to eq channel1
     end
   end
 
