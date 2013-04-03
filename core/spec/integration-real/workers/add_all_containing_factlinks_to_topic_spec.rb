7d13139b970b4b446fac17c44b5bcc20994368d7
Mark IJbema <markijbema@gmail.com>
2013-04-03 08:05:58 +0200

    use 'as' instead of pavlov options

--- core/spec/integration-real/workers/add_all_containing_factlinks_to_topic_spec.rb
+++ core/spec/integration-real/workers/add_all_containing_factlinks_to_topic_spec.rb
@@ -1,42 +1,48 @@
 require 'spec_helper'
 
 describe AddAllContainingFactlinksToTopic do
-  include Pavlov::Helpers
+  include PavlovSupport
 
-  before do
+  it 'adds all containing Factlinks to the Topics Facts' do
+    title = 'Channel Title'
     @user1 = create :user
     @user2 = create :user
     @user3 = create :user
 
-    @channel1 = create :channel, title: "Channel Title", created_by: @user1.graph_user
-    @channel2 = create :channel, title: "Channel Title", created_by: @user2.graph_user
-    @channel3 = create :channel, title: "Channel Title", created_by: @user3.graph_user
+    as(@user1) do |pavlov|
+      @fact1 = create :fact
+      @channel1 = create :channel, title: title, created_by: @user1.graph_user
+      pavlov.interactor :"channels/add_fact", @fact1, @channel1
+    end
+
+    as(@user2) do |pavlov|
+      @fact2 = create :fact
+      @channel2 = create :channel, title: title, created_by: @user2.graph_user
+      pavlov.interactor :"channels/add_fact", @fact2, @channel2
+    end
+
+    as(@user3) do |pavlov|
+      @fact3 = create :fact
+      @channel3 = create :channel, title: title, created_by: @user3.graph_user
+      pavlov.interactor :"channels/add_fact", @fact3, @channel3
+    end
 
     @slug_title = @channel1.slug_title
 
-    @fact1 = create :fact
-    @fact2 = create :fact
-    @fact3 = create :fact
-
-    interactor :"channels/add_fact", @fact1, @channel1
-    interactor :"channels/add_fact", @fact2, @channel2
-    interactor :"channels/add_fact", @fact3, @channel3
-  end
-
-  it 'adds all containing Factlinks to the Topics Facts' do
     Topic.redis[@slug_title][:facts].del
 
-    expect(interactor(:"topics/facts", @slug_title, nil, nil).length).to eq 0
+    as(@user1) do |pavlov|
+      nr_of_facts_before_running_job =
+        pavlov.interactor(:"topics/facts", @slug_title, nil, nil).length
 
-    AddAllContainingFactlinksToTopic.perform(Topic.find_by(slug_title: @slug_title).id)
+      AddAllContainingFactlinksToTopic.perform(Topic.find_by(slug_title: @slug_title).id)
 
-    expect(interactor(:"topics/facts", @slug_title, nil, nil).length).to eq 3
+      nr_of_facts_after_running_job =
+        pavlov.interactor(:"topics/facts", @slug_title, nil, nil).length
+
+      expect(nr_of_facts_before_running_job).to eq 0
+      expect(nr_of_facts_after_running_job).to eq 3
+    end
   end
 
-  def pavlov_options
-    {
-      no_current_user: true,
-      current_user: true
-    }
-  end
 end
