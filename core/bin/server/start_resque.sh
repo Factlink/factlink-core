c6ae6ff9003d90798b437fe31d0534248a9ee6c4
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-03 18:35:27 +0200

    Added some debugging to the start_resque script

--- core/bin/server/start_resque.sh
+++ core/bin/server/start_resque.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 count=`ps x | grep -v grep | grep -c "resque-[0-9]"`
 
+echo "Resque instances running: $count"
+
 if [ "$count" -lt "1" ]; then
-    . /home/deploy/.bash_profile
+  echo "Starting Resque"
+  source ~/.bash_profile
 
-    cd /applications/core/current
+  cd /applications/core/current
 
-    export PIDFILE=/home/deploy/resque.pid
-    export QUEUE=*
-    export PIDFILE=$PIDFILE
+  export PIDFILE=/home/deploy/resque.pid
+  export QUEUE=*
 
-    nohup bundle exec rake environment resque:work &> /applications/core/shared/log/resque.log &
+  nohup bundle exec rake environment resque:work &> /applications/core/shared/log/resque.log &
+else
+  echo "Not starting Resque"
 fi
 exit 0
