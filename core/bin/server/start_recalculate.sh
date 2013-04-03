69eb335a899605193cab3e3ad48429b1c73e877b
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-03 09:02:55 +0200

    Fixed recalculate script so it works with rbenv bundle (and doesn't need a bash_profile anymore)

--- core/bin/server/start_recalculate.sh
+++ core/bin/server/start_recalculate.sh
@@ -3,13 +3,11 @@
 count=`ps x | grep -v grep | grep -c 'rake fact_graph:recalculate'`
 
 if [ "$count" -lt "1" ]; then
-    . /home/deploy/.bash_profile
-
     cd /applications/core/current
 
     export PIDFILE=/home/deploy/recalculate.pid
 
     export NEWRELIC_DISPATCHER="FactGraph recalculate"
-    nohup bundle exec rake environment fact_graph:recalculate PIDFILE=$PIDFILE >> /applications/core/current/log/fact_graph.log 2>&1
+    nohup /usr/local/rbenv/shims/bundle exec rake environment fact_graph:recalculate PIDFILE=$PIDFILE >> /applications/core/current/log/fact_graph.log 2>&1
 fi
 exit 0
