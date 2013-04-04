09f1be97090f3d34deaaeef780708e29570881ff
Jan Deelstra <jan@deelstra.org>
2013-04-04 09:51:51 +0200

    Interactor to unfollow a user.

---
+++ core/spec-unit/interactors/interactors/users/unfollow_user_spec.rb
@@ -0,0 +1,85 @@
+require 'pavlov_helper'
+require_relative '../../../../app/interactors/interactors/users/unfollow_user'
+
+describe Interactors::Users::UnfollowUser do
+  include PavlovSupport
+
+  describe '#authorized?' do
+    before do
+      described_class.any_instance.
+        should_receive(:validate).
+        and_return(true)
+    end
+
+    it 'throws when no current_user' do
+      user_id = mock
+      user_to_unfollow_id = mock
+
+      expect { described_class.new user_id, user_to_unfollow_id }.
+        to raise_error Pavlov::AccessDenied,'Unauthorized'
+    end
+  end
+
+  describe '.new' do
+    before do
+      described_class.any_instance.
+        should_receive(:authorized?).
+        and_return(true)
+      described_class.any_instance.
+        should_receive(:validate).
+        and_return(true)
+    end
+
+    it 'returns an object' do
+      user_id = mock
+      user_to_unfollow_id = mock
+      interactor = described_class.new user_id, user_to_unfollow_id
+
+      expect(interactor).to_not be_nil
+    end
+  end
+
+  describe '#execute' do
+    before do
+      described_class.any_instance.
+        should_receive(:authorized?).
+        and_return(true)
+      described_class.any_instance.
+        should_receive(:validate).
+        and_return(true)
+    end
+
+    it 'calls a command to unfollow user and returns the user' do
+      user_id = mock
+      user_to_unfollow_id = mock
+      user = mock
+
+      interactor = described_class.new user_id, user_to_unfollow_id
+      interactor.should_receive(:command).
+        with(:'users/unfollow_user', user_id, user_to_unfollow_id).
+        and_return(user)
+
+      returned_user = interactor.execute
+
+      expect(returned_user).to eq user
+    end
+  end
+
+  describe '#validate' do
+    before do
+      described_class.any_instance.
+        should_receive(:authorized?).
+        and_return(true)
+    end
+
+    it 'calls the correct validation methods' do
+      user_id = mock
+      user_to_unfollow_id = mock
+
+      described_class.any_instance.should_receive(:validate_hexadecimal_string).with(:user_id, user_id)
+      described_class.any_instance.should_receive(:validate_hexadecimal_string).with(:user_to_unfollow_id, user_to_unfollow_id)
+
+      interactor = described_class.new user_id, user_to_unfollow_id
+    end
+  end
+end
