3c3c9d313a19fd1809551199ecdc1898969f0512
Jan Deelstra <jan@deelstra.org>
2013-04-08 16:52:24 +0200

    Added two integration tests for following users and seeing there facts in the stream.

--- core/spec/acceptance/profile_spec.rb
+++ core/spec/acceptance/profile_spec.rb
@@ -1,17 +1,89 @@
 require 'acceptance_helper'
 
-feature "the profile page", type: :request do
-  include Acceptance
+feature 'the profile page', type: :request do
   include Acceptance::ProfileHelper
-
-  background do
-    @user = sign_in_user FactoryGirl.create :active_user
-    @channel = create :channel, created_by: @user.graph_user
-  end
+  include Acceptance::FactHelper
+  include PavlovSupport
 
   scenario "the users top channels should render" do
-    go_to_profile_page_of @user
+    user = sign_in_user FactoryGirl.create :active_user
+    channel = create :channel, created_by: user.graph_user
 
-    find('.top-channels *', text: @channel.title)
+    go_to_profile_page_of user
+
+    find('.top-channels *', text: channel.title)
+  end
+
+  scenario 'follow a user and unfollow a user' do
+    following_user = sign_in_user FactoryGirl.create :active_user
+    followed_user = FactoryGirl.create :active_user
+
+    visit user_path(followed_user)
+
+    # initial state check of other user profile
+    check_follower_following_count 0, 0
+
+    # follow other user
+    click_button 'Follow'
+
+    # check if info is updated directly
+    check_follower_following_count 0, 1
+
+    # visit own profile
+    visit user_path(following_user)
+
+    # check if own profile is updated
+    check_follower_following_count 1, 0
+
+    # goto followed user profile with a full refresh
+    visit user_path(followed_user)
+
+    # check if info is persisted
+    check_follower_following_count 0, 1
+
+    # unfollow page
+    click_button 'Following'
+
+    # check if info is updated directly
+    check_follower_following_count 0, 0
+
+    # refresh page
+    visit user_path(followed_user)
+
+    # check if info is persisted
+    check_follower_following_count 0, 0
+
+    # visit own profile
+    visit user_path(following_user)
+
+    # check if own profile is updated
+    check_follower_following_count 0, 0
+  end
+
+  scenario 'follow a user and facts created by that user show up in your stream' do
+    following_user = sign_in_user FactoryGirl.create :active_user
+    followed_user = FactoryGirl.create :active_user
+
+    visit user_path(followed_user)
+
+    # initial state check of other user profile
+    check_follower_following_count 0, 0
+
+    # follow other user
+    click_button 'Follow'
+
+    # check if info is updated directly
+    check_follower_following_count 0, 1
+
+    displaystring = 'this is a displaystring for fact'
+
+    factlink = nil
+    as(followed_user) do |backend|
+      backend.interactor :'facts/create', displaystring,'','title'
+    end
+
+    visit channel_activities_path(following_user.username,following_user.graph_user.stream_id)
+
+    page.should have_content displaystring
   end
 end
