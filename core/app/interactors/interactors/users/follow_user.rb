ba10e3c47b9e831aafcad633cb763ced4621df7e
Mark IJbema <markijbema@gmail.com>
2013-04-08 14:00:26 +0200

    Don't use ivars anymore

--- core/app/interactors/interactors/users/follow_user.rb
+++ core/app/interactors/interactors/users/follow_user.rb
@@ -12,16 +12,16 @@ module Interactors
       end
 
       def execute
-        user = query :user_by_username, @user_name
-        user_to_follow = query :user_by_username, @user_to_follow_user_name
+        user = query :user_by_username, user_name
+        user_to_follow = query :user_by_username, user_to_follow_user_name
         command :'users/follow_user', user.graph_user_id, user_to_follow.graph_user_id
         command :'create_activity', user.graph_user, :followed_user, user_to_follow.graph_user, nil
         nil
       end
 
       def validate
-        validate_nonempty_string :user_name, @user_name
-        validate_nonempty_string :user_to_follow_user_name, @user_to_follow_user_name
+        validate_nonempty_string :user_name, user_name
+        validate_nonempty_string :user_to_follow_user_name, user_to_follow_user_name
       end
     end
   end
