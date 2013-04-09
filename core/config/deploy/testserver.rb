5fba06cb13d5b822227be078de4001a76c090e66
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-09 13:27:29 +0200

    Removed branch specification for testserver, ready to merge back

--- core/config/deploy/testserver.rb
+++ core/config/deploy/testserver.rb
@@ -5,8 +5,6 @@ set :full_url, 'https://factlink-testserver.inverselink.com'
 set :deploy_env, 'testserver'
 set :rails_env,  'testserver' # Also used by capistrano for some specific tasks
 
-set :branch, 'feature/chef'
-
 role :web, "factlink-testserver.inverselink.com"                   # Your HTTP server, Apache/etc
 role :app, "factlink-testserver.inverselink.com"                   # This may be the same as your `Web` server
 role :db,  "factlink-testserver.inverselink.com", :primary => true # This is where Rails migrations will run
