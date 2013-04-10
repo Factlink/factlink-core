7ac601af8e70e87cfa34e2a65e7ff9834bdea6cc
Martijn Russchen <martijn@factlink.com>
2013-04-10 11:41:00 +0200

    New paths to servers

--- js-library/config/deploy/testserver.rb
+++ js-library/config/deploy/testserver.rb
@@ -1,7 +1,7 @@
-server 'testserver.factlink.com', :app, :web, :primary => true
+server 'factlink-testserver.inverselink.com', :app, :web, :primary => true
 
 set :deploy_env, 'testserver'
 
-role :web, "testserver.factlink.com"                          # Your HTTP server, Apache/etc
-role :app, "testserver.factlink.com"                          # This may be the same as your `Web` server
-role :db,  "testserver.factlink.com", :primary => true # This is where Rails migrations will run
+role :web, "factlink-testserver.inverselink.com"                          # Your HTTP server, Apache/etc
+role :app, "factlink-testserver.inverselink.com"                          # This may be the same as your `Web` server
+role :db,  "factlink-testserver.inverselink.com", :primary => true # This is where Rails migrations will run
