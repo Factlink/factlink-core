0a4f41a9dd64b5d4c502a4cc96a7a6cf5f43650d
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-10 11:14:42 +0200

    Moved to new location for staging

--- core/config/deploy/staging.rb
+++ core/config/deploy/staging.rb
@@ -1,10 +1,10 @@
-server 'staging.factlink.com', :app, :web, :primary => true
+server 'factlink-staging.inverselink.com', :app, :web, :primary => true
 
-set :full_url, 'https://staging.factlink.com'
+set :full_url, 'https://factlink-staging.inverselink.com'
 
 set :deploy_env, 'staging'
 set :rails_env,  'staging' # Also used by capistrano for some specific tasks
 
-role :web, "staging.factlink.com"                          # Your HTTP server, Apache/etc
-role :app, "staging.factlink.com"                          # This may be the same as your `Web` server
-role :db,  "staging.factlink.com", :primary => true # This is where Rails migrations will run
+role :web, "factlink-staging.inverselink.com"                          # Your HTTP server, Apache/etc
+role :app, "factlink-staging.inverselink.com"                          # This may be the same as your `Web` server
+role :db,  "factlink-staging.inverselink.com", :primary => true # This is where Rails migrations will run
