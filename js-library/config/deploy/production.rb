756214e0f38cf815c57681933b4a99ac0c0edea6
tomdev <tom@north26.nl>
2013-04-16 08:57:05 +0200

    Deploy to static.factlink.com

--- js-library/config/deploy/production.rb
+++ js-library/config/deploy/production.rb
@@ -1,7 +1,7 @@
-server 'beta.factlink.com', :app, :web, :primary => true
+server 'static.factlink.com', :app, :web, :primary => true
 
 set :deploy_env, 'production'
 
-role :web, "beta.factlink.com"                          # Your HTTP server, Apache/etc
-role :app, "beta.factlink.com"                          # This may be the same as your `Web` server
-role :db,  "beta.factlink.com", :primary => true # This is where Rails migrations will run
+role :web, "static.factlink.com"                          # Your HTTP server, Apache/etc
+role :app, "static.factlink.com"                          # This may be the same as your `Web` server
+role :db,  "static.factlink.com", :primary => true # This is where Rails migrations will run
