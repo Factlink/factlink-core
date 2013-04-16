325200661c3b359a2f55b9ed57b6756cc90ce18c
Jens Kanis <me@jenskanis.nl>
2013-04-16 11:55:27 +0200

    Beatify account setup

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -2,6 +2,8 @@ class Users::PasswordsController < Devise::PasswordsController
 
   before_filter :setup_step_in_process, only: [:edit, :update]
 
+  layout "waiting_list"
+
   def setup_step_in_process
     @step_in_signup_process = :account if params[:msg] == 'welcome'
   end
