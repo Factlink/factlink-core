fa75c7a982f93e2861629dafa3dc73f9dcb97dfa
Jan Paul Posma <me@janpaulposma.nl>
2013-04-23 16:01:52 +0200

    Added UserFavouritedTopics.

---
+++ core/spec-unit/redis-models/user_favourited_topics_spec.rb
@@ -0,0 +1,44 @@
+require_relative '../../app/redis-models/user_favourited_topics'
+
+describe UserFavouritedTopics do
+
+  let(:relation) { mock }
+  let(:graph_user_id) { mock }
+  let(:topic_id) { mock }
+  let(:ids) { mock }
+  let(:user_favourited_topics) { UserFavouritedTopics.new graph_user_id, relation }
+
+  describe '.favourite' do
+    it 'calls relation.add' do
+      relation.should_receive(:add).with(graph_user_id, topic_id)
+      user_favourited_topics.favourite topic_id
+    end
+  end
+
+  describe '.unfavourite' do
+    it 'calls relation.remove' do
+      relation.should_receive(:remove).with(graph_user_id, topic_id)
+      user_favourited_topics.unfavourite topic_id
+    end
+  end
+
+  describe '.topic_ids' do
+    it 'returns relation.ids' do
+      relation.should_receive(:ids).with(graph_user_id).and_return(ids)
+      result = user_favourited_topics.topic_ids
+      expect(result).to eq ids
+    end
+  end
+
+  describe '.favourited?' do
+    it 'calls relation.remove' do
+      boolean = mock
+
+      relation.should_receive(:has?).with(graph_user_id, topic_id).and_return(boolean)
+      result = user_favourited_topics.favourited? topic_id
+
+      expect(result).to eq boolean
+    end
+  end
+
+end
