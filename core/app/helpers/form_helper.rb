4201348b238ceae72c703424f77405b0e99599dd
Jan Paul Posma <me@janpaulposma.nl>
2013-04-23 16:42:55 +0200

    Use semicolon to separate error messages.

--- core/app/helpers/form_helper.rb
+++ core/app/helpers/form_helper.rb
@@ -1,7 +1,7 @@
 module FormHelper
   def possible_error_for_field(resource, fieldname)
     if resource.errors[fieldname].any?
-      content = resource.errors[fieldname].join(", ")
+      content = resource.errors[fieldname].join("; ")
 
       content_tag :div, content, class: 'text-error'
     end
