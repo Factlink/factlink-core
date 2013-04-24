d029084f9d19b5646d505eacfcc5aa6b02d17adb
Jan Paul Posma <me@janpaulposma.nl>
2013-04-24 16:14:10 +0200

    Don't set flash message when filling in account details.

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -46,8 +46,10 @@ class Users::PasswordsController < Devise::PasswordsController
     # Copied from Devise::PasswordsController
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
-      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-      set_flash_message(:notice, flash_message) if is_navigational_format?
+      unless params[:msg]
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_navigational_format?
+      end
       sign_in(resource_name, resource)
       respond_with resource, :location => after_sign_in_path_for(resource)
     else
