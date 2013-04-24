abafff8a6d7ec34b746bec896f9350beb7d9a8c5
Jan Paul Posma <me@janpaulposma.nl>
2013-04-24 14:38:09 +0200

    Implemented routes and controller for favourites.

--- core/app/controllers/user_favourite_topics_controller.rb
+++ core/app/controllers/user_favourite_topics_controller.rb
@@ -1,7 +1,36 @@
 class UserFavouriteTopicsController < ApplicationController
 
   def index
-    # TODO
+    @topics = interactor :'topics/favourites', username
+
+    render 'topics/index', format: 'json'
+  end
+
+  def update
+    interactor :'topics/favourite', username, slug_title
+    track 'Topic: Favourited'
+    return_ok
+  end
+
+  def destroy
+    interactor :'topics/unfavourite', username, slug_title
+    track 'Topic: Unfavourited'
+    return_ok
+  end
+
+  private
+  def return_ok
+    respond_to do |format|
+      format.json { head :ok }
+    end
+  end
+
+  def username
+    params[:username]
+  end
+
+  def slug_title
+    params[:id]
   end
   
 end
