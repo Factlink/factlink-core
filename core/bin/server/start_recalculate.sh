5492ac847b2114cbc1197aacc9d469e51b72c1cd
Mark IJbema <markijbema@gmail.com>
2013-04-24 14:50:38 +0200

    updated config based on comments from new relic support

--- core/bin/server/start_recalculate.sh
+++ core/bin/server/start_recalculate.sh
@@ -7,6 +7,9 @@ if [ "$count" -lt "1" ]; then
 
   cd /applications/core/current
 
+  export NRCONFIG=/applications/core/current/config/newrelic.yml
+  export NEW_RELIC_LOG=/applications/core/shared/log/newrelic_agent.log
+
   export PIDFILE=/home/deploy/recalculate.pid
   export NEWRELIC_DISPATCHER="FactGraph recalculate"
 
