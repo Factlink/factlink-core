d9043dc34e1b55e6d68069e7c17b8ad02160059a
Mark IJbema <markijbema@gmail.com>
2013-04-24 12:27:22 +0200

    Cleaned up spec a little

--- core/spec/ohm-models/site_spec.rb
+++ core/spec/ohm-models/site_spec.rb
@@ -1,38 +1,38 @@
 require 'spec_helper'
 
 describe Site do
-  subject {FactoryGirl.create(:site)}
-  let(:fact1) {FactoryGirl.create(:fact)}
-  let(:fact2) {FactoryGirl.create(:fact)}
+  subject(:site) { create :site }
+  let(:fact1)    { create :fact }
+  let(:fact2)    { create :fact }
 
   context "initially" do
     it "should have an empty facts list" do
-      subject.facts.to_a.should =~ []
-    end      
+      site.facts.to_a.should =~ []
+    end
   end
-  
+
   context "after creating one fact with this site " do
     before do
-      fact1.site = subject
+      fact1.site = site
       fact1.save
     end
     it "should contain this fact in the factslist" do
-      subject.facts.to_a.should =~ [fact1]
+      site.facts.to_a.should =~ [fact1]
     end
   end
 
   context "after creating two facts with this site " do
     before do
-      fact1.site = subject
+      fact1.site = site
       fact1.save
-      fact2.site = subject
+      fact2.site = site
       fact2.save
     end
     it "should contain this fact in the factslist" do
-      subject.facts.to_a.should =~ [fact1,fact2]
+      site.facts.to_a.should =~ [fact1,fact2]
     end
   end
-  
+
   it "should have a working find_or_create_by" do
     site = Site.find_or_create_by(:url => 'http://example.org')
     site.should_not == nil
