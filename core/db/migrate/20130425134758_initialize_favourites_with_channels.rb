9a6aab11cc845ff6567a5f0c61f4b58e1cbaf1a2
Mark IJbema <markijbema@gmail.com>
2013-04-25 16:14:34 +0200

    we need strings, and command is a migration helper

--- core/db/migrate/20130425134758_initialize_favourites_with_channels.rb
+++ core/db/migrate/20130425134758_initialize_favourites_with_channels.rb
@@ -1,12 +1,10 @@
 class InitializeFavouritesWithChannels < Mongoid::Migration
-  include Pavlov::Helpers
-
   def self.up
     Channel.all.each do |ch|
       if ch.type == 'channel'
-        graph_user_id = ch.created_by_id
-        topic_id = ch.topic.id
-        command 'topics/favourite', graph_user_id, topic_id
+        graph_user_id = ch.created_by_id.to_s
+        topic_id = ch.topic.id.to_s
+        Pavlov.command 'topics/favourite', graph_user_id, topic_id
       end
     end
   end
