18bd86072a2fdd95f402a8e859c9c65bae9edb45
tomdev <tom@north26.nl>
2013-04-29 14:53:40 +0200

    FIx routes to map to backbone behaviour
    Backbone calls 'update' because the Topic already has a slug_title set(the ID we use for Topics)

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -213,7 +213,7 @@ FactlinkUI::Application.routes.draw do
 
     resources :followers, :only => [:destroy, :update, :index], controller: 'user_followers'
     resources :following, :only => [:index], controller: 'user_following'
-    resources :favourite_topics, :only => [:destroy, :create, :index], controller: 'user_favourite_topics'
+    resources :favourite_topics, :only => [:destroy, :update, :index], controller: 'user_favourite_topics'
   end
 
   resources :topics, path: 't', only: [:show] do
@@ -221,7 +221,7 @@ FactlinkUI::Application.routes.draw do
       get :top
       get :top_channels
     end
-    
+
     member do
       get :related_user_channels
 
