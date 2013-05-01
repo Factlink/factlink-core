53917c70a0da66f11102fb59e5cd3618b52218e6
Jan Paul Posma <me@janpaulposma.nl>
2013-05-01 09:50:56 +0200

    Fixed view topic spec to test for favouriting functionality.

--- core/spec/acceptance/view_topic_spec.rb
+++ core/spec/acceptance/view_topic_spec.rb
@@ -6,24 +6,25 @@ feature "viewing a topic", type: :request do
   include Acceptance::ProfileHelper
   include Acceptance::AddToChannelModalHelper
 
-  scenario "When I follow a channel I should be able to navigate to the topic in my sidebar" do
+  scenario "When I favourite a topic I should be able to navigate to it in my sidebar" do
     @user = create :active_user
     other_user = create :active_user
 
     other_users_channel =
       backend_create_viewable_channel_for other_user, title: 'Henk'
 
+    topic = other_users_channel.topic
+
     sign_in_user @user
 
-    go_to_channel_page_of other_users_channel
-    assert_on_channel_page other_users_channel.title
+    go_to_topic_page_of topic
 
-    within_channel_header do
-      find('button', text: 'Follow').click
+    within_topic_header do
+      find('button', text: 'Favorite').click
     end
 
     go_to_profile_page_of @user
     click_topic_in_sidebar 'Henk'
-    assert_on_topic_page other_users_channel.topic
+    assert_on_topic_page topic
   end
 end
