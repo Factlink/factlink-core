d150e74681b0b06cc109b630c90cb17718cd908e
Jan Paul Posma <me@janpaulposma.nl>
2013-05-02 16:26:47 +0200

    Converted favourites_spec to new style.

--- core/spec/integration-real/interactors/topics/favourites_spec.rb
+++ core/spec/integration-real/interactors/topics/favourites_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe 'favouriting topics' do
-  include Pavlov::Helpers
   include PavlovSupport
 
-  let(:current_user) { create :user }
-  let(:other_user)   { create :user }
-  let(:topic1)       { create :topic }
-  let(:topic2)       { create :topic }
-  let(:ability)      { mock(can?: true) }
+  let(:user1)   { create :user }
+  let(:user2)   { create :user }
+  let(:topic1)  { create :topic }
+  let(:topic2)  { create :topic }
+  let(:ability) { mock(can?: true) }
 
   before do
     Interactors::Topics::Favourite.any_instance.stub :track
@@ -16,77 +15,93 @@ describe 'favouriting topics' do
   end
 
   def pavlov_options
-    {current_user: current_user, ability: ability}
+    {user1: user1, ability: ability}
   end
 
   describe 'user initially' do
     it 'has no favourites' do
-      results = interactor :'topics/favourites', current_user.username
-
-      expect(results.size).to eq 0
+      as(user1) do |pavlov|
+        results = pavlov.interactor :'topics/favourites', user1.username
+        expect(results.size).to eq 0
+      end
     end
   end
 
   describe 'user favouriting two topics' do
     before do
-      interactor :'topics/favourite', current_user.username, topic1.slug_title
-      interactor :'topics/favourite', current_user.username, topic2.slug_title
+      as(user1) do |pavlov|
+        pavlov.interactor :'topics/favourite', user1.username, topic1.slug_title
+        pavlov.interactor :'topics/favourite', user1.username, topic2.slug_title
+      end
     end
 
     it 'has two favourites' do
-      results = interactor :'topics/favourites', current_user.username
-      titles = results.map(&:title)
+      as(user1) do |pavlov|
+        results = pavlov.interactor :'topics/favourites', user1.username
+        titles = results.map(&:title)
 
-      expect(results.size).to eq 2
-      expect(titles).to include topic1.title
-      expect(titles).to include topic2.title
+        expect(results.size).to eq 2
+        expect(titles).to include topic1.title
+        expect(titles).to include topic2.title
+      end
     end
 
     it 'has still two favourites after favouriting one again' do
-      interactor :'topics/favourite', current_user.username, topic1.slug_title
+      as(user1) do |pavlov|
+        pavlov.interactor :'topics/favourite', user1.username, topic1.slug_title
 
-      results = interactor :'topics/favourites', current_user.username
-      titles = results.map(&:title)
+        results = pavlov.interactor :'topics/favourites', user1.username
+        titles = results.map(&:title)
 
-      expect(results.size).to eq 2
-      expect(titles).to include topic1.title
-      expect(titles).to include topic2.title
+        expect(results.size).to eq 2
+        expect(titles).to include topic1.title
+        expect(titles).to include topic2.title
+      end
     end
 
     describe 'user after unfavouriting the second topic' do
       before do
-        interactor :'topics/unfavourite', current_user.username, topic2.slug_title
+        as(user1) do |pavlov|
+          pavlov.interactor :'topics/unfavourite', user1.username, topic2.slug_title
+        end
       end
 
       it 'has one favourite' do
-        results = interactor :'topics/favourites', current_user.username
-
-        expect(results.size).to eq 1
-        expect(results[0].title).to eq topic1.title
+        as(user1) do |pavlov|
+          results = pavlov.interactor :'topics/favourites', user1.username
+          expect(results.size).to eq 1
+          expect(results[0].title).to eq topic1.title
+        end
       end
 
       it 'still has one favourite after unfavouriting the same one again' do
-        interactor :'topics/unfavourite', current_user.username, topic2.slug_title
+        as(user1) do |pavlov|
+          pavlov.interactor :'topics/unfavourite', user1.username, topic2.slug_title
 
-        results = interactor :'topics/favourites', current_user.username
-
-        expect(results.size).to eq 1
-        expect(results[0].title).to eq topic1.title
+          results = pavlov.interactor :'topics/favourites', user1.username
+          expect(results.size).to eq 1
+          expect(results[0].title).to eq topic1.title
+        end
       end
     end
   end
 
   describe 'two users favouriting one topic' do
     before do
-      interactor :'topics/favourite', current_user.username, topic1.slug_title
-      interactor :'topics/favourite', other_user.username, topic1.slug_title
+      as(user1) do |pavlov|
+        pavlov.interactor :'topics/favourite', user1.username, topic1.slug_title
+      end
+      as(user2) do |pavlov|
+        pavlov.interactor :'topics/favourite', user2.username, topic1.slug_title
+      end
     end
-
-    it 'current_user has one favourite' do
-      results = interactor :'topics/favourites', current_user.username
-
-      expect(results.size).to eq 1
-      expect(results[0].title).to eq topic1.title
+  
+    it 'user1 has one favourite' do
+      as(user1) do |pavlov|
+        results = pavlov.interactor :'topics/favourites', user1.username
+        expect(results.size).to eq 1
+        expect(results[0].title).to eq topic1.title
+      end
     end
   end
 
