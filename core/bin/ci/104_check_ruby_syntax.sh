e3802b41dd6f2edc14af968f917672eaca8ea64b
Mark IJbema <markijbema@gmail.com>
2013-05-07 15:52:56 +0200

    fixed one speed issue, fixed that we actually throw errors
    didn't upgrade to the version by jobert yet, since that one does not support filenames yet

--- core/bin/ci/104_check_ruby_syntax.sh
+++ core/bin/ci/104_check_ruby_syntax.sh
@@ -2,10 +2,10 @@
 
 # Note: alternative to using ruby -wc we could also use rubocop
 
-find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK'
+find app/classes -name'*.rb$' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK'
 
 
-TODO=`find app/classes | grep -E '\.rb$' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK' | wc -l`
+TODO=`find app/classes -name '*.rb' | xargs -n1 ruby -wc 2>&1 | grep -v 'Syntax OK' | wc -l | perl -pe 's/\s*//'`
 
 if [ "$TODO" -gt "0" ]; then
   echo "There are warnings in the code"
