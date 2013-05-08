9acbb8657e0efe39348c59fe800950c7fe521cfa
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-05-08 14:22:07 +0200

    Moved url logic to separate function

--- web-proxy/lib/blacklist.js
+++ web-proxy/lib/blacklist.js
@@ -14,23 +14,27 @@ function set_API_OPTIONS(api_options) {
 }
 
 function if_allowed(url_to_check, successFn, errorFn) {
-  var factlink_blacklist_url = url.parse(API_URL + '/site/blacklisted');
-
-  factlink_blacklist_url.query = {
-    'url': url_to_check
-  };
-
-  if (API_OPTIONS !== undefined) {
-    factlink_blacklist_url.auth = API_OPTIONS.username + ':' + API_OPTIONS.password;
-  }
-
-  get(url.format(factlink_blacklist_url)).asString(function(err, data) {
+  get(factlink_blacklist_url()).asString(function(err, data) {
     if (data && "blacklisted" in data) {
       errorFn();
     } else {
       successFn();
     }
   });
+}
+
+function factlink_blacklist_url(url_to_check) {
+  var blacklist_url = url.parse(API_URL + '/site/blacklisted');
+
+  blacklist_url.query = {
+    url: url_to_check
+  };
+
+  if (API_OPTIONS !== undefined) {
+    blacklist_url.auth = API_OPTIONS.username + ':' + API_OPTIONS.password;
+  }
+
+  return url.format(blacklist_url);
 }
 
 exports.set_API_URL = set_API_URL;
