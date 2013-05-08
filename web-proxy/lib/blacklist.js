f9bd204778984f30a17c0662d6e7939d163bd8c6
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-05-08 13:49:29 +0200

    Moved blacklist from restler to get

--- web-proxy/lib/blacklist.js
+++ web-proxy/lib/blacklist.js
@@ -1,40 +1,36 @@
-var restler       = require('restler');
-var _             = require('underscore');
+var get = require('get');
+var _   = require('underscore');
+var url = require('url');
 
 var API_URL,
     API_OPTIONS;
+
 function set_API_URL(api_url) {
   API_URL = api_url;
 }
+
 function set_API_OPTIONS(api_options) {
   API_OPTIONS = api_options;
 }
 
-function if_allowed(url, successFn, errorFn) {
-  var factlink_blacklist_url = API_URL + '/site/blacklisted?url=' + encodeURIComponent(url);
+function if_allowed(url_to_check, successFn, errorFn) {
+  var factlink_blacklist_url = url.parse(API_URL + '/site/blacklisted');
 
-  var options = { parser: restler.parsers.json }
+  factlink_blacklist_url.query = {
+    'url': url_to_check
+  };
 
   if (API_OPTIONS !== undefined) {
-    options.username = API_OPTIONS.username,
-    options.password = API_OPTIONS.password
+    factlink_blacklist_url.auth = API_OPTIONS.username + ':' + API_OPTIONS.password;
   }
 
-  onceSuccessFn = _.once(successFn);
-
-  restler.get(factlink_blacklist_url, options)
-  .on('complete', _.once(function(data) {
-    if ("blacklisted" in data) {
+  get(url.format(factlink_blacklist_url)).asString(function(err, data) {
+    if (data && "blacklisted" in data) {
       errorFn();
     } else {
-      onceSuccessFn();
+      successFn();
     }
-  }))
-  .on('error', _.once(function(data) {
-    // In case something went wrong with the call to the blacklist API,
-    // allow the site.
-    onceSuccessFn();
-  }));
+  });
 }
 
 exports.set_API_URL = set_API_URL;
