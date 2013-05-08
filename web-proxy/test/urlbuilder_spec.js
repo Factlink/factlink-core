497ad0e91eb81869f565c1f4ffca0dae09103932
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-05-08 13:12:45 +0200

    Fixed syntax error

--- web-proxy/test/urlbuilder_spec.js
+++ web-proxy/test/urlbuilder_spec.js
@@ -5,6 +5,6 @@ var urlbuilder = require('../lib/urlbuilder');
 
 exports['search_redir_url should have the url parameter encoded'] = function(beforeExit, assert){
   beforeExit(function(){
-    assert.match(urlbuilder.search_redir_url(proxy_url, query,), /url=http%3A%2F/);
+    assert.match(urlbuilder.search_redir_url(proxy_url, query), /url=http%3A%2F/);
   });
-}
+};
