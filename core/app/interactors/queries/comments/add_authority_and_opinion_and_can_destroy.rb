21737579edde3d89e251ba281ed3b3fd87db094f
Jan Paul Posma <me@janpaulposma.nl>
2013-05-14 10:58:58 +0200

    Fixed add_authority_and_opinion_and_can_destroy.

--- core/app/interactors/queries/comments/add_authority_and_opinion_and_can_destroy.rb
+++ core/app/interactors/queries/comments/add_authority_and_opinion_and_can_destroy.rb
@@ -23,12 +23,10 @@ module Queries
       end
 
       def current_user_opinion
-        if current_graph_user
-          query :'comments/graph_user_opinion',
+        return nil if current_graph_user.nil?
+        
+        query :'comments/graph_user_opinion',
               @comment.id.to_s, current_graph_user
-        else
-          nil
-        end
       end
 
       def current_graph_user
@@ -36,15 +34,9 @@ module Queries
       end
 
       def can_destroy
-        if current_user_id
-          query :'comments/can_destroy', @comment.id.to_s, current_user_id
-        else
-          false
-        end
-      end
-
-      def current_user_id
-        @options[:current_user].andand.id.to_s
+        return false if @options[:current_user].nil?
+        
+        query :'comments/can_destroy', @comment.id.to_s, @options[:current_user].id.to_s
       end
     end
   end
