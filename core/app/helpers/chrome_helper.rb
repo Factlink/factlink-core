5f7dc603a215299e2369f6f9942aa02e94bf9354
Jan Paul Posma <me@janpaulposma.nl>
2013-05-15 14:42:03 +0200

    Cleanup and minor fixes on install extension page.

--- core/app/helpers/chrome_helper.rb
+++ core/app/helpers/chrome_helper.rb
@@ -1,9 +1,5 @@
 module ChromeHelper
 
-  def use_chrome_webstore
-    Rails.env == "production"
-  end
-
   def chrome_extension_link &block
     if use_chrome_webstore
       link_to "#", onclick: "chrome.webstore.install();return false;".html_safe, &block
