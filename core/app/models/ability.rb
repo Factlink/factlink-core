561c963d013567681846bb1de9e742ac9d907edb
Jan Paul Posma <me@janpaulposma.nl>
2013-05-15 11:55:54 +0300

    Fixed incorrect merge

--- core/app/models/ability.rb
+++ core/app/models/ability.rb
@@ -155,11 +155,7 @@ class Ability
     end
   end
 
-<<<<<<< HEAD
-  FEATURES = %w(pink_feedback_button skip_create_first_factlink memory_profiling act_as_non_signed_in sees_channels)
-=======
-  FEATURES = %w(pink_feedback_button skip_create_first_factlink memory_profiling)
->>>>>>> develop
+  FEATURES = %w(pink_feedback_button skip_create_first_factlink memory_profiling sees_channels)
   GLOBAL_ENABLED_FEATURES = []
 
   def enable_features list
