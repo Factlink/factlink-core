b30872c1186afe3f39e9ab678e8724bc8bb127a2
Jan Paul Posma <me@janpaulposma.nl>
2013-05-15 14:55:24 +0200

    Fixed use_chrome_webstore

--- core/app/helpers/chrome_helper.rb
+++ core/app/helpers/chrome_helper.rb
@@ -1,7 +1,7 @@
 module ChromeHelper
 
   def chrome_extension_link &block
-    if use_chrome_webstore
+    if FactlinkUI::Application.config.use_chrome_webstore
       link_to "#", onclick: "chrome.webstore.install();return false;".html_safe, &block
     else
       link_to "#{FactlinkUI::Application.config.static_url}/chrome/factlink-latest.crx", &block
