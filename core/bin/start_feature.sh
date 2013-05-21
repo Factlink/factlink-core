6dcb7c478a1660eb111855d604f73c8e14ee1794
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-05-21 15:35:34 +0200

    Added Jenkins support

--- core/bin/start_feature.sh
+++ core/bin/start_feature.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CREDENTIALS="jenkins:b0rst1g3b4z3n"
+JENKINS_URL="https://ci-factlink.inverselink.com"
 
 git status | grep 'Changes not staged for commit'>/dev/null
 if [ $? -eq 0 ]; then
@@ -23,3 +25,9 @@ git push origin "feature/$1"
 echo "giving github some time"
 hub pull-request "$1" -b Factlink:develop -h Factlink:feature/$1
 
+echo "Setting up Jenkins build"
+curl --user $CREDENTIALS $JENKINS_URL/job/FeatureTemplateCore/config.xml > new_config.xml
+perl -pi -e "s/PUT_FEATURE_HERE/"$1"/; s#(<disabled>)true(</disabled>)#\1false\2#" new_config.xml
+CAMEL_CASED_BRANCH_NAME=`echo $1 | ruby -pe 'require "rails"; $_.gsub!(/-/, "_");$_="#{$_.camelize}"'`
+curl --user $CREDENTIALS -H "Content-Type: text/xml" -s --data "`cat new_config.xml`" "$JENKINS_URL/createItem?name=Feature${CAMEL_CASED_BRANCH_NAME}Core"
+rm new_config.xml
