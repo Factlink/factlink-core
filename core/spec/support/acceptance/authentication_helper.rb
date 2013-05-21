52d245b27cfbfc4ef86aca1b2a7b1acf2728d17d
Mark IJbema <markijbema@gmail.com>
2013-05-21 15:57:00 +0200

    made test a bit more specific

--- core/spec/support/acceptance/authentication_helper.rb
+++ core/spec/support/acceptance/authentication_helper.rb
@@ -2,6 +2,7 @@ module Acceptance
   module AuthenticationHelper
     def assert_on_login_page
       page.find('h2', text: 'Sign in to Factlink')
+      page.find('input.btn', text: 'Sign In')
     end
   end
 end
