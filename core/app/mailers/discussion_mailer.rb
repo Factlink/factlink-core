7e3b2cb12758865b29a97801bb71c7d37061ac80
Jan Paul Posma <me@janpaulposma.nl>
2013-05-22 10:00:20 +0200

    Added DiscussionMailer.

---
+++ core/app/mailers/discussion_mailer.rb
@@ -0,0 +1,20 @@
+class DiscussionMailer < ActionMailer::Base
+  include Resque::Mailer
+
+  default from: "Factlink <support@factlink.com>"
+
+  def discussion_of_the_week_mass_mail(fact_id)
+    fact = Fact[fact_id]
+    User.receives_digest.each do |user|
+      mail_discussion_of_the_week_to_user(user, fact)
+    end
+  end
+
+  def discussion_of_the_week(user, fact)
+    @fact = fact
+    @user = user
+
+    mail to: @user.email, subject: 'Discussion of the Week'
+  end
+
+end
