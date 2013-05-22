1e10c11b8049f0c370989c93fc45194599d172f3
tomdev <tom@north26.nl>
2013-05-22 11:41:51 +0200

    Move discussion email from UserMailer to DigestMailer

--- core/app/mailers/digest_mailer.rb
+++ core/app/mailers/digest_mailer.rb
@@ -4,8 +4,8 @@ class DigestMailer < ActionMailer::Base
   default from: "Factlink <support@factlink.com>"
 
   def discussion_of_the_week(user, fact, url)
-    @fact = fact
     @user = user
+    @fact = fact
     @url  = url
 
     mail to: @user.email, subject: 'Discussion of the Week'
--- core/app/mailers/user_mailer.rb
+++ core/app/mailers/user_mailer.rb
@@ -8,13 +8,4 @@ class UserMailer < ActionMailer::Base
     mail to: @user.email, subject: 'Start using Factlink'
   end
 
-  # TODO: Remove me when done developing
-  def discussion(fact_id, user_id)
-    @fact = Fact[fact_id]
-    @user = User.find(user_id)
-    @url  = url
-
-    mail to: @user.email, subject: 'Discussion of the Week'
-  end
-
 end
