451b53e1375c3cb25b3454f6fd0c5d580b7c7ea8
Mark IJbema <markijbema@gmail.com>
2013-05-23 09:11:49 +0200

    Use dead_fact in calculation as well

--- core/spec-unit/entities/dead_fact_spec.rb
+++ core/spec-unit/entities/dead_fact_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../../app/entities/dead_fact.rb'
 
 describe DeadFact do
+  describe 'initializing' do
+    it "should only require the id" do
+      dead_fact = DeadFact.new 1
+    end
+  end
   describe "#to_s" do
     it "returns the displaystring" do
       displaystring = 'displaystring'
