f3fb9864e77a913941ce9f9386a8c5b3b765f811
Jan Paul Posma <me@janpaulposma.nl>
2013-05-27 14:29:47 +0200

    Added commands for adding and removing handpicked users.

---
+++ core/spec-unit/interactors/commands/users/remove_handpicked_user_spec.rb
@@ -0,0 +1,39 @@
+require 'pavlov_helper'
+require_relative '../../../../app/interactors/commands/users/remove_handpicked_user'
+
+describe Commands::Users::RemoveHandpickedUser do
+  include PavlovSupport
+
+  describe '#execute' do
+    before do
+      described_class.any_instance.stub validate: true
+      stub_classes 'HandpickedTourUsers'
+    end
+
+    it 'calls a HandpickedTourUsers.remove to remove user' do
+      user_id = mock
+      handpicked_tour_users = mock
+
+      HandpickedTourUsers.stub(:new)
+                        .and_return(handpicked_tour_users)
+
+      handpicked_tour_users.should_receive(:remove)
+                           .with(user_id)
+
+      command = described_class.new user_id
+
+      command.execute
+    end
+  end
+
+  describe '#validate' do
+    it 'calls the correct validation methods' do
+      user_id = mock
+
+      described_class.any_instance.should_receive(:validate_hexadecimal_string)
+                                  .with(:user_id, user_id)
+
+      command = described_class.new user_id
+    end
+  end
+end
