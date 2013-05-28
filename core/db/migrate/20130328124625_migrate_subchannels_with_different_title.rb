90921f8d9315d58726add28b2b9adaec7f259b54
Mark IJbema <markijbema@gmail.com>
2013-05-28 08:37:35 +0200

    Removed code of old migrations
    so we don't need to run test anymore either. Tests for these two migrations were in the top ten of slowests tests

--- core/db/migrate/20130328124625_migrate_subchannels_with_different_title.rb
+++ core/db/migrate/20130328124625_migrate_subchannels_with_different_title.rb
@@ -1,10 +1,6 @@
 class MigrateSubchannelsWithDifferentTitle < Mongoid::Migration
   def self.up
-    say_with_time "Creating jobs to migrate away subchannels with a different title" do
-      Channel.all.ids.each do |channel_id|
-        Resque.enqueue MoveAwaySubchannelsWithOtherTitle, channel_id
-      end
-    end
+    puts "old migration doesn't do anything anymore"
   end
 
   def self.down
