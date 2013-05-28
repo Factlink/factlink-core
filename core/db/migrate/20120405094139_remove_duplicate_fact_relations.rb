a8c4959b3d9dd25469a82a368427bbbe06bb4e11
Mark IJbema <markijbema@gmail.com>
2013-05-28 08:58:42 +0200

    Removed some more migrations without external dependencies

--- core/db/migrate/20120405094139_remove_duplicate_fact_relations.rb
+++ core/db/migrate/20120405094139_remove_duplicate_fact_relations.rb
@@ -1,23 +1,6 @@
 class RemoveDuplicateFactRelations < Mongoid::Migration
   def self.up
-    Fact.all.each do |fact|
-      factArray = Array.new
-      fact.supporting_facts.each do |supporting|
-        if factArray[supporting.from_fact_id.to_i] == nil
-          factArray[supporting.from_fact_id.to_i] = true;
-        else
-          supporting.delete
-        end
-      end
-      factArray = Array.new
-      fact.weakening_facts.each do |weakening|
-        if factArray[weakening.from_fact_id.to_i] == nil
-          factArray[weakening.from_fact_id.to_i] = true;
-        else
-          weakening.delete
-        end
-      end
-    end
+    puts "old migration doesn't do anything anymore"
   end
 
   def self.down
