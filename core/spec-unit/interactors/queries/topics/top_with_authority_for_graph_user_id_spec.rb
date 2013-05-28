e6ed01f9cc6f853647c978a5005fea946ab6892a
Mark IJbema <markijbema@gmail.com>
2013-05-28 15:29:25 +0200

    created test

---
+++ core/spec-unit/interactors/queries/topics/top_with_authority_for_graph_user_id_spec.rb
@@ -0,0 +1,13 @@
+require 'pavlov_helper'
+require_relative '../../../../app/interactors/queries/topics/top_with_authority_for_graph_user_id'
+
+describe Queries::Topics::TopWithAuthorityForGraphUserId do
+  include PavlovSupport
+
+
+  describe '#execute' do
+    it 'returns the top #limit_topics topics, with authority' do
+    end
+  end
+
+end
