ebfd7c514f4cc6f9d85c929b49bcf615ab7dc39e
Mark IJbema <markijbema@gmail.com>
2013-05-28 10:06:26 +0200

    Remove migration which resets top topics
    also removed all used files, rewritten tests to test
    functionality instead of migration, and cleaned up a little

--- core/db/migrate/20121211072352_seed_sites_with_top_topics.rb
+++ core/db/migrate/20121211072352_seed_sites_with_top_topics.rb
@@ -1,10 +1,6 @@
-require_relative '../../app/interactors/commands/site/reset_top_topics.rb'
-
 class SeedSitesWithTopTopics < Mongoid::Migration
   def self.up
-    Site.all.ids.each do |site_id|
-      Resque.enqueue(Commands::Site::ResetTopTopics, site_id.to_i)
-    end
+    puts "old migration doesn't do anything anymore"
   end
 
   def self.down
