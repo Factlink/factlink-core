bddbbec0288952f885e4e399e64d66388657a64e
Mark IJbema <markijbema@gmail.com>
2013-05-29 12:11:46 +0200

    added user topics to tour users

--- core/spec-unit/interactors/interactors/users/tour_users_spec.rb
+++ core/spec-unit/interactors/interactors/users/tour_users_spec.rb
@@ -5,7 +5,7 @@ describe Interactors::Users::TourUsers do
   include PavlovSupport
 
   before do
-      stub_classes 'User'
+      stub_classes 'User', 'KillObject'
   end
 
   describe '#authorized?' do
@@ -25,17 +25,37 @@ describe Interactors::Users::TourUsers do
     end
   end
 
-  describe '#execute' do
+  describe '#call' do
     it 'calls the handpicked users query' do
-      dead_users = mock
+      dead_users = [
+        mock(:user, graph_user_id: 3),
+        mock(:user, graph_user_id: 7)
+      ]
+      user_topics = [mock, mock]
+
+      dead_users_with_topics = [mock, mock]
+
       options = { ability: mock(can?: true) }
 
       interactor = described_class.new options
-      Pavlov.should_receive(:query)
+      Pavlov.stub(:query)
             .with(:'users/handpicked', options)
             .and_return(dead_users)
+      Pavlov.stub(:query)
+            .with(:'user_topics/top_with_authority_for_graph_user_id', dead_users[0].graph_user_id, 2, options)
+            .and_return(user_topics[0])
+      Pavlov.stub(:query)
+            .with(:'user_topics/top_with_authority_for_graph_user_id', dead_users[1].graph_user_id, 2, options)
+            .and_return(user_topics[1])
 
-      expect(interactor.call).to eq dead_users
+      KillObject.stub(:user)
+                .with(dead_users[0], top_user_topics: user_topics[0])
+                .and_return(dead_users_with_topics[0])
+      KillObject.stub(:user)
+                .with(dead_users[1], top_user_topics: user_topics[1])
+                .and_return(dead_users_with_topics[1])
+
+      expect(interactor.call).to eq dead_users_with_topics
     end
   end
 
