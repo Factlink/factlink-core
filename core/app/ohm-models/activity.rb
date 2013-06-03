e5d50c9b2b237e3ef6e4bcd704f49d80bfc6aa1e
Mark IJbema <markijbema@gmail.com>
2013-06-03 08:56:09 +0200

    fixed syntax so it works multi-line

--- core/app/ohm-models/activity.rb
+++ core/app/ohm-models/activity.rb
@@ -33,8 +33,8 @@ class Activity < OurOhm
   index     :action
 
   def self.for(search_for)
-    res = find(subject_id: search_for.id, subject_class: search_for.class)
-        | find(object_id: search_for.id, object_class: search_for.class)
+    res = find(subject_id: search_for.id, subject_class: search_for.class) |
+          find(object_id: search_for.id, object_class: search_for.class)
 
     res |= find(user_id: search_for.id) if search_for.class == GraphUser
 
