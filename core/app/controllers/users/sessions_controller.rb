0af119e21178bbb5b3d124a8066122f5d38b96c8
Martijn Russchen <martijn@factlink.com>
2013-06-03 13:14:03 +0200

    Removed useless spaces

--- core/app/controllers/users/sessions_controller.rb
+++ core/app/controllers/users/sessions_controller.rb
@@ -7,10 +7,10 @@ class Users::SessionsController < Devise::SessionsController
   after_filter  :track_sign_in, only: :create
   before_filter :track_sign_out, only: :destroy
 
-  def track_sign_out 
-    mp_track "User: Sign out" 
+  def track_sign_out
+    mp_track "User: Sign out"
   end
-  
+
   def track_sign_in
     track_people_event sign_in_count: current_user.sign_in_count
     mp_track "User: Sign in"
