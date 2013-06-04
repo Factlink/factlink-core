142ae861cbed217f0b97bd70f1fea0bd9f0fb12c
Martijn Russchen <martijn@factlink.com>
2013-06-04 11:04:31 +0200

    Removed old mixpanel function

--- core/app/interactors/util/mixpanel.rb
+++ core/app/interactors/util/mixpanel.rb
@@ -14,10 +14,6 @@ module Util
       Resque.enqueue(::Mixpanel::TrackEventJob, event, opts, {})
     end
 
-    def track_event *args
-      @options[:mixpanel].track_event(*args)
-    end
-
     def increment_person_event event
       current_user_id = @options[:current_user].id.to_s
       @options[:mixpanel].increment_person_event current_user_id, {event => 1}
