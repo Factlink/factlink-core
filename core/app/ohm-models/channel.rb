0ef82689a02ef8ba8d6353c0a9e101c4bb7c040f
Jan Paul Posma <me@janpaulposma.nl>
2013-06-05 14:12:41 +0200

    Cleaned up guards and whitespace

--- core/app/ohm-models/channel.rb
+++ core/app/ohm-models/channel.rb
@@ -20,11 +20,10 @@ class Channel < OurOhm
 
   after :create, :increment_mixpanel_count
   def increment_mixpanel_count
-    if type == 'channel' and self.created_by.user
-      mixpanel = FactlinkUI::Application.config.mixpanel.new({}, true)
+    return unless type == 'channel' and self.created_by.user
 
-      mixpanel.increment_person_property self.created_by.user.id.to_s, channels_created: 1
-    end
+    mixpanel = FactlinkUI::Application.config.mixpanel.new({}, true)
+    mixpanel.increment_person_property self.created_by.user.id.to_s, channels_created: 1
   end
 
   alias :old_set_title :title= unless method_defined?(:old_set_title)
