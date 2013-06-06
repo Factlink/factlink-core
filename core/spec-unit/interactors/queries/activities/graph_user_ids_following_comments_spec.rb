3196f0ccd3467f57d53ec053f00ebc6b1fada94d
tomdev <tom@north26.nl>
2013-06-06 15:54:18 +0200

    Add specs for GraphUserIdsFollowingFactComments query

--- core/spec-unit/interactors/queries/activities/graph_user_ids_following_comments_spec.rb
+++ core/spec-unit/interactors/queries/activities/graph_user_ids_following_comments_spec.rb
@@ -1,1 +1,108 @@
-# graph_user_ids_following_comments_spec.rb
+require_relative '../../../../app/interactors/queries/activities/graph_user_ids_following_comments'
+require 'pavlov_helper'
+
+describe Queries::Activities::GraphUserIdsFollowingComments do
+  include PavlovSupport
+
+  describe '#call' do
+    it 'uniques the follower ids' do
+      ids = [1, 2, 3]
+      follower_ids = [1, 2, 2, 2, 2, 3]
+
+      query = described_class.new mock
+
+      query.should_receive(:follower_ids)
+              .and_return(follower_ids)
+      follower_ids.should_receive(:uniq)
+                      .and_return(ids)
+
+      expect(query.call).to eq ids
+    end
+  end
+
+  describe '#follower_ids' do
+    it 'combines the ids of all followers' do
+      comments_creators_ids = [1, 2]
+      comments_opinionated_ids = [2, 3]
+      sub_comments_on_comments_creators_ids = [3, 4]
+
+      query = described_class.new mock
+
+      query.should_receive(:comments_creators_ids)
+              .and_return(comments_creators_ids)
+      query.should_receive(:comments_opinionated_ids)
+              .and_return(comments_opinionated_ids)
+      query.should_receive(:sub_comments_on_comments_creators_ids)
+              .and_return(sub_comments_on_comments_creators_ids)
+
+      expect(query.follower_ids).to eq [1, 2, 2, 3, 3, 4]
+    end
+  end
+
+  describe '#comments_creators_ids' do
+    it 'returns ids of the comment creator' do
+      graph_user = stub graph_user_id: 37
+      comments   = [stub(created_by: graph_user)]
+
+      query = described_class.new comments
+
+      expect(query.comments_creators_ids).to eq [37]
+    end
+  end
+
+  describe '#comments_opinionated_ids' do
+    it 'returns ids of the comment opinionators' do
+      believable1 = stub opinionated_users_ids: 27
+      believable2 = stub opinionated_users_ids: 28
+      comments    = [stub(believable: believable1),
+                     stub(believable: believable2)]
+
+      query = described_class.new comments
+
+      expect(query.comments_opinionated_ids).to eq [27, 28]
+    end
+  end
+
+  describe '#sub_comments_on_comments_creators_ids' do
+    it 'returns ids of creators of subcomments of the comments' do
+      stub_classes 'SubComment'
+
+      finder = mock
+
+      comments = [
+        mock( id: '1' ),
+        mock( id: '2' )
+      ]
+
+      sub_comments = [
+        mock( created_by: mock( graph_user_id: 5 )),
+        mock( created_by: mock( graph_user_id: 6 ))
+      ]
+
+      query = described_class.new comments
+
+      query.should_receive(:comments_ids)
+              .and_return(comments.map(&:id))
+
+      SubComment.should_receive(:where)
+                  .with(parent_class: 'Comment')
+                  .and_return(finder)
+
+      finder.should_receive(:any_in)
+                  .with(parent_id: comments.map(&:id))
+                  .and_return(sub_comments)
+
+      expect(query.sub_comments_on_comments_creators_ids).to eq [5, 6]
+    end
+  end
+
+  describe '#comments_ids' do
+    it 'returns the comments ids' do
+      comments = [stub(id: 73), stub(id: 74)]
+
+      query = described_class.new comments
+
+      expect(query.comments_ids).to eq [73, 74]
+    end
+  end
+end
