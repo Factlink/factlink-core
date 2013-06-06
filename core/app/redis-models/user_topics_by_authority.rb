17f0b56c0f9bee39a3903208ca04a41399f1d478
Mark IJbema <markijbema@gmail.com>
2013-06-06 16:58:16 +0200

    defined the key in one central place

--- core/app/redis-models/user_topics_by_authority.rb
+++ core/app/redis-models/user_topics_by_authority.rb
@@ -3,11 +3,11 @@ class UserTopicsByAuthority
 
   def initialize user_id, nest_key=Nest.new(:user)[:topics_by_authority]
     @user_id = user_id
-    @key = nest_key
+    @base_key = nest_key
   end
 
   def set topic_id, authority
-    @key[@user_id].zadd authority, topic_id
+    key.zadd authority, topic_id
   end
 
   def ids_and_authorities_desc
@@ -21,7 +21,11 @@ class UserTopicsByAuthority
   private
 
   def zrevrange_with_scores start, stop
-    @key[@user_id].zrevrange start, stop, withscores: true
+    key.zrevrange start, stop, withscores: true
+  end
+
+  def key
+    @base_key[@user_id]
   end
 
   def flat_array_to_hash flat_array
