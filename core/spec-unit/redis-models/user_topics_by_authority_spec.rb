b6b635a25e2cb253642af3c81a0543d3273d9c65
Mark IJbema <markijbema@gmail.com>
2013-06-06 16:58:38 +0200

    use limit for the limit

--- core/spec-unit/redis-models/user_topics_by_authority_spec.rb
+++ core/spec-unit/redis-models/user_topics_by_authority_spec.rb
@@ -51,7 +51,7 @@ describe UserTopicsByAuthority do
         with(0, limit-1, withscores: true).
         and_return ["2", "20", "1", "10"]
 
-      result = user_topics_by_authority.ids_and_authorities_desc_limit 5
+      result = user_topics_by_authority.ids_and_authorities_desc_limit limit
       expect(result).to match_array [{id: "2", authority: 21}, {id: "1", authority: 11}]
     end
   end
