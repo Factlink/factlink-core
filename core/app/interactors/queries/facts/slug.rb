0fb5a3a1bed8bee83404f749feefa4b4b4d83eab
Jan Paul Posma <me@janpaulposma.nl>
2013-06-12 15:12:51 +0200

    Made tests actually work and extracted max_slug_length

--- core/app/interactors/queries/facts/slug.rb
+++ core/app/interactors/queries/facts/slug.rb
@@ -1,21 +1,25 @@
 module Queries
   module Facts
-    class SharingUrl
+    class Slug
       include Pavlov::Query
 
-      arguments :fact, :max_slug_length
+      arguments :fact, :max_slug_length_in
+
+      private
 
       def execute
         return fact.id if fact.to_s.blank?
 
-        max_slug_length ||= 1024
-        fact.to_s.parameterize.slice(0, max_slug_length)
+        fact.to_s.parameterize[0, max_slug_length]
       end
 
       def validate
         validate_not_nil :fact, fact
+        validate_integer :max_slug_length, max_slug_length
+      end
 
-        validate_integer :max_slug_length, max_slug_length unless max_slug_length.nil?
+      def max_slug_length
+        max_slug_length_in || 1024
       end
     end
   end
--- core/spec-unit/interactors/queries/facts/slug_spec.rb
+++ core/spec-unit/interactors/queries/facts/slug_spec.rb
@@ -1,6 +1,6 @@
 require 'pavlov_helper'
 require_relative '../../../../app/helpers/fact_helper.rb'
-require_relative '../../../../app/interactors/queries/facts/sharing_url.rb'
+require_relative '../../../../app/interactors/queries/facts/slug.rb'
 
 describe Queries::Facts::Slug do
   include PavlovSupport
@@ -11,7 +11,7 @@ describe Queries::Facts::Slug do
 
       query = described_class.new fact, nil
 
-      expect(query.call).to eq "/bla-bla/f/1"
+      expect(query.call).to eq "bla-bla"
     end
 
     it "falls back to id on empty displaystring" do
@@ -19,7 +19,7 @@ describe Queries::Facts::Slug do
 
       query = described_class.new fact, nil
 
-      expect(query.call).to eq "/1/f/1"
+      expect(query.call).to eq "1"
     end
 
     it "should have a maximum length" do
@@ -28,7 +28,7 @@ describe Queries::Facts::Slug do
 
       query = described_class.new fact, max_slug_length
 
-      expect(query.call).to eq "/bla-b/f/1"
+      expect(query.call).to eq "bla-b"
     end
   end
 
@@ -44,7 +44,7 @@ describe Queries::Facts::Slug do
 
     it 'checks max_slug_length if it is set' do
       fact = mock
-      max_slug_length = mock
+      max_slug_length = 10
 
       described_class.any_instance.should_receive(:validate_integer)
         .with(:max_slug_length, max_slug_length)
