f72ef6247004f50e3e3b5ee5a56dac75fa960a0b
Jan Paul Posma <me@janpaulposma.nl>
2013-06-12 11:55:39 +0200

    Addressed some more comments by @markijbema

--- core/app/interactors/util/validations.rb
+++ core/app/interactors/util/validations.rb
@@ -2,7 +2,8 @@ module Util
   module Validations
     def validate_string_length param_name, param, length
       if param.length > length
-        raise Pavlov::ValidationError, "#{param_name.to_s} should not be longer than #{length} characters."
+        raise Pavlov::ValidationError,
+          "#{param_name.to_s} should not be longer than #{length} characters."
       end
     end
   end
--- core/spec-unit/interactors/commands/twitter/post_spec.rb
+++ core/spec-unit/interactors/commands/twitter/post_spec.rb
@@ -4,11 +4,11 @@ require_relative '../../../../app/interactors/commands/twitter/post'
 describe Commands::Twitter::Post do
   include PavlovSupport
 
-  describe '#call' do
-    before do
-      stub_classes 'Twitter::Client'
-    end
+  before do
+    stub_classes 'Twitter::Client'
+  end
 
+  describe '#call' do
     it 'calls Twitter::Client#update' do
       message  = 'some message'
       token  = 'qwerty'
@@ -21,8 +21,8 @@ describe Commands::Twitter::Post do
         .with(oauth_token: token, oauth_token_secret: secret)
         .and_return(client)
 
-      client.should_receive(:update).
-        with(message)
+      client.should_receive(:update)
+        .with(message)
 
       command = described_class.new message, current_user: user
       command.call
@@ -47,8 +47,8 @@ describe Commands::Twitter::Post do
       message  = 'message'
       user = mock(identities: {'twitter' => nil})
 
-      expect { described_class.new message, current_user: user }.
-        to raise_error(Pavlov::ValidationError, 'no twitter account linked')
+      expect { described_class.new message, current_user: user }
+        .to raise_error(Pavlov::ValidationError, 'no twitter account linked')
     end
   end
 end
