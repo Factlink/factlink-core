9eb74f1d6835c2a2e6555b8e779f3331458a6f95
Jan Paul Posma <me@janpaulposma.nl>
2013-06-12 09:55:55 +0200

    Added validations for string length

---
+++ core/app/interactors/util/validations.rb
@@ -0,0 +1,9 @@
+module Util
+  module Validations
+    def validate_string_length param_name, param, length
+      if param.length > length
+        raise Pavlov::ValidationError, "#{param_name.to_s} should not be longer than #{length} characters."
+      end
+    end
+  end
+end
