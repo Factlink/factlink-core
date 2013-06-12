3b46862e98297104b85d90df438ce4b6fb1b565d
Jan Paul Posma <me@janpaulposma.nl>
2013-06-12 09:43:26 +0200

    More fixes per @markijbema's comments

--- core/spec-unit/interactors/interactors/facts/post_to_twitter_spec.rb
+++ core/spec-unit/interactors/interactors/facts/post_to_twitter_spec.rb
@@ -5,31 +5,26 @@ require_relative '../../../../app/interactors/interactors/facts/post_to_twitter.
 describe Interactors::Facts::PostToTwitter do
   include PavlovSupport
 
+  let(:maximum_message_length) { 130 }
+
   before do
-    stub_classes 'FactHelper', 'Fact'
+    stub_classes 'FactHelper', 'Fact', 'Twitter'
+
+    short_url_length_https = 140-maximum_message_length-1 # -1 for space
+    Twitter.stub configuration: mock(short_url_length_https: short_url_length_https)
   end
 
   describe '#authorized?' do
-    before do
-      described_class.any_instance.stub(validate: true)
-    end
-
     it 'throws when cannot share facts' do
       ability = stub
       ability.stub(:can?).with(:share, Fact).and_return(false)
 
-      pavlov_options = {current_user: stub, ability: ability}
-
-      expect { described_class.new mock, mock, pavlov_options }.
+      expect { described_class.new '1', 'message', ability: ability }.
         to raise_error Pavlov::AccessDenied, 'Unauthorized'
     end
   end
 
   describe '#call' do
-    before do
-      described_class.any_instance.stub(validate: true, authorized?: true)
-    end
-
     it 'posts a fact with a proxy_scroll_url if available' do
       user = mock(username: "username")
       message = "message"
@@ -75,15 +70,6 @@ describe Interactors::Facts::PostToTwitter do
   end
 
   describe '#validate' do
-    let(:maximum_message_length) { 130 }
-
-    before do
-      described_class.any_instance.stub(authorized?: true)
-      stub_classes 'Twitter'
-      short_url_length_https = 140-maximum_message_length-1 # -1 for space
-      Twitter.stub configuration: mock(short_url_length_https: short_url_length_https)
-    end
-
     it 'calls the correct validation methods' do
       fact_id = "1"
       message = "a"*maximum_message_length
@@ -93,7 +79,7 @@ describe Interactors::Facts::PostToTwitter do
       described_class.any_instance.should_receive(:validate_nonempty_string)
         .with(:message, message)
 
-      interactor = described_class.new fact_id, message
+      interactor = described_class.new fact_id, message, ability: mock(can?: true)
     end
 
     it 'disallows messages longer than maximum_message_length' do
@@ -103,7 +89,7 @@ describe Interactors::Facts::PostToTwitter do
 
       described_class.any_instance.stub(:validate_nonempty_string)
 
-      expect {described_class.new fact_id, message}.
+      expect {described_class.new fact_id, message, ability: mock(can?: true)}.
         to raise_error(Pavlov::ValidationError,
           "message cannot be longer than #{maximum_message_length} characters")
     end
