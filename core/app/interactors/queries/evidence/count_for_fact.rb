f6c5a1411140a4b15e4f10935e51a65b31caf2c8
Jan Paul Posma <me@janpaulposma.nl>
2013-06-13 09:48:18 +0200

    Removed support for dead facts in evidence/count_for_fact

--- core/app/interactors/queries/evidence/count_for_fact.rb
+++ core/app/interactors/queries/evidence/count_for_fact.rb
@@ -18,22 +18,12 @@ module Queries
       end
 
       def fact_relations_count
-        real_fact.evidence.size
+        fact.evidence.size
       end
 
       def comments_count
-        fact_data_id = real_fact.data_id
+        fact_data_id = fact.data_id
         Comment.where(fact_data_id: fact_data_id).size
-      end
-
-      def real_fact
-        @real_fact ||= begin
-          if fact.respond_to?(:evidence)
-            fact
-          else
-            Fact[fact.id]
-          end
-        end
       end
     end
   end
