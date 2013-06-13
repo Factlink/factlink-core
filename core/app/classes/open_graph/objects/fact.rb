0d50010b45688a589c110a96daa4122b16d1c781
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-13 14:37:44 +0200

    Render fact_open_graph stuff in /facts/:id

---
+++ core/app/classes/open_graph/objects/fact.rb
@@ -0,0 +1,9 @@
+module OpenGraph::Objects
+  class Fact < OpenGraph::Object
+    def initialize fact
+      open_graph_field :type, 'factlinkdevelopment:factlink'
+      open_graph_field :url, fact.proxy_scroll_url
+      open_graph_field :title, fact.displaystring
+    end
+  end
+end
