d86a71763cb68469c7b529d0b005bccfccf5b8bb
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-18 13:43:22 +0200

    Created open_graph_hash method which replaces the attr_accessor

--- core/app/classes/open_graph/object.rb
+++ core/app/classes/open_graph/object.rb
@@ -1,14 +1,17 @@
 module OpenGraph
   class Object
-    attr_accessor :open_graph_hash
-
     def to_hash
-      Hash[@open_graph_hash.map { |key,value| ["og:#{key}", value] }]
+      Hash[open_graph_hash.map { |key,value| ["og:#{key}", value] }]
     end
 
+    private
+
     def open_graph_field key, value
+      open_graph_hash[key] = value
+    end
+
+    def open_graph_hash
       @open_graph_hash ||= {}
-      @open_graph_hash[key] = value if value
     end
   end
 end
