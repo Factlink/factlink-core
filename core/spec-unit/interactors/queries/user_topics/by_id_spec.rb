91e49efbc5df27be0bd7591f8db2a03dcf1e044e
Jan Paul Posma <me@janpaulposma.nl>
2013-06-18 13:53:47 +0200

    Fixed specs style

--- core/spec-unit/interactors/queries/user_topics/by_id_spec.rb
+++ core/spec-unit/interactors/queries/user_topics/by_id_spec.rb
@@ -5,45 +5,37 @@ describe Queries::UserTopics::ById do
   include PavlovSupport
 
   describe '#validate' do
-    before do
-      described_class.any_instance.stub(authorized?: true)
-    end
-
     it 'calls the correct validation methods' do
       id = mock
 
-      described_class.any_instance.should_receive(:validate_hexadecimal_string).
-        with(:id, id)
+      described_class.any_instance.should_receive(:validate_hexadecimal_string)
+        .with(:id, id)
 
       interactor = described_class.new id
     end
   end
 
-  describe '#execute' do
-    before do
-      described_class.any_instance.stub(validate: true)
-    end
-
+  describe '#call' do
     it 'returns the topic Topic.find' do
       stub_classes 'Topic', 'DeadUserTopic'
 
-      id = mock
+      id = '1a'
       topic = mock(slug_title: mock, title: mock)
       dead_topic = mock
       pavlov_options = {current_user: mock}
 
+
+      Topic.stub(:find)
+        .with(id)
+        .and_return(topic)
+
+      Pavlov.stub(:query)
+        .with(:'user_topics/by_topic', topic, pavlov_options)
+        .and_return(dead_topic)
+
       query = described_class.new id, pavlov_options
 
-      Topic.stub(:find).
-        with(id).
-        and_return(topic)
-
-      query.stub(:query).
-        with(:'user_topics/by_topic', topic).
-        and_return(dead_topic)
-
-      result = query.execute
-      expect(result).to eq dead_topic
+      expect(query.call).to eq dead_topic
     end
   end
 end
--- core/spec-unit/interactors/queries/user_topics/by_topic_spec.rb
+++ core/spec-unit/interactors/queries/user_topics/by_topic_spec.rb
@@ -8,18 +8,14 @@ describe Queries::UserTopics::ByTopic do
     it 'calls the correct validation methods' do
       topic = mock
 
-      described_class.any_instance.should_receive(:validate_not_nil).
-        with(:alive_topic, topic)
+      described_class.any_instance.should_receive(:validate_not_nil)
+        .with(:alive_topic, topic)
 
       interactor = described_class.new topic
     end
   end
 
   describe '#execute' do
-    before do
-      described_class.any_instance.stub(validate: true)
-    end
-
     it 'returns the user topic' do
       stub_classes 'DeadUserTopic'
 
@@ -28,23 +24,23 @@ describe Queries::UserTopics::ByTopic do
       current_user_authority = mock
       current_user = mock(graph_user: mock)
       dead_topic = mock
+      pavlov_options = {current_user: current_user}
 
-      query = described_class.new topic, current_user: current_user
+      Pavlov.stub(:query)
+        .with(:'topics/facts_count', topic.slug_title, pavlov_options)
+        .and_return(facts_count)
 
-      query.stub(:query).
-        with(:'topics/facts_count', topic.slug_title).
-        and_return(facts_count)
+      Pavlov.stub(:query)
+        .with(:authority_on_topic_for, topic, current_user.graph_user, pavlov_options)
+        .and_return(current_user_authority)
 
-      query.stub(:query).
-        with(:authority_on_topic_for, topic, current_user.graph_user).
-        and_return(current_user_authority)
+      DeadUserTopic.stub(:new)
+        .with(topic.slug_title, topic.title, current_user_authority, facts_count)
+        .and_return(dead_topic)
 
-      DeadUserTopic.stub(:new).
-        with(topic.slug_title, topic.title, current_user_authority, facts_count).
-        and_return(dead_topic)
+      query = described_class.new topic, pavlov_options
 
-      result = query.execute
-      expect(result).to eq dead_topic
+      expect(query.call).to eq dead_topic
     end
   end
 end
