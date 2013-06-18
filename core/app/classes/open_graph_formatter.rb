e5fee93be79e2f4184ce5eeb18ecaab9cca5613f
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-18 13:45:11 +0200

    Moved default logic to rules method

--- core/app/classes/open_graph_formatter.rb
+++ core/app/classes/open_graph_formatter.rb
@@ -1,14 +1,18 @@
 class OpenGraphFormatter
-  attr_reader :rules
+  def add graph_object
+    rules.merge! graph_object.to_hash
+  end
 
-  def initialize
-    @rules = {
+  def to_hash
+    rules
+  end
+
+  private
+
+  def rules
+    @rules ||= {
       'fb:app_id' => FactlinkUI::Application.config.omniauth_conf['facebook']['id'],
       'og:image'  => 'http://cdn.factlink.com/1/avatar.png'
     }
-  end
-
-  def add graph_object
-    @rules.merge! graph_object.to_hash
   end
 end
