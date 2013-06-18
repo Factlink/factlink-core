a37e06e733418ef50802c4da0e190aca28b081ba
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-18 09:30:37 +0200

    Revert "Default OpenGraph url field to fact.url"
    This reverts commit 1ba1f5e37b75739844a39f3885d5d85650ae22b5.
    
    Spoke with @tomdev, this commit collides with my stashed changes, which solve the problem in a different way. Sorry bro!

--- core/app/classes/open_graph/objects/fact.rb
+++ core/app/classes/open_graph/objects/fact.rb
@@ -2,9 +2,8 @@ module OpenGraph::Objects
   class Fact < OpenGraph::Object
     def initialize fact
       open_graph_field :type, 'factlinkdevelopment:factlink'
-      open_graph_field :url, fact.url
+      open_graph_field :url, fact.proxy_scroll_url
       open_graph_field :title, fact.displaystring
-      open_graph_field :see_also, fact.proxy_scroll_url
     end
   end
 end
