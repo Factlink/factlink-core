f6f846ee6c8e1599777258d495a5d3f4ef360858
Mark IJbema <markijbema@gmail.com>
2013-06-18 10:22:49 +0200

    added comment indicating this method should be removed
    This is a rather expensive method, if we really need this,
    we should keep lists of containers, like activity itself does

--- core/app/ohm-models/activity.rb
+++ core/app/ohm-models/activity.rb
@@ -44,6 +44,9 @@ class Activity < OurOhm
   end
 
 
+  # OBSOLETE
+  # Please don't use this method in new stuff
+  # only channel.rb and tests still uses this
   def self.for(search_for)
     res = find(subject_id: search_for.id, subject_class: search_for.class) |
           find(object_id: search_for.id, object_class: search_for.class)
