b47b2516f2f2709406d1f0f6cee1d7a0bd670bd4
tomdev <tom@north26.nl>
2013-06-18 17:46:22 +0200

    Add friendly_fact_url to UrlBuilder

--- core/spec/classes/url_builder_spec.rb
+++ core/spec/classes/url_builder_spec.rb
@@ -2,16 +2,34 @@ require_relative '../../app/classes/url_builder'
 
 describe UrlBuilder do
 
-  describe '.fact_url' do
+  describe 'url helpers' do
 
-    it 'returns the correct url for a fact' do
+    before do
       stub_const 'FactlinkUI::Application', Class.new
+      FactlinkUI::Application.stub( config: stub(core_url: "https://factlink.com/"))
+    end
 
-      fact     = stub id: '1'
-      config   = stub core_url: "https://factlink.com/"
-      FactlinkUI::Application.stub( config: config)
+    describe '.fact_url' do
+      it 'returns the correct url for a fact' do
+        fact = stub id: '1'
 
-      expect(described_class.fact_url fact).to eq "https://factlink.com/facts/1"
+        expect(described_class.fact_url(fact))
+          .to eq "https://factlink.com/facts/1"
+      end
+    end
+
+    describe '.friendly_fact_url' do
+      it 'returns the friendly_fact_url' do
+        fact = stub id: '1'
+        slug = 'this-is-a-friendly-fact'
+
+        Pavlov.stub(:query)
+              .with(:'facts/slug', fact, nil)
+              .and_return(slug)
+
+        expect(described_class.friendly_fact_url fact)
+          .to eq "https://factlink.com/this-is-a-friendly-fact/f/1"
+      end
     end
   end
 
