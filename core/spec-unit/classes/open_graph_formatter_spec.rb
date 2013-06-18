b08cdba53a914c8d00cddfbf4c9c765d2ce8eae1
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-18 14:21:21 +0200

    Added unit tests for open_graph_formatter

---
+++ core/spec-unit/classes/open_graph_formatter_spec.rb
@@ -0,0 +1,40 @@
+require_relative '../../app/classes/open_graph_formatter.rb'
+
+describe OpenGraphFormatter do
+  describe '#to_hash' do
+    it 'returns a hash with the two default keys by default' do
+      default_rules = {}
+      formatter = described_class.new
+
+      formatter.should_receive(:default_rules).and_return(default_rules)
+
+      expect(formatter.to_hash).to eq default_rules
+    end
+  end
+
+  describe '#add' do
+    it 'adds the passed GraphObjects hash presentation to the rules' do
+      default_rules = {foo: 'bar'}
+      graph_object = mock(to_hash: {bla: 'foo'})
+      formatter = described_class.new
+
+      formatter.should_receive(:default_rules).and_return(default_rules)
+
+      formatter.add graph_object
+
+      expect(formatter.to_hash).to eq({foo: 'bar', bla: 'foo'})
+    end
+
+    it 'overwrites the default values when a GraphObject with the same key gets added' do
+      default_rules = { foo: 'bar' }
+      graph_object = mock(to_hash: { foo: 'bla' })
+      formatter = described_class.new
+
+      formatter.should_receive(:default_rules).and_return(default_rules)
+
+      formatter.add graph_object
+
+      expect(formatter.to_hash).to eq graph_object.to_hash
+    end
+  end
+end
