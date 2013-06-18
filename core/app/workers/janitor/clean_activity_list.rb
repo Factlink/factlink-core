0ce08189593bae4c52bda79d36df81de0c29dec1
Mark IJbema <markijbema@gmail.com>
2013-06-18 10:32:48 +0200

    converted job to Pavlov::Command

--- core/app/workers/janitor/clean_activity_list.rb
+++ core/app/workers/janitor/clean_activity_list.rb
@@ -1,27 +1,25 @@
 module Janitor
   class CleanActivityList
+    include Pavlov::Command
     @queue = :janitor
+    arguments :list_key
 
-    def initialize(list_key)
-      @list = Nest.new(list_key)
+    def list
+      @list ||= Nest.new(list_key)
     end
 
     def members_to_remove
-      @list.zrange(0,-1).select do |id|
+      list.zrange(0,-1).select do |id|
         a = Activity[id]
         a.nil?
       end
     end
 
-    def perform
+    def execute
       # the redis lib does not seem to accept removal of multiple elements
-      members_to_remove.each do |member|
-        @list.zrem member
+      members_to_remove.each do |member_id|
+        list.zrem member_id
       end
-    end
-
-    def self.perform(list_key)
-      new(list_key).perform()
     end
   end
 end
