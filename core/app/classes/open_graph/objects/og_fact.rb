70237f64ac96bf519855290ff568a344d8e44d2b
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-18 15:58:36 +0200

    Default to fact.url

--- core/app/classes/open_graph/objects/og_fact.rb
+++ core/app/classes/open_graph/objects/og_fact.rb
@@ -2,7 +2,7 @@ module OpenGraph::Objects
   class OgFact < OpenGraph::Object
     def initialize fact
       open_graph_field :type, 'factlinkdevelopment:factlink'
-      open_graph_field :url, fact.proxy_scroll_url if fact.proxy_scroll_url
+      open_graph_field :url, fact.proxy_scroll_url || fact.url
       open_graph_field :title, fact.displaystring
     end
   end
