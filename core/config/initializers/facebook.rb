61bc10b3b135b0f1565dd34de10ae481a9b6b30e
tomdev <tom@north26.nl>
2013-06-18 15:37:11 +0200

    Only set facebook_app_namespace when available

--- core/config/initializers/facebook.rb
+++ core/config/initializers/facebook.rb
@@ -1,3 +1,5 @@
-app_namespace = YAML::load_file(Rails.root.join('config/facebook.yml'))[Rails.env]['facebook_app_namespace']
+app_namespace = YAML::load_file(Rails.root.join('config/facebook.yml'))[Rails.env].andand['facebook_app_namespace']
 
-FactlinkUI::Application.config.facebook_app_namespace = app_namespace
+if app_namespace
+  FactlinkUI::Application.config.facebook_app_namespace = app_namespace
+end
