5183f7f113dbd8a8486fc84e579560be81079d23
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-06-19 10:27:11 +0200

    Renamed DummyClass and OtherDummyClass constants to dummy_class
    And onelined raise_error expectation

--- core/spec-unit/classes/open_graph/object_spec.rb
+++ core/spec-unit/classes/open_graph/object_spec.rb
@@ -3,27 +3,25 @@ require_relative '../../../app/classes/open_graph/object.rb'
 describe OpenGraph::Object do
   describe '#open_graph_field' do
     it 'should add the field to the open_graph_hash' do
-      DummyClass = Class.new OpenGraph::Object do
+      dummy_class = Class.new OpenGraph::Object do
         def initialize
           open_graph_field :foo, 'bar'
         end
       end
 
-      open_graph_object = DummyClass.new
+      open_graph_object = dummy_class.new
 
       expect(open_graph_object.to_hash).to eq({ 'og:foo' => 'bar' })
     end
 
     it 'should raise when no value is passed' do
-      OtherDummyClass = Class.new OpenGraph::Object do
+      dummy_class = Class.new OpenGraph::Object do
         def initialize
           open_graph_field :foo
         end
       end
 
-      expect {
-        OtherDummyClass.new
-      }.to raise_error
+      expect { dummy_class.new }.to raise_error
     end
   end
 end
