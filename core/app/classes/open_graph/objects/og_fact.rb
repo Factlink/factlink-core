4575653e13dd9cdeecc5a673fe8529a20dd2f66b
tomdev <tom@north26.nl>
2013-06-19 17:22:02 +0200

    Use sharing url

--- core/app/classes/open_graph/objects/og_fact.rb
+++ core/app/classes/open_graph/objects/og_fact.rb
@@ -2,7 +2,7 @@ module OpenGraph::Objects
   class OgFact < OpenGraph::Object
     def initialize fact
       open_graph_field :type, 'factlinkdevelopment:factlink'
-      open_graph_field :url, fact.proxy_scroll_url || fact.url
+      open_graph_field :url, fact.url.sharing_url
       open_graph_field :title, fact.displaystring
     end
   end
