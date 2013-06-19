b4f2777c4d0698ff3080ad144057586e2ea6b09b
tomdev <tom@north26.nl>
2013-06-19 15:19:59 +0200

    Rename old variables to fact_url

--- core/spec/classes/fact_url_spec.rb
+++ core/spec/classes/fact_url_spec.rb
@@ -13,9 +13,9 @@ describe FactUrl do
     it 'returns the correct url' do
       fact = stub id: '1'
 
-      url_builder = FactUrl.new fact
+      fact_url = FactUrl.new fact
 
-      expect(url_builder.fact_url)
+      expect(fact_url.fact_url)
         .to eq 'https://a-random-website.com/facts/1'
     end
   end
@@ -29,9 +29,9 @@ describe FactUrl do
             .with(:'facts/slug', fact, nil)
             .and_return(slug)
 
-      url_builder = FactUrl.new fact
+      fact_url = FactUrl.new fact
 
-      expect(url_builder.friendly_fact_url)
+      expect(fact_url.friendly_fact_url)
         .to eq "https://a-random-website.com/this-is-a-friendly-fact/f/2"
     end
   end
@@ -40,17 +40,17 @@ describe FactUrl do
     it 'returns the correct url when fact has a site_url' do
       fact = stub id: '3', site_url: 'http://sciencedaily.com'
 
-      url_builder = FactUrl.new fact
+      fact_url = FactUrl.new fact
 
-      expect(url_builder.proxy_scroll_url)
+      expect(fact_url.proxy_scroll_url)
         .to eq 'http://the-proxy.com/?url=http%3A%2F%2Fsciencedaily.com&scrollto=3'
     end
 
     it 'returns nil when fact has no site_url' do
       fact = stub id: '4', site_url: nil
 
-      url_builder = FactUrl.new fact
-      expect(url_builder.proxy_scroll_url)
+      fact_url = FactUrl.new fact
+      expect(fact_url.proxy_scroll_url)
         .to eq nil
     end
   end
