4ebe9c3b0d6dc131354b24c5c1bc5b3a4c3d981d
tomdev <tom@north26.nl>
2013-06-19 10:10:46 +0200

    Removed unneeded describe block

--- core/spec/classes/url_builder_spec.rb
+++ core/spec/classes/url_builder_spec.rb
@@ -2,34 +2,31 @@ require_relative '../../app/classes/url_builder'
 
 describe UrlBuilder do
 
-  describe 'url helpers' do
+  before do
+    stub_const 'FactlinkUI::Application', Class.new
+    FactlinkUI::Application.stub( config: stub(core_url: "https://a-random-website.com"))
+  end
 
-    before do
-      stub_const 'FactlinkUI::Application', Class.new
-      FactlinkUI::Application.stub( config: stub(core_url: "https://a-random-website.com"))
+  describe '.fact_url' do
+    it 'returns the correct url for a fact' do
+      fact = stub id: '1'
+
+      expect(described_class.fact_url(fact))
+        .to eq "https://a-random-website.com/facts/1"
     end
+  end
 
-    describe '.fact_url' do
-      it 'returns the correct url for a fact' do
-        fact = stub id: '1'
+  describe '.friendly_fact_url' do
+    it 'returns the friendly_fact_url' do
+      fact = stub id: '1'
+      slug = 'this-is-a-friendly-fact'
 
-        expect(described_class.fact_url(fact))
-          .to eq "https://a-random-website.com/facts/1"
-      end
-    end
+      Pavlov.stub(:query)
+            .with(:'facts/slug', fact, nil)
+            .and_return(slug)
 
-    describe '.friendly_fact_url' do
-      it 'returns the friendly_fact_url' do
-        fact = stub id: '1'
-        slug = 'this-is-a-friendly-fact'
-
-        Pavlov.stub(:query)
-              .with(:'facts/slug', fact, nil)
-              .and_return(slug)
-
-        expect(described_class.friendly_fact_url fact)
-          .to eq "https://a-random-website.com/this-is-a-friendly-fact/f/1"
-      end
+      expect(described_class.friendly_fact_url fact)
+        .to eq "https://a-random-website.com/this-is-a-friendly-fact/f/1"
     end
   end
 
