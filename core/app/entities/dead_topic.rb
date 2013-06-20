d88861a25d94c93561091b3bfc1545bc1f566a08
Jan Paul Posma <me@janpaulposma.nl>
2013-06-20 12:06:44 +0200

    Call the authority `current_user_authority` in the DeadTopic.

--- core/app/entities/dead_topic.rb
+++ core/app/entities/dead_topic.rb
@@ -1,5 +1,2 @@
-DeadTopic = Struct.new(:slug_title, :title, :authority, :facts_count) do
-  def formatted_authority
-    NumberFormatter.new(authority).as_authority
-  end
+DeadTopic = Struct.new(:slug_title, :title, :current_user_authority, :facts_count) do
 end
