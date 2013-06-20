5e538307b76c3496a66219ea994c120055b27ad4
Jan Paul Posma <me@janpaulposma.nl>
2013-06-20 13:32:01 +0200

    Moved NumberFormatter call from jbuilder file to DeadTopic

--- core/app/entities/dead_topic.rb
+++ core/app/entities/dead_topic.rb
@@ -1,2 +1,5 @@
 DeadTopic = Struct.new(:slug_title, :title, :current_user_authority, :facts_count) do
+  def formatted_current_user_authority
+    NumberFormatter.new(current_user_authority).as_authority
+  end
 end
