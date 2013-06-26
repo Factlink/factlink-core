0b8574444d9d9f2e1c80648154536c8e1989a5f0
Mark IJbema <markijbema@gmail.com>
2013-06-26 12:07:21 +0200

    updated pavlov one more commit, surpress warns

---
+++ core/config/initializers/pavlov_monkey_patches.rb
@@ -0,0 +1,9 @@
+# TODO fix this in pavlov
+require 'pavlov'
+
+module Pavlov
+  module Validations
+    def warn *args
+    end
+  end
+end
--- core/spec/pavlov_helper.rb
+++ core/spec/pavlov_helper.rb
@@ -2,4 +2,7 @@ require 'pavlov'
 require_relative '../app/interactors/util/can_can.rb'
 require_relative '../app/interactors/util/validations.rb'
 require_relative '../app/interactors/util/sanitations.rb'
+
+require_relative '../config/initializers/pavlov_monkey_patches.rb'
+
 require_relative 'support/unit/pavlov_support.rb'
