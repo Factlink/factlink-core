102b62ac7289303ab4d59bb2618a676ea32e49bc
Mark IJbema <markijbema@gmail.com>
2013-06-26 12:29:10 +0200

    upgraded pavlov, changes authorised? behaviour
    from this moment on, don't declare private authorised? until todo is fixed

--- core/config/initializers/pavlov_monkey_patches.rb
+++ core/config/initializers/pavlov_monkey_patches.rb
@@ -1,9 +1,11 @@
-# TODO fix this in pavlov
 require 'pavlov'
 
+# TODO revert warnings in pavlov
 module Pavlov
   module Validations
     def warn *args
     end
   end
 end
+
+# TODO: merge private authorized pullrequest in pavlov
