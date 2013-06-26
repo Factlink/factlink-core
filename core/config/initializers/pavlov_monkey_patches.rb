30928cb82f033713e04941518cfd838362a37ad1
Mark IJbema <markijbema@gmail.com>
2013-06-26 14:30:09 +0200

    upgraded pavlov, removed no longer necessary monkey patch

--- core/config/initializers/pavlov_monkey_patches.rb
+++ core/config/initializers/pavlov_monkey_patches.rb
@@ -1,13 +1,5 @@
 require 'pavlov'
 
-# TODO revert warnings in pavlov
-module Pavlov
-  module Validations
-    def warn *args
-    end
-  end
-end
-
 # TODO: merge private authorized pullrequest in pavlov
 
 # TODO: validate should throw, also when calling valid?
