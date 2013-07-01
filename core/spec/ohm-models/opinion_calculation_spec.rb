1fd44d6bb45c4ad5e5eb9bf86fe2a394d83601f5
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-01 16:49:41 +0200

    Add several scenario's highlighting current belief calculation.

--- core/spec/ohm-models/opinion_calculation_spec.rb
+++ core/spec/ohm-models/opinion_calculation_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 
-describe "opinion should work as described in the google doc" do
+describe "computed opinion" do
   include BeliefExpressions
 
   before do
     Commands::Topics::UpdateUserAuthority.stub new: (stub call: nil)
   end
 
-  let(:u1) {create(:graph_user)}
-  let(:u2) {create(:graph_user)}
-  let(:u3) {create(:graph_user)}
-  let(:u4) {create(:graph_user)}
+  let(:u1) { create(:graph_user) }
+  let(:u2) { create(:graph_user) }
+  let(:u3) { create(:graph_user) }
+  let(:u4) { create(:graph_user) }
 
-  let(:f1) {create(:fact)}
-  let(:f2) {create(:fact)}
+  let(:f1) { create(:fact) }
+  let(:f2) { create(:fact) }
 
   # f1 --> f2
   let(:f1sup2) { f2.add_evidence(:supporting, f1, u1) }
@@ -23,35 +23,44 @@ describe "opinion should work as described in the google doc" do
   let(:f1weak2) { f2.add_evidence(:weakening, f1, u1) }
 
   before do
-    # TODO: remove this once activities are not created in the models any more, but in interactors
+    # TODO: remove this once activities are not created in the models any more,
+    # but in interactors
     stub_const 'Activity::Subject', Class.new
     Activity::Subject.should_receive(:activity).any_number_of_times
     Fact.any_instance.stub(:add_to_created_facts).and_return(true)
   end
 
-  it "scenario 0" do
+  it "should be unsure when without votes and evidence" do
     opinion?(f1) == _(0.0, 0.0, 1.0, 0.0)
   end
 
-  it "scenario 1" do
+  it "should be belief when both votes are" do
     believes(u1, f1)
     believes(u2, f1)
     opinion?(f1) == _(1.0, 0.0, 0.0, 2.0)
   end
 
-  it "scenario 2" do
+  it "should be disbelief when both votes are" do
+    disbelieves(u1, f1)
+    disbelieves(u2, f1)
+    opinion?(f1) == _(0.0, 1.0, 0.0, 2.0)
+  end
+
+  it "should be divided when there's a pro and contra vote" do
     believes(u1, f1)
     disbelieves(u2, f1)
     opinion?(f1) == _(0.5, 0.5, 0.0, 2.0)
   end
 
-  it "scenario 3" do
+  it "should be somewhat believed but with authority 2
+      when there's a pro and unsure vote" do
     believes(u1, f1)
     doubts(u2, f1)
     opinion?(f1) == _(0.5, 0.0, 0.5, 2.0)
   end
 
-  it "scenario 4" do
+  it "should propagate to supported facts, but authority is capped to the
+      relation's authority" do
     believes(u1, f1)
     believes(u2, f1)
     believes(u2, f1sup2)
@@ -59,7 +68,17 @@ describe "opinion should work as described in the google doc" do
     opinion?(f2) == _(1.0, 0.0, 0.0, 1.0)
   end
 
-  it "scenario 5" do
+  it "should propagate to supported facts, but authority is capped to the
+      supporting fact's authority" do
+    believes(u1, f1)
+    believes(u1, f1sup2)
+    believes(u2, f1sup2)
+    opinion?(f1sup2) == _(1.0, 0.0, 0.0, 2.0)
+    opinion?(f2) == _(1.0, 0.0, 0.0, 1.0)
+  end
+
+  it "should propagate to weakened facts, but authority is capped to the
+      relation's authority" do
     believes(u1, f1)
     believes(u2, f1)
     believes(u2, f1weak2)
@@ -67,30 +86,53 @@ describe "opinion should work as described in the google doc" do
     opinion?(f2) == _(0.0, 1.0, 0.0, 1.0)
   end
 
-  it "scenario 6" do
+  it "should propagate to weakened facts, but authority is capped to the
+      weaking fact's authority" do
+    believes(u1, f1)
+    believes(u1, f1weak2)
+    believes(u2, f1weak2)
+    opinion?(f1weak2) == _(1.0, 0.0, 0.0, 2.0)
+    opinion?(f2) == _(0.0, 1.0, 0.0, 1.0)
+  end
+
+  it "should be zero when there is only a supporting fact relation that is
+      entirely disputed, even if supporting fact is true" do
     believes(u1, f1)
     believes(u2, f1)
     believes(u1, f1sup2)
     disbelieves(u2, f1sup2)
     opinion?(f1sup2) == _(0.5, 0.5, 0.0, 2.0)
-    opinion?(f2) == _(0.5, 0.0, 0.5, 2.0)
+    opinion?(f2) == Opinion.identity
   end
 
-  it "scenario 7" do
+  it "should be zero when there is only a supporting fact relation that is
+      unanimously believed but the supporting fact is in dispute" do
     believes(u1, f1)
     disbelieves(u2, f1)
     believes(u1, f1sup2)
     opinion?(f1) == _(0.5, 0.5, 0.0, 2.0)
     opinion?(f1sup2) == _(1.0, 0.0, 0.0, 1.0)
-    opinion?(f2) == _(0.5, 0.0, 0.5, 1.0)
+    opinion?(f2) == Opinion.identity
   end
 
-  it "scenario 8" do
+  it "should weigh one vote for supporting evidence as heavily as a direct
+      disbelief vote when the supporting fact is strongly believed" do
     believes(u1, f1)
     believes(u2, f1)
     believes(u1, f1sup2)
     disbelieves(u3, f2)
     opinion?(f1) == _(1.0, 0.0, 0.0, 2.0)
+    opinion?(f1sup2) == _(1.0, 0.0, 0.0, 1.0)
+    opinion?(f2) == _(0.5, 0.5, 0.0, 2.0)
+  end
+
+  it "should weigh one vote for supporting evidence as heavily as a direct
+      disbelief vote when the supporting fact has equal authority to the
+      disbelieving user" do
+    believes(u2, f1)
+    believes(u1, f1sup2)
+    disbelieves(u3, f2)
+    opinion?(f1) == _(1.0, 0.0, 0.0, 1.0)
     opinion?(f1sup2) == _(1.0, 0.0, 0.0, 1.0)
     opinion?(f2) == _(0.5, 0.5, 0.0, 2.0)
   end
