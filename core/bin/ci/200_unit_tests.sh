e2d12cc70db55f1898614d86b8d8e2a85c3dcae7
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-05 12:09:57 +0200

    Try yarjuf rather than the rspec formatter, for variation.

--- core/bin/ci/200_unit_tests.sh
+++ core/bin/ci/200_unit_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running unit tests"
 
-bundle exec rspec spec-unit --format RspecJunitFormatter  --out tmp/rspec-unit.xml
+bundle exec rspec spec-unit --format JUnit --require yarjuf  --out tmp/rspec-unit.xml
 
 echo "Aborting to keep things fast..."
 exit 10 #intentional failure for now
