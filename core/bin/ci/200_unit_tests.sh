d30fd61064331fe2de7cb590dce506b03c880fb5
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-08 08:56:09 +0200

    Also use a JUnit formatter for konacha; requires fork of konacha

--- core/bin/ci/200_unit_tests.sh
+++ core/bin/ci/200_unit_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Running unit tests"
 
-bundle exec rspec spec-unit --format JUnit --require yarjuf  --out tmp/rspec-unit.xml
+bundle exec rspec --require yarjuf --format JUnit spec-unit --out tmp/spec-unit.xml
+
+bundle exec rspec --require yarjuf --format JUnit spec --out tmp/spec.xml
 
 echo "Aborting to keep things fast..."
 exit 10 #intentional failure for now
