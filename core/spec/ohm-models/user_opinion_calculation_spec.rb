1cf15a3616105fccafcf96fd2be4d31cd5dbbb19
Mark IJbema <markijbema@gmail.com>
2013-07-11 12:24:02 +0200

    made the test more explicitly about UserOpinionCalculation

--- core/spec/ohm-models/user_opinion_calculation_spec.rb
+++ core/spec/ohm-models/user_opinion_calculation_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+# NOTE: since this test doesn't test authority calculation
+#       we take it to be 1 everywhere
 
 describe "calculating an opinion based on a set of believers, disbelievers and doubters" do
   subject {create(:basefact)}
@@ -9,32 +11,24 @@ describe "calculating an opinion based on a set of believers, disbelievers and d
   let(:user)  {create(:graph_user)}
   let(:user2) {create(:graph_user)}
 
-  def self.opinions
-    [:believes, :doubts, :disbelieves]
-  end
-
-  def opinion_on(subject)
-    calculation = UserOpinionCalculation.new(subject) { 1 }
-    calculation.opinion
-  end
-
-  def user_fact_opinion(user, opinion, fact)
-    Opinion.for_type(opinion,1)
-  end
-
   describe 'a basefact with no creator' do
     it 'has no opinion' do
-      actual_opinion = opinion_on(subject)
+      calculation = UserOpinionCalculation.new(subject) { 1 }
+      actual_opinion = calculation.opinion
+
       expect(actual_opinion).to eq Opinion.zero
     end
   end
 
-  opinions.each do |opinion|
+  [:believes, :doubts, :disbelieves].each do |opinion|
     context "after 1 person has stated she #{opinion}" do
       it 'has the opinion #{opinion} with authority of the user' do
         subject.add_opinion(opinion, user)
-        actual_opinion = opinion_on(subject)
-        expected_opinion = user_fact_opinion(user, opinion, subject)
+
+        calculation = UserOpinionCalculation.new(subject) { 1 }
+        actual_opinion = calculation.opinion
+
+        expected_opinion = Opinion.for_type(opinion, 1)
         expect(actual_opinion).to eq expected_opinion
       end
     end
@@ -44,9 +38,12 @@ describe "calculating an opinion based on a set of believers, disbelievers and d
     it 'returns a beliefing opinion with the authority of both users combined' do
       subject.add_opinion(:believes, user)
       subject.add_opinion(:believes, user2)
-      actual_opinion = opinion_on(subject)
-      expected_opinion = user_fact_opinion(user, :believes, subject) +
-                         user_fact_opinion(user2, :believes, subject)
+
+      calculation = UserOpinionCalculation.new(subject) { 1 }
+      actual_opinion = calculation.opinion
+
+      expected_opinion = Opinion.for_type(:believes, 1) +
+                         Opinion.for_type(:believes, 1)
       expect(actual_opinion).to eq expected_opinion
     end
   end
@@ -55,9 +52,12 @@ describe "calculating an opinion based on a set of believers, disbelievers and d
     it 'returns a opinion which combines the two' do
       subject.add_opinion(:believes, user)
       subject.add_opinion(:disbelieves, user2)
-      actual_opinion = opinion_on(subject)
-      expected_opinion = user_fact_opinion(user, :believes, subject) +
-                         user_fact_opinion(user2, :disbelieves, subject)
+
+      calculation = UserOpinionCalculation.new(subject) { 1 }
+      actual_opinion = calculation.opinion
+
+      expected_opinion = Opinion.for_type(:believes, 1) +
+                         Opinion.for_type(:disbelieves, 1)
       expect(actual_opinion).to eq expected_opinion
     end
   end
