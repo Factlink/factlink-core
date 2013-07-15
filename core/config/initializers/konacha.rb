10a66b6ad0deefed2f7011316a825d0d0aee22e1
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-15 10:48:47 +0200

    Get rid of development logging.

--- core/config/initializers/konacha.rb
+++ core/config/initializers/konacha.rb
@@ -1,14 +1,11 @@
 if defined?(Konacha)
   require 'rspec'
-  # require 'ci/reporter/rake/rspec_loader'
-  # require 'yarjuf'
   Konacha.configure do |config|
     config.spec_dir     = "spec/javascripts"
     config.spec_matcher = /_spec\./
     config.driver       = :poltergeist
     config.stylesheets  = []
     if ENV['JENKINS_URL']
-      puts "!!!!!REOPENING FILE!!!!!"
       config.formatters = [
         RspecJunitFormatter.new(
           File.new('tmp/konacha.junit.xml', 'a')
