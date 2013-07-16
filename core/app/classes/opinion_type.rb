c8892d68e77dbee5f4ea93c30caecbb0ebfed9c1
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-07-16 16:26:45 +0200

    Realy realy fixed denial of service

--- core/app/classes/opinion_type.rb
+++ core/app/classes/opinion_type.rb
@@ -1,9 +1,9 @@
 class OpinionType
   def self.real_for type
-    case type.to_sym
-    when :beliefs, :believes       then :believes
-    when :doubts                   then :doubts
-    when :disbeliefs, :disbelieves then :disbelieves
+    case type.to_s
+    when 'beliefs', 'believes'       then :believes
+    when 'doubts'                    then :doubts
+    when 'disbeliefs', 'disbelieves' then :disbelieves
     else raise "invalid opinion"
     end
   end
