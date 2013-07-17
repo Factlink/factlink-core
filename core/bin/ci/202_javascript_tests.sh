ac79e5d3fc37b2173489d2dc3c9ebeb24cc7a25f
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-17 16:46:54 +0200

    (minor) whitespace.

--- core/bin/ci/202_javascript_tests.sh
+++ core/bin/ci/202_javascript_tests.sh
@@ -6,7 +6,6 @@ REPORTFILE=tmp/konacha.junit.xml
 bundle exec rake konacha:load_poltergeist konacha:run \
   || echo > TEST_FAILURE
 
-
 if ! grep -qe '<testcase' < $REPORTFILE ; then
   echo "FAILING BUILD: No testcases found in $REPORTFILE"
   exit 1
