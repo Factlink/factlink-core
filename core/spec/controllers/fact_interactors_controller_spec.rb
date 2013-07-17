6de3a7ccee42391ac4e397a6c79887a22e480559
Jan Paul Posma <me@janpaulposma.nl>
2013-07-17 16:06:44 +0200

    Added fixtures

--- core/spec/controllers/fact_interactors_controller_spec.rb
+++ core/spec/controllers/fact_interactors_controller_spec.rb
@@ -8,22 +8,32 @@ describe FactInteractorsController do
   let(:user) { FactoryGirl.create(:user) }
 
   describe :interactors do
-    it "should render successful" do
+    it "should keep the same content" do
+      Timecop.freeze Time.local(1995, 4, 30, 15, 35, 45)
+      FactoryGirl.reload # hack because of fixture in check
+
       fact = create :fact
 
       fact.add_opiniated :believes, (create :user).graph_user
-
       5.times do
         fact.add_opiniated :disbelieves, (create :user).graph_user
       end
 
       get :interactors, id: fact.id
       response.should be_success
+
+      response_body = response.body.to_s
+      # strip mongo id, since otherwise comparison will always fail
+      response_body.gsub!(/"id":\s*"[^"]*"/, '"id": "<STRIPPED>"')
+      Approvals.verify(response_body, format: :json, name: 'fact_interactors#interactors should keep the same content')
     end
   end
 
   describe :filtered_interactors do
-    it "should render successful" do
+    it "should keep the same content" do
+      Timecop.freeze Time.local(1995, 4, 30, 15, 35, 45)
+      FactoryGirl.reload # hack because of fixture in check
+
       fact = create :fact
 
       5.times do
@@ -32,6 +42,11 @@ describe FactInteractorsController do
 
       get :filtered_interactors, id: fact.id, type: 'disbelieves'
       response.should be_success
+
+      response_body = response.body.to_s
+      # strip mongo id, since otherwise comparison will always fail
+      response_body.gsub!(/"id":\s*"[^"]*"/, '"id": "<STRIPPED>"')
+      Approvals.verify(response_body, format: :json, name: 'fact_interactors#filtered_interactors should keep the same content')
     end
   end
 
