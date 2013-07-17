bbebdbba059a09f9b6907ecfba44d204f752d8c1
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-17 17:10:58 +0200

    Suppress curl progress bar in output.
    Curl on MacOSX seems to have a bug that won't let you do that unless you also do -#, so hence the slightly illogical command line.

--- core/script/set-status.sh
+++ core/script/set-status.sh
@@ -6,7 +6,7 @@ if [ $# -ne 4 ]; then
   exit 1
 fi
 
-curl -H 'Authorization: token b5e2ff58da90bd178ee24c7e51260653db7386d2' \
+curl -# --silent -S -H 'Authorization: token b5e2ff58da90bd178ee24c7e51260653db7386d2' \
   "https://api.github.com/repos/Factlink/core/statuses/$1" \
   -d "{\"state\":\"$2\",\"target_url\":\"$3\",\"description\":\"$4\"}"
 
