eac327986410ab9a598581d528cdc9455431e2ce
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-22 14:54:40 +0200

    do unit tests like screenshot tests.

--- core/bin/ci/200_unit_tests.sh
+++ core/bin/ci/200_unit_tests.sh
@@ -2,9 +2,23 @@
 echo "Running unit tests"
 
 REPORTFILE=tmp/spec-unit.junit.xml
+OUTPUTFILE=rspec-unit-output.log
 
-bundle exec rspec --format RspecJunitFormatter spec-unit \
- --out $REPORTFILE || touch TEST_FAILURE
+function do_tests {
+  bundle exec rspec --format RspecJunitFormatter spec-unit \
+    --out $REPORTFILE \
+    2>&1 | tee $OUTPUTFILE \
+    || touch TEST_FAILURE
+}
+
+
+do_tests
+if grep -qe 'PhantomJS has crashed.' < $OUTPUTFILE ; then
+  echo "Detected random fail, retrying"
+  do_tests
+fi
+
+
 
 if ! grep -qe '<testcase' < $REPORTFILE ; then
   echo "FAILING BUILD: No testcases found in $REPORTFILE"
