0e5202bcb799d2b622205bb442f8640d52983ac8
Mark IJbema <markijbema@gmail.com>
2013-07-22 16:49:11 +0200

    renamed environment variable

--- core/spec/acceptance/website_spec.rb
+++ core/spec/acceptance/website_spec.rb
@@ -7,7 +7,7 @@ describe "When visiting the Factlink website", type: :request do
 
   context "when not logged in" do
     it "should be able to view the About page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "About"
 
@@ -17,7 +17,7 @@ describe "When visiting the Factlink website", type: :request do
     end
 
     it "should be able to view the Team page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "Team"
       within(:css, "h1") do
@@ -26,7 +26,7 @@ describe "When visiting the Factlink website", type: :request do
     end
 
     it "should be able to view the Jobs page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "Jobs"
       within(:css, "h1") do
@@ -35,7 +35,7 @@ describe "When visiting the Factlink website", type: :request do
     end
 
     it "should be able to view the Contact page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "Contact"
       within(:css, "h1") do
@@ -44,7 +44,7 @@ describe "When visiting the Factlink website", type: :request do
     end
 
     it "should be able to view the TOS page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "Terms of Service"
       within(:css, "h1") do
@@ -53,7 +53,7 @@ describe "When visiting the Factlink website", type: :request do
     end
 
     it "should be able to view the Privacy Policy page" do
-      pending if ENV['skip_improbable_acceptance']
+      pending if ENV['IS_FEATURE_BUILD']
 
       click_link "Privacy"
       within(:css, "h1") do
