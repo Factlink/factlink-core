408c673efff9be84b64d0018f484b1bacc2a24bf
Mark IJbema <markijbema@gmail.com>
2013-07-23 08:40:50 +0200

    removed old tests

--- core/spec/controllers/js_lib_controller_spec.rb
+++ core/spec/controllers/js_lib_controller_spec.rb
@@ -3,31 +3,16 @@ require 'spec_helper'
 describe JsLibController do
   render_views
 
-  let (:user) {FactoryGirl.create(:user)}
-
-  describe :show_template do
-    it "routes to valid templates" do
-      {get: "/templates/create"}.should route_to controller: 'js_lib', action: 'show_template', name: 'create'
-    end
-
-    it "does not route with directory traversal" do
-      {get: "/templates/../../../../etc/passwd"}.should_not be_routable
-    end
-
-    it "retrieves a valid template when not logged in" do
-      get :show_template, name: 'create'
+  describe :create do
+    it "is renders the create template" do
+      get :create
       response.should be_success
     end
+  end
 
-    it "retrieves a valid template when logged in" do
-      authenticate_user! user
-      get :show_template, name: 'create'
-      response.should be_success
-    end
-
-    it "renders the indicator template when not logged in without raising errors" do
-      subject.stub(:current_user) { user }
-      get :show_template, name: 'indicator'
+  describe :indicator do
+    it "renders the indicator template" do
+      get :indicator
       response.should be_success
     end
   end
