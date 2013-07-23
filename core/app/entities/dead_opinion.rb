81a81a43403bcbcf4af34ef161618e0a53e119bd
Jan Paul Posma <me@janpaulposma.nl>
2013-07-23 16:23:26 +0200

    Made user_opinion_for_fact_relation return dead opinion

--- core/app/entities/dead_opinion.rb
+++ core/app/entities/dead_opinion.rb
@@ -2,4 +2,11 @@ DeadOpinion = Struct.new(:b, :d, :u, :a) do
   def self.from_opinion(opinion)
     DeadOpinion.new opinion.b, opinion.d, opinion.u, opinion.a
   end
+
+  def ==(other)
+    self.a == other.a and
+      self.b == other.b and
+      self.d == other.d and
+      self.u == other.u
+  end
 end
