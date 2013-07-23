c9105a8a09c240889efdad26e12cd4ffbf5c87d5
Jan Paul Posma <me@janpaulposma.nl>
2013-07-23 16:25:03 +0200

    Made opinion_for_fact return dead opinion

--- core/spec-unit/interactors/queries/opinions/opinion_for_fact_spec.rb
+++ core/spec-unit/interactors/queries/opinions/opinion_for_fact_spec.rb
@@ -6,20 +6,23 @@ describe Queries::Opinions::OpinionForFact do
 
   describe '#call' do
     before do
-      stub_classes 'Opinion::FactCalculation'
+      stub_classes 'Opinion::FactCalculation', 'DeadOpinion'
     end
 
     it 'returns the opinion on the fact' do
       opinion = mock
+      dead_opinion = mock
       fact = mock
       fact_calculation = mock(get_opinion: opinion)
       Opinion::FactCalculation.stub(:new).with(fact)
         .and_return(fact_calculation)
 
+      DeadOpinion.stub(:from_opinion).with(opinion).and_return(dead_opinion)
+
       query = described_class.new fact
       result = query.call
 
-      expect(result).to eq opinion
+      expect(result).to eq dead_opinion
     end
   end
 end
