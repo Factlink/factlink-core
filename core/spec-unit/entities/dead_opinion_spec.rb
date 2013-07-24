4495db5f77050f98a3fef26c4ddad7333fd5240a
Jan Paul Posma <me@janpaulposma.nl>
2013-07-24 12:10:57 +0200

    New rspec syntax

--- core/spec-unit/entities/dead_opinion_spec.rb
+++ core/spec-unit/entities/dead_opinion_spec.rb
@@ -7,10 +7,10 @@ describe DeadOpinion do
 
   describe ".new" do
     subject {DeadOpinion.new(1,1.4,1.5,1.6)}
-    its(:believes) {should == 1.0}
-    its(:disbelieves) {should == 1.4}
-    its(:doubts) {should == 1.5}
-    its(:authority) {should == 1.6}
+    its(:believes)    {should eq 1.0}
+    its(:disbelieves) {should eq 1.4}
+    its(:doubts)      {should eq 1.5}
+    its(:authority)   {should eq 1.6}
   end
 
   describe "attributes" do
@@ -26,39 +26,39 @@ describe DeadOpinion do
     a = DeadOpinion.new(1,0,0,1)
     b = DeadOpinion.new(0,1,0,1)
     c = a + b
-    a.believes.should == 1
-    b.disbelieves.should == 1
+    expect(a.believes).to    eq 1
+    expect(b.disbelieves).to eq 1
   end
 
   it "should be equal to an opinion with the same numbers" do
-    DeadOpinion.new(1,2,3,4).should == DeadOpinion.new(1,2,3,4)
+    expect(DeadOpinion.new(1,2,3,4)).to eq DeadOpinion.new(1,2,3,4)
   end
 
   it "should be unequal to an opinion with different numbers" do
     a = DeadOpinion.new(1,1,1,1)
 
-    a.should_not == DeadOpinion.new(2,1,1,1)
-    a.should_not == DeadOpinion.new(1,2,1,1)
-    a.should_not == DeadOpinion.new(1,2,1,1)
-    a.should_not == DeadOpinion.new(1,2,1,1)
+    expect(a).to_not eq DeadOpinion.new(2,1,1,1)
+    expect(a).to_not eq DeadOpinion.new(1,2,1,1)
+    expect(a).to_not eq DeadOpinion.new(1,2,1,1)
+    expect(a).to_not eq DeadOpinion.new(1,2,1,1)
   end
 
   it "should have the proper values when retrieved with for_type without explicit authority" do
-    DeadOpinion.for_type(:believes).should    == DeadOpinion.new(1,0,0,0)
-    DeadOpinion.for_type(:disbelieves).should == DeadOpinion.new(0,1,0,0)
-    DeadOpinion.for_type(:doubts).should     == DeadOpinion.new(0,0,1,0)
+    expect(DeadOpinion.for_type(:believes)).to    eq DeadOpinion.new(1,0,0,0)
+    expect(DeadOpinion.for_type(:disbelieves)).to eq DeadOpinion.new(0,1,0,0)
+    expect(DeadOpinion.for_type(:doubts)).to      eq DeadOpinion.new(0,0,1,0)
   end
 
   it "should have the proper values when retrieved with for_type with explicit authority" do
-    DeadOpinion.for_type(:believes,1.3).should    == DeadOpinion.new(1,0,0,1.3)
-    DeadOpinion.for_type(:disbelieves,1.3).should == DeadOpinion.new(0,1,0,1.3)
-    DeadOpinion.for_type(:doubts,1.3).should      == DeadOpinion.new(0,0,1,1.3)
+    expect(DeadOpinion.for_type(:believes,1.3)).to    eq DeadOpinion.new(1,0,0,1.3)
+    expect(DeadOpinion.for_type(:disbelieves,1.3)).to eq DeadOpinion.new(0,1,0,1.3)
+    expect(DeadOpinion.for_type(:doubts,1.3)).to      eq DeadOpinion.new(0,0,1,1.3)
   end
 
   it "should result in the same opinion when you sum it with an opinion with 0 authority" do
     a = DeadOpinion.new(1,2,3,1)
     zero = DeadOpinion.new(1000,1000,1000,0)
-    a.should == a+zero
+    expect(a).to eq a+zero
   end
 
   it "should have a + which does not crash when adding opinions with 0 authority" do
@@ -66,24 +66,24 @@ describe DeadOpinion do
   end
 
   it "should have a commutative + operation" do
-    (o1+o2).should == (o2+o1)
-    (o2+o3).should == (o3+o2)
-    (o1+o3).should == (o3+o1)
+    expect((o1+o2)).to eq (o2+o1)
+    expect((o2+o3)).to eq (o3+o2)
+    expect((o1+o3)).to eq (o3+o1)
   end
 
   it "should have an associative + operation" do
-    ((o1+o2)+o3).should == (o1+(o2+o3))
+    expect(((o1+o2)+o3)).to eq (o1+(o2+o3))
   end
 
   it "should have a combine operation which does the same as the + operation" do
-    DeadOpinion.combine([]).should == DeadOpinion.zero
-    DeadOpinion.combine([o1]).should == o1
-    DeadOpinion.combine([o1,o2]).should == o1+o2
-    DeadOpinion.combine([o1,o2,o3]).should == o1+o2+o3
+    expect(DeadOpinion.combine([])).to         eq DeadOpinion.zero
+    expect(DeadOpinion.combine([o1])).to       eq o1
+    expect(DeadOpinion.combine([o1,o2])).to    eq o1+o2
+    expect(DeadOpinion.combine([o1,o2,o3])).to eq o1+o2+o3
   end
 
   it "should have authorities which add up" do
-    (o1+o2).authority.should == o1.authority + o2.authority
-    (o1+o2+o3).authority.should == o1.authority + o2.authority + o3.authority
+    expect((o1+o2).authority).to    eq o1.authority + o2.authority
+    expect((o1+o2+o3).authority).to eq o1.authority + o2.authority + o3.authority
   end
 end
