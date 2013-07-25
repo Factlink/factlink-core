1696c2a8a790dbf38f4241fd2e12e3e7166cb671
Jan Paul Posma <me@janpaulposma.nl>
2013-07-25 12:16:11 +0200

    Made real_calculate_influencing_opinion functional

--- core/app/ohm-models/opinion/fact_relation_calculation.rb
+++ core/app/ohm-models/opinion/fact_relation_calculation.rb
@@ -12,18 +12,21 @@ module Opinion
     end
 
     def calculate_influencing_opinion
-      opinion_store.store :FactRelation, fact_relation.id, :influencing_opinion, real_calculate_influencing_opinion(fact_relation)
+      from_fact_opinion = FactCalculation.new(fact_relation.from_fact).get_opinion
+      user_opinion = BaseFactCalculation.new(fact_relation).get_user_opinion
+      evidence_type = OpinionType.for_relation_type(fact_relation.type)
+
+      influencing_opinion = real_calculate_influencing_opinion(from_fact_opinion, user_opinion, evidence_type)
+
+      opinion_store.store :FactRelation, fact_relation.id, :influencing_opinion, influencing_opinion
     end
 
     private
 
-    def real_calculate_influencing_opinion(fact_relation)
-      net_fact_authority = FactCalculation.new(fact_relation.from_fact).get_opinion.net_authority
-      net_relevance_authority = BaseFactCalculation.new(fact_relation).get_user_opinion.net_authority
-
+    def real_calculate_influencing_opinion(from_fact_opinion, user_opinion, evidence_type)
+      net_fact_authority = from_fact_opinion.net_authority
+      net_relevance_authority = user_opinion.net_authority
       authority = [[net_fact_authority, net_relevance_authority].min, 0].max
-
-      evidence_type = OpinionType.for_relation_type(fact_relation.type)
 
       DeadOpinion.for_type(evidence_type, authority)
     end
