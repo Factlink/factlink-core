144e2d37e686d8a1f4e74d7968a37cf0c32265ca
tomdev <tom@north26.nl>
2013-07-25 20:00:34 +0200

    Add Command::GlobalFeatures::Set

---
+++ core/app/interactors/commands/global_features/set.rb
@@ -0,0 +1,21 @@
+require 'pavlov'
+
+module Commands
+  module GlobalFeatures
+    class Set
+      include Pavlov::Command
+
+      arguments :features
+
+      def execute
+        features.each do |feature|
+          set.sadd feature
+        end
+      end
+
+      def set
+        Nest.new(:admin_global_features)
+      end
+    end
+  end
+end
