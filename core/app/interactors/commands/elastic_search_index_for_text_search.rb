93bcbe71d8d55e1b90f77f47e5a33bcf19a58f30
tomdev <tom@north26.nl>
2013-07-29 17:03:23 +0200

    Extract methods

--- core/app/interactors/commands/elastic_search_index_for_text_search.rb
+++ core/app/interactors/commands/elastic_search_index_for_text_search.rb
@@ -7,34 +7,48 @@ module Commands
     arguments :object
 
     def execute
-      @missing_fields = []
-      @document = {}
       define_index
 
-      @missing_fields << :id unless field_exists :id
+      missing_fields << :id unless field_exists :id
 
-      raise 'Type_name is not set.' unless @type_name
+      raise 'Type_name is not set.' unless type_name
 
-      raise "#{@type_name} missing fields (#{@missing_fields})." unless @missing_fields.count == 0
+      raise "#{type_name} missing fields (#{missing_fields})." unless missing_fields.count == 0
 
-      index = ElasticSearch::Index.new @type_name
       index.add object.id, document.to_json
     end
 
     private
-    def type type_name
-      @type_name = type_name
+
+    attr_reader :type_name
+
+    def index
+      ElasticSearch::Index.new type_name
     end
 
     def field_exists name
       object.respond_to? name
     end
 
+    def missing_fields
+      @missing_fields ||= []
+    end
+
+    def document
+      @document ||= {}
+    end
+
+    # DSL
+    def type type_name
+      @type_name = type_name
+    end
+
+    # DSL
     def field name
       if field_exists name
-        @document[name] = object.send name
+        document[name] = object.send name
       else
-        @missing_fields << name
+        missing_fields << name
       end
     end
   end
