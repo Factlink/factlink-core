61e720c74fa56f8a8bbf6f3ba8468c497e3c743c
Mark IJbema <markijbema@gmail.com>
2013-07-30 08:51:37 +0200

    Revert "Added tests for doubt -1 and 1"
    This reverts commit 979e5f68fdfb99f6911cc19cf81252242f874854.

--- core/spec-unit/classes/opinion_presenter_spec.rb
+++ core/spec-unit/classes/opinion_presenter_spec.rb
@@ -1,4 +1,3 @@
-require_relative '../../app/entities/dead_opinion'
 require_relative '../../app/classes/opinion_presenter'
 
 describe OpinionPresenter do
@@ -57,28 +56,6 @@ describe OpinionPresenter do
       opinion = mock believes: 0, disbelieves: 0, doubts: 0, authority: authority
       calculated_friendly_authority = OpinionPresenter.new(opinion).as_percentages_hash[:authority]
       expect(calculated_friendly_authority).to eq friendly_authority
-    end
-
-    describe 'doubt should be zero' do
-      before do
-        number_formatter = mock as_authority: mock
-        NumberFormatter.stub(:new).and_return(number_formatter)
-      end
-
-      it 'when it is actually zero' do
-        expect(OpinionPresenter.new(DeadOpinion.new(1,0,0,0)).
-            as_percentages_hash[:doubt][:percentage]).to eq 0
-      end
-
-      it 'when it is zero, and the other values are more than 100, when rounded off' do
-        expect(OpinionPresenter.new(DeadOpinion.new(99.5,0.5,0,1)).
-            as_percentages_hash[:doubt][:percentage]).to eq 0
-      end
-
-      it 'when it is the smallest percentage of the three' do
-        expect(OpinionPresenter.new(DeadOpinion.new(99.4,0.4,0.2,1)).
-            as_percentages_hash[:doubt][:percentage]).to eq 0
-      end
     end
   end
 end
