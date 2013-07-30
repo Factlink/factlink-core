1cffe55624c1e5dc1090d10b3629242e35df5ed1
Jan Paul Posma <me@janpaulposma.nl>
2013-07-30 17:00:48 +0200

    A really bad job… :-(

--- core/app/interactors/interactors/comments/update_opinion.rb
+++ core/app/interactors/interactors/comments/update_opinion.rb
@@ -9,22 +9,14 @@ module Interactors
 
       def execute
         if opinion
-<<<<<<< HEAD
-          command :'comments/set_opinion', comment_id, opinion, pavlov_options[:current_user].graph_user
+          old_command :'comments/set_opinion', comment_id, opinion, pavlov_options[:current_user].graph_user
         else
-          command :'comments/remove_opinion', comment_id, pavlov_options[:current_user].graph_user
+          old_command :'comments/remove_opinion', comment_id, pavlov_options[:current_user].graph_user
         end
 
-        command :'opinions/recalculate_comment_user_opinion', query(:'comments/get', comment_id)
+        old_command :'opinions/recalculate_comment_user_opinion', query(:'comments/get', comment_id)
 
-        query :'comments/get', comment_id
-=======
-          old_command 'comments/set_opinion', comment_id, opinion, pavlov_options[:current_user].graph_user
-        else
-          old_command 'comments/remove_opinion', comment_id, pavlov_options[:current_user].graph_user
-        end
         old_query :'comments/get', comment_id
->>>>>>> develop
       end
 
       def validate
