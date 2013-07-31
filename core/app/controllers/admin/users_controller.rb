f3328c98bbe5d4d4757c68e53475f669a62482ac
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-31 10:30:55 +0200

    In user controller, show which features are globally enabled (greyed-out) and therefore irrelevant to change per-user.

--- core/app/controllers/admin/users_controller.rb
+++ core/app/controllers/admin/users_controller.rb
@@ -68,5 +68,6 @@ class Admin::UsersController < AdminController
 
   def set_available_user_features
     @user_features = Ability::FEATURES
+    @globally_enabled_features = (interactor :'global_features/all').to_set
   end
 end
