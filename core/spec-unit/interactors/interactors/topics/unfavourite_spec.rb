c9485a56da053d1990d0c82eeff53df10a18cd13
tomdev <tom@north26.nl>
2013-07-31 15:59:58 +0200

    Cleaned tests
    Removed test that tests no error is raised
    CLeaned validations

--- core/spec-unit/interactors/interactors/topics/unfavourite_spec.rb
+++ core/spec-unit/interactors/interactors/topics/unfavourite_spec.rb
@@ -10,29 +10,32 @@ describe Interactors::Topics::Unfavourite do
     end
 
     it 'throws when no current_user' do
-      expect { described_class.new mock, mock, {} }.
+      expect { described_class.new(mock, mock).call }.
         to raise_error Pavlov::AccessDenied,'Unauthorized'
     end
 
     it 'throws when cannot edit favourites' do
       user = stub
       current_user = stub
-
       ability = stub
       ability.stub(:can?).with(:edit_favourites, user).and_return(false)
-
       pavlov_options = { current_user: current_user, ability: ability }
 
-      described_class.any_instance.stub(:old_query).
-        with(:user_by_username, 'username').
+      Pavlov.stub(:old_query).
+        with(:user_by_username, 'username', pavlov_options).
         and_return(user)
 
       expect { described_class.new 'username', 'slug_title', pavlov_options }.
         to raise_error Pavlov::AccessDenied, 'Unauthorized'
     end
+  end
 
-    it 'does not throw if current_user is set and favourites can be edited' do
-      user = stub
+  describe '#call' do
+    it 'calls a command to unfavourite topic' do
+      user = mock(graph_user_id: mock)
+      user_name = 'username'
+      slug_title = 'slug-title'
+
       current_user = stub
 
       ability = stub
@@ -40,58 +43,35 @@ describe Interactors::Topics::Unfavourite do
 
       pavlov_options = { current_user: current_user, ability: ability }
 
-      described_class.any_instance.stub(:old_query).
-        with(:user_by_username, 'username').
-        and_return(user)
-
-      described_class.new 'username', 'slug_title', pavlov_options
-    end
-  end
-
-  describe '#execute' do
-    before do
-      described_class.any_instance.stub(authorized?: true, validate: true)
-    end
-
-    it 'calls a command to unfavourite topic' do
-      user_name = mock
-      slug_title = mock
-      interactor = described_class.new user_name, slug_title
-      user = mock(graph_user_id: mock)
       topic = mock(id: mock)
 
-      interactor.stub(:old_query)
-        .with(:'user_by_username', user_name)
+      Pavlov.stub(:old_query)
+        .with(:'user_by_username', user_name, pavlov_options)
         .and_return(user)
-      interactor.stub(:old_query)
-        .with(:'topics/by_slug_title', slug_title)
+      Pavlov.stub(:old_query)
+        .with(:'topics/by_slug_title', slug_title, pavlov_options)
         .and_return(topic)
-      interactor.should_receive(:old_command)
-        .with(:'topics/unfavourite', user.graph_user_id, topic.id.to_s)
+      Pavlov.should_receive(:old_command)
+        .with(:'topics/unfavourite', user.graph_user_id, topic.id.to_s, pavlov_options)
+
+      interactor = described_class.new user_name, slug_title, pavlov_options
+
       interactor.should_receive(:mp_track)
         .with('Topic: Unfavourited', slug_title: slug_title)
 
-      result = interactor.execute
-
-      expect(result).to eq nil
+      expect(interactor.call).to eq nil
     end
   end
 
-  describe '#validate' do
-    before do
-      described_class.any_instance.stub(authorized?: true)
+  describe 'validations' do
+    it 'without user_id doesn\t validate' do
+      expect_validating(1, 'headline')
+        .to fail_validation('user_name should be a nonempty string.')
     end
 
-    it 'calls the correct validation methods' do
-      user_name = mock
-      slug_title = mock
-
-      described_class.any_instance.should_receive(:validate_nonempty_string)
-        .with(:user_name, user_name)
-      described_class.any_instance.should_receive(:validate_nonempty_string)
-        .with(:slug_title, slug_title)
-
-      interactor = described_class.new user_name, slug_title
+    it 'without user_id doesn\t validate' do
+      expect_validating('karel', 43)
+        .to fail_validation('slug_title should be a nonempty string.')
     end
   end
 end
