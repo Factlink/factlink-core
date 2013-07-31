9ee0eadddf50b24c7f3cc9fdcbd9091f1979d125
Mark IJbema <markijbema@gmail.com>
2013-07-31 08:38:23 +0200

    use old_ in al tests in integration-real

--- core/spec/integration-real/interactors/accounts/setup_spec.rb
+++ core/spec/integration-real/interactors/accounts/setup_spec.rb
@@ -11,7 +11,7 @@ describe 'setting up an account' do
   end
 
   def create_approved_user username, email
-    # method, since there is no interactor yet to do this
+    # method, since there is no old_interactor yet to do this
     user = User.new
     user.username = username
     user.email = email
@@ -33,7 +33,7 @@ describe 'setting up an account' do
       }
 
       as(anonymous) do |pavlov|
-        pavlov.interactor :'accounts/setup_approved',
+        pavlov.old_interactor :'accounts/setup_approved',
             user.reset_password_token, attributes
       end
 
@@ -58,7 +58,7 @@ describe 'setting up an account' do
       }
 
       as(anonymous) do |pavlov|
-        returned_user = pavlov.interactor :'accounts/setup_approved',
+        returned_user = pavlov.old_interactor :'accounts/setup_approved',
             attributes[:reset_password_token], attributes
       end
 
@@ -79,7 +79,7 @@ describe 'setting up an account' do
       }
 
       as(anonymous) do |pavlov|
-        returned_user = pavlov.interactor :'accounts/setup_approved',
+        returned_user = pavlov.old_interactor :'accounts/setup_approved',
             user.reset_password_token, attributes
       end
 
@@ -100,7 +100,7 @@ describe 'setting up an account' do
       }
 
       as(anonymous) do |pavlov|
-        returned_user = pavlov.interactor :'accounts/setup_approved',
+        returned_user = pavlov.old_interactor :'accounts/setup_approved',
             user.reset_password_token, attributes
       end
 
--- core/spec/integration-real/interactors/topics/top_for_user_spec.rb
+++ core/spec/integration-real/interactors/topics/top_for_user_spec.rb
@@ -8,7 +8,7 @@ describe 'top user topics per user' do
   context 'user initially' do
     it 'has no top topics' do
       as(user) do |pavlov|
-        results = pavlov.query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
+        results = pavlov.old_query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
         expect(results).to eq []
       end
     end
@@ -17,10 +17,10 @@ describe 'top user topics per user' do
   context 'after creating some topics' do
     it 'has no top topics' do
       as(user) do |pavlov|
-        topic1 = pavlov.command :'topics/create', 'Foo'
-        topic2 = pavlov.command :'topics/create', 'Bar'
+        topic1 = pavlov.old_command :'topics/create', 'Foo'
+        topic2 = pavlov.old_command :'topics/create', 'Bar'
 
-        results = pavlov.query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
+        results = pavlov.old_query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
         expect(results).to eq []
       end
     end
@@ -29,13 +29,13 @@ describe 'top user topics per user' do
   context 'after getting authority on some topics' do
     it 'has those as top topics' do
       as(user) do |pavlov|
-        topic1 = pavlov.command :'topics/create', 'Foo'
-        topic2 = pavlov.command :'topics/create', 'Bar'
+        topic1 = pavlov.old_command :'topics/create', 'Foo'
+        topic2 = pavlov.old_command :'topics/create', 'Bar'
 
-        pavlov.command :'topics/update_user_authority', user.graph_user_id, 'foo', 10
-        pavlov.command :'topics/update_user_authority', user.graph_user_id, 'bar', 1
+        pavlov.old_command :'topics/update_user_authority', user.graph_user_id, 'foo', 10
+        pavlov.old_command :'topics/update_user_authority', user.graph_user_id, 'bar', 1
 
-        results = pavlov.query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
+        results = pavlov.old_query :'user_topics/top_with_authority_for_graph_user_id', user.graph_user_id, 10
 
         expected_results = [
           DeadUserTopic.new('foo', 'Foo', 11),
