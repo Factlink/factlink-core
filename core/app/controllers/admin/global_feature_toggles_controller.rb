3335135b6b4f4177f886d821f9419d74cf255f0b
Eamon Nerbonne <eamon@nerbonne.org>
2013-07-31 10:28:10 +0200

    rename global_features to enabled_features (better matches what variable is.)

--- core/app/controllers/admin/global_feature_toggles_controller.rb
+++ core/app/controllers/admin/global_feature_toggles_controller.rb
@@ -3,12 +3,12 @@ class Admin::GlobalFeatureTogglesController < AdminController
   layout "admin"
 
   def show
-    @global_features = interactor :'global_features/all'
+    @enabled_features = interactor :'global_features/all'
   end
 
   def update
-    @global_features = params[:features].andand.keys || []
-    interactor :'global_features/set', @global_features
+    @enabled_features = params[:features].andand.keys || []
+    interactor :'global_features/set', @enabled_features
     render :show
   end
 
