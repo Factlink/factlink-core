2ecb02765af79b8749210a8b24380ff508a62764
tomdev <tom@north26.nl>
2013-08-01 12:00:46 +0200

    Removed merge conflict

--- core/spec-unit/interactors/commands/twitter/share_factlink_spec.rb
+++ core/spec-unit/interactors/commands/twitter/share_factlink_spec.rb
@@ -56,13 +56,8 @@ describe Commands::Twitter::ShareFactlink do
 
   describe 'validations' do
     it 'requires integer fact_id' do
-<<<<<<< HEAD
-      expect_validating(fact_id: '').
-        to fail_validation('fact_id should be an integer string.')
-=======
-      expect_validating('')
+      expect_validating(fact_id: '')
         .to fail_validation('fact_id should be an integer string.')
->>>>>>> develop
     end
   end
 end
