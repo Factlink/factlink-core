b6f781f672029e7430cd93ef238295ef8479bf4d
Eamon Nerbonne <eamon@nerbonne.org>
2013-08-05 11:33:08 +0200

    Add comment explaining busy-waiting loop.

--- core/spec/acceptance_helper.rb
+++ core/spec/acceptance_helper.rb
@@ -65,6 +65,9 @@ RSpec.configure do |config|
 
   config.after(:each) do
     Timeout.timeout(Capybara.default_wait_time) do
+      # wait for all ajax requests to complete
+      # if we don't wait, the server may see it after the db is cleaned
+      # and a request for a removed object will cause a crash (nil ref).
       sleep(0.1) until page.evaluate_script('jQuery.active') == 0
     end
     Capybara.reset!
