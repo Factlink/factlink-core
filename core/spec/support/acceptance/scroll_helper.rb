bc16faa3ad4522dcf5b9319955281f715d04d4b4
Eamon Nerbonne <eamon@nerbonne.org>
2013-08-06 13:09:47 +0200

    No idea why, but firefox seems to ignore body's overflow-y

--- core/spec/support/acceptance/scroll_helper.rb
+++ core/spec/support/acceptance/scroll_helper.rb
@@ -1,7 +1,7 @@
 module Acceptance
   module ScrollHelper
     def get_scroll_top
-      page.evaluate_script('$("html").scrollTop()')
+      page.evaluate_script('$("body").scrollTop()||$("html").scrollTop()')
     end
 
     def scroll_top_should_eq y
@@ -10,7 +10,7 @@ module Acceptance
 
     def set_scroll_top_to y
       eventually_succeeds do
-        page.execute_script("$('html').scrollTo(#{y});")
+        page.execute_script("$('html,body').scrollTop(#{y});")
         scroll_top_should_eq y
       end
     end
