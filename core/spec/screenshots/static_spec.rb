14b846ffa6c54fec4bf62d6e8fa43ebd9dad60a3
Martijn Russchen <martijn@factlink.com>
2013-08-06 17:52:17 +0200

    Added tests for static pages

--- core/spec/screenshots/static_spec.rb
+++ core/spec/screenshots/static_spec.rb
@@ -27,4 +27,32 @@ describe "Static pages:", type: :request do
       assume_unchanged_screenshot "static_team"
     end
   end
+
+  describe "Jobs page" do
+    it "it renders correctly" do
+      visit "/p/jobs"
+      assume_unchanged_screenshot "static_jobs"
+    end
+  end
+
+  describe "Contact page" do
+    it "it renders correctly" do
+      visit "/p/contact"
+      assume_unchanged_screenshot "static_contact"
+    end
+  end
+
+  describe "Privacy page" do
+    it "it renders correctly" do
+      visit "/p/privacy"
+      assume_unchanged_screenshot "static_privacy"
+    end
+  end
+
+  describe "TOS page" do
+    it "it renders correctly" do
+      visit "/p/tos"
+      assume_unchanged_screenshot "static_tos"
+    end
+  end
 end
