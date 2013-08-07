2f6f91dd6d60c598f1c40d25ca59bd9593d1e36e
Martijn Russchen <martijn@factlink.com>
2013-08-07 10:15:40 +0200

    Added test for feed

--- core/spec/screenshots/tour_spec.rb
+++ core/spec/screenshots/tour_spec.rb
@@ -35,4 +35,20 @@ describe "Check the tour", type: :request do
 
     assume_unchanged_screenshot 'interests'
   end
+
+  it 'Feed should be the same' do
+    @user1 = create :user
+    Pavlov.old_command :"users/add_handpicked_user", @user1.id.to_s
+
+    as(@user1) do |pavlov|
+      @user1_channel1 = pavlov.old_command :'channels/create', 'toy'
+      pavlov.old_command :'topics/update_user_authority', @user1.graph_user_id.to_s, @user1_channel1.slug_title, 0
+      @user1_channel2 = pavlov.old_command :'channels/create', 'story'
+      pavlov.old_command :'topics/update_user_authority', @user1.graph_user_id.to_s, @user1_channel2.slug_title, 3
+    end
+
+    visit activities_path(@user1)
+
+    assume_unchanged_screenshot 'feed'
+  end
 end
