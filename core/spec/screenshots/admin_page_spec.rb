1166829253c8e0376c88b960e6df9a213666fb14
Martijn Russchen <martijn@factlink.com>
2013-08-07 09:53:12 +0200

    Added test for the admin page

---
+++ core/spec/screenshots/admin_page_spec.rb
@@ -0,0 +1,18 @@
+require 'screenshot_helper'
+
+describe "factlink", type: :request do
+  before :each do
+    user1 = sign_in_user create :active_user
+    user2 = create :active_user
+    user3 = create :active_user
+    user4 = create :active_user
+
+    user1.admin = true
+    user1.save
+  end
+
+  it "the layout of the admin page is correct" do
+    visit admin_users_path
+    assume_unchanged_screenshot "admin_page"
+  end
+end
