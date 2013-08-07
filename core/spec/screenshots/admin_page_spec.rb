380f405cd09f08288ab06dc3f17f210dd5a9fb25
Martijn Russchen <martijn@factlink.com>
2013-08-07 10:49:47 +0200

    Moved tim freeze

--- core/spec/screenshots/admin_page_spec.rb
+++ core/spec/screenshots/admin_page_spec.rb
@@ -9,11 +9,10 @@ describe "factlink", type: :request do
 
     user1.admin = true
     user1.save
-
-    Timecop.freeze Time.local(1995, 4, 30, 15, 35, 45)
   end
 
   it "the layout of the admin page is correct" do
+    Timecop.freeze Time.local(1995, 4, 30, 15, 35, 45)
     visit admin_users_path
     assume_unchanged_screenshot "admin_page"
   end
