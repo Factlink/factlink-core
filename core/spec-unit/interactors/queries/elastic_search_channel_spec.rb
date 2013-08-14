1267b93099a7443e2cf61cbd3b9db43b7b6f2147
Mark IJbema <markijbema@gmail.com>
2013-08-14 15:17:35 +0200

    Reorganized specs

--- core/spec-unit/interactors/queries/elastic_search_channel_spec.rb
+++ core/spec-unit/interactors/queries/elastic_search_channel_spec.rb
@@ -13,59 +13,19 @@ describe Queries::ElasticSearchChannel do
   end
 
   describe '#call' do
-    it 'correctly' do
+    it "invokes queries/elastic_search" do
       keywords = 'searching for this channel'
       wildcard_keywords = '(searching*+OR+searching)+AND+(for*+OR+for)+AND+(this*+OR+this)+AND+(channel*+OR+channel)'
       query = described_class.new keywords: keywords, page: 1,
         row_count: 20
-      hit = double
-      results = double(parsed_response: { 'hits' => { 'hits' => [ hit ] } },
-        code: 200)
-      url = 'test'
-      return_object = double
+      result = mock
 
-      hit.should_receive(:[]).with('_id').and_return(1)
-      hit.should_receive(:[]).with('_type').and_return('topic')
-      HTTParty.should_receive(:get).
-        with("http://#{base_url}/topic/_search?q=#{wildcard_keywords}&from=0&size=20&analyze_wildcard=true").
-        and_return(results)
+      Pavlov.stub(:query)
+            .with(:elastic_search, keywords: keywords, page: 1,
+                     row_count: 20, types: [:topic])
+            .and_return(result)
 
-      Pavlov.stub(:old_query).
-        with(:'topics/by_id_with_authority_and_facts_count', 1).
-        and_return(return_object)
-
-      query.call.should eq [return_object]
-    end
-
-    it 'logs and raises an error when HTTParty returns a non 2xx status code.' do
-      keywords = 'searching for this channel'
-      results = double(response: 'error has happened server side',
-        code: 501)
-      query = described_class.new keywords: keywords, page: 1, row_count: 20
-
-      HTTParty.should_receive(:get).and_return(results)
-      error_message = "Server error, status code: 501, response: '#{results.response}'."
-
-      expect { query.call }.to raise_error(RuntimeError, error_message)
-    end
-
-    it 'url encodes correctly' do
-      keywords = '$+,:; @=?&=/'
-      wildcard_keywords = '($%5C+,%5C:;*+OR+$%5C+,%5C:;)+AND+(@=%5C?&=/*+OR+@=%5C?&=/)'
-      query = described_class.new keywords: keywords, page: 1, row_count: 20
-      hit = {'_id' => 1, '_type' => 'topic' }
-      response = { 'hits' => { 'hits' => [ hit ] } }
-      results = double(parsed_response: response, code: 200)
-
-      Pavlov.stub(:old_query)
-            .with(:'topics/by_id_with_authority_and_facts_count', 1)
-            .and_return nil
-
-      HTTParty.should_receive(:get).
-        with("http://#{base_url}/topic/_search?q=#{wildcard_keywords}&from=0&size=20&analyze_wildcard=true").
-        and_return(results)
-
-      query.call
+      query.call.should eq result
     end
   end
 end
