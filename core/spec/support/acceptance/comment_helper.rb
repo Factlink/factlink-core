424f86ac9aed5a88f642e041da0a5f5300ab6574
Jan Paul Posma <me@janpaulposma.nl>
2013-08-14 09:33:40 +0200

    Removed now unnecessary click

--- core/spec/support/acceptance/comment_helper.rb
+++ core/spec/support/acceptance/comment_helper.rb
@@ -43,7 +43,6 @@ module Acceptance
 
         within '.fact-relation-search' do
           text = evidence_factlink.to_s
-          page.find("input[type=text]").click
           page.find("input[type=text]").set(text)
           page.find("li", text: text).click
           # We assume a request immediately fires, and button reads "Posting..."
