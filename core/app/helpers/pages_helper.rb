c1dc90d0f0c28f092d0c814017ca422fe7d98299
Eamon Nerbonne <eamon@nerbonne.org>
2013-08-15 11:37:04 +0200

    fix menu-item help to ignore query string.

--- core/app/helpers/pages_helper.rb
+++ core/app/helpers/pages_helper.rb
@@ -1,6 +1,7 @@
 module PagesHelper
   def menu_item(page_path, page_title)
-    html_class = pages_path(page_path) == request.fullpath ? 'active': ''
+    request_path = URI.parse(request.fullpath).path
+    html_class = pages_path(page_path) == request_path ? 'active': ''
     content_tag :li,
       link_to(page_title, pages_path(page_path)),
       class: html_class
