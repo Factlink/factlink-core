d4c0c72bd2f30670afea6f31c8291f8d0b3828fa
Mark IJbema <markijbema@gmail.com>
2013-08-19 13:27:38 +0200

    use factory girl for specs

--- core/spec/integration-real/interactors/search_spec.rb
+++ core/spec/integration-real/interactors/search_spec.rb
@@ -8,21 +8,6 @@ describe 'full text search' do
     ElasticSearch.stub synchronous: true
   end
 
-  def create_user name
-    u = User.new(
-      username: name,
-      password: 'password',
-      password_confirmation: 'password',
-      first_name: name,
-      last_name: name)
-    u.approved = true
-    u.agrees_tos = true
-    u.agreed_tos_on = DateTime.now
-    u.email = "#{name}@#{name}.com"
-    u.confirmed_at = DateTime.now
-    u.save or fail "oh noes"
-  end
-
   describe 'searching for users' do
     it 'when nothing is added, nothing is found' do
       as(current_user) do |pavlov|
@@ -33,7 +18,7 @@ describe 'full text search' do
 
     it 'when a user is added he is found' do
       as(current_user) do |pavlov|
-        create_user('mark')
+        create :approved_confirmed_user, username: 'mark'
 
         users = pavlov.interactor :search_user, keywords: 'mark'
         expect(users.map(&:username)).to eq ['mark']
@@ -42,7 +27,7 @@ describe 'full text search' do
 
     it 'after reindex a user is still found' do
       as(current_user) do |pavlov|
-        create_user('mark')
+        create :approved_confirmed_user, username: 'mark'
 
         pavlov.interactor :'full_text_search/reindex'
 
