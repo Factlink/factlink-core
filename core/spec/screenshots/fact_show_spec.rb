35d2bb743c277193a9cd3d65a5aa07c69a5c13f5
Eamon Nerbonne <eamon@nerbonne.org>
2013-08-23 17:26:19 +0200

    Fix ndp fact_show test

--- core/spec/screenshots/fact_show_spec.rb
+++ core/spec/screenshots/fact_show_spec.rb
@@ -93,7 +93,7 @@ describe "factlink", type: :feature do
     sign_out_user
 
     go_to_fact_show_of factlink
-    find('.evidence-item', text: 'Fact 1').find('a', text:'1 comment')
+    find('.evidence-box', text: 'Fact 1').find('a', text:'1 comment')
 
     page.should have_content @factlink.data.displaystring
 
