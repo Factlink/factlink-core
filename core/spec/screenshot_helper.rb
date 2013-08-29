130d7612c7621311ffbe3fceee117820896dad49
Mark IJbema <markijbema@gmail.com>
2013-08-29 12:04:37 +0200

    Don't test screenshots on the old page

--- core/spec/screenshot_helper.rb
+++ core/spec/screenshot_helper.rb
@@ -3,5 +3,6 @@ require 'capybara-screenshot'
 
 RSpec.configure do |config|
   config.include ScreenshotTest
+  config.running_screenshot_tests = true
 end
 Capybara::Screenshot.autosave_on_failure = false
