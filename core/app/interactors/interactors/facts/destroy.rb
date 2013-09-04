1aead6c5d8c4a48c28b012319b40909820dba22a
Tom de Vries <tom@north26.nl>
2013-09-04 18:01:41 +0200

    Add Interactor::Facts::Destroy

---
+++ core/app/interactors/interactors/facts/destroy.rb
@@ -0,0 +1,31 @@
+module Interactors
+  module Facts
+    class Destroy
+      include Pavlov::Interactor
+      include Util::CanCan
+
+      arguments :fact_id
+
+      def authorized?
+        can? :manage, Fact
+      end
+
+      private
+
+      def execute
+        return if fact.evidence_count > 0
+
+        command :'facts/destroy', fact_id: fact_id
+      end
+
+      def fact
+        @fact ||= query :'facts/get_dead', fact_id: fact_id
+      end
+
+      def validate
+        validate_integer_string :fact_id, fact_id
+      end
+
+    end
+  end
+end
