08acc319c00b0f3c0e3e2acbc1b2dc3476c1ad5e
Jan Paul Posma <me@janpaulposma.nl>
2013-09-10 10:51:03 +0200

    Moved start of highlighting and annotating to FactlinkConfig, and added sane defaults

--- js-library/app/js/wrap/last.js
+++ js-library/app/js/wrap/last.js
@@ -1,2 +1,5 @@
   if (window.jQuery) window.jQuery(window).trigger('factlink.libraryLoaded');
+
+  if (FactlinkConfig.autoStartHighlighting) FACTLINK.startHighlighting();
+  if (FactlinkConfig.autoStartAnnotating) FACTLINK.startAnnotating();
 })(window.parent, window.parent.document);
