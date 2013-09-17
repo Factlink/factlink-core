52c4a998377ce9b26a8bc8f69ae43e8c194075bc
Jan Paul Posma <me@janpaulposma.nl>
2013-09-17 11:25:28 +0200

    Boyscouting of spec

--- core/spec-unit/redis-models/user_following_users_spec.rb
+++ core/spec-unit/redis-models/user_following_users_spec.rb
@@ -11,6 +11,7 @@ describe UserFollowingUsers do
   describe '.follow' do
     it 'calls relation.add_now' do
       relation.should_receive(:add_now).with(graph_user_id, other_id)
+
       user_following_users.follow other_id
     end
   end
@@ -18,23 +19,24 @@ describe UserFollowingUsers do
   describe '.unfollow' do
     it 'calls relation.remove' do
       relation.should_receive(:remove).with(graph_user_id, other_id)
+
       user_following_users.unfollow other_id
     end
   end
 
   describe '.following_ids' do
     it 'returns relation.ids' do
-      relation.should_receive(:ids).with(graph_user_id).and_return(ids)
-      result = user_following_users.following_ids
-      expect(result).to eq ids
+      relation.stub(:ids).with(graph_user_id).and_return(ids)
+
+      expect(user_following_users.following_ids).to eq ids
     end
   end
 
   describe '.followers_ids' do
     it 'returns relation.reverse_ids' do
-      relation.should_receive(:reverse_ids).with(graph_user_id).and_return(ids)
-      result = user_following_users.followers_ids
-      expect(result).to eq ids
+      relation.stub(:reverse_ids).with(graph_user_id).and_return(ids)
+
+      expect(user_following_users.followers_ids).to eq ids
     end
   end
 
@@ -42,9 +44,9 @@ describe UserFollowingUsers do
     it 'calls relation.remove' do
       boolean = double
 
-      relation.should_receive(:has?).with(graph_user_id, other_id).and_return(boolean)
-      result = user_following_users.follows? other_id
+      relation.stub(:has?).with(graph_user_id, other_id).and_return(boolean)
 
+      result = user_following_users.follows? other_id
       expect(result).to eq boolean
     end
   end
