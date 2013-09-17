1bb9e54302bbf9734c484bdd0a50470aa481ad42
Jan Paul Posma <me@janpaulposma.nl>
2013-09-17 14:51:48 +0200

    Added top_topic statistic

--- core/app/interactors/queries/users_by_ids.rb
+++ core/app/interactors/queries/users_by_ids.rb
@@ -17,13 +17,15 @@ module Queries
 
     def kill user
       KillObject.user user,
-        statistics: statistics(user)
+        statistics: statistics(user.graph_user)
     end
 
-    def statistics user
+    def statistics graph_user
       {
         # TODO: more efficient fact count
-        created_fact_count: user.graph_user.created_facts_channel.sorted_cached_facts.size
+        created_fact_count: graph_user.created_facts_channel.sorted_cached_facts.size,
+        top_topic: query(:'user_topics/top_with_authority_for_graph_user_id',
+                  graph_user_id: graph_user.id, limit_topics: 1).first
       }
     end
   end
--- core/spec-unit/interactors/queries/users_by_ids_spec.rb
+++ core/spec-unit/interactors/queries/users_by_ids_spec.rb
@@ -24,25 +24,34 @@ describe Queries::UsersByIds do
     end
 
     it 'adds statistics' do
+      top_topic = double
       created_fact_count = 10
       created_facts_channel = double(sorted_cached_facts: double(size: created_fact_count))
-      user = double(graph_user: double(created_facts_channel: created_facts_channel))
-      query = described_class.new(user_ids: [0], pavlov_options: { current_user: double })
+      user = double(graph_user: double(id: '10', created_facts_channel: created_facts_channel))
+      query = described_class.new(user_ids: [0])
 
       User.stub(:any_in).with(_id: [0]).and_return([user])
 
+      Pavlov.stub(:query)
+        .with(:'user_topics/top_with_authority_for_graph_user_id',
+                  graph_user_id: user.graph_user.id, limit_topics: 1)
+        .and_return([top_topic])
+
       expect(query.call[0].statistics[:created_fact_count]).to eq created_fact_count
+      expect(query.call[0].statistics[:top_topic]).to eq top_topic
     end
 
     it 'should work with multiple ids' do
       user_ids = [0, 1]
       created_facts_channel0 = double(sorted_cached_facts: double(size: 10))
       created_facts_channel1 = double(sorted_cached_facts: double(size: 20))
-      user0 = double(graph_user: double(created_facts_channel: created_facts_channel0))
-      user1 = double(graph_user: double(created_facts_channel: created_facts_channel1))
+      user0 = double(graph_user: double(id: '10', created_facts_channel: created_facts_channel0))
+      user1 = double(graph_user: double(id: '20', created_facts_channel: created_facts_channel1))
       query = described_class.new(user_ids: user_ids, pavlov_options: { current_user: double })
 
       User.stub(:any_in).with(_id: user_ids).and_return([user0, user1])
+
+      Pavlov.stub(:query).and_return([])
 
       expect(query.call.length).to eq 2
     end
