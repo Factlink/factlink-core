c096b4a38b9eabfaac1d5c924478c58b2ef70132
Eamon Nerbonne <eamon@nerbonne.org>
2013-09-19 14:36:19 +0200

    Don't delete keys if there aren't any.

--- core/db/migrate/20130919122432_delete_basefact_remnants.rb
+++ core/db/migrate/20130919122432_delete_basefact_remnants.rb
@@ -2,8 +2,10 @@ class DeleteBasefactRemnants < Mongoid::Migration
   def self.up
     db = Redis.current
     basefact_keys = db.keys("Basefact:*")
-    deleted_key_count = db.del(*basefact_keys)
-    puts "Deleted #{deleted_key_count} keys."
+    if basefact_keys.size > 0 then
+      deleted_key_count = db.del(*basefact_keys)
+      puts "Deleted #{deleted_key_count} keys."
+    end
   end
 
   def self.down
