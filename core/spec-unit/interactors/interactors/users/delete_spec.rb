bf36ba831eb936ea08b6c5074651ac1ddac2495e
Eamon Nerbonne <eamon@nerbonne.org>
2013-09-24 14:48:06 +0200

    replace a few value-type doubles with explicit examples.

--- core/spec-unit/interactors/interactors/users/delete_spec.rb
+++ core/spec-unit/interactors/interactors/users/delete_spec.rb
@@ -12,13 +12,10 @@ describe Interactors::Users::Delete do
     end
 
     it 'throws when non-existant user passed' do
-      user_id = double(:user_id)
+      user_id = 'a1234'
       User.stub(:find).with(user_id).and_return(nil)
       ability = double(:ability)
       ability.stub(:can?).with(:delete, nil).and_return(false)
-
-      #TODO: initial workaround, but this self-stubbing needs cleanup
-      described_class.any_instance.stub(:validate_hexadecimal_string).and_return(true)
 
       interactor = described_class.new(user_id: user_id, pavlov_options: { ability: ability } )
       expect do
@@ -27,36 +24,28 @@ describe Interactors::Users::Delete do
     end
 
     it 'throws when unauthorized' do
-      other_user = double(:user)
-      other_user_id = double(:user_id)
+      other_user = double(:user, id: 'b234')
 
       ability = double(:ability)
 
-      User.stub(:find).with(other_user_id).and_return(other_user)
+      User.stub(:find).with(other_user.id).and_return(other_user)
 
       ability.stub(:can?).with(:delete, other_user).and_return(false)
 
-      #TODO: initial workaround, but this self-stubbing needs cleanup
-      described_class.any_instance.stub(:validate_hexadecimal_string).and_return(true)
-
-      interactor = described_class.new(user_id: other_user_id, pavlov_options: {ability: ability})
+      interactor = described_class.new(user_id: other_user.id, pavlov_options: {ability: ability})
       expect { interactor.call }
         .to raise_error(Pavlov::AccessDenied)
     end
 
     it 'is authorized when cancan says so' do
-      user = double(:user)
-      user_id = double(:user_id)
-
+      user = double :user, id: 'a123'
+      User.stub(:find).with(user.id).and_return(user)
       ability = double(:ability)
-
-      User.stub(:find).with(user_id).and_return(user)
-
       ability.stub(:can?).with(:delete, user).and_return(true)
 
-      interactor = described_class.new(user_id: user_id, pavlov_options: {ability: ability})
-      expect(interactor.authorized?)
-        .to eq(true)
+      interactor = described_class.new(user_id: user.id, pavlov_options: {ability: ability})
+
+      expect(interactor.authorized?).to eq(true)
     end
   end
 
