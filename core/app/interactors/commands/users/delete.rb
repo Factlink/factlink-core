eaa24a66af296a4ec088242803c91210c84902c7
Eamon Nerbonne <eamon@nerbonne.org>
2013-09-24 10:39:57 +0200

    Delete command skeleton

---
+++ core/app/interactors/commands/users/delete.rb
@@ -0,0 +1,16 @@
+module Commands
+  module User
+    class Delete
+      include Pavlov::Command
+
+      arguments :user
+
+      def validate
+
+      end
+      def execute
+
+      end
+    end
+  end
+end
