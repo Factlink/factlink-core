f3121fb504fac5ae9578c19bbc3b853cae275518
Jan Paul Posma <me@janpaulposma.nl>
2013-09-24 12:22:09 +0200

    Made test more negative

--- core/spec/interactors/commands/users/anonymize_user_model_spec.rb
+++ core/spec/interactors/commands/users/anonymize_user_model_spec.rb
@@ -4,29 +4,31 @@ describe Commands::Users::AnonymizeUserModel do
   describe '#call' do
     it 'anonymizes fields of the user that could contain personal data' do
       user = create :active_user,
+        username: 'data',
+        first_name: 'data',
+        last_name: 'data',
+        location: 'data',
+        biography: 'data',
+        twitter: 'data',
+        identities: {'twitter' => 'data', 'facebook' => 'data'},
         password: '123hoi',
         password_confirmation: '123hoi'
 
       described_class.new(user_id: user.id).call
 
       saved_user = User.find(user.id)
-      expect(saved_user.first_name).to eq 'anonymous'
-      expect(saved_user.last_name).to eq 'anonymous'
-      expect(saved_user.location).to eq ''
-      expect(saved_user.biography).to eq ''
-      expect(saved_user.twitter).to eq ''
-      expect(saved_user.valid_password?('123hoi')).to be_false
+      expect(saved_user.first_name).to_not include('data')
+      expect(saved_user.last_name).to_not include('data')
+      expect(saved_user.location).to_not include('data')
+      expect(saved_user.biography).to_not include('data')
+      expect(saved_user.twitter).to_not include('data')
+      expect(saved_user.username).to_not include('data')
+
       expect(saved_user.identities['twitter']).to eq nil
       expect(saved_user.identities['facebook']).to eq nil
-    end
 
-    it 'generates some anonymous username and email' do
-      user = create :active_user
+      expect(saved_user.valid_password?('123hoi')).to be_false
 
-      described_class.new(user_id: user.id).call
-
-      saved_user = User.find(user.id)
-      expect(saved_user.username).to include('anonymous')
       expect(saved_user.email).to eq "deleted+#{saved_user.username}@factlink.com"
     end
   end
