034e2cca3bc7a63e0567b9f5d69244c6e776aa21
Eamon Nerbonne <eamon@nerbonne.org>
2013-09-25 13:21:27 +0200

    Added anonymization check to integration test

--- core/spec/integration-real/interactors/users/delete_spec.rb
+++ core/spec/integration-real/interactors/users/delete_spec.rb
@@ -5,11 +5,13 @@ describe Interactors::Users::Delete do
 
   it 'persistant stores the delete flag' do
     user = create :full_user
+    initial_username = user.username
     expect(user.deleted).to eq(false)
     as(user) do |pavlov|
       described_class.new(user_id: user.id.to_s, pavlov_options: pavlov.pavlov_options).call
     end
     reloaded_user = Pavlov.query(:'users_by_ids', user_ids:[user.id])[0]
     expect(reloaded_user.deleted).to eq(true)
+    expect(reloaded_user.username).to_not include(initial_username)
   end
 end
