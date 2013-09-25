5044fd9668e41e0be8e5c1cc14ceb787c653c148
Eamon Nerbonne <eamon@nerbonne.org>
2013-09-25 17:30:16 +0200

    Pass password in integration spec

--- core/spec/integration-real/interactors/users/delete_spec.rb
+++ core/spec/integration-real/interactors/users/delete_spec.rb
@@ -4,12 +4,15 @@ describe Interactors::Users::Delete do
   include PavlovSupport
 
   it 'persistant stores the delete flag' do
-    user = create :full_user
+    password = 'qwerty'
+    user = create(:full_user, password: password, password_confirmation: password)
     initial_username = user.username
     expect(user.deleted).to eq(false)
+
     as(user) do |pavlov|
-      pavlov.interactor :'users/delete', user_id: user.id.to_s
+      pavlov.interactor :'users/delete', user_id: user.id, current_user_password: password
     end
+
     reloaded_user = Pavlov.query(:'users_by_ids', user_ids:[user.id])[0]
     expect(reloaded_user.deleted).to eq(true)
     expect(reloaded_user.username).to_not include(initial_username)
