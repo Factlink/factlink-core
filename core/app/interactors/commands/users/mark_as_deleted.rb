9ac16ff8ceae0efae531b63bfde4cc633118686e
tomdev <tom@north26.nl>
2013-09-26 12:18:51 +0200

    Track user delete in MixPanel

--- core/app/interactors/commands/users/mark_as_deleted.rb
+++ core/app/interactors/commands/users/mark_as_deleted.rb
@@ -2,6 +2,7 @@ module Commands
   module Users
     class MarkAsDeleted
       include Pavlov::Command
+      include Util::Mixpanel
 
       arguments :user
 
@@ -12,8 +13,14 @@ module Commands
       end
 
       def execute
+        track_user_delete
         user.deleted = true
         user.save!
+      end
+
+      def track_user_delete
+        mp_track "Factlink: Created conversation",
+          user_id: user.id,
       end
     end
   end
