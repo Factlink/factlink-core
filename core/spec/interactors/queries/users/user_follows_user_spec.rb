72b0816594ee50366f9a4531fc5e23f03306e619
Jan Paul Posma <me@janpaulposma.nl>
2013-09-26 11:34:49 +0200

    Inlined lets

--- core/spec/interactors/queries/users/user_follows_user_spec.rb
+++ core/spec/interactors/queries/users/user_follows_user_spec.rb
@@ -5,29 +5,39 @@ describe Queries::Users::UserFollowsUser do
 
   describe '#call' do
 
-    let(:from_user) {create :full_user}
-    let(:to_user) {create :full_user}
-
-    let(:query) {described_class.new from_graph_user_id: from_user.graph_user_id,
-                                  to_graph_user_id: to_user.graph_user_id}
-
     it "should return false if the from_user doesn't follow the to_user" do
+      from_user = create :full_user
+      to_user = create :full_user
+
+      query = described_class.new from_graph_user_id: from_user.graph_user_id,
+                                    to_graph_user_id: to_user.graph_user_id
+
       expect(query.call).to be_false
     end
 
     it "should return true if the from_user does follow the to_user" do
+      from_user = create :full_user
+      to_user = create :full_user
       as(from_user) do |pavlov|
         pavlov.interactor(:'users/follow_user', user_name: from_user.username, user_to_follow_user_name: to_user.username)
       end
+
+      query = described_class.new from_graph_user_id: from_user.graph_user_id,
+                                    to_graph_user_id: to_user.graph_user_id
 
       expect(query.call).to be_true
     end
 
     it "should return false after unfollowing again" do
+      from_user = create :full_user
+      to_user = create :full_user
       as(from_user) do |pavlov|
         pavlov.interactor(:'users/follow_user', user_name: from_user.username, user_to_follow_user_name: to_user.username)
         pavlov.interactor(:'users/unfollow_user', user_name: from_user.username, user_to_unfollow_user_name: to_user.username)
       end
+
+      query = described_class.new from_graph_user_id: from_user.graph_user_id,
+                                    to_graph_user_id: to_user.graph_user_id
 
       expect(query.call).to be_false
     end
