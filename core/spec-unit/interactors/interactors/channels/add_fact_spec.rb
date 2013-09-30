414fb06c1b8e4632c364a7fd0adc06022b358b05
Mark IJbema <markijbema@gmail.com>
2013-09-30 13:52:50 +0200

    Fixed authorisation checks

--- core/spec-unit/interactors/interactors/channels/add_fact_spec.rb
+++ core/spec-unit/interactors/interactors/channels/add_fact_spec.rb
@@ -51,8 +51,10 @@ describe Interactors::Channels::AddFact do
     end
 
     it 'returns false when neither :current_user or :no_current_user are passed' do
-      expect_validating( fact: double, channels: double )
-        .to raise_error(Pavlov::AccessDenied)
+      expect do
+        interactor = described_class.new( fact: double, channels: double )
+        interactor.call
+      end.to raise_error(Pavlov::AccessDenied)
     end
   end
 end
