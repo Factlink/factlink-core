2f2a704f646d407931d1eff2bc122b4534789d63
Jan Paul Posma <me@janpaulposma.nl>
2013-10-02 16:47:50 +0200

    Don't use current_user in views per @markijbema's suggestion

--- core/app/controllers/users/setup_controller.rb
+++ core/app/controllers/users/setup_controller.rb
@@ -10,13 +10,14 @@ class Users::SetupController < ApplicationController
 
   def edit
     mp_track "Tour: Started account setup", current_user: current_user
+    @user = current_user
   end
 
   def update
-    user = interactor(:'accounts/setup', user: current_user, attribuutjes: params[:user])
+    @user = interactor(:'accounts/setup', user: current_user, attribuutjes: params[:user])
 
-    if user.errors.empty?
-      sign_in user, bypass: true # http://stackoverflow.com/questions/4264750/devise-logging-out-automatically-after-password-change
+    if @user.errors.empty?
+      sign_in @user, bypass: true # http://stackoverflow.com/questions/4264750/devise-logging-out-automatically-after-password-change
       redirect_to '/'
     else
       render 'users/setup/edit'
