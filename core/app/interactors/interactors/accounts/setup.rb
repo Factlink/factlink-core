cd1175f0ff12e6c9245ba7788cc88d40816ffad4
Jan Paul Posma <me@janpaulposma.nl>
2013-10-02 17:33:03 +0200

    Removed now unused user.persisted? logic, as the user should always be logged in anyway

--- core/app/interactors/interactors/accounts/setup.rb
+++ core/app/interactors/interactors/accounts/setup.rb
@@ -10,12 +10,9 @@ module Interactors
       end
 
       def execute
-        if user.persisted?
-          user.attributes = attribuutjes.slice(:first_name, :last_name)
-          user.set_up = true
-
-          user.reset_password!(attribuutjes[:password], attribuutjes[:password_confirmation])
-        end
+        user.attributes = attribuutjes.slice(:first_name, :last_name)
+        user.set_up = true
+        user.reset_password!(attribuutjes[:password], attribuutjes[:password_confirmation])
 
         user
       end
