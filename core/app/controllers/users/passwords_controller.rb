373502aa1db5dae7c971169bb5bea839e7383e9a
Jan Paul Posma <me@janpaulposma.nl>
2013-10-02 16:25:59 +0200

    Make brakeman happy

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -8,12 +8,12 @@ class Users::PasswordsController < Devise::PasswordsController
     resource.reset_password_token = params[:reset_password_token]
     # end of copy
 
-    @user = User.where(reset_password_token: params[:reset_password_token]).first
+    @user = User.where(reset_password_token: params[:reset_password_token].to_s).first
 
     if params[:msg]
       if @user
         sign_in @user, bypass: true
-        redirect_to after_sign_in_path_for(@user)
+        redirect_to after_sign_in_path_for(@user).to_s
       else
         redirect_to new_user_session_path,
           notice: 'Your account is already set up. Please log in to continue.'
