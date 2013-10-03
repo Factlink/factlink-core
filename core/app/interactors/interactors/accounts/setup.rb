0cc30819c774f9a666de42c5f3ba4d853c1fe253
Jan Paul Posma <me@janpaulposma.nl>
2013-10-03 13:18:59 +0200

    Added clarification

--- core/app/interactors/interactors/accounts/setup.rb
+++ core/app/interactors/interactors/accounts/setup.rb
@@ -13,6 +13,7 @@ module Interactors
         user.attributes = attribuutjes.slice(:first_name, :last_name)
         user.set_up = true
         user.reset_password!(attribuutjes[:password], attribuutjes[:password_confirmation])
+        # reset_password! calls "user.save"
 
         user
       end
