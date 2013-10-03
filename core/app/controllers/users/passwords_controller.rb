bd8db9430544063ba52cbbf4f74b877ab9725375
Jan Paul Posma <me@janpaulposma.nl>
2013-10-03 13:22:11 +0200

    Simplified PasswordsController by just calling super

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -3,14 +3,9 @@ class Users::PasswordsController < Devise::PasswordsController
   layout "one_column_simple"
 
   def edit
-    # Copied from Devise::PasswordsController
-    self.resource = resource_class.new
-    resource.reset_password_token = params[:reset_password_token]
-    # end of copy
-
-    @user = User.where(reset_password_token: params[:reset_password_token].to_s).first
-
     if params[:msg]
+      @user = User.where(reset_password_token: params[:reset_password_token].to_s).first
+
       if @user
         sign_in @user, bypass: true
         redirect_to after_sign_in_path_for(@user).to_s
@@ -19,7 +14,7 @@ class Users::PasswordsController < Devise::PasswordsController
           notice: 'Your account is already set up. Please log in to continue.'
       end
     else
-      render :edit
+      super
     end
   end
 
