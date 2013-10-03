2fcd04309a9ccbd0868d62fe509d9b6923194cd9
Jan Paul Posma <me@janpaulposma.nl>
2013-10-03 14:27:11 +0200

    Use pavlov.query syntax

--- core/spec/integration-real/queries/users/user_follows_user_spec.rb
+++ core/spec/integration-real/queries/users/user_follows_user_spec.rb
@@ -18,28 +18,30 @@ describe Queries::Users::UserFollowsUser do
     it "should return true if the from_user does follow the to_user" do
       from_user = create :full_user
       to_user = create :full_user
+
       as(from_user) do |pavlov|
         pavlov.interactor(:'users/follow_user', user_name: from_user.username, user_to_follow_user_name: to_user.username)
+
+        result = pavlov.query 'users/user_follows_user', from_graph_user_id: from_user.graph_user_id,
+                                                         to_graph_user_id: to_user.graph_user_id
+
+        expect(result).to be_true
       end
-
-      query = described_class.new from_graph_user_id: from_user.graph_user_id,
-                                    to_graph_user_id: to_user.graph_user_id
-
-      expect(query.call).to be_true
     end
 
     it "should return false after unfollowing again" do
       from_user = create :full_user
       to_user = create :full_user
+
       as(from_user) do |pavlov|
         pavlov.interactor(:'users/follow_user', user_name: from_user.username, user_to_follow_user_name: to_user.username)
         pavlov.interactor(:'users/unfollow_user', user_name: from_user.username, user_to_unfollow_user_name: to_user.username)
+
+        result = pavlov.query 'users/user_follows_user', from_graph_user_id: from_user.graph_user_id,
+                                                         to_graph_user_id: to_user.graph_user_id
+
+        expect(result).to be_false
       end
-
-      query = described_class.new from_graph_user_id: from_user.graph_user_id,
-                                    to_graph_user_id: to_user.graph_user_id
-
-      expect(query.call).to be_false
     end
   end
 end
