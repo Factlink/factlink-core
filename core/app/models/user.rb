85d9063d536c7c4ea95663b763eb00cdb5b7cab5
Jan Paul Posma <me@janpaulposma.nl>
2013-10-05 14:48:27 +0200

    Don't use approved? anymore for checking if a user can retrieve password, use suspended?

--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -324,7 +324,7 @@ class User
   # don't send reset password instructions when the account is not approved yet
   def self.send_reset_password_instructions(attributes={})
     recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-    if !recoverable.approved?
+    if recoverable.suspended?
       recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
     elsif recoverable.persisted?
       recoverable.send_reset_password_instructions
