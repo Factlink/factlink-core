82bcfb60e0ac3fb4273e7234c098a1ae10c7f802
Martijn Russchen <martijn@factlink.com>
2013-10-08 10:39:20 +0200

    Removed collapse

--- core/app/assets/javascripts/twitter/bootstrap.js
+++ core/app/assets/javascripts/twitter/bootstrap.js
@@ -1,6 +1,5 @@
 //= require twitter/bootstrap/alert.js
 //= require twitter/bootstrap/button.js
-//= require twitter/bootstrap/collapse.js
 //= require twitter/bootstrap/dropdown.js
 //= require twitter/bootstrap/tooltip.js
 //= require twitter/bootstrap/popover.js
