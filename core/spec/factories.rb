9ca68a9312f1e0b2be2ee4b7c3cada16a00be6c0
Mark IJbema <markijbema@gmail.com>
2013-10-09 13:48:55 +0200

    Removed sharing feature toggles

--- core/spec/factories.rb
+++ core/spec/factories.rb
@@ -49,12 +49,10 @@ FactoryGirl.define do
     end
 
     trait :connected_twitter do
-      features [:share_to_twitter]
       identities('twitter' => {'credentials' => {'token' => 'token', 'secret' => 'secret'}})
     end
 
     trait :connected_facebook do
-      features [:share_to_facebook]
       identities('facebook' => {'credentials' => {
         'token'      => 'token',
         'expires_at' => (DateTime.now + 4.weeks).to_i,
--- core/spec/models/ability_spec.rb
+++ core/spec/models/ability_spec.rb
@@ -270,15 +270,7 @@ describe Ability do
       it "should be possible to share to Twitter" do
         user.identities['twitter'] = {}
 
-        subject.should_not be_able_to :share_to, :twitter
-      end
-      context "with the feature toggle enabled" do
-        it "should be possible to share to Twitter" do
-          user.identities['twitter'] = {}
-          user.features = [:share_to_twitter]
-
-          Ability.new(user).should be_able_to :share_to, :twitter
-        end
+        Ability.new(user).should be_able_to :share_to, :twitter
       end
     end
 
@@ -286,15 +278,7 @@ describe Ability do
       it "should be possible to share to Facebook" do
         user.identities['facebook'] = {}
 
-        subject.should_not be_able_to :share_to, :facebook
-      end
-      context "with the feature toggle enabled" do
-        it "should be possible to share to Facebook" do
-          user.identities['facebook'] = {}
-          user.features = [:share_to_facebook]
-
-          Ability.new(user).should be_able_to :share_to, :facebook
-        end
+        Ability.new(user).should be_able_to :share_to, :facebook
       end
     end
   end
