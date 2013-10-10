3ff9c36ada70920ee1fbf698a6e478d4a5914b82
Martijn Russchen <martijn@factlink.com>
2013-10-10 11:19:51 +0200

    Changed migration that only active users will get a token

--- core/db/migrate/20130918133016_users_notification_token.rb
+++ core/db/migrate/20130918133016_users_notification_token.rb
@@ -1,6 +1,6 @@
 class UsersNotificationToken < Mongoid::Migration
   def self.up
-    User.each do |user|
+    User.active.each do |user|
       user.reset_notification_settings_edit_token
       user.save!
     end
