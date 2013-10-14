575146ce3941515b23f10b5e6f15174c82e8621c
Martijn Russchen <martijn@factlink.com>
2013-10-14 14:29:54 +0200

    Refactored controller

--- core/app/controllers/mail_subscriptions_controller.rb
+++ core/app/controllers/mail_subscriptions_controller.rb
@@ -1,16 +1,14 @@
 class MailSubscriptionsController < ApplicationController
   layout "responsive_notifications"
 
+  before_filter :get_user
+
   def update
-    if @user = User.where("user_notification.notification_settings_edit_token" => params[:token]).first
-      success = case params[:subscribe_action].to_s
-                when 'unsubscribe'
-                  @user.user_notification.unsubscribe(params[:type])
-                when 'subscribe'
-                  @user.user_notification.subscribe(params[:type])
-                else false
-                end
-      if success
+    @type = params[:type]
+    @subscribe_action = params[:subscribe_action]
+
+    if @user
+      if update_subscription(@subscribe_action, @type, @user)
         render 'success'
       else
         render 'error'
@@ -20,4 +18,22 @@ class MailSubscriptionsController < ApplicationController
     end
   end
 
+  private
+
+  def get_user
+    @user = User.where("user_notification.notification_settings_edit_token" => params[:token]).first
+  end
+
+  def update_subscription(subscribe_action, type, user)
+    case subscribe_action.to_s
+    when 'unsubscribe'
+      success = user.user_notification.unsubscribe(type)
+      SubscriptionsMailer.unsubscribe(user.id, type).deliver
+      success
+    when 'subscribe'
+      user.user_notification.subscribe(type)
+    else
+      false
+    end
+  end
 end
