baacc8cef9eae223ee62200a1697a7abaca05377
Jan Paul Posma <me@janpaulposma.nl>
2013-10-15 16:23:27 +0200

    Made new confirmation mail page working again

--- core/app/controllers/users/confirmations_controller.rb
+++ core/app/controllers/users/confirmations_controller.rb
@@ -28,4 +28,21 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     self.resource.save!(validate: false)
   end
 
+  def new
+    super
+
+    self.resource = current_user if current_user
+  end
+
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+
+    if successfully_sent?(resource)
+      flash[:notice] = 'Resent confirmation instructions to your email address.'
+      render 'users/confirmations/new'
+    else
+      respond_with(resource)
+    end
+  end
+
 end
