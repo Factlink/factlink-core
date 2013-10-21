559575594ab5c44053ef2b45ce62b04b54bdea4a
Mark IJbema <markijbema@gmail.com>
2013-10-21 14:28:35 +0200

    use stub instead of expectation
    switched to new spec expectation syntax

--- core/spec-unit/interactors/queries/channels/facts_spec.rb
+++ core/spec-unit/interactors/queries/channels/facts_spec.rb
@@ -15,14 +15,14 @@ describe Queries::Channels::Facts do
 
       query = described_class.new id: channel.id, from: from, count: count
 
-      Pavlov.stub(:query)
-            .with(:'channels/get', id: channel.id)
-            .and_return(channel)
-      Pavlov.should_receive(:query)
-            .with(:'facts/get_paginated',
-                  key: channel.sorted_cached_facts.key.to_s,
-                  count: count, from: from)
-            .and_return(sorted_facts_page)
+      allow(Pavlov).to receive(:query)
+        .with(:'channels/get', id: channel.id)
+        .and_return(channel)
+      allow(Pavlov).to receive(:query)
+        .with(:'facts/get_paginated',
+              key: channel.sorted_cached_facts.key.to_s,
+              count: count, from: from)
+        .and_return(sorted_facts_page)
 
       expect(query.call).to eq sorted_facts_page
     end
