2b3e64f69c379ac2421d3f8d5d4a8dc5b3cd9377
Mark IJbema <markijbema@gmail.com>
2013-10-21 14:32:04 +0200

    made spec a bit clearer

--- core/spec/integration-real/queries/facts/get_paginated_spec.rb
+++ core/spec/integration-real/queries/facts/get_paginated_spec.rb
@@ -9,10 +9,10 @@ describe Queries::Facts::GetPaginated do
     as(user) do |pavlov|
       fact1 = pavlov.command :'facts/create',
                 displaystring: 'foo', title: 'foo', creator: user
-      sleep 0.1
+      sleep 0.1 # ensure different timestamp
       fact2 = pavlov.command :'facts/create',
                 displaystring: 'foo', title: 'foo', creator: user
-      sleep 0.1
+      sleep 0.1 # ensure different timestamp
       fact3 = pavlov.command :'facts/create',
                 displaystring: 'foo', title: 'foo', creator: user
 
@@ -20,11 +20,11 @@ describe Queries::Facts::GetPaginated do
                  key: user.graph_user.sorted_created_facts.key.to_s,
                  from: 'inf', count: 2
 
-      fact_ids = result.map do |fact_with_score|
+      result_fact_ids = result.map do |fact_with_score|
         fact_with_score[:item].id
       end
 
-      expect(fact_ids).to eq [fact3.id, fact2.id]
+      expect(result_fact_ids).to eq [fact3.id, fact2.id]
     end
   end
 end
