ff06c3af2a56ec031eab35905b7c083d759a32f6
Martijn Russchen <martijn@factlink.com>
2013-10-22 16:18:46 +0200

    Fixed migration for users without a first or last name

--- core/db/migrate/20131022081212_merge_first_and_last_name.rb
+++ core/db/migrate/20131022081212_merge_first_and_last_name.rb
@@ -1,10 +1,12 @@
 class MergeFirstAndLastName < Mongoid::Migration
   def self.up
     User.all.each do |u|
-      u.full_name = u[:first_name] + ' ' + u[:last_name]
-      u.remove_attribute(:first_name)
-      u.remove_attribute(:last_name)
-      u.save(validate: false)
+      if u[:full_name].blank?
+        u.full_name = "#{u[:first_name]} #{u[:last_name]}"
+        u.remove_attribute(:first_name)
+        u.remove_attribute(:last_name)
+        u.save(validate: false)
+      end
     end
   end
 
