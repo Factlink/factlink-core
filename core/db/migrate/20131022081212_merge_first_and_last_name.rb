cc8f955120626242796fb606e14d9af89d151e69
Eamon Nerbonne <eamon@nerbonne.org>
2013-10-22 11:59:50 +0200

    Remove debug output.

--- core/db/migrate/20131022081212_merge_first_and_last_name.rb
+++ core/db/migrate/20131022081212_merge_first_and_last_name.rb
@@ -1,9 +1,7 @@
 class MergeFirstAndLastName < Mongoid::Migration
   def self.up
     User.all.each do |u|
-      puts "first name:#{u[:first_name]}; last name: #{u[:last_name]}"
       u.full_name = u[:first_name] + ' ' + u[:last_name]
-      puts "full name:#{u[:full_name]}"
       u.remove_attribute(:first_name)
       u.remove_attribute(:last_name)
       u.save!
