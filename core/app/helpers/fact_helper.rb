2d182863a6ae7f0f070765446ea9bf88e0c60a5c
Martijn Russchen <martijn@factlink.com>
2013-10-23 15:56:35 +0200

    Removed open tab path

--- core/app/helpers/fact_helper.rb
+++ core/app/helpers/fact_helper.rb
@@ -2,9 +2,6 @@ module FactHelper
 
   # IE 8 max url length is 2083 characters according to
   # http://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url
-  def friendly_fact_with_opened_tab_path(fact, tab_type)
-    "#{friendly_fact_path(fact)}?tab=#{tab_type}"
-  end
 
   def friendly_fact_path(fact, max_slug_length = 1024)
     # Obsolete method
