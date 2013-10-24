f6535fcb1f282a32cfd668d13fb0a7ceac46e7a9
Martijn Russchen <martijn@factlink.com>
2013-10-24 10:27:15 +0200

    Removed another spec

--- core/spec/views/tos/show_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "tos/show" do
-  it "should render" do
-    view.stub(:current_user) { create :user }
-    render
-    rendered.should have_selector('div')
-  end
-end
