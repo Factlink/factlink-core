c1af52999fdaf0122d56b84eba832a3c14cde3ac
Martijn Russchen <martijn@factlink.com>
2013-10-28 17:44:23 +0100

    Use button within header class

--- core/spec/screenshots/static_spec.rb
+++ core/spec/screenshots/static_spec.rb
@@ -8,7 +8,10 @@ describe "Static pages:", type: :feature do
     end
     it "with empty submit and open feedback as expected" do
       visit "/"
-      find(:button, 'Create account').click
+
+      within '.header' do
+        find(:button, 'Create account').click
+      end
 
       find('a', text: 'Feedback').click
       within_frame 0 do
