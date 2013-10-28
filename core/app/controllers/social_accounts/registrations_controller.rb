72bededd96b11d64b6e7c2e656b6fc83ef4a84e9
Jan Paul Posma <me@janpaulposma.nl>
2013-10-28 10:56:53 +0100

    Inlined method

--- core/app/controllers/social_accounts/registrations_controller.rb
+++ core/app/controllers/social_accounts/registrations_controller.rb
@@ -2,7 +2,21 @@ class SocialAccounts::RegistrationsController < ApplicationController
   layout 'social_account_popup'
 
   def callback_sign_in
-    sign_in_through_provider params[:provider_name], request.env['omniauth.auth']
+    social_account = SocialAccount.find_by_provider_and_uid(provider_name, omniauth_obj['uid'])
+
+    if social_account and social_account.user
+      @user = social_account.user
+      sign_in @user
+      @event = { name: 'signed_in' }
+
+      render :'social_accounts/callback'
+    else
+      social_account.delete if social_account # TODO: update existing one
+      @social_account = SocialAccount.create! provider_name: provider_name, omniauth_obj: omniauth_obj
+      @user = User.new
+
+      render :'social_accounts/registrations/sign_up_or_in'
+    end
   rescue Exception => error
     @event = { name: "social_error", details: error.message }
     render :'social_accounts/callback'
@@ -28,24 +42,6 @@ class SocialAccounts::RegistrationsController < ApplicationController
   end
 
   private
-
-  def sign_in_through_provider provider_name, omniauth_obj
-    social_account = SocialAccount.find_by_provider_and_uid(provider_name, omniauth_obj['uid'])
-
-    if social_account and social_account.user
-      @user = social_account.user
-      sign_in @user
-      @event = { name: 'signed_in' }
-
-      render :'social_accounts/callback'
-    else
-      social_account.delete if social_account # TODO: update existing one
-      @social_account = SocialAccount.create! provider_name: provider_name, omniauth_obj: omniauth_obj
-      @user = User.new
-
-      render :'social_accounts/registrations/sign_up_or_in'
-    end
-  end
 
   def sign_in_and_connect_existing_user email, password
     return if email.blank?
@@ -85,5 +81,13 @@ class SocialAccounts::RegistrationsController < ApplicationController
     user.save
 
     user
+  end
+
+  def provider_name
+    params[:provider_name]
+  end
+
+  def omniauth_obj
+    request.env['omniauth.auth']
   end
 end
