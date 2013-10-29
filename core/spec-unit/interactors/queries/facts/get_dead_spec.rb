4cda2d63aa63128370bd9509dbfced091fd4ac4f
Mark IJbema <markijbema@gmail.com>
2013-10-29 12:35:35 +0100

    Replaced evidence_count by deletable?

--- core/spec-unit/interactors/queries/facts/get_dead_spec.rb
+++ core/spec-unit/interactors/queries/facts/get_dead_spec.rb
@@ -11,45 +11,57 @@ describe Queries::Facts::GetDead do
 
   describe 'validations' do
     it 'requires fact_id to be an integer' do
-      expect_validating(id: 'a').
-        to fail_validation('id should be an integer string.')
+      expect_validating(id: 'a')
+        .to fail_validation('id should be an integer string.')
     end
   end
 
   describe '#call' do
-    before do
-      stub_classes 'Fact', 'FactlinkUI::Application', 'CGI'
-      FactlinkUI::Application.stub config: double(proxy_url: "proxy_url")
-      CGI.stub escape: ''
+    let(:fact_data) do
+      double :fact_data,
+        displaystring: 'example fact text',
+        created_at: 15,
+        title: 'title'
+    end
+    let(:live_fact) do
+      double :fact,
+        id: '1',
+        has_site?: false,
+        data: fact_data,
+        deletable?: false
+    end
+    let(:wheel) { double }
 
-      described_class.any_instance.stub(:url).and_return("")
+    let(:site) do
+      double :site, url: 'http://example.org/'
+    end
+
+    let(:live_fact_with_site) do
+      double :fact,
+        id: '2',
+        has_site?: true,
+        site: site,
+        data: fact_data,
+        deletable?: false
+    end
+
+    before do
+      Fact.stub(:[])
+          .with(live_fact.id)
+          .and_return(live_fact)
+      Fact.stub(:[])
+          .with(live_fact_with_site.id)
+          .and_return(live_fact_with_site)
+      Pavlov.stub(:query)
+            .with(:'facts/get_dead_wheel', id: live_fact.id)
+            .and_return(wheel)
+      Pavlov.stub(:query)
+            .with(:'facts/get_dead_wheel', id: live_fact_with_site.id)
+            .and_return(wheel)
     end
 
     it 'returns a fact' do
-      fact_data = double :fact_data,
-          displaystring: 'example fact text',
-          created_at: 15,
-          title: 'title'
-      live_fact = double :fact,
-          id: '1',
-          has_site?: false,
-          data: fact_data
-      wheel = double
-      evidence_count = 10
-
       interactor = Queries::Facts::GetDead.new id: live_fact.id
-
-      Fact.stub(:[]).with(live_fact.id).and_return(live_fact)
-
-      Pavlov.stub(:query)
-            .with(:'facts/get_dead_wheel',
-                      id: live_fact.id)
-            .and_return(wheel)
-
-      Pavlov.stub(:query)
-            .with(:'evidence/count_for_fact',
-                      fact: live_fact)
-            .and_return(evidence_count)
 
       dead_fact = interactor.call
 
@@ -58,23 +70,11 @@ describe Queries::Facts::GetDead do
       expect(dead_fact.created_at).to eq live_fact.data.created_at
       expect(dead_fact.title).to eq live_fact.data.title
       expect(dead_fact.wheel).to eq wheel
-      expect(dead_fact.evidence_count).to eq evidence_count
+      expect(dead_fact.deletable?).to eq live_fact.deletable?
     end
 
     it 'returns a fact which has no site or proxy_scroll_url without site_url' do
-      fact_data = double :fact_data,
-          displaystring: 'example fact text',
-          created_at: 15,
-          title: 'title'
-      live_fact = double :fact,
-          id: '1',
-          has_site?: false,
-          data: fact_data
-
       query = Queries::Facts::GetDead.new id: live_fact.id
-
-      Pavlov.stub query: double
-      Fact.stub(:[]).with(live_fact.id).and_return(live_fact)
 
       dead_fact = query.call
 
@@ -82,21 +82,7 @@ describe Queries::Facts::GetDead do
     end
 
     it 'returns a fact which has a site with site_url' do
-      fact_data = double :fact_data,
-          displaystring: 'example fact text',
-          created_at: 15,
-          title: 'title'
-      site = double :site, url: 'http://example.org/'
-      live_fact = double :fact,
-          id: '1',
-          has_site?: true,
-          site: site,
-          data: fact_data
-
-      query = Queries::Facts::GetDead.new id: live_fact.id
-
-      Pavlov.stub query: double
-      Fact.stub(:[]).with(live_fact.id).and_return(live_fact)
+      query = Queries::Facts::GetDead.new id: live_fact_with_site.id
 
       dead_fact = query.call
 
