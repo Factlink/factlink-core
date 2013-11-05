29a8621eae79c4bde12e483b2f35fe1227f7043a
Jan Paul Posma <me@janpaulposma.nl>
2013-11-05 15:57:07 +0100

    Extracted an EditPasswordController out of RegistrationsController

--- core/app/controllers/users/registrations_controller.rb
+++ core/app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout "frontend"
 
-  before_filter :load_user, only: [:edit_password, :update_password]
   before_filter :authenticate_user!
 
   def new
@@ -49,23 +48,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
 
       render json: error_hash, :status => :unprocessable_entity
-    end
-  end
-
-  def edit_password
-    authorize! :update, @user
-
-    render "users/edit_password"
-  end
-
-  def update_password
-    authorize! :update, @user
-
-    if @user.update_with_password(params[:user])
-      sign_in @user, bypass: true # http://stackoverflow.com/questions/4264750/devise-logging-out-automatically-after-password-change
-      redirect_to user_password_edit_url(@user), notice: 'Your password was successfully updated.'
-    else
-      render "users/edit_password"
     end
   end
 
