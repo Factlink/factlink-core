7537bc96e5b9acbbfdc86b1c26388ba761f919d9
Jan Paul Posma <me@janpaulposma.nl>
2013-11-06 10:28:06 +0100

    Fixed interactor that failed when running all unit tests together

--- core/app/interactors/interactors/normalize_all_site_urls.rb
+++ core/app/interactors/interactors/normalize_all_site_urls.rb
@@ -9,7 +9,7 @@ module Interactors
     private
 
     def execute
-      Site.all.ids.each do |id|
+      ::Site.all.ids.each do |id|
         Resque.enqueue Interactors::NormalizeSiteUrl, site_id: id,
           normalizer_class_name: :UrlNormalizer
       end
