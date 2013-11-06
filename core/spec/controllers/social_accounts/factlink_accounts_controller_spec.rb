2de99683a999f521daa51cffb631550e27c4ef2c
Jan Paul Posma <me@janpaulposma.nl>
2013-11-06 14:39:44 +0100

    Added tests for FactlinkAccountsController

---
+++ core/spec/controllers/social_accounts/factlink_accounts_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe SocialAccounts::FactlinkAccountsController do
+  render_views
+
+  describe :new do
+    it 'renders' do
+      get :new
+
+      expect(response).to be_success
+    end
+  end
+
+  describe :create_session do
+    context 'giving correct credentials' do
+      it 'signs in' do
+        email = 'janpaul@factlink.com'
+        password = '123hoi'
+        user = create :user, email: email, password: password
+
+        post :create_session, user_new_session: {login: email, password: password}
+
+        expect(response.body).to match 'eventName = "signed_in"'
+      end
+    end
+
+    context 'giving incorrect credentials' do
+      it 'shows an error' do
+        post :create_session
+
+        expect(response.body).to match 'incorrect email address or password'
+      end
+    end
+  end
+
+  describe :create_account do
+    context 'giving correct information' do
+      it 'signs in' do
+        full_name = 'Jan Paul Posma'
+        email = 'janpaul@factlink.com'
+        password = '123hoi'
+
+        post :create_account, user_new_account: {
+          full_name: full_name, email: email,
+          password: password, password_confirmation: password
+        }
+
+        expect(response.body).to match 'eventName = "signed_in"'
+        expect(User.last.email).to eq email
+      end
+    end
+
+    context 'giving incorrect information' do
+      it 'shows an error' do
+        post :create_account
+
+        expect(response.body).to match 'is required'
+      end
+    end
+  end
+end
