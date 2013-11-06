e0d417b1181ada9176140be0b7ada80e2a438fa8
Jan Paul Posma <me@janpaulposma.nl>
2013-11-06 10:13:44 +0100

    Added interactor for normalizing all sites again

---
+++ core/app/interactors/interactors/normalize_all_site_urls.rb
@@ -0,0 +1,18 @@
+module Interactors
+  class NormalizeAllSiteUrls
+    include Pavlov::Interactor
+
+    def authorized?
+      true # Only migrations should call this
+    end
+
+    private
+
+    def execute
+      Site.all.ids.each do |id|
+        Resque.enqueue Interactors::NormalizeSiteUrl, site_id: id,
+          normalizer_class_name: :UrlNormalizer
+      end
+    end
+  end
+end
