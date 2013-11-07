2047f5d4ab5cf2f9bfb9f476a03bd36687b13358
Jan Paul Posma <me@janpaulposma.nl>
2013-11-07 17:46:06 +0100

    Removed old "dimmed" class

--- core/spec/support/acceptance/conversation_helper.rb
+++ core/spec/support/acceptance/conversation_helper.rb
@@ -59,7 +59,7 @@ module Acceptance
     end
 
     def open_message_with_content(message_str)
-      find(".navbar-inner:not(.dimmed) .conversations-link").click
+      find(".navbar-inner .conversations-link").click
 
       within '.conversations li' do
         page.should have_content(message_str)
