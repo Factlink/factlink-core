e4ffe62713789d8b428b042fe873dcc2f102fb18
Jan Paul Posma <me@janpaulposma.nl>
2013-11-13 14:54:49 +0100

    Removed tests for old confirmation behaviour

--- core/spec/controllers/users/confirmations_controller_spec.rb
+++ core/spec/controllers/users/confirmations_controller_spec.rb
@@ -20,29 +20,6 @@ describe Users::ConfirmationsController do
       expect(response).to redirect_to setup_account_path
     end
 
-    it "doesn't allow tokens of more than a month old" do
-      user = create :user
-
-      Timecop.travel(40.days) do
-        get :show, confirmation_token: user.confirmation_token
-      end
-
-      user.reload
-      expect(user).to_not be_confirmed
-
-      expect(response.body).to match /needs to be confirmed within 1 month, please request a new one/
-    end
-
-    it "redirects to setup page when clicking for a second time" do
-      user = create :user
-
-      get :show, confirmation_token: user.confirmation_token
-      sign_out(user)
-      get :show, confirmation_token: user.confirmation_token
-
-      expect(response).to redirect_to setup_account_path
-    end
-
     it "works when the user is already signed in" do
       user = create :user
 
