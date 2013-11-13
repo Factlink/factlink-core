b2cdf3495e3a87572695baec68a015fb7e38a714
Jan Paul Posma <me@janpaulposma.nl>
2013-11-13 13:54:37 +0100

    Remove old approval flow, just have people use the reset password functionality

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -1,24 +1,3 @@
 class Users::PasswordsController < Devise::PasswordsController
-
   layout "one_column"
-
-  def edit
-    if params[:msg] # Warning! Also used for logging in unconfirmed users who request new password
-      @user = User.where(reset_password_token: params[:reset_password_token].to_s).first
-
-      if @user
-        @user.skip_confirmation!
-        @user.save(validate: false)
-
-        sign_in @user, bypass: true
-        redirect_to after_sign_in_path_for(@user).to_s
-      else
-        redirect_to new_user_session_path,
-          notice: 'Your account is already set up. Please log in to continue.'
-      end
-    else
-      super
-    end
-  end
-
 end
