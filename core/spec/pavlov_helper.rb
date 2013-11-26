654f24cf9225bf7c0c80d951d8acc43ecb4abc57
Mark IJbema <markijbema@gmail.com>
2013-11-26 08:21:19 +0100

    Removed KillObject.topic
    switched to using DeadTopic. Also prefer not to stub value objects in tests.

--- core/spec/pavlov_helper.rb
+++ core/spec/pavlov_helper.rb
@@ -4,3 +4,7 @@ require_relative '../app/interactors/util/can_can.rb'
 require_relative '../app/interactors/util/validations.rb'
 require_relative '../app/interactors/util/pavlov_context_serialization.rb'
 require_relative 'support/unit/pavlov_support.rb'
+
+# We want to be able to always use these simple value classes
+# They should not be stubbed/mocked, but just used
+require_relative '../app/entities/dead_topic.rb'
