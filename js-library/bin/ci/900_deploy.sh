584edccb180d1ac5797c7db522df323cb6004d42
Martijn Russchen <martijn@factlink.com>
2013-11-28 11:20:58 +0100

    bundle before deploy

--- js-library/bin/ci/900_deploy.sh
+++ js-library/bin/ci/900_deploy.sh
@@ -3,6 +3,7 @@ if [[ ! -z $DEPLOY_SERVER ]]; then
   echo "Deploying..."
 
   echo "deploying to $DEPLOY_SERVER"
+  bundle
   bundle exec cap -S "branch=$GIT_COMMIT" $DEPLOY_SERVER deploy -s "build_number=$BUILD_NUMBER" || exit 1
   echo "deployed to $DEPLOY_SERVER"
 else
