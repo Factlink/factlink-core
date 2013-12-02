29aad50e42ff3ad1c3bad68e0411dcd50d9d69b0
Martijn Russchen <martijn@factlink.com>
2013-12-02 16:27:52 +0100

    Create discussion and add fact to channel

--- core/spec/screenshots/non_signed_in_spec.rb
+++ core/spec/screenshots/non_signed_in_spec.rb
@@ -1,9 +1,17 @@
 require 'screenshot_helper'
 
 describe "Non signed in pages:", type: :feature do
+  include Acceptance::ChannelHelper
+  include Screenshots::DiscussionHelper
+
   describe "Profile page page" do
     it "it renders correctly" do
       @user = sign_in_user create :full_user
+
+      factlink = backend_create_fact
+      channel = backend_create_channel_of_user @user
+      backend_add_fact_to_channel factlink, channel
+
       sign_out_user
       visit user_path(@user)
 
