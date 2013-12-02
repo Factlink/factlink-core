500efdd40312a0f2e58f92609b0dfefb3e6f6657
Jan Paul Posma <me@janpaulposma.nl>
2013-12-02 14:52:05 +0100

    Removed some simple migrations that didn't really use any other code

--- core/db/migrate/20120920152043_create_elastic_search_index_for_fact_data.rb
+++ core/db/migrate/20120920152043_create_elastic_search_index_for_fact_data.rb
@@ -1,10 +1,6 @@
 class CreateElasticSearchIndexForFactData < Mongoid::Migration
   def self.up
-    say_with_time "Preparing to index ElasticSearch for all FactData" do
-      FactData.all.each do |fact_data|
-        Resque.enqueue(CreateSearchIndexForFactData, fact_data.id)
-      end
-    end
+    puts "old migration doesn't do anything anymore"
   end
 
   def self.down
