2eb457e3e6865e3d53fe43b60216143dc4e91de0
Martijn Russchen <martijn@factlink.com>
2013-12-03 12:12:49 +0100

    Group subjects

--- core/app/mailers/activity_mailer.rb
+++ core/app/mailers/activity_mailer.rb
@@ -35,18 +35,10 @@ class ActivityMailer < ActionMailer::Base
     user = activity.user.user
 
     case activity.action
-    when 'added_supporting_evidence'
-      "#{user} added a supporting argument to a Factlink"
-    when 'added_weakening_evidence'
-      "#{user} added a weakening argument to a Factlink"
     when 'created_conversation'
       "#{user} has sent you a message"
     when 'replied_message'
       "#{user} has replied to a message"
-    when 'created_comment'
-      "#{user} has commented on a Factlink"
-    when 'created_sub_comment'
-      "#{user} has commented on a Factlink"
     when 'followed_user'
       "#{user} is now following you on Factlink"
     else
