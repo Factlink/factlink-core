a4170077fb9cc80c91cb2e9fdd860bff2bc55fe4
Jan Paul Posma <me@janpaulposma.nl>
2013-12-03 10:37:30 +0100

    Removed authority from sub comment json

--- core/spec-unit/interactors/interactors/sub_comments/index_for_comment_spec.rb
+++ core/spec-unit/interactors/interactors/sub_comments/index_for_comment_spec.rb
@@ -34,9 +34,7 @@ describe Interactors::SubComments::IndexForComment do
   describe '#call' do
     it do
       comment_id = '2b'
-      sub_comments = [double, double]
-      dead_sub_comments = [double, double]
-      authorities = [10, 20]
+      dead_sub_comments = double
       options = {ability: double(can?: true)}
       interactor = described_class.new(comment_id: comment_id,
         pavlov_options: options)
@@ -47,19 +45,7 @@ describe Interactors::SubComments::IndexForComment do
             .with(:'sub_comments/index',
                       parent_ids_in: comment_id, parent_class: 'Comment',
                       pavlov_options: options)
-            .and_return(sub_comments)
-      interactor.should_receive(:authority_of_user_who_created).
-        with(sub_comments[0]).
-        and_return(authorities[0])
-      interactor.should_receive(:authority_of_user_who_created).
-        with(sub_comments[1]).
-        and_return(authorities[1])
-      KillObject.should_receive(:sub_comment).
-        with(sub_comments[0], authority: authorities[0]).
-        and_return(dead_sub_comments[0])
-      KillObject.should_receive(:sub_comment).
-        with(sub_comments[1], authority: authorities[1]).
-        and_return(dead_sub_comments[1])
+            .and_return(dead_sub_comments)
 
       expect( interactor.call ).to eq dead_sub_comments
     end
@@ -75,65 +61,6 @@ describe Interactors::SubComments::IndexForComment do
       expect do
         interactor.call
       end.to raise_error(Pavlov::ValidationError, 'comment does not exist any more')
-    end
-  end
-
-  describe '#top_fact' do
-    before do
-      described_class.any_instance.stub(:authorized?).and_return(true)
-    end
-
-    it 'returns the top fact for the comment_id' do
-      comment_id = '2a'
-      fact = double
-      comment = double(fact_data: double(fact:fact))
-      interactor = described_class.new(comment_id: comment_id)
-
-      Comment.should_receive(:find).with(comment_id).and_return(comment)
-
-      result = interactor.top_fact
-
-      result.should eq fact
-    end
-
-    it 'caches the fact' do
-      comment_id = '2a'
-      fact = double
-      comment = double(fact_data: double(fact:fact))
-      interactor = described_class.new(comment_id: comment_id)
-
-      Comment.should_receive(:find).with(comment_id).and_return(comment)
-
-      interactor.top_fact
-      next_result = interactor.top_fact
-
-      expect( next_result ).to eq fact
-    end
-  end
-
-  describe '.authority_of_user_who_created' do
-    before do
-      stub_classes 'Queries::AuthorityOnFactFor'
-      described_class.any_instance.stub(:authorized?).and_return(true)
-    end
-
-    it 'retrieves the authority and kills the subcomment' do
-      comment_id = '2a'
-      fact = double
-      graph_user = double
-      authority = double
-      sub_comment = double(created_by: double(graph_user: graph_user))
-      interactor = described_class.new(comment_id: comment_id)
-
-      interactor.should_receive(:top_fact).and_return(fact)
-      Pavlov.should_receive(:query)
-            .with(:'authority_on_fact_for',
-                      fact: fact, graph_user: graph_user)
-            .and_return authority
-
-      result = interactor.authority_of_user_who_created sub_comment
-
-      expect(result).to eq authority
     end
   end
 end
