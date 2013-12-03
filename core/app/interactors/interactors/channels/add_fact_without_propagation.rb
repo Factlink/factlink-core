a00cd9e3804087e0216ad0cc0c5a903055531551
Mark IJbema <markijbema@gmail.com>
2013-12-03 11:49:00 +0100

    Removed unread_facts

--- core/app/interactors/interactors/channels/add_fact_without_propagation.rb
+++ core/app/interactors/interactors/channels/add_fact_without_propagation.rb
@@ -10,7 +10,6 @@ module Interactors
 
         if success
           add_fact_to_topic
-          update_unread_facts
           true
         else
           false
@@ -23,17 +22,6 @@ module Interactors
 
       def add_fact_to_topic
         command :"topics/add_fact", fact_id: fact.id, topic_slug_title: channel.slug_title, score: score.to_s
-      end
-
-      def update_unread_facts
-        return unless should_add_to_unread
-        return if fact_created_by_channel_owner?
-
-        add_to_unread_facts
-      end
-
-      def add_to_unread_facts
-        channel.unread_facts.add(fact)
       end
 
       def fact_created_by_channel_owner?
