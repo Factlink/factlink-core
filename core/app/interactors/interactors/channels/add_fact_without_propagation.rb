6b15dd3b3419112a67dc31efb3dcca3f64fb30a3
Mark IJbema <markijbema@gmail.com>
2013-12-04 10:29:10 +0100

    Inlined command/add_fact_without_propagation

--- core/app/interactors/interactors/channels/add_fact_without_propagation.rb
+++ core/app/interactors/interactors/channels/add_fact_without_propagation.rb
@@ -3,29 +3,16 @@ module Interactors
     class AddFactWithoutPropagation
       include Pavlov::Interactor
 
-      arguments :fact, :channel, :score, :should_add_to_unread
+      arguments :fact, :channel, :score
 
       def execute
-        success = execute_actual_addition
+        fact.channels.add channel
 
-        if success
-          add_fact_to_topic
-          true
-        else
-          false
-        end
-      end
-
-      def execute_actual_addition
-        command :'channels/add_fact_without_propagation', fact: fact, channel: channel, score: score
-      end
-
-      def add_fact_to_topic
-        command :"topics/add_fact", fact_id: fact.id, topic_slug_title: channel.slug_title, score: score.to_s
-      end
-
-      def fact_created_by_channel_owner?
-        channel.created_by_id == fact.created_by_id
+        command :"topics/add_fact",
+          fact_id: fact.id,
+          topic_slug_title: channel.slug_title,
+          score: score.to_s
+        true
       end
 
       def authorized?
--- core/spec-unit/interactors/interactors/channels/add_fact_without_propagation_spec.rb
+++ core/spec-unit/interactors/interactors/channels/add_fact_without_propagation_spec.rb
@@ -8,35 +8,20 @@ describe Interactors::Channels::AddFactWithoutPropagation do
     it 'adds the fact to the topic and the channel' do
       fact = double :fact,
                  id: double,
-                 created_by_id: 14
+                 created_by_id: 14,
+                 channels: double
       channel = double :channel,
                     slug_title: double,
                     created_by_id: fact.created_by_id
       score = double(:score, to_s: double)
 
-      interactor = described_class.new fact: fact, channel: channel, score: score,
-        should_add_to_unread: false
+      interactor = described_class.new fact: fact, channel: channel, score: score
 
-      Pavlov.should_receive(:command)
-            .with(:"channels/add_fact_without_propagation", fact: fact, channel: channel, score: score)
-            .and_return true
-
+      fact.channels.should_receive(:add).with(channel)
       Pavlov.should_receive(:command)
             .with(:"topics/add_fact", fact_id: fact.id, topic_slug_title: channel.slug_title, score: score.to_s)
 
       expect(interactor.call).to be_true
-    end
-
-    it "returns false if the fact did not need to be added, or wasn't added " do
-      fact, channel, score = double, double, double
-
-      interactor = described_class.new fact: fact, channel: channel, score: score,
-        should_add_to_unread: false
-      Pavlov.should_receive(:command)
-            .with(:"channels/add_fact_without_propagation", fact: fact, channel: channel, score: score)
-            .and_return false
-
-      expect(interactor.call).to be_false
     end
   end
 end
