8ac5254d1f9c7d50ef541319c3cef85a3cdec9da
Mark IJbema <markijbema@gmail.com>
2013-12-04 10:42:16 +0100

    Simplified worker

--- core/app/workers/add_fact_to_channel_job.rb
+++ core/app/workers/add_fact_to_channel_job.rb
@@ -1,48 +1,17 @@
 # TODO: extract out interactor, containing both this, and also
 #       some methods which are always called together with this
-
-# README:
-# This classed should be called when doing a repost. It will propagate
-# to its direct followers.
-#
-# It is assumed that this action is performed by the owner of the
-# channel and therefore the unread bit should never be set for the
-# direct add
 class AddFactToChannelJob
   include Pavlov::Helpers
 
-  @queue = :mmm_channel_operations
+  def self.perform(fact_id, channel_id, options={})
+    fact    = Fact[fact_id]
+    channel = Channel[channel_id]
+    score   = options['score']
 
-  attr_reader :fact, :channel
-
-  def initialize(fact_id, channel_id, options={})
-    @fact ||= Fact[fact_id]
-    @channel ||= Channel[channel_id]
-    @options = options
-  end
-
-  def perform
     return unless fact and channel
 
-    executed = interactor(:'channels/add_fact_without_propagation',
-                              fact: fact, channel: channel,
-                              score: score, should_add_to_unread: false)
-
-    propagate_to_channels if executed
-  end
-
-  def score
-    @options['score']
-  end
-
-  def initiated_by_id
-    @options['initiated_by_id']
-  end
-
-  def propagate_to_channels
-  end
-
-  def self.perform(fact_id, channel_id, options={})
-    new(fact_id, channel_id, options).perform
+    Pavlov.interactor(:'channels/add_fact_without_propagation',
+                      fact: fact, channel: channel,
+                      score: score)
   end
 end
