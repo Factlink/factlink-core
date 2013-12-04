0d0a59b157a5585cc22e051846b713babc1dacb7
Jan Paul Posma <me@janpaulposma.nl>
2013-12-04 16:11:46 +0100

    Also stop recalculate just to be sure

--- core/bin/server/stop_background_scripts.sh
+++ core/bin/server/stop_background_scripts.sh
@@ -4,6 +4,9 @@ for x in {0..120}
 do
   echo -n "."
 
+  # DEPRECATED: should be removed in the near future
+  sudo monit stop recalculate > /dev/null 2>&1
+
   # When monit is already busy quiting OR starting, it can throw an error message:
   # monit: action failed -- Other action already in progress -- please try again later
   # We don't care for that message, so we just pipe every message to /dev/null
