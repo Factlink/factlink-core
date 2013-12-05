ebc597b15f017c926cf28dfb128961231e9c67ca
Martijn Russchen <martijn@factlink.com>
2013-12-05 18:49:38 +0100

    Removed tooltip js

--- core/app/assets/javascripts/twitter/bootstrap.js
+++ core/app/assets/javascripts/twitter/bootstrap.js
@@ -1,6 +1,5 @@
 //= require twitter/bootstrap/alert.js
 //= require twitter/bootstrap/dropdown.js
-//= require twitter/bootstrap/tooltip.js
 //= require twitter/bootstrap/popover.js
 //= require twitter/bootstrap/tab.js
 //= require twitter/bootstrap/transition.js
