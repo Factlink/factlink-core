23fad98620aba5c10b5de0319f9f593caaee0f61
Eamon Nerbonne <eamon@nerbonne.org>
2013-12-05 14:54:15 +0100

    Don't show favicons.

--- core/spec/poltergeist_style_overrides.rb
+++ core/spec/poltergeist_style_overrides.rb
@@ -8,7 +8,7 @@ module PoltergeistStyleOverrides
         html.phantom_js body { -webkit-transform: rotate(0.00001deg); }
         html.phantom_js body.client { overflow: auto; }
         html.phantom_js .discussion-modal-container { position: absolute; min-height:100%; overflow-y: auto; bottom: auto; }
-        img[src*='gravatar'] {
+        img[src*='gravatar'], img.proxy-link-favicon {
           content: '';/*invalid css, but works in phantomjs!*/
           display: inline-block;
           border: 1px solid #f0f;
