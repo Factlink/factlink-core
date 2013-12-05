027196d9bce20b8bf6ebd6b81a3c91d19ce07a48
Mark IJbema <markijbema@gmail.com>
2013-12-05 16:07:24 +0100

    Removed validation in commands

--- core/app/interactors/commands/topics/create.rb
+++ core/app/interactors/commands/topics/create.rb
@@ -9,10 +9,6 @@ module Commands
         topic = Topic.create title: title
         DeadTopic.new topic.slug_title, topic.title
       end
-
-      def validate
-        validate_nonempty_string :title, @title
-      end
     end
   end
 end
