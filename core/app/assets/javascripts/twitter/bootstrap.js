e0f2175e863d561bcd675f2bd6c933542d02b053
Martijn Russchen <martijn@factlink.com>
2013-12-05 18:58:47 +0100

    Removed popovers

--- core/app/assets/javascripts/twitter/bootstrap.js
+++ core/app/assets/javascripts/twitter/bootstrap.js
@@ -1,5 +1,4 @@
 //= require twitter/bootstrap/alert.js
 //= require twitter/bootstrap/dropdown.js
-//= require twitter/bootstrap/popover.js
 //= require twitter/bootstrap/tab.js
 //= require twitter/bootstrap/transition.js
