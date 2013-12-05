a66882d913d3ed0fd329f158fc13891e2d65dd81
Martijn Russchen <martijn@factlink.com>
2013-12-05 09:36:31 +0100

    Refactored test

--- core/spec/screenshots/conversations_spec.rb
+++ core/spec/screenshots/conversations_spec.rb
@@ -1,26 +1,19 @@
 require 'screenshot_helper'
 
 describe "factlink", type: :feature do
-  include Screenshots::DiscussionHelper
+  include Acceptance::ConversationHelper
+  include Acceptance::FactHelper
+
+  before :each do
+    @user = sign_in_user(create :full_user, :confirmed)
+    @recipients = create_list :full_user, 2
+  end
 
   it "the layout of the conversations overview page is correct" do
-    @user = sign_in_user create :full_user
-    factlink = create_discussion
+    factlink = backend_create_fact
+    message_content = 'content'
 
-    go_to_discussion_page_of factlink
-
-    within '.top-fact' do
-      click_on "Share"
-    end
-
-    within '.start-conversation' do
-      recipients.each {|r| add_recipient r.name}
-      find(:css, 'textarea').click
-      sleep 0.1
-      find(:css, 'textarea').set(message)
-
-      click_button 'Send'
-    end
+    send_message(message_content, factlink, @recipients)
 
     visit "/m"
 
