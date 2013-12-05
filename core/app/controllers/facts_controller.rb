f4cf81d7507e101fb5851d6909d80f6ffb861736
Jan Paul Posma <me@janpaulposma.nl>
2013-12-05 10:16:10 +0100

    Removed unnecessary render_votes, as this is now done exclusively in the frontend

--- core/app/controllers/facts_controller.rb
+++ core/app/controllers/facts_controller.rb
@@ -88,7 +88,7 @@ class FactsController < ApplicationController
     @fact.add_opinion(type, current_user.graph_user)
     Activity::Subject.activity(current_user.graph_user, OpinionType.real_for(type), @fact)
 
-    render_votes(@fact.id)
+    render json: {}
   end
 
   def remove_opinions
@@ -97,12 +97,7 @@ class FactsController < ApplicationController
     @fact.remove_opinions(current_user.graph_user)
     Activity::Subject.activity(current_user.graph_user,:removed_opinions,@fact)
 
-    render_votes(@fact.id)
-  end
-
-  def render_votes(fact_id)
-    fact_votes = query(:'facts/get_votes', id: fact_id.to_s)
-    render 'facts/_fact_votes', format: :json, locals: {fact_votes: fact_votes}
+    render json: {}
   end
 
   # TODO: This search is way to simple now, we need to make sure already
