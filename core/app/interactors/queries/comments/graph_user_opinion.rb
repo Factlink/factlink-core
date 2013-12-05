bf0c38dc36c1597989f4b7eb427dd9ba38c51963
Jan Paul Posma <me@janpaulposma.nl>
2013-12-05 12:12:30 +0100

    Use opinion_of_graph_user in Queries::Comments::GraphUserOpinion

--- core/app/interactors/queries/comments/graph_user_opinion.rb
+++ core/app/interactors/queries/comments/graph_user_opinion.rb
@@ -4,31 +4,12 @@ module Queries
       include Pavlov::Query
       arguments :comment_id, :graph_user
 
-      def validate
-        validate_hexadecimal_string :comment_id, @comment_id
-      end
-
       def execute
-        possible_opinions.each do |opinion|
-          return opinion if has_opinion?(opinion)
-        end
-        nil
-      end
-
-      def possible_opinions
-        OpinionType.types
-      end
-
-      def has_opinion? type
-        people_who_believe(type).include? @graph_user
-      end
-
-      def people_who_believe type
-        believable.opiniated(type)
+        believable.opinion_of_graph_user graph_user
       end
 
       def believable
-        @believable ||= Believable::Commentje.new @comment_id
+        @believable ||= Believable::Commentje.new comment_id
       end
     end
   end
