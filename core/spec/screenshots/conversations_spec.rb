3fbe38f1e9c0e1c4aaf2dd71a24755b0d466a803
Martijn Russchen <martijn@factlink.com>
2013-12-05 09:41:40 +0100

    Add test for conversation detail

--- core/spec/screenshots/conversations_spec.rb
+++ core/spec/screenshots/conversations_spec.rb
@@ -19,4 +19,22 @@ describe "factlink", type: :feature do
 
     assume_unchanged_screenshot "conversations_overview"
   end
+
+  it "the layout of the conversations detail page is correct" do
+    factlink = backend_create_fact
+    message_content = 'content'
+
+    send_message(message_content, factlink, @recipients)
+
+    recipient_ids = Message.last.conversation.recipients.map(&:id)
+
+    @recipients.each do |recipient|
+      expect(recipient_ids).to include recipient.id
+
+      switch_to_user @user
+      open_message_with_content message_content
+    end
+
+    assume_unchanged_screenshot "conversations_detail"
+  end
 end
