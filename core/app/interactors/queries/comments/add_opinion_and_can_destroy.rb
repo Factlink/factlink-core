5b752bf515d1e68c641842c0caf40adaf446883f
Jan Paul Posma <me@janpaulposma.nl>
2013-12-09 10:37:35 +0100

    Return votes from AddOpinionAndCanDestroy

--- core/app/interactors/queries/comments/add_opinion_and_can_destroy.rb
+++ core/app/interactors/queries/comments/add_opinion_and_can_destroy.rb
@@ -7,29 +7,17 @@ module Queries
 
       def execute
         KillObject.comment comment,
-          impact_opinion: impact_opinion,
-          current_user_opinion: current_user_opinion,
+          votes: votes,
           can_destroy?: can_destroy,
           sub_comments_count: comment.sub_comments_count
       end
 
-      def impact_opinion
-        query(:'opinions/impact_opinion_for_comment', comment: comment)
+      def votes
+        query(:'believable/votes', believable: believable)
       end
 
-      def current_user_opinion
-        return unless current_graph_user
-
-        query(:'comments/graph_user_opinion',
-                  comment_id: comment.id.to_s, graph_user: current_graph_user)
-      end
-
-      def current_graph_user
-        if pavlov_options[:current_user]
-          pavlov_options[:current_user].graph_user
-        else
-          nil
-        end
+      def believable
+        @believable ||= Believable::Commentje.new comment.id
       end
 
       def can_destroy
