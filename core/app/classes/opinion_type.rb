989cfd4a000c28c67cdf6bbc41fd66127e55fe19
Jan Paul Posma <me@janpaulposma.nl>
2013-12-12 14:27:22 +0100

    Removed unused OpinionType.real_for

--- core/app/classes/opinion_type.rb
+++ core/app/classes/opinion_type.rb
@@ -1,13 +1,4 @@
 class OpinionType
-  def self.real_for type
-    case type.to_s
-    when 'beliefs', 'believes'       then :believes
-    when 'doubts'                    then :doubts
-    when 'disbeliefs', 'disbelieves' then :disbelieves
-    else raise "invalid opinion"
-    end
-  end
-
   def self.types
     ['believes', 'disbelieves', 'doubts']
   end
--- core/spec-unit/classes/opinion_type_spec.rb
+++ core/spec-unit/classes/opinion_type_spec.rb
@@ -1,19 +1,6 @@
 require_relative '../../app/classes/opinion_type'
 
 describe OpinionType do
-  describe '.real_for' do
-    it "returns the proper opinion" do
-      expect(OpinionType.real_for(:believes)).to eq :believes
-      expect(OpinionType.real_for(:beliefs)).to eq :believes
-      expect(OpinionType.real_for(:disbelieves)).to eq :disbelieves
-      expect(OpinionType.real_for(:disbeliefs)).to eq :disbelieves
-      expect(OpinionType.real_for(:doubts)).to eq :doubts
-    end
-    it "raises when improper input is given" do
-      expect { OpinionType.real_for(:henk) }.to raise_error RuntimeError, "invalid opinion"
-    end
-  end
-
   describe '.types' do
     it "includes the valid types" do
       expect(OpinionType.types).to include 'believes'
