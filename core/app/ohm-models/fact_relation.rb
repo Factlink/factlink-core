619f759786aab725d2239bc7c20b62c67994516d
Jan Paul Posma <me@janpaulposma.nl>
2013-12-12 09:56:23 +0100

    Allow doubts type in FactRelation

--- core/app/ohm-models/fact_relation.rb
+++ core/app/ohm-models/fact_relation.rb
@@ -10,7 +10,7 @@ class FactRelation < OurOhm
   reference :fact, Fact
   reference :created_by, GraphUser
 
-  attribute :type # => :believes || :disbelieves
+  attribute :type # => :believes || :disbelieves || :doubts
   index :type
 
   delegate :opinionated_users_ids, :opiniated, :add_opiniated, :remove_opinionateds,
@@ -21,7 +21,8 @@ class FactRelation < OurOhm
     assert_present :from_fact_id
     assert_present :fact_id
     assert_present :type
-    assert_member :type, [:believes, :disbelieves, 'believes', 'disbelieves']
+    assert_member :type, [:believes, :disbelieves, :doubts,
+                         'believes', 'disbelieves', 'doubts']
     assert_unique [:from_fact_id, :fact_id, :type]
     assert_present :created_by
   end
