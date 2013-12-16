f8c764eab2f100efe0a04591939b62a85e042a09
Mark IJbema <markijbema@gmail.com>
2013-12-16 14:01:20 +0100

    Simplified redis setup

--- core/config/initializers/redis.rb
+++ core/config/initializers/redis.rb
@@ -1,10 +1,17 @@
-redis_conf = YAML::load_file(Rails.root.join('config/redis.yml'))[Rails.env]['redis']
-
-# Setting db doesn't always work, check with ohm and redis client, if you add it again.
-connection_hash = {
-  :host => redis_conf['host'],
-  :port => redis_conf['port']
-}
+connection_hash = case Rails.env.to_sym
+  when :development
+    {host: 'localhost', port: 6381}
+  when :test
+    {host: 'localhost', port: 6379}
+  when :testserver
+    {host: '172.18.64.5', port: 6379}
+  when :vagrant # huh? who uses this
+    {host: '10.253.0.1', port: 6379}
+  when :staging
+    {host: '172.18.64.14', port: 6379}
+  when :production
+    {host: '172.18.64.22', port: 6379}
+  end
 
 Ohm.connect(connection_hash)
 Redis.current = Redis.new(connection_hash)
