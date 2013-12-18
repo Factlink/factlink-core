c8ad436a0b138ca63c1e7a5dbe88564cda13f6e9
Mark IJbema <markijbema@gmail.com>
2013-12-18 08:51:50 +0100

    Brought channel remove under test

---
+++ core/app/interactors/interactors/channels/remove_fact.rb
@@ -0,0 +1,20 @@
+module Interactors
+  module Channels
+    class RemoveFact
+      include Pavlov::Interactor
+
+      arguments :fact, :channel
+
+      def execute
+        channel.remove_fact(fact)
+      end
+
+      def authorized?
+        # TODO use cancan
+        return false unless pavlov_options[:current_user]
+
+        pavlov_options[:current_user].graph_user_id == channel.created_by_id
+      end
+    end
+  end
+end
--- core/spec/integration-real/interactors/channels/fact_management_spec.rb
+++ core/spec/integration-real/interactors/channels/fact_management_spec.rb
@@ -4,7 +4,7 @@ describe 'channel facts management' do
   include PavlovSupport
 
   let(:user) { create :full_user }
-  it "adds the fact to the channel" do
+  it "after adding a fact it is in the channel" do
     as(user) do |pavlov|
       fact = pavlov.interactor :'facts/create', displaystring: 'a fact', url: '', title: ''
 
@@ -12,6 +12,18 @@ describe 'channel facts management' do
       pavlov.interactor :'channels/add_fact', fact: fact, channel: channel
 
       expect(channel).to include(fact)
+    end
+  end
+
+  it "after adding and removing a fact the channel is empty" do
+    as(user) do |pavlov|
+      fact = pavlov.interactor :'facts/create', displaystring: 'a fact', url: '', title: ''
+
+      channel = pavlov.command :'channels/create', title: 'something'
+      pavlov.interactor :'channels/add_fact', fact: fact, channel: channel
+      pavlov.interactor :'channels/remove_fact', fact: fact, channel: channel
+
+      expect(channel.sorted_internal_facts).to be_empty
     end
   end
 
