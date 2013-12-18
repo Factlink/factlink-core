0d9574f43ae6d584af922e013dc83e8f3312a6e8
Martijn Russchen <martijn@factlink.com>
2013-12-18 17:57:01 +0100

    Removed alsmost duplicate test

--- core/spec/screenshots/fact_show_spec.rb
+++ core/spec/screenshots/fact_show_spec.rb
@@ -3,25 +3,6 @@ require 'screenshot_helper'
 describe "factlink", type: :feature, driver: :poltergeist_slow do
   include Screenshots::DiscussionHelper
 
-  it "the layout of the new discussion page is correct with doubters on top, and
-      adding weakening comment" do
-    @user = sign_in_user create :full_user
-
-    factlink = create_discussion
-
-    factlink.add_opiniated :believes, (create :user).graph_user
-
-    10.times do
-      factlink.add_opiniated :doubts, (create :user).graph_user
-    end
-
-    go_to_fact_show_of factlink
-
-    page.should have_content factlink.data.displaystring
-
-    assume_unchanged_screenshot "fact_show_A"
-  end
-
   it "the layout of the new discussion page is correct with believers on top,
       and adding supporting factlink" do
     @user = sign_in_user create :full_user
@@ -42,7 +23,7 @@ describe "factlink", type: :feature, driver: :poltergeist_slow do
 
     find('.js-switch-to-factlink').click
 
-    assume_unchanged_screenshot "fact_show_B"
+    assume_unchanged_screenshot "fact_show"
   end
 
   it "the layout of the new discussion page is correct for an anonymous user" do
