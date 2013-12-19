309e51f55262d42245bd243bcc529cf0a8fee7df
Mark IJbema <markijbema@gmail.com>
2013-12-19 09:33:53 +0100

    Removed unneeded duplicate specs

--- core/spec-unit/interactors/queries/facts/opinionators_spec.rb
+++ core/spec-unit/interactors/queries/facts/opinionators_spec.rb
@@ -23,35 +23,5 @@ describe Queries::Facts::Opinionators do
       expect(result[:total]).to eq 1
       expect(result[:users].first.id).to eq user.id
     end
-
-    it 'returns a user who disbelieves the fact' do
-      user = double(id: 2, username: 'my_username', name: 'Joop Bouwhuis' )
-      graph_user = double(user: user, id: 13)
-      fact = double(id: 1)
-      query = described_class.new(fact_id: fact.id, opinion: 'disbelieves')
-
-      Fact.stub(:[]).with(fact.id).and_return(fact)
-      fact.stub(:opiniated).with('disbelieves').and_return([graph_user])
-
-      result = query.call
-
-      expect(result[:total]).to eq 1
-      expect(result[:users].first.id).to eq user.id
-    end
-
-    it 'returns a user who doubts the fact' do
-      user = double(id: 2, username: 'my_username', name: 'Joop Bouwhuis' )
-      graph_user = double(user: user, id: 13)
-      fact = double(id: 1)
-      query = described_class.new(fact_id: fact.id, opinion: 'doubts')
-
-      Fact.stub(:[]).with(fact.id).and_return(fact)
-      fact.stub(:opiniated).with('doubts').and_return([graph_user])
-
-      result = query.call
-
-      expect(result[:total]).to eq 1
-      expect(result[:users].first.id).to eq user.id
-    end
   end
 end
