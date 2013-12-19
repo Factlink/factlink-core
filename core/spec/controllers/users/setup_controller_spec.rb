cacc33254e48a300b1c030cfe2c8086351f39be2
Jan Paul Posma <me@janpaulposma.nl>
2013-12-19 14:14:13 +0100

    Improved test a bit

--- core/spec/controllers/users/setup_controller_spec.rb
+++ core/spec/controllers/users/setup_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Users::SetupController do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, set_up: false) }
   render_views
 
   describe :edit do
@@ -20,6 +20,8 @@ describe Users::SetupController do
   describe :update do
     it 'updates full name' do
       sign_in user
+
+      expect(user.set_up).to be_false
 
       post :update, user: {full_name: 'full_name'}
 
