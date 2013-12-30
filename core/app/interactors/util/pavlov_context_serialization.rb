0da7b6cd80d2167b60aa2e8c7b290d5fc962006d
Jan Paul Posma <me@janpaulposma.nl>
2013-12-30 17:11:45 +0100

    Removed unused methods

--- core/app/interactors/util/pavlov_context_serialization.rb
+++ core/app/interactors/util/pavlov_context_serialization.rb
@@ -3,20 +3,6 @@ module Util
 
     module_function
 
-    def serialize_pavlov_context pavlov_context
-      return nil unless pavlov_context[:current_user]
-
-      {'serialize_id' => pavlov_context[:current_user].id.to_s}
-    end
-
-    def deserialize_pavlov_context pavlov_context
-      pavlov_context_by_user_id pavlov_context['serialize_id']
-    end
-
-    def pavlov_context_by_user_id user_id
-      pavlov_context_by_user User.find(user_id)
-    end
-
     def pavlov_context_by_user user, ability=nil
       {
         current_user: user,
