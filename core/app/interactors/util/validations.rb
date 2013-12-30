3658ad5c83a529e3acb049794f6816fe10679445
Jan Paul Posma <me@janpaulposma.nl>
2013-12-30 17:45:05 +0100

    Removed unused routes and direct dependencies (routes->controllers->interactors->queries/commands->auxiliary methods)
    Still to remove: tests, models, data, activities, emails, notifications in frontend

--- core/app/interactors/util/validations.rb
+++ core/app/interactors/util/validations.rb
@@ -6,13 +6,5 @@ module Util
           "should not be longer than #{length} characters."
       end
     end
-
-    def validate_non_empty_list param_name, param
-      if !param.respond_to? :each
-        errors.add param_name, 'should be a list'
-      elsif param.length == 0
-        errors.add param_name, 'should not be empty'
-      end
-    end
   end
 end
