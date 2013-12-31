ecf3a41ed924ef9609bd1c208a6ef3268189c485
Jan Paul Posma <me@janpaulposma.nl>
2013-12-31 12:04:11 +0100

    Removed in this case unnecessary migrations, as we only need to completely remove some activities

--- core/db/migrate/20131231084328_remove_old_activities_again.rb
+++ core/db/migrate/20131231084328_remove_old_activities_again.rb
@@ -1,10 +1,6 @@
 class RemoveOldActivitiesAgain < Mongoid::Migration
   def self.up
     Resque.enqueue CleanupActivitiesWorker
-
-    GraphUser.all.ids.each do |graph_user_id|
-      Resque.enqueue MigrateRemoveOldActivitiesForUserWorker, graph_user_id
-    end
   end
 
   def self.down
