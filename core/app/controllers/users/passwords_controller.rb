625fe9bdec16c506f19b31b512aeac0b52d9aca8
Jan Paul Posma <me@janpaulposma.nl>
2014-01-02 10:35:52 +0100

    Log users out when requesting a new password to be compatible with devise flow

--- core/app/controllers/users/passwords_controller.rb
+++ core/app/controllers/users/passwords_controller.rb
@@ -1,3 +1,17 @@
 class Users::PasswordsController < Devise::PasswordsController
   layout "one_column"
+
+  skip_before_filter :require_no_authentication, only: [:new]
+  prepend_before_filter :sign_out_user, except: [:new]
+
+  def new
+    self.resource = current_user || super
+  end
+
+  private
+
+  def sign_out_user
+    sign_out if current_user
+    true
+  end
 end
