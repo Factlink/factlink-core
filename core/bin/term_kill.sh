f813ee6004d6282239c21d1d1434830d10625d02
Eamon Nerbonne <eamon@nerbonne.org>
2014-01-07 15:19:43 +0100

    kill -9 if kill isn't enough

--- core/bin/term_kill.sh
+++ core/bin/term_kill.sh
@@ -2,3 +2,6 @@
 # Kills all processes attached to the current terminal.
 # This is a helper to workaround https://github.com/ddollar/foreman/issues/357
 pkill -t `tty |egrep -o 'pts/[0-9]+'`
+sleep 0.5
+pkill -9 -t `tty |egrep -o 'pts/[0-9]+'`
+
