500cde87aad26a270f687d8bf85f2e060e22d2e4
Jan Paul Posma <me@janpaulposma.nl>
2014-01-09 11:04:31 +0100

    Extracted factlink_link, use friendly_fact_url in there, and use case/when

--- core/app/classes/formatted_comment_content.rb
+++ core/app/classes/formatted_comment_content.rb
@@ -21,15 +21,20 @@ class FormattedCommentContent
       link_text = href
       href = 'http://' + href unless scheme
 
-      if FACTLINK_PRETTY_URL.match href
-        fact_id = Regexp.last_match[1]
-        dead_fact = Pavlov.query :'facts/get_dead', id: fact_id
-
-        content_tag :a, dead_fact.displaystring, href: href, rel: 'backbone',
-          class: 'formatted-comment-content-factlink'
+      case href
+      when FACTLINK_PRETTY_URL
+        factlink_link Regexp.last_match[1]
       else
         content_tag :a, link_text, href: href, target: '_blank'
       end
     end
+  end
+
+  def factlink_link fact_id
+    dead_fact = Pavlov.query :'facts/get_dead', id: fact_id
+    url = FactUrl.new(dead_fact).friendly_fact_url
+
+    content_tag :a, dead_fact.displaystring, href: url, rel: 'backbone',
+      class: 'formatted-comment-content-factlink'
   end
 end
