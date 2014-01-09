9f225afb69350bb293215e34afe6baf4327f504f
Jan Paul Posma <me@janpaulposma.nl>
2014-01-09 11:05:38 +0100

    Renamed methods to reflect that they return link tags

--- core/app/classes/formatted_comment_content.rb
+++ core/app/classes/formatted_comment_content.rb
@@ -6,7 +6,7 @@ class FormattedCommentContent
   end
 
   def html
-    urls_to_links ERB::Util.html_escape(@text).to_str
+    urls_to_link_tags ERB::Util.html_escape(@text).to_str
   end
 
   private
@@ -14,7 +14,7 @@ class FormattedCommentContent
   AUTO_LINK_RE = %r{(?:(http|https)://|www\.)[^\s<\u00A0]+}i
   FACTLINK_PRETTY_URL = %r{#{FactlinkUI::Application.config.core_url}/[a-z0-9\-_]+/f/([0-9]+)}i
 
-  def urls_to_links text
+  def urls_to_link_tags text
     text.gsub(AUTO_LINK_RE) do |href|
       scheme = Regexp.last_match[1]
 
@@ -23,14 +23,14 @@ class FormattedCommentContent
 
       case href
       when FACTLINK_PRETTY_URL
-        factlink_link Regexp.last_match[1]
+        factlink_link_tag Regexp.last_match[1]
       else
         content_tag :a, link_text, href: href, target: '_blank'
       end
     end
   end
 
-  def factlink_link fact_id
+  def factlink_link_tag fact_id
     dead_fact = Pavlov.query :'facts/get_dead', id: fact_id
     url = FactUrl.new(dead_fact).friendly_fact_url
 
