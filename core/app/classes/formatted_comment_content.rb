7d2486f2c744266e2345333203ada107d4048772
Jan Paul Posma <me@janpaulposma.nl>
2014-01-09 10:52:11 +0100

    Fixed XSS and integrated methods

--- core/app/classes/formatted_comment_content.rb
+++ core/app/classes/formatted_comment_content.rb
@@ -6,12 +6,14 @@ class FormattedCommentContent
   end
 
   def html
-    displaystring_in_factlinks urls_to_links ERB::Util.html_escape(@text).to_str
+    urls_to_links ERB::Util.html_escape(@text).to_str
   end
 
   private
 
   AUTO_LINK_RE = %r{(?:(http|https)://|www\.)[^\s<\u00A0]+}i
+  FACTLINK_PRETTY_URL = %r{#{FactlinkUI::Application.config.core_url}/[a-z0-9\-_]+/f/([0-9]+)}i
+
   def urls_to_links text
     text.gsub(AUTO_LINK_RE) do |href|
       scheme = Regexp.last_match[1]
@@ -19,17 +21,15 @@ class FormattedCommentContent
       link_text = href
       href = 'http://' + href unless scheme
 
-      content_tag(:a, link_text, href: href, target: '_blank')
-    end
-  end
+      if FACTLINK_PRETTY_URL.match href
+        fact_id = Regexp.last_match[1]
+        dead_fact = Pavlov.query :'facts/get_dead', id: fact_id
 
-  FACTLINKS_IN_A_TAGS = %r{ target="_blank">#{FactlinkUI::Application.config.core_url}/[a-z0-9\-_]+/f/([0-9]+)</a>}i
-  def displaystring_in_factlinks text
-    text.gsub(FACTLINKS_IN_A_TAGS) do |href|
-      fact_id = Regexp.last_match[1]
-      dead_fact = Pavlov.query :'facts/get_dead', id: fact_id
-
-      ' rel="backbone" class="formatted-comment-content-factlink">' + dead_fact.displaystring + '</a>'
+        content_tag :a, dead_fact.displaystring, href: href, rel: 'backbone',
+          class: 'formatted-comment-content-factlink'
+      else
+        content_tag :a, link_text, href: href, target: '_blank'
+      end
     end
   end
 end
--- core/spec/classes/formatted_comment_content_spec.rb
+++ core/spec/classes/formatted_comment_content_spec.rb
@@ -26,7 +26,7 @@ describe FormattedCommentContent do
       displaystring = fact.data.displaystring
 
       formatted_comment = described_class.new friendly_fact_url
-      expected_html = "<a href=\"#{friendly_fact_url}\" rel=\"backbone\" class=\"formatted-comment-content-factlink\">#{displaystring}</a>"
+      expected_html = "<a class=\"formatted-comment-content-factlink\" href=\"http://localhost:3000/fact-1/f/1\" rel=\"backbone\">Fact 1</a>"
 
       expect(formatted_comment.html).to eq expected_html
     end
