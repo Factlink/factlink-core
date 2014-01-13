e6c136c469518101e6fa6a6fd9a96104f214e3ae
Eamon Nerbonne <eamon@nerbonne.org>
2014-01-13 17:32:27 +0100

    Start writing screenshot updater: interact with circle API

---
+++ core/bin/update_screenshots.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+
+
+class ScreenshotUpdater
+  def ci_token
+    @ci_token ||= begin
+      ENV['CIRCLE_CI_TOKEN'] or
+        fail '''
+          ENV variable CIRCLE_CI_TOKEN is missing.
+          You should create an api token @ https://circleci.com/account/api and
+          insert it in your .bashrc (or equivalent).  For example:
+          export CIRCLE_CI_TOKEN="<your-40-char-hexadecimal-token>"
+        '''
+    end
+  end
+
+  def ci_projects_uri
+    @ci_projects_uri ||= URI.parse("https://circleci.com/api/v1/projects?circle-token=#{ci_token}")
+  end
+
+  #https://circleci.com/docs/api#build-artifacts
+  #URI.parse('https://circleci.com/api/v1/project/:username/:project/:build_num/artifacts?circle-token=:token
+
+end
+
