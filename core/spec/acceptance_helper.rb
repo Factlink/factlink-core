040e335cc6e1512c083112780b4c2f49ba615657
Eamon Nerbonne <eamon@nerbonne.org>
2014-01-13 15:13:03 +0100

    Explicitly require webmock

--- core/spec/acceptance_helper.rb
+++ core/spec/acceptance_helper.rb
@@ -11,7 +11,7 @@ require 'capybara/email/rspec'
 require 'capybara-screenshot'
 require 'test_request_syncer'
 require 'poltergeist_style_overrides'
-
+require 'webmock'
 require 'capybara-screenshot/rspec'
 require 'database_cleaner'
 
