0c41d791dc023d2804e673e3b004d90963e517d0
Jan Paul Posma <me@janpaulposma.nl>
2014-01-15 15:09:42 +0100

    Use bash 3 syntax

--- core/start-db.sh
+++ core/start-db.sh
@@ -7,10 +7,10 @@ onexit() {
   kill -9 0
 }
 trap onexit SIGINT SIGTERM EXIT INT QUIT TERM
-(elasticsearch -f -Des.config=config/developmentservers/elasticsearch.yml -Des.cluster.name="elasticsearch_$(hostname)"  || kill $$)  |& sed "s/^/\x1b[0;31m[elasticsearch] \x1b[0m/" &
-(redis-server config/developmentservers/redis.conf  || kill $$)  |& sed "s/^/\x1b[0;32m[redis] \x1b[0m/" &
-(redis-server config/developmentservers/redis-for-tests.conf  || kill $$)  |& sed "s/^/\x1b[0;33m[redisfortests] \x1b[0m/" &
-(redis-server config/developmentservers/redis-resque.conf  || kill $$)  |& sed "s/^/\x1b[0;34m[resqueredis] \x1b[0m/" &
-(mongod --config config/developmentservers/mongo.conf  || kill $$)  |& sed "s/^/\x1b[0;35m[mongo] \x1b[0m/" &
-(bundle exec mailcatcher -fv || kill $$) |& sed "s/^/\x1b[0;36m[mailcatcher] \x1b[0m/" &
+(elasticsearch -f -Des.config=config/developmentservers/elasticsearch.yml -Des.cluster.name="elasticsearch_$(hostname)"  || kill $$)  2>&1| sed "s/^/\x1b[0;31m[elasticsearch] \x1b[0m/" &
+(redis-server config/developmentservers/redis.conf  || kill $$)  2>&1| sed "s/^/\x1b[0;32m[redis] \x1b[0m/" &
+(redis-server config/developmentservers/redis-for-tests.conf  || kill $$)  2>&1| sed "s/^/\x1b[0;33m[redisfortests] \x1b[0m/" &
+(redis-server config/developmentservers/redis-resque.conf  || kill $$)  2>&1| sed "s/^/\x1b[0;34m[resqueredis] \x1b[0m/" &
+(mongod --config config/developmentservers/mongo.conf  || kill $$)  2>&1| sed "s/^/\x1b[0;35m[mongo] \x1b[0m/" &
+(bundle exec mailcatcher -fv || kill $$) 2>&1| sed "s/^/\x1b[0;36m[mailcatcher] \x1b[0m/" &
 wait
