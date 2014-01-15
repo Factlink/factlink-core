e125c81247f01f0764c10e8332480fb9328d6261
Mark IJbema <markijbema@gmail.com>
2014-01-15 16:27:58 +0100

    Removed obsolete spec
    Made other pending. This functionality existed for fact relations, and should be implemented for comments, but isn't yet.

--- core/spec/acceptance/sub_commmmmments_spec.rb
+++ core/spec/acceptance/sub_commmmmments_spec.rb
@@ -33,27 +33,8 @@ feature "sub_comments", type: :feature do
     assert_sub_comment_exists sub_comment_text
   end
 
-  scenario "A user can comment on a fact relation" do
-    @fact_relation_user_b = create :fact, created_by: @user_b.graph_user
-    @factlink_user_a.add_evidence("believes", @fact_relation_user_b, @user_b)
-
-    sub_comment_text = "Sub Comment 1"
-
-    go_to_discussion_page_of @factlink_user_a
-
-    click_link 'Comment'
-
-    add_sub_comment(sub_comment_text)
-    assert_sub_comment_exists sub_comment_text
-
-    switch_to_user(@user_b)
-    go_to_discussion_page_of @factlink_user_a
-
-    click_link '1 comment'
-    assert_sub_comment_exists sub_comment_text
-  end
-
   scenario "After adding a subcomment the evidence can not be removed any more" do
+    pending
     @fact_relation_user_b = create :fact, created_by: @user_b.graph_user
     @factlink_user_a.add_evidence("believes", @fact_relation_user_b, @user_a)
 
