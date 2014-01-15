f4a17aac7d988ee8b95cccf5f5c39afcac331764
Eamon Nerbonne <eamon@nerbonne.org>
2014-01-15 17:00:11 +0100

    Use mv to ensure that old screenshots aren't continually re-copied

--- core/bin/update_screenshots_locally.sh
+++ core/bin/update_screenshots_locally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`/..
-ls tmp/capybara/ |grep -v -e '-diff.png' | xargs -n 1 -I '{}' cp 'tmp/capybara/{}' spec/screenshots/screenshots/
+ls tmp/capybara/ |grep -v -e '-diff.png' | xargs -n 1 -I '{}' mv 'tmp/capybara/{}' spec/screenshots/screenshots/
