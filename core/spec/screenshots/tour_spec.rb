a0700812ba0977cbdfcec427e1966c7cdbc5a4cf
Jan Paul Posma <me@janpaulposma.nl>
2014-01-16 15:10:18 +0100

    Fix having one discussion on interests page

--- core/spec/screenshots/tour_spec.rb
+++ core/spec/screenshots/tour_spec.rb
@@ -17,6 +17,7 @@ describe "Check the tour", type: :feature, driver: :poltergeist_slow do
   it 'Interests page should be the same' do
     user = create :user
     Pavlov.command(:'users/add_handpicked_user', user_id: user.id.to_s)
+    create :fact, created_by: user.graph_user
 
     visit interests_path
 
