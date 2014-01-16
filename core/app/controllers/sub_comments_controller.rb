afa26c280b1e65f0f738f51a142f7e48d791aed7
Mark IJbema <markijbema@gmail.com>
2014-01-16 10:30:01 +0100

    Minor cleanup

--- core/app/controllers/sub_comments_controller.rb
+++ core/app/controllers/sub_comments_controller.rb
@@ -1,12 +1,12 @@
 class SubCommentsController < ApplicationController
   def index
-    @sub_comments = interactor(:'sub_comments/index_for_comment', comment_id: parent_id)
+    @sub_comments = interactor(:'sub_comments/index_for_comment', comment_id: comment_id)
     render 'sub_comments/index', formats: [:json]
   end
 
   def create
     @sub_comment = interactor(:'sub_comments/create_for_comment',
-                                  comment_id: parent_id, content: params[:content])
+                                  comment_id: comment_id, content: params[:content])
     render 'sub_comments/show', formats: [:json]
   rescue Pavlov::ValidationError
     render text: 'something went wrong', status: 400
@@ -17,11 +17,7 @@ class SubCommentsController < ApplicationController
     render json: {}, status: :ok
   end
 
-  def parent_is_comment
+  def comment_id
     params[:comment_id]
-  end
-
-  def parent_id
-    params[:comment_id] || params[:id]
   end
 end
--- core/app/interactors/interactors/sub_comments/create_for_comment.rb
+++ core/app/interactors/interactors/sub_comments/create_for_comment.rb
@@ -2,18 +2,23 @@ module Interactors
   module SubComments
     class CreateForComment
       include Pavlov::Interactor
+      include Util::CanCan
 
       arguments :comment_id, :content
 
-      include Util::CanCan
 
       def authorized?
-        can?(:show, parent) and
-          can?(:create, SubComment)
+        can?(:show, comment) && can?(:create, SubComment)
+      end
+
+      def validate
+        validate_hexadecimal_string :comment_id, comment_id
+        validate_regex   :content, content, /\S/,
+          "should not be empty."
       end
 
       def execute
-        fail Pavlov::ValidationError, "parent does not exist any more" unless parent
+        fail Pavlov::ValidationError, "parent does not exist any more" unless comment
 
         sub_comment = create_sub_comment
 
@@ -29,19 +34,9 @@ module Interactors
                     object: top_fact)
       end
 
-      def validate
-        validate_hexadecimal_string :comment_id, comment_id
-        validate_regex   :content, content, /\S/,
-          "should not be empty."
-      end
-
-      def parent
-        comment
-      end
-
       def create_sub_comment
         command(:'sub_comments/create_xxx',
-                    parent_id: comment_id, parent_class: 'Comment',
+                    parent_id: comment_id,
                     content: content, user: pavlov_options[:current_user])
       end
 
