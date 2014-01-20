3a6966302f606b3e2a2b72ef9274bf7496ee7ca4
Mark IJbema <markijbema@gmail.com>
2014-01-20 11:27:32 +0100

    Move the last steps of our building process to circle

--- core/bin/ci/106_check_code_quality.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ -z $SUPPRESS_METRICS ] || exit 0
-
-echo "Running Cane"
-
-bundle exec cane
--- core/bin/ci/107_check_rails_best_practices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ -z $SUPPRESS_METRICS ] || exit 0
-
-echo "Running Rails Best Practices"
-
-bundle exec rails_best_practices --silent
