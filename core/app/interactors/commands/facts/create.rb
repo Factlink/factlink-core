6694d3763fa8fe2ee7a8d9b19a201f83b0e9b656
Jan Paul Posma <me@janpaulposma.nl>
2014-01-20 16:21:25 +0100

    Inlined hash

--- core/app/interactors/commands/facts/create.rb
+++ core/app/interactors/commands/facts/create.rb
@@ -6,8 +6,7 @@ module Commands
       arguments :displaystring, :title, :creator, :site
 
       def execute
-        fact_params = {created_by: creator.graph_user, site: site}
-        fact = Fact.new fact_params
+        fact = Fact.new created_by: creator.graph_user, site: site
         fact.data = create_fact_data
         fact.save
         fact.data.fact_id = fact.id
