a1ec90b39e7efa525d1fd628bdc253d288c876af
Jan Paul Posma <me@janpaulposma.nl>
2014-01-20 16:39:38 +0100

    Added missing urls

--- core/spec/integration-real/queries/facts/get_paginated_spec.rb
+++ core/spec/integration-real/queries/facts/get_paginated_spec.rb
@@ -7,14 +7,14 @@ describe Queries::Facts::GetPaginated do
     user = create :full_user
 
     as(user) do |pavlov|
-      fact1 = pavlov.command :'facts/create',
-                displaystring: 'foo', title: 'foo', creator: user
+      fact1 = pavlov.interactor :'facts/create',
+                displaystring: 'foo', title: 'foo', url: 'http://example.org'
       sleep 0.001 # ensure different timestamp
-      fact2 = pavlov.command :'facts/create',
-                displaystring: 'foo', title: 'foo', creator: user
+      fact2 = pavlov.interactor :'facts/create',
+                displaystring: 'foo', title: 'foo', url: 'http://example.org'
       sleep 0.001 # ensure different timestamp
-      fact3 = pavlov.command :'facts/create',
-                displaystring: 'foo', title: 'foo', creator: user
+      fact3 = pavlov.interactor :'facts/create',
+                displaystring: 'foo', title: 'foo', url: 'http://example.org'
 
       result = pavlov.query :'facts/get_paginated',
                  key: user.graph_user.sorted_created_facts.key.to_s,
