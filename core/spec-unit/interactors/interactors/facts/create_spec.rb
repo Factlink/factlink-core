bc4f9e8f19ffa6e91bbf8931a075c11e8fc08475
Jan Paul Posma <me@janpaulposma.nl>
2014-01-20 16:33:54 +0100

    Removed another test for creating fact without site

--- core/spec-unit/interactors/interactors/facts/create_spec.rb
+++ core/spec-unit/interactors/interactors/facts/create_spec.rb
@@ -43,47 +43,7 @@ describe Interactors::Facts::Create do
   end
 
   describe '#call' do
-    it 'correctly when site doesn''t exist' do
-      url = 'www.fmf.nl'
-      displaystring = 'this is the annotated text'
-      title = 'this is the title'
-      site = double
-      fact_data = double(persisted?: true)
-      fact = double(id: '1', errors: [], data: fact_data)
-      user = double(id: '123abc')
-      Blacklist.stub default: double
-      Blacklist.default.stub(:matches?).with(url).and_return false
-
-      pavlov_options = { current_user: user, ability: double(can?: true) }
-      interactor = described_class.new displaystring: displaystring, url: url,
-                                       title: title,
-                                       pavlov_options: pavlov_options
-
-      Pavlov.stub(:query)
-            .with(:'sites/for_url',
-                      url: url, pavlov_options: pavlov_options)
-            .and_return(nil)
-
-      Pavlov.should_receive(:command)
-            .with(:'sites/create',
-                      url: url, pavlov_options: pavlov_options)
-            .and_return(site)
-
-      Pavlov.should_receive(:command)
-            .with(:'facts/create',
-                      displaystring: displaystring, title: title, creator: user,
-                      site: site, pavlov_options: pavlov_options)
-            .and_return(fact)
-
-      Pavlov.should_receive(:command)
-            .with(:'facts/add_to_recently_viewed',
-                      fact_id: fact.id.to_i, user_id: user.id.to_s,
-                      pavlov_options: pavlov_options)
-
-      expect(interactor.call).to eq fact
-    end
-
-    it 'correctly when site exists' do
+    it 'works when site exists' do
       url = 'www.fmf.nl'
       displaystring = 'this is the annotated text'
       title = 'this is the title'
