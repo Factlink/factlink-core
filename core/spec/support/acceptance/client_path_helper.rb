fdafcb7bd8771a042200d99127a6c3331489a68d
Jan Paul Posma <me@janpaulposma.nl>
2014-01-20 15:44:42 +0100

    Added url to new_fact_path

--- core/spec/support/acceptance/client_path_helper.rb
+++ core/spec/support/acceptance/client_path_helper.rb
@@ -1,6 +1,8 @@
 module Acceptance::ClientPathHelper
-  def new_fact_path displaystring: 'something'
+  def new_fact_path displaystring: 'something', url: 'http://example.org'
     encoded_displaystring = CGI.escape(displaystring)
-    "/client/facts/new?displaystring=#{encoded_displaystring}"
+    encoded_url = CGI.escape(url)
+
+    "/client/facts/new?displaystring=#{encoded_displaystring}&url=#{encoded_url}"
   end
 end
