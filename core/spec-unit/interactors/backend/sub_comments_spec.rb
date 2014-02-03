f8898d6e2d2e4c914b759da90c607a08b7a5ae16
Mark IJbema <markijbema@gmail.com>
2014-02-03 08:46:32 +0100

    Removed dead code

--- core/spec-unit/interactors/backend/sub_comments_spec.rb
+++ core/spec-unit/interactors/backend/sub_comments_spec.rb
@@ -25,7 +25,6 @@ describe Backend::SubComments do
   describe '#index' do
     it 'no subcomments' do
       parent_id = 1
-      sub_comment_finder1, sub_comment_finder2 = double, double
 
       SubComment.stub(:any_in)
         .with(parent_id: [parent_id])
@@ -42,7 +41,6 @@ describe Backend::SubComments do
       parent_id = '2a'
       sub_comments = [double, double]
       dead_sub_comments = [double, double]
-      sub_comment_finder1, sub_comment_finder2 = double, double
 
       SubComment.stub(:any_in)
         .with(parent_id: [parent_id])
@@ -65,19 +63,12 @@ describe Backend::SubComments do
       parent_ids = ['2a', '2b']
       sub_comments = [double, double]
       dead_sub_comments = [double, double]
-      sub_comment_finder1, sub_comment_finder2 = double, double
 
       SubComment.stub(:any_in)
         .with(parent_id: parent_ids)
         .and_return(sub_comment_finder2)
       sub_comment_finder2.stub(:asc)
         .with(:created_at).and_return(sub_comments)
-      KillObject.stub(:sub_comment)
-        .with(sub_comments[0])
-        .and_return(dead_sub_comments[0])
-      KillObject.stub(:sub_comment)
-        .with(sub_comments[1])
-        .and_return(dead_sub_comments[1])
 
       result = Backend::SubComments.index(parent_ids_in: parent_ids)
 
