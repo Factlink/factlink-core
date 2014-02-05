c5839e08800be187f6aad69b7a57623673cb6540
Mark IJbema <markijbema@gmail.com>
2014-02-05 15:49:19 +0100

    Simplified spec

--- core/spec-unit/interactors/queries/comments/for_fact_spec.rb
+++ core/spec-unit/interactors/queries/comments/for_fact_spec.rb
@@ -11,15 +11,14 @@ describe Queries::Comments::ForFact do
 
   describe '#call' do
     it 'correctly' do
-      comment = double(id: '2a', class: 'Comment')
-      fact = double(data_id: '10')
+      fact_data_id = '10'
       dead_comment = double
       pavlov_options = { current_user: double }
-      query = described_class.new fact_data_id: fact.data_id, pavlov_options: pavlov_options
+      query = described_class.new fact_data_id: fact_data_id, pavlov_options: pavlov_options
 
       Pavlov.should_receive(:query)
             .with(:'comments/by_ids',
-                      by: :fact_data_id, ids: [fact.data_id], pavlov_options: pavlov_options)
+                      by: :fact_data_id, ids: [fact_data_id], pavlov_options: pavlov_options)
             .and_return([dead_comment])
 
       expect(query.call).to eq [dead_comment]
