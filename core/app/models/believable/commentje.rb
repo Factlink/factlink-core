5d5fa391178585cd0a7573ce6dd21c29b8adc5a4
Mark IJbema <markijbema@gmail.com>
2014-02-10 09:51:23 +0100

    Made Believable::Commentje a bit faster

--- core/app/models/believable/commentje.rb
+++ core/app/models/believable/commentje.rb
@@ -4,5 +4,20 @@ class Believable
       key = Ohm::Key.new("Comment:#{id}:believable")
       super key
     end
+
+    # hide doubts
+    def votes
+      {
+        believes: opiniated(:believes).count,
+        disbelieves: opiniated(:disbelieves).count,
+      }
+    end
+
+    def opinion_of_graph_user graph_user
+      [:believes, :disbelieves].each do |opinion|
+        return opinion if opiniated(opinion).include? graph_user
+      end
+      return :no_vote
+    end
   end
 end
