2cc57cbacbc7749acf0554b67eff2c08b58f3a7f
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-12 09:42:23 +0100

    Add script to start db, web, & grunt watch.

---
+++ start-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+onexit() {
+  kill -2 $(jobs -p)
+  kill $(jobs -p)
+  echo "Waiting for children to exit..."
+  sleep 1.1
+  kill -9 0
+}
+trap onexit SIGINT SIGTERM EXIT INT QUIT TERM
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+./start-db.sh &
+./start-web.sh &
+(cd js-library; grunt default watch) &
+wait
