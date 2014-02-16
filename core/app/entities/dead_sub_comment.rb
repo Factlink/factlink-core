ca0fe33092043c3827d2ca200d40d462f39a8f1f
Mark IJbema <markijbema@gmail.com>
2014-02-16 15:27:35 +0100

    Upgraded strict_struct
    Removed temporary fixes from Dead*, needed because of previous shortcomings in StrictStruct

--- core/app/entities/dead_sub_comment.rb
+++ core/app/entities/dead_sub_comment.rb
@@ -1,17 +1,14 @@
 DeadSubComment = StrictStruct.new(:id, :created_by, :created_by_id, :created_at, :content, :parent_id) do
-  alias :old_to_h :to_h
   def formatted_content
     FormattedCommentContent.new(content).html
   end
 
-  def to_h
-    res = old_to_h.merge \
+  def to_hash
+    res = super.merge \
       formatted_content: formatted_content
     res.delete(:created_by_id)
     res.delete(:content)
     res.delete(:parent_id)
     res
   end
-
-  alias :to_hash :to_h
 end
