1e8d23aa2320f2802697eb6871ba665e9e2f31c2
Martijn Russchen <martijn@factlink.com>
2014-02-17 15:06:17 +0100

    Simply use some basic html

--- core/app/helpers/pages_helper.rb
+++
@@ -1,9 +0,0 @@
-module PagesHelper
-  def menu_item(page_path, page_title)
-    request_path = URI.parse(request.fullpath).path
-    html_class = pages_path(page_path) == request_path ? 'active': ''
-    content_tag :li,
-      link_to(page_title, pages_path(page_path)),
-      class: html_class
-  end
-end
