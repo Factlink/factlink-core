e2a80bcc4e4bb57f0c56cb2d2010e00434fa5d7b
Mark IJbema <markijbema@gmail.com>
2014-02-18 15:13:53 +0100

    Inlined bubble

--- core/app/assets/javascripts/frontend.js
+++ core/app/assets/javascripts/frontend.js
@@ -1,4 +1,5 @@
-//= require bubble
+//= require jquery.scrollTo
+//= require lib/prevent_scroll_propagation
 //= require postFactlinkObject
 //= require_tree ./frontend
 //= require factlink.backbone
