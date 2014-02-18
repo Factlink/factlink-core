ceb642815dc38ac5d423ff0b7f5bb6949838b255
Mark IJbema <markijbema@gmail.com>
2014-02-18 09:13:57 +0100

    Fixed specs
    removed any_instance cruft

--- core/spec-unit/interactors/interactors/users/unfollow_user_spec.rb
+++ core/spec-unit/interactors/interactors/users/unfollow_user_spec.rb
@@ -5,53 +5,45 @@ describe Interactors::Users::UnfollowUser do
   include PavlovSupport
 
   describe '#authorized?' do
-    before do
-      described_class.any_instance.stub(validate: true)
-    end
-
     it 'throws when no current_user' do
       expect do
-        described_class.new(username: double,
-                            user_to_unfollow_username: double).call
+        described_class.new(username: 'foo',
+                            user_to_unfollow_username: 'bar').call
       end.to raise_error Pavlov::AccessDenied, 'Unauthorized'
     end
 
     it 'throws when updating someone else\'s follow' do
-      username = double
-      other_username = double
-      current_user = double(username: username)
+      current_user = double(username: 'username')
 
       expect do
-        described_class.new(username: other_username, user_to_unfollow_username: double, pavlov_options:  {current_user: current_user}).call
+        described_class.new(username: 'other_username', user_to_unfollow_username: 'gerard', pavlov_options:  {current_user: current_user}).call
       end.to raise_error Pavlov::AccessDenied,'Unauthorized'
     end
   end
 
   describe '#call' do
-    before do
-      described_class.any_instance.stub(authorized?: true, validate: true)
-    end
-
     it 'calls a command to unfollow' do
-      username = double
-      user_to_unfollow_username = double
-      user = double(graph_user_id: double)
+      username = 'jan'
+      user_to_unfollow_username = 'henk'
+      user = double(graph_user_id: double, username: 'jan')
+      pavlov_options = { current_user: user }
       user_to_unfollow = double(graph_user_id: double)
       interactor = described_class.new username: username,
-                                       user_to_unfollow_username: user_to_unfollow_username
+                                       user_to_unfollow_username: user_to_unfollow_username,
+                                       pavlov_options: pavlov_options
 
       Pavlov.should_receive(:query)
-            .with(:'user_by_username',
-                      username: username)
+            .with(:'user_by_username', username: username, pavlov_options: pavlov_options)
             .and_return(user)
       Pavlov.should_receive(:query)
             .with(:'user_by_username',
-                      username: user_to_unfollow_username)
+                      username: user_to_unfollow_username, pavlov_options: pavlov_options)
             .and_return(user_to_unfollow)
       Pavlov.should_receive(:command)
             .with(:'users/unfollow_user',
                       graph_user_id: user.graph_user_id,
-                      user_to_unfollow_graph_user_id: user_to_unfollow.graph_user_id)
+                      user_to_unfollow_graph_user_id: user_to_unfollow.graph_user_id,
+                      pavlov_options: pavlov_options)
 
       expect(interactor.call).to eq nil
     end
