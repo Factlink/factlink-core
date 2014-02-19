ca910e69c8197caf51715b8dc70ab1fd8a931e99
Martijn Russchen <martijn@factlink.com>
2014-02-19 14:47:47 +0100

    Removed old stuff

--- core/spec/controllers/client_controller_spec.rb
+++ core/spec/controllers/client_controller_spec.rb
@@ -14,8 +14,6 @@ describe ClientController do
       get :show, page: 'blank', format: :html
 
       response_body = response.body
-        .gsub(/"id":"[^"]*"/, '"id: "#ID#"')
-        .gsub(/"created_at":"[^"]*"/, '"created_at: "#CREATED_AT#"')
         .gsub(/mixpanel.identify\("[^"]*"\)/, 'mixpanel.identify("#IDENTITY#")')
         .gsub(/localStorage.factlink_csrf_token\s+=\s+"[^"]*"/, 'localStorage.factlink_csrf_token = "#CSRF_TOKEN#"')
 
