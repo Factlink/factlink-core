eded92847f94fc981f8eae5fa7a8df64e398c7c1
Martijn Russchen <martijn@factlink.com>
2014-02-26 11:04:04 +0100

    trying to fix feed spec

--- core/spec/screenshots/feed_spec.rb
+++ core/spec/screenshots/feed_spec.rb
@@ -3,22 +3,25 @@ require 'acceptance_helper'
 describe "factlink", type: :feature, driver: :poltergeist_slow do
   include ScreenshotTest
   include Acceptance::FactHelper
+  include Acceptance::CommentHelper
 
   before do
     @user = sign_in_user create :full_user
     @user2 = create :full_user
   end
 
-  it 'it renders 2 Factlinks' do
-    # user create a factlink
-    factlink = backend_create_fact
+  let(:factlink) { create :fact }
 
-    # user2 interact with user's factlink
-    factlink.add_opiniated :believes, @user2.graph_user
+  it 'it renders 2 Factlinks' do
+
+    go_to_discussion_page_of factlink
+
+    comment = "The tomcat hopped on the bus after Berlioz' death"
+    add_comment comment
 
     # user2 follow user
     Pavlov.interactor(:'users/follow_user', username: @user.username, pavlov_options: {current_user: @user2})
-
+    switch_to_user(@user_2)
     visit feed_path
 
     assume_unchanged_screenshot 'feed'
