736ac5653ce5f368fe655431b765247e8be9a84f
Martijn Russchen <martijn@factlink.com>
2014-02-26 14:27:39 +0100

    Fixed spec

--- core/spec-unit/interactors/queries/users/filter_mail_subscribers_spec.rb
+++ core/spec-unit/interactors/queries/users/filter_mail_subscribers_spec.rb
@@ -11,7 +11,6 @@ describe Queries::Users::FilterMailSubscribers do
   describe '#call' do
     it 'returns only the users which want to receive digest' do
       user = double
-      dead_user = double
       graph_user_ids = ['1', '2']
       query = described_class.new graph_user_ids: graph_user_ids, type: 'digest'
 
@@ -20,7 +19,8 @@ describe Queries::Users::FilterMailSubscribers do
       UserNotification.stub(:users_receiving).with('digest').and_return(scope)
       scope.stub(:any_in).with(graph_user_id: graph_user_ids).and_return([user])
 
-      expect(query.call).to eq([dead_user])
+
+      expect(query.call).to eq([user])
     end
   end
 end
