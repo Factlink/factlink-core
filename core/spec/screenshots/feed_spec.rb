c3c46843323197cd73458be2004cc072e3388aa0
Martijn Russchen <martijn@factlink.com>
2014-02-26 11:33:27 +0100

    Fixed feed spec

--- core/spec/screenshots/feed_spec.rb
+++ core/spec/screenshots/feed_spec.rb
@@ -2,26 +2,38 @@ require 'acceptance_helper'
 
 describe "factlink", type: :feature, driver: :poltergeist_slow do
   include ScreenshotTest
-  include Acceptance::FactHelper
-  include Acceptance::CommentHelper
+  include PavlovSupport
+
+  let(:user) { create :user }
 
   before do
-    @user_1 = sign_in_user create :full_user, :confirmed
-    @user_2 = create :full_user, :confirmed
+    other_user = create :user
+    as(user) do |p|
+      p.interactor :'users/follow_user', username: other_user.username
+    end
+
+    fact = create :fact
+    as(other_user) do |p|
+      p.interactor :'comments/create', fact_id: fact.id.to_i, content: 'hoi'
+    end
+
+    fact2 = create :fact
+    comment2 = nil
+    as(user) do |p|
+      comment2 = p.interactor :'comments/create', fact_id: fact2.id.to_i, content: 'hoi'
+    end
+    as(other_user) do |p|
+      p.interactor :'sub_comments/create', comment_id: comment2.id.to_s, content: 'hoi'
+    end
+
+    as(other_user) do |p|
+      p.interactor :'users/follow_user', username: (create :user).username
+      p.interactor :'users/follow_user', username: (create :user).username
+    end
   end
 
-  let(:factlink) { create :fact }
-
   it 'it renders 2 Factlinks' do
-
-    go_to_discussion_page_of factlink
-
-    comment = "The tomcat hopped on the bus after Berlioz' death"
-    add_comment comment
-
-    # user2 follow user
-    Pavlov.interactor(:'users/follow_user', username: @user_1.username, pavlov_options: {current_user: @user2})
-    switch_to_user(@user_2)
+    sign_in_user(user)
 
     visit feed_path
     assume_unchanged_screenshot 'feed'
