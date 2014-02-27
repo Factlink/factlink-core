4d36f343a983588fc2f707110392a9b4a0d7075f
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-27 13:42:59 +0100

    Simplify test using PavlovSupport

--- core/spec/models/evidence_deletable_spec.rb
+++ core/spec/models/evidence_deletable_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe EvidenceDeletable do
+  include PavlovSupport
 
   context 'Comment' do
     let(:created_by_user) { create :full_user }
     let(:fact)            { create :fact }
 
     let(:comment) do
-      pavlov_options = {
-        current_user: created_by_user,
-        ability: Ability.new(created_by_user)
-      }
-      interactor = Interactors::Comments::Create.new(fact_id: fact.id.to_i,
-                                                     type: 'believes', content: 'content', pavlov_options:pavlov_options )
-      dead_comment = interactor.call
+      dead_comment = as(created_by_user) do |context|
+        context.interactor(:'comments/create',
+                           fact_id: fact.id.to_i,
+                           type: 'believes',
+                           content: 'content')
+      end
       Comment.find(dead_comment.id)
     end
 
@@ -21,14 +21,11 @@ describe EvidenceDeletable do
       EvidenceDeletable.deletable?(comment).should be_true
     end
     it "should be false after someone comments on it" do
-      pavlov_options = {
-        current_user: created_by_user,
-        ability: Ability.new(created_by_user)
-      }
-
-      interactor = Interactors::SubComments::Create.new(comment_id: comment.id.to_s,
-                                                        content: 'hoi', pavlov_options: pavlov_options)
-      interactor.execute
+      as(created_by_user) do |context|
+        context.interactor(:'sub_comments/create',
+                           comment_id: comment.id.to_s,
+                           content: 'hoi')
+      end
       EvidenceDeletable.deletable?(comment).should be_false
     end
   end
