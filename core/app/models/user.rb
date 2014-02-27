2a90dd62a491dbc7a17bdfc4f2a73b41162a5a73
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-27 11:29:34 +0100

    No need for destroy on a devise property (whatever that is)

--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -282,7 +282,7 @@ class User
 
   # Override login mechanism to allow username or email logins
   def self.find_for_database_authentication(conditions)
-    login = conditions.delete(:login) #TODO:otherdelete? unlikely, devise
+    login = conditions.delete(:login)
     any_of({ :username =>  /^#{Regexp.escape(login)}$/i }, { :email =>  /^#{Regexp.escape(login)}$/i }).first
   end
 
