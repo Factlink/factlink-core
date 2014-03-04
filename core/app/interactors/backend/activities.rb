3bd878c54e14111ed51d11aeffb056f01072ba4c
Jan Paul Posma <me@janpaulposma.nl>
2014-03-04 14:00:24 +0100

    Return comment and subcomment in activities

--- core/app/interactors/backend/activities.rb
+++ core/app/interactors/backend/activities.rb
@@ -21,8 +21,13 @@ module Backend
             id: activity.id
           }
           case activity.action
-          when "created_comment", "created_sub_comment"
+          when "created_comment"
             h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object.id.to_s)
+            h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.id.to_s])
+          when "created_sub_comment"
+            h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object.id.to_s)
+            h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent.id.to_s])
+            h[:sub_comment] = Backend::SubComments::dead_for(activity.subject)
           when "followed_user"
             subject_user = Pavlov.query(:dead_users_by_ids, user_ids: activity.subject.user_id).first
             h[:followed_user] = subject_user
