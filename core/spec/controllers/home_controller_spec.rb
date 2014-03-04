28ea3c3518029dd7f479e7544908971e80f2f266
Jan Paul Posma <me@janpaulposma.nl>
2014-03-04 10:18:25 +0100

    Fixed tests

--- core/spec/controllers/home_controller_spec.rb
+++ core/spec/controllers/home_controller_spec.rb
@@ -21,12 +21,9 @@ describe HomeController do
 
   describe "Routed to general pages should work" do
     it "should be routed to for valid templates" do
-      {get: "/p/about"}.should route_to controller: 'home', action: 'pages', name: 'about'
+      {get: "/about"}.should route_to controller: 'home', action: 'pages', name: 'about'
     end
 
-    it "should not be routed with directory traversal" do
-      {get: "/p/../../../../etc/passwd"}.should_not be_routable
-    end
     it "should be able to retrieve a valid template" do
       get :pages, name: 'about'
       response.should be_success
