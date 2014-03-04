1b480db6d7e71243c9e202a81eae745b342cd03c
Jan Paul Posma <me@janpaulposma.nl>
2014-03-04 11:41:45 +0100

    Removed unnecessary tests

--- core/spec/controllers/home_controller_spec.rb
+++ core/spec/controllers/home_controller_spec.rb
@@ -17,15 +17,4 @@ describe HomeController do
       response.should redirect_to(in_your_browser_path)
     end
   end
-
-  describe "Routed to general pages should work" do
-    it "should be routed to for valid templates" do
-      {get: "/about"}.should route_to controller: 'home', action: 'pages', name: 'about'
-    end
-
-    it "should be able to retrieve a valid template" do
-      get :pages, name: 'about'
-      response.should be_success
-    end
-  end
 end
