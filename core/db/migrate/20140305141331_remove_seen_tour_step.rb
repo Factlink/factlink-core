ba51dbc66f72a5fd66a5eb8bd791284456ce6e13
Jan Paul Posma <me@janpaulposma.nl>
2014-03-05 16:08:15 +0100

    Moved to other branch

--- core/db/migrate/20140305141331_remove_seen_tour_step.rb
+++ core/db/migrate/20140305141331_remove_seen_tour_step.rb
@@ -1,15 +1,5 @@
 class RemoveSeenTourStep < Mongoid::Migration
   def self.up
-    User.where(set_up: false).destroy_all
-
-    if User.where(set_up: false).size > 0
-      fail "There are still some non-set-up users left"
-    end
-
-    if User.where(suspended: true).size > 0
-      fail "There are still some suspended users left"
-    end
-
     User.all.each do |user|
       user.remove_attribute(:seen_tour_step)
       user.remove_attribute(:suspended)
