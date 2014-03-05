bb88078bfef39457a4cd28e044d72c900d5f7075
Jan Paul Posma <me@janpaulposma.nl>
2014-03-05 14:52:38 +0100

    Removed tour interests page

--- core/app/controllers/tour_controller.rb
+++ core/app/controllers/tour_controller.rb
@@ -2,8 +2,4 @@ class TourController < ApplicationController
   def install_extension # TODO: remove
     render layout: "tour"
   end
-
-  def interests # TODO: remove
-    render text: "", layout: "tour"
-  end
 end
