b9f211d447c896db8f262f7dac252baeb512666a
Jan Paul Posma <me@janpaulposma.nl>
2014-03-05 17:18:54 +0100

    Somewhat smarter fetching of activities

--- core/app/interactors/backend/activities.rb
+++ core/app/interactors/backend/activities.rb
@@ -13,7 +13,6 @@ module Backend
 
         h = {
           timestamp: activity_hash[:score],
-          user: Pavlov.query(:dead_users_by_ids, user_ids: activity.user.user_id).first,
           action: activity.action,
           created_at: activity.created_at.to_time,
           time_ago: TimeFormatter.as_time_ago(activity.created_at.to_time),
@@ -21,15 +20,18 @@ module Backend
         }
         case activity.action
         when "created_comment"
-          h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object.id.to_s)
-          h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.id.to_s]).first
+          h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s)
+          h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject_id.to_s]).first
+          h[:user] = h[:comment].created_by
         when "created_sub_comment"
-          h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object.id.to_s)
-          h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent.id.to_s]).first
+          h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s)
+          h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent_id.to_s]).first
           h[:sub_comment] = Backend::SubComments::dead_for(activity.subject)
+          h[:user] = h[:sub_comment].created_by
         when "followed_user"
-          subject_user = Pavlov.query(:dead_users_by_ids, user_ids: activity.subject.user_id).first
+          subject_user = Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.subject_id).first
           h[:followed_user] = subject_user
+          h[:user] = Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.user_id).first
         end
 
         h
