9826493d3ba6e3cd8cc78df684dcb964610f91b0
Eamon Nerbonne <eamon@nerbonne.org>
2014-03-10 15:45:59 +0100

    minor: indenting

--- core/app/interactors/backend/activities.rb
+++ core/app/interactors/backend/activities.rb
@@ -39,26 +39,26 @@ module Backend
       }
       specialized_data =
           case activity.action
-            when "created_comment"
-              comment = Pavlov.query(:'comments/by_ids', ids: [activity.subject_id.to_s]).first
-              {
-                  fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
-                  comment: comment,
-                  user: comment.created_by,
-              }
-            when "created_sub_comment"
-              sub_comment = Backend::SubComments::dead_for(activity.subject)
-              {
-                  fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
-                  comment: Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent_id.to_s]).first,
-                  sub_comment: sub_comment,
-                  user: sub_comment.created_by,
-              }
-            when "followed_user"
-              {
-                  followed_user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.subject_id).first,
-                  user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.user_id).first,
-              }
+          when "created_comment"
+            comment = Pavlov.query(:'comments/by_ids', ids: [activity.subject_id.to_s]).first
+            {
+                fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
+                comment: comment,
+                user: comment.created_by,
+            }
+          when "created_sub_comment"
+            sub_comment = Backend::SubComments::dead_for(activity.subject)
+            {
+                fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
+                comment: Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent_id.to_s]).first,
+                sub_comment: sub_comment,
+                user: sub_comment.created_by,
+            }
+          when "followed_user"
+            {
+                followed_user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.subject_id).first,
+                user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.user_id).first,
+            }
           end
 
       base_activity_data.merge(specialized_data)
