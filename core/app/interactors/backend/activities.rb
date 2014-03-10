3c304a7e7f6dac388f33cfb9d3fbb1c8837fbf30
Eamon Nerbonne <eamon@nerbonne.org>
2014-03-10 13:57:34 +0100

    Split out activity object->hash conversion into a method

--- core/app/interactors/backend/activities.rb
+++ core/app/interactors/backend/activities.rb
@@ -8,38 +8,47 @@ module Backend
                                reversed: true,
                                withscores: true).compact
 
-      retrieved_activities.map do |activity_hash|
-        begin
-          activity = activity_hash[:item]
+      retrieved_activities.map { |activity_with_score| activity_hash activity_with_score }.compact
+    end
 
-          h = {
-            timestamp: activity_hash[:score],
-            action: activity.action,
-            created_at: activity.created_at.to_time,
-            time_ago: TimeFormatter.as_time_ago(activity.created_at.to_time),
-            id: activity.id
-          }
-          case activity.action
+    def activity_hash(item: , score:)
+      activity = item
+
+      base_activity_data = {
+          timestamp: score,
+          action: activity.action,
+          created_at: activity.created_at.to_time,
+          time_ago: TimeFormatter.as_time_ago(activity.created_at.to_time),
+          id: activity.id
+      }
+      specialized_data =
+        case activity.action
           when "created_comment"
-            h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s)
-            h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject_id.to_s]).first
-            h[:user] = h[:comment].created_by
+            comment = Pavlov.query(:'comments/by_ids', ids: [activity.subject_id.to_s]).first
+            {
+                fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
+                comment: comment,
+                user: comment.created_by,
+            }
           when "created_sub_comment"
-            h[:fact] = Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s)
-            h[:comment] = Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent_id.to_s]).first
-            h[:sub_comment] = Backend::SubComments::dead_for(activity.subject)
-            h[:user] = h[:sub_comment].created_by
+            sub_comment = Backend::SubComments::dead_for(activity.subject)
+            {
+                fact: Pavlov.query(:'facts/get_dead', id: activity.object_id.to_s),
+                comment: Pavlov.query(:'comments/by_ids', ids: [activity.subject.parent_id.to_s]).first,
+                sub_comment: sub_comment,
+                user: sub_comment.created_by,
+            }
           when "followed_user"
-            subject_user = Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.subject_id).first
-            h[:followed_user] = subject_user
-            h[:user] = Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.user_id).first
-          end
-
-          h
-        rescue
-          nil
+            {
+                followed_user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.subject_id).first,
+                user: Pavlov.query(:dead_users_by_ids, by: :graph_user_id, user_ids: activity.user_id).first,
+            }
         end
-      end.compact
+
+      base_activity_data.merge(specialized_data)
+    rescue
+      nil #activities may become "invalid" in which case the facts/comments/users they refer to
+      #are gone.  This  causes errors.  We ignore such activities.
     end
   end
 end
