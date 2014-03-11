bce10b57c815366fed3cfcb7177b27198c89396f
Mark IJbema <markijbema@gmail.com>
2014-03-11 08:00:07 +0100

    Moved stuff belonging together together

--- ruby-web-proxy/lib/add_factlink_to_page.rb
+++ ruby-web-proxy/lib/add_factlink_to_page.rb
@@ -4,26 +4,33 @@ class AddFactlinkToPage
   include Goliath::Rack::AsyncMiddleware
   def post_process(env, status, headers, body)
     if env[:web_proxy_proxied]
-      location = env[:web_proxy_proxied_location]
-
-      jsonified_location = location.gsub(/</, '%3C').gsub(/>/, '%3E').to_json
-      jslib_uri = env.config[:jslib_uri]
-
-      factlink_proxied_url_js_var = "
-        <script>window.FactlinkProxiedUri = #{jsonified_location};</script>
-      "
-
-      factlink_publisher_script = "
-       <script async defer
-               src=\"#{jslib_uri}\"
-               onload=\"__internalFactlinkState(\'proxyLoaded\')\"
-         ></script>
-      "
-
       html_editor = HtmlEditor.new(body)
-      html_editor.prepend_to_head(factlink_proxied_url_js_var + factlink_publisher_script)
+      html_editor.prepend_to_head(
+        factlink_proxied_url_js_var(env) +
+        factlink_publisher_script(env)
+      )
       body = html_editor.to_s
     end
     [status, headers, body]
+  end
+
+  def factlink_proxied_url_js_var env
+    location = env[:web_proxy_proxied_location]
+    jsonified_location = location.gsub(/</, '%3C').gsub(/>/, '%3E').to_json
+
+    "
+      <script>window.FactlinkProxiedUri = #{jsonified_location};</script>
+    "
+  end
+
+  def factlink_publisher_script env
+    jslib_uri = env.config[:jslib_uri]
+
+    factlink_publisher_script = "
+     <script async defer
+             src=\"#{jslib_uri}\"
+             onload=\"__internalFactlinkState(\'proxyLoaded\')\"
+       ></script>
+    "
   end
 end
