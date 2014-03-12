fea7dce412032a3d024ba890b0206a8f79533490
Jan Paul Posma <me@janpaulposma.nl>
2014-03-12 13:07:44 +0100

    Added another blog post

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -82,6 +82,7 @@ FactlinkUI::Application.routes.draw do
 
   get "/blog" => "blog#index", as: 'blog_index'
   get "/blog/the-annotated-web" => "blog#the_annotated_web", as: 'blog_the_annotated_web'
+  get "/blog/learning-from-discussions" => "blog#learning_from_discussions", as: 'blog_learning_from_discussions'
 
   authenticated :user do
     namespace :admin, path: 'a' do
