e58c76b617395050167d6b061c9da8cba759131c
Jan Paul Posma <me@janpaulposma.nl>
2014-03-13 15:38:07 +0100

    Made pavlov_actions

--- core/app/controllers/facts_controller.rb
+++ core/app/controllers/facts_controller.rb
@@ -1,31 +1,13 @@
 class FactsController < ApplicationController
   pavlov_action :show, Interactors::Facts::Get
+  pavlov_action :create, Interactors::Facts::Create
+  pavlov_action :search, Interactors::Facts::Search
+  pavlov_action :recently_viewed, Interactors::Facts::RecentlyViewed
 
   def discussion_page_redirect
     dead_fact = interactor(:'facts/get', id: params[:id])
 
     redirect_to FactUrl.new(dead_fact).proxy_open_url, status: :moved_permanently
-  end
-
-  def create
-    dead_fact = interactor(:'facts/create',
-                           displaystring: params[:displaystring].to_s,
-                           url: params[:url].to_s,
-                           site_title: params[:site_title].to_s)
-
-    render json: dead_fact
-  end
-
-  def search
-    facts = interactor(:'facts/search', keywords: params[:keywords])
-
-    render json: facts
-  end
-
-  def recently_viewed
-    facts = interactor :"facts/recently_viewed"
-
-    render json: facts
   end
 
   def share
