25980a54663b9af035806cc52b222dc3ed154f0c
Jan Paul Posma <me@janpaulposma.nl>
2014-03-13 13:42:55 +0100

    Removed unnecessary ancestor

--- core/spec/screenshots/static_spec.rb
+++ core/spec/screenshots/static_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages:", type: :feature, driver: :poltergeist_slow do
       create_default_activities_for create(:user)
 
       visit "/"
-      find('.spec-feed-activities-fixed .spec-feed-activity:first-child')
+      find('.spec-feed-activity')
 
       assume_unchanged_screenshot "static_homepage"
     end
