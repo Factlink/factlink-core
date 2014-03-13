67af2994e9a0151dd3900c4651937328ea4054e1
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-13 14:13:45 +0100

    - added the comment option for the fact dsl - added comment data to the seed file

--- core/app/classes/load_dsl/fact_dsl.rb
+++ core/app/classes/load_dsl/fact_dsl.rb
@@ -8,9 +8,18 @@ class LoadDsl
       set_opinion(:disbelieves, *l)
     end
 
+    def comment(username, comment)
+
+      u = User.where(username: username).first
+
+      pavlov_options = { current_user: u }
+
+      Pavlov.interactor(:'comments/create', fact_id: fact.id.to_i, content: comment, pavlov_options: pavlov_options)
+    end
+
     def set_opinion(opinion_type, *users)
       users.each do |username|
-        gu = User.where(username: username).first.graph_user
+        gu = User.where(username: username).first.graph_user || fail("user not found " + username)
         fact.add_opinion opinion_type, gu
       end
     end
