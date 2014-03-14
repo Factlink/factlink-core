a45b5a8e52c41465646c37b504f46f26d3582a99
Martijn Russchen <martijn@factlink.com>
2014-03-14 16:15:11 +0100

    Add screenshot test

--- core/spec/screenshots/static_spec.rb
+++ core/spec/screenshots/static_spec.rb
@@ -32,6 +32,16 @@ describe "Static pages:", type: :feature do
     end
   end
 
+  describe "Customer landing page" do
+    it "renders correct" do
+      visit "/"
+
+      first('a', text: 'In your browser').click
+
+      assume_unchanged_screenshot "static_in_your_browser"
+    end
+  end
+
   describe "Blog page" do
     it do
       visit blog_stubbing_the_object_under_test_and_getting_away_without_it_path
