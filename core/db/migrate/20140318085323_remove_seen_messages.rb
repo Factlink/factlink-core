2cb6d3db71ebaf9be2994c2842696d20dae13f56
Jan Paul Posma <me@janpaulposma.nl>
2014-03-18 10:02:04 +0100

    Remove keys using redis keys

--- core/db/migrate/20140318085323_remove_seen_messages.rb
+++ core/db/migrate/20140318085323_remove_seen_messages.rb
@@ -1,7 +1,10 @@
 class RemoveSeenMessages < Mongoid::Migration
   def self.up
-    User.all.each do |user|
-      Redis.current.del user.seen_messages.key
+    db = Redis.current
+    seen_messages_keys = db.keys("*:seen_messages")
+    if seen_messages_keys.size > 0 then
+      deleted_key_count = db.del(*seen_messages_keys)
+      puts "Deleted #{deleted_key_count} keys."
     end
   end
 
