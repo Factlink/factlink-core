6bef9943be589597da738fcd06eca2408ecd52b3
Jan Paul Posma <me@janpaulposma.nl>
2014-03-24 12:01:17 +0100

    Extracted interactor

--- core/app/controllers/api/users_controller.rb
+++ core/app/controllers/api/users_controller.rb
@@ -1,17 +1,10 @@
 class Api::UsersController < ApplicationController
   pavlov_action :feed, Interactors::Users::Feed
   pavlov_action :show, Interactors::Users::GetFull
+  pavlov_action :current, Interactors::Users::GetCurrent
 
   def update
     render json: interactor(:'users/update', original_username: params[:original_username], fields: params)
-  end
-
-  def current
-    if current_user
-      render json: interactor(:'users/get_full', username: current_user.username)
-    else
-      render json: interactor(:'users/get_non_signed_in')
-    end
   end
 
   def change_password
