cfa46d60a590e7eda5314671c2aee807c1504b53
Jan Paul Posma <me@janpaulposma.nl>
2014-03-25 17:10:43 +0100

    Re-added test

--- core/spec-unit/interactors/interactors/users/delete_spec.rb
+++ core/spec-unit/interactors/interactors/users/delete_spec.rb
@@ -7,6 +7,28 @@ describe Interactors::Users::Delete do
     stub_classes 'User'
   end
 
+  describe '#validate' do
+    it 'raises when current_user_password is invalid' do
+      wrong_password = 'qwerty'
+      user = double(username: 'username')
+      pavlov_options = {
+        ability: double(:ability, can?: true ),
+        current_user: user
+      }
+
+      user.stub(:valid_password?).with(wrong_password)
+          .and_return(false)
+
+      User.stub(:find).with(user.username).and_return(user)
+
+      interactor = described_class.new username: user.username,
+                                       current_user_password: wrong_password,
+                                       pavlov_options: pavlov_options
+
+      expect(interactor.valid?).to eq(false)
+    end
+  end
+
   describe '#call' do
     it 'it calls the delete+anonymize commands' do
       user = double(:user, id: 'a234', username: 'username')
