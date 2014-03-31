de80cb47d1ca084ae9bcc3f9081a6f64c5f08a43
Jan Paul Posma <me@janpaulposma.nl>
2014-03-31 13:56:37 +0200

    Removed unused methods

--- core/app/classes/load_dsl/user_dsl.rb
+++ core/app/classes/load_dsl/user_dsl.rb
@@ -1,13 +1,5 @@
 class LoadDsl
   UserDsl = Struct.new(:state_user) do
-    def state_graph_user
-      state_user.graph_user
-    end
-
-    def load_site(url)
-      Site.find(:url => url).first || Site.create(:url => url)
-    end
-
     def load_fact(fact_string,url="http://example.org/", opts={})
       dead_fact = Backend::Facts.create displaystring: fact_string,
         url: url, site_title: opts[:title] || url
