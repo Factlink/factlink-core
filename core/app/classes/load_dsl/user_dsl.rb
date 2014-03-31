100f6519a95bd84f45ec1b6377ff8764a6af72f8
Jan Paul Posma <me@janpaulposma.nl>
2014-03-31 13:57:24 +0200

    Inlined method

--- core/app/classes/load_dsl/user_dsl.rb
+++ core/app/classes/load_dsl/user_dsl.rb
@@ -1,15 +1,10 @@
 class LoadDsl
   UserDsl = Struct.new(:state_user) do
-    def load_fact(fact_string,url="http://example.org/", opts={})
+    def fact(fact_string,url="http://example.org/", opts={}, &block)
       dead_fact = Backend::Facts.create displaystring: fact_string,
         url: url, site_title: opts[:title] || url
 
-      Fact[dead_fact.id]
-    end
-
-    def fact(fact_string,url="http://example.org/", opts={}, &block)
-      f = load_fact(fact_string,url, opts)
-      FactDsl.new(f).instance_eval(&block) if block_given?
+      FactDsl.new(Fact[dead_fact.id]).instance_eval(&block) if block_given?
     end
 
   end
