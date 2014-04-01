22134ce9f1fbbe32e1debea246f1b62b0322561b
Jan Paul Posma <me@janpaulposma.nl>
2014-04-01 15:23:46 +0200

    Added test

---
+++ core/spec/classes/export_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Export do
+  it '#export' do
+    user = create :user, created_at: Time.at(0)
+    user.updated_at = Time.at(1000)
+    user.confirmation_token = 'moi'
+    user.confirmation_sent_at = Time.at(2000)
+    user.save!
+
+    create :social_account, :twitter, user: user
+
+    verify(format: :text) { Export.new.export }
+  end
+end
