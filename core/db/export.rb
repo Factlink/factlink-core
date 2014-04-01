d318b6caad212e249ffec8a2e8feee17675b45cd
Jan Paul Posma <me@janpaulposma.nl>
2014-04-01 14:45:50 +0200

    Fixed skipping of confirmation mails

--- core/db/export.rb
+++ core/db/export.rb
@@ -28,12 +28,12 @@ def export_database(filename:)
       user_export += hash_field_for(user, 'receives_digest')
       user_export += '}); '
 
+
       user_export += assignment_for(user, 'user', 'created_at')
       user_export += assignment_for(user, 'user', 'updated_at')
       user_export += assignment_for(user, 'user', 'deleted')
       user_export += assignment_for(user, 'user', 'admin')
       user_export += assignment_for(user, 'user', 'email')
-      user_export += assignment_for(user, 'user', 'confirmed_at')
       user_export += assignment_for(user, 'user', 'registration_code')
       user_export += assignment_for(user, 'user', 'reset_password_token')
       user_export += assignment_for(user, 'user', 'reset_password_sent_at')
@@ -43,11 +43,16 @@ def export_database(filename:)
       user_export += assignment_for(user, 'user', 'last_sign_in_at')
       user_export += assignment_for(user, 'user', 'current_sign_in_ip')
       user_export += assignment_for(user, 'user', 'last_sign_in_ip')
-      user_export += assignment_for(user, 'user', 'confirmation_token')
-      user_export += assignment_for(user, 'user', 'confirmation_sent_at')
 
       user_export += 'user.password = "some_dummy"; ' # before setting encrypted_password
       user_export += assignment_for(user, 'user', 'encrypted_password')
+
+      user_export += 'user.skip_confirmation_notification!; '
+      user_export += 'user.save!; '
+      user_export += assignment_for(user, 'user', 'confirmed_at')
+      user_export += assignment_for(user, 'user', 'confirmation_token')
+      user_export += assignment_for(user, 'user', 'confirmation_sent_at')
+      user_export += assignment_for(user, 'user', 'updated_at') # set here again explicitly to prevent overwriting
       user_export += 'user.save!'
 
       file.puts user_export
