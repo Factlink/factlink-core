e1e52b3fd8f93714dc15e42b5e0e4e680e240415
Jan Paul Posma <me@janpaulposma.nl>
2014-04-01 09:22:53 +0200

    Add test for removing opinion

--- core/spec/controllers/opinionators_controller_spec.rb
+++ core/spec/controllers/opinionators_controller_spec.rb
@@ -23,6 +23,14 @@ describe OpinionatorsController do
         pavlov.interactor(:'facts/set_opinion', fact_id: fact.id, opinion: 'disbelieves')
       end
 
+      last_user = create :user
+      as(last_user) do |pavlov|
+        pavlov.interactor(:'facts/set_opinion', fact_id: fact.id, opinion: 'disbelieves')
+      end
+      as(last_user) do |pavlov|
+        pavlov.interactor(:'facts/remove_opinion', fact_id: fact.id)
+      end
+
       get :index, fact_id: fact.id
 
       verify { response.body }
