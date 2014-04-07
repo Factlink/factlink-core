a61656f413b80e13d7ee5277f73b077a1b72f569
Jan Paul Posma <jp@Jan-Paul-Posmas-MacBook-Pro.local>
2014-04-07 08:39:55 +0200

    Made method private

--- core/app/interactors/backend/activities.rb
+++ core/app/interactors/backend/activities.rb
@@ -29,45 +29,6 @@ module Backend
         .to_a
     end
 
-    def scored_activity_to_dead_activity(item: , score:)
-      activity = item
-
-      base_activity_data = {
-          timestamp: score,
-          action: activity.action,
-          created_at: activity.created_at.to_time,
-          id: activity.id,
-      }
-      specialized_data =
-          case activity.action
-          when "created_comment"
-            comment = Backend::Comments.by_ids(ids: [activity.subject_id.to_s]).first
-            {
-                fact: Backend::Facts.get(fact_id: activity.subject.fact_data.fact_id),
-                comment: comment,
-                user: comment.created_by,
-            }
-          when "created_sub_comment"
-            sub_comment = Backend::SubComments::dead_for(activity.subject)
-            {
-                fact: Backend::Facts.get(fact_id: activity.subject.parent.fact_data.fact_id),
-                comment: Backend::Comments.by_ids(ids: [activity.subject.parent_id.to_s]).first,
-                sub_comment: sub_comment,
-                user: sub_comment.created_by,
-            }
-          when "followed_user"
-            {
-                followed_user: Backend::Users.by_ids(by: :graph_user_id, user_ids: activity.subject_id).first,
-                user: Backend::Users.by_ids(by: :graph_user_id, user_ids: activity.user_id).first,
-            }
-          end
-
-      base_activity_data.merge(specialized_data)
-    rescue
-      nil #activities may become "invalid" in which case the facts/comments/users they refer to
-      #are gone.  This  causes errors.  We ignore such activities.
-    end
-
     def add_activities_to_follower_stream(followed_user_graph_user_id:, current_graph_user_id:)
       activities_set = GraphUser[followed_user_graph_user_id].own_activities
 
@@ -108,6 +69,45 @@ module Backend
       recipients.each do |user|
         Resque.enqueue SendActivityMailToUser, user.id, activity.id
       end
+    end
+
+    def scored_activity_to_dead_activity(item: , score:)
+      activity = item
+
+      base_activity_data = {
+          timestamp: score,
+          action: activity.action,
+          created_at: activity.created_at.to_time,
+          id: activity.id,
+      }
+      specialized_data =
+          case activity.action
+          when "created_comment"
+            comment = Backend::Comments.by_ids(ids: [activity.subject_id.to_s]).first
+            {
+                fact: Backend::Facts.get(fact_id: activity.subject.fact_data.fact_id),
+                comment: comment,
+                user: comment.created_by,
+            }
+          when "created_sub_comment"
+            sub_comment = Backend::SubComments::dead_for(activity.subject)
+            {
+                fact: Backend::Facts.get(fact_id: activity.subject.parent.fact_data.fact_id),
+                comment: Backend::Comments.by_ids(ids: [activity.subject.parent_id.to_s]).first,
+                sub_comment: sub_comment,
+                user: sub_comment.created_by,
+            }
+          when "followed_user"
+            {
+                followed_user: Backend::Users.by_ids(by: :graph_user_id, user_ids: activity.subject_id).first,
+                user: Backend::Users.by_ids(by: :graph_user_id, user_ids: activity.user_id).first,
+            }
+          end
+
+      base_activity_data.merge(specialized_data)
+    rescue
+      nil #activities may become "invalid" in which case the facts/comments/users they refer to
+      #are gone.  This  causes errors.  We ignore such activities.
     end
   end
 end
