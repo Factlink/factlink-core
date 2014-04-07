d67d628e9932361bd0a310f1662ff246873b8c3c
Jan Paul Posma <jp@Jan-Paul-Posmas-MacBook-Pro.local>
2014-04-07 11:01:06 +0200

    Removed User#avatar_url, use new Gravatar.url instead

--- core/app/classes/gravatar.rb
+++ core/app/classes/gravatar.rb
@@ -1,16 +1,6 @@
 module Gravatar
-  # from: http://douglasfshearer.com/blog/gravatar-for-ruby-and-ruby-on-rails
-  # Returns a Gravatar URL associated with the email parameter.
-  #
-  # Gravatar Options:
-  # - rating: Can be one of G, PG, R or X. Default is X.
-  # - size: Size of the image. Default is 80px.
-  # - default: URL for fallback image if none is found or image exceeds rating.
-  def self.gravatar_url(email,gravatar_options={})
-    grav_url = 'https://secure.gravatar.com/avatar/'
-    grav_url << hash(email) << "?"
-    grav_url << (gravatar_options.slice(:rating,:size,:default).map { |k,v| "#{k}=#{v}" }.join "&")
-    grav_url
+  def self.url(hash:, size:)
+    "https://secure.gravatar.com/avatar/#{hash}?size=#{size}&rating=PG&default=retro"
   end
 
   def self.hash(email)
