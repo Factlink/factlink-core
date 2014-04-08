fe7a6861e49208304babf9e231c768b69c86febc
Mark IJbema <markijbema@gmail.com>
2014-04-08 15:28:18 +0200

    Fixed mail subscriptions test

--- core/spec/controllers/mail_subscriptions_controller_spec.rb
+++ core/spec/controllers/mail_subscriptions_controller_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe MailSubscriptionsController do
   let (:user)  { create :user }
 
+  render_views
+
   describe "#update" do
     it 'works' do
       post :update,
@@ -11,6 +13,7 @@ describe MailSubscriptionsController do
            subscribe_action: :unsubscribe
 
       expect(response).to be_success
+      expect(response.body).to match 'succesfully'
       refetched_user = User.find(user.id)
       expect(refetched_user.receives_digest).to be_false
     end
