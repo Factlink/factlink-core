cfc256af9f170d7da249dfcdf19d4ce29699bc00
Jan Paul Posma <jp@Jan-Paul-Posmas-MacBook-Pro.local>
2014-04-09 10:55:53 +0200

    Added test for exporting facts

--- core/spec/classes/export_spec.rb
+++ core/spec/classes/export_spec.rb
@@ -13,6 +13,8 @@ describe Export do
     create :social_account, :twitter, user: user,
       created_at: Time.at(3000), updated_at: Time.at(4000)
 
+    create :fact_data, created_at: Time.at(5000), updated_at: Time.at(6000)
+
     verify(format: :text) { Export.new.export }
   end
 end
