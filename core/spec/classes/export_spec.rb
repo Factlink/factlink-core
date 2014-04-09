9190ac8f8f629391c716ca2fe4d12b60bcc72797
Jan Paul Posma <jp@Jan-Paul-Posmas-MacBook-Pro.local>
2014-04-09 11:46:47 +0200

    FU Hound

--- core/spec/classes/export_spec.rb
+++ core/spec/classes/export_spec.rb
@@ -4,16 +4,16 @@ describe Export do
   it '#export' do
     FactoryGirl.reload
 
-    user = create :user, created_at: Time.parse("1970-01-01")
-    user.updated_at = Time.parse("1970-01-02")
+    user = create :user, created_at: Time.parse('1970-01-01')
+    user.updated_at = Time.parse('1970-01-02')
     user.confirmation_token = 'moi'
-    user.confirmation_sent_at = Time.parse("1970-01-03")
+    user.confirmation_sent_at = Time.parse('1970-01-03')
     user.save!
 
     create :social_account, :twitter, user: user,
-      created_at: Time.parse("1970-01-04"), updated_at: Time.parse("1970-01-05")
+      created_at: Time.parse('1970-01-04'), updated_at: Time.parse('1970-01-05')
 
-    create :fact_data, created_at: Time.parse("1970-01-06"), updated_at: Time.parse("1970-01-07")
+    create :fact_data, created_at: Time.parse('1970-01-06'), updated_at: Time.parse('1970-01-07')
 
     verify(format: :text) { Export.new.export }
   end
