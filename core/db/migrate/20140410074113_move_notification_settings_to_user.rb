8f2092bc2e7d91d34b3ba34601bb3e776ec8e4fa
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-10 13:25:45 +0200

    Fix migration; user_notification field may be nil

--- core/db/migrate/20140410074113_move_notification_settings_to_user.rb
+++ core/db/migrate/20140410074113_move_notification_settings_to_user.rb
@@ -1,8 +1,12 @@
 class MoveNotificationSettingsToUser < Mongoid::Migration
   def self.up
     User.all.each do |user|
-      user['notification_settings_edit_token'] =
-        user['user_notification']['notification_settings_edit_token']
+      if user['user_notification'].nil?
+        Backend::Notifications.reset_edit_token(user: user)
+      else
+        user['notification_settings_edit_token'] ||=
+          user['user_notification']['notification_settings_edit_token']
+      end
       user.save!
     end
   end
