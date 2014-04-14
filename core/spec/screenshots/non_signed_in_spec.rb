ef8a5b9db3e2443f42c93cfe0928ad91daea8d2f
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-14 10:27:46 +0200

    Update non-signed-in-screenshot to include personal feed

--- core/spec/screenshots/non_signed_in_spec.rb
+++ core/spec/screenshots/non_signed_in_spec.rb
@@ -6,13 +6,16 @@ describe "Non signed in pages:", type: :feature do
 
   describe "Profile page page" do
     it "renders correctly" do
-      @user = sign_in_user create :user
+      user = sign_in_user create :user
 
-      create :fact_data
-
+      fact_data = create :fact_data, displaystring: 'Throngs of them!!1!'
+      as(user) do |pavlov|
+        pavlov.interactor(:'comments/create',
+                           fact_id: fact_data.fact_id,
+                           content: 'content')
+      end
       sign_out_user
-      visit user_profile_path(@user)
-      sleep 5
+      visit user_profile_path(user)
       assume_unchanged_screenshot "non_signed_in_profile"
     end
   end
