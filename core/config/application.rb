3520f661579658c669cc4e73a91b8b5790bc4fae
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-22 16:18:05 +0200

    Placate the houd

--- core/config/application.rb
+++ core/config/application.rb
@@ -108,7 +108,7 @@ module FactlinkUI
 
     # Version of your assets, change this if you want to expire all your assets
     # Therefore, we change the asset version for whenever kennisland is toggled.
-    config.assets.version = FactlinkUI.Kennisland? ? '2.0': '1.0'
+    config.assets.version = FactlinkUI.Kennisland? ? '2.0' : '1.0'
 
     config.assets.precompile = [
       /\w+\.(?!js|css|less).+/,
