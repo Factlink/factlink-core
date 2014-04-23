e4d50c1d829d5ea1e5d69fba0aa9e4f5dc49cb4f
Jan Paul Posma <janpaul@factlink.com>
2014-04-23 10:19:34 +0200

    Removed downvoting for Kennisland

--- core/spec/acceptance/add_comment_spec.rb
+++ core/spec/acceptance/add_comment_spec.rb
@@ -76,15 +76,17 @@ feature "adding comments to a fact", type: :feature do
   scenario 'comments and comments with links to annotations should be sorted on relevance' do
     open_discussion_sidebar_for factlink.fact_id
 
-    comment1 = 'Buffels zijn niet klein te krijgen joh'
+    if !FactlinkUI.Kennisland?
+      comment1 = 'Buffels zijn niet klein te krijgen joh'
+      add_comment comment1
+      vote_comment :down, comment1
+    end
+
     factlink2 = create :fact_data
-    comment3 = 'Geert is een baas'
-
-    add_comment comment1
     add_existing_factlink factlink2.displaystring
-    add_comment comment3
 
-    vote_comment :down, comment1
+    comment3 = 'Geert is een baas'
+    add_comment comment3
     vote_comment :up  , comment3
 
     open_discussion_sidebar_for factlink.fact_id
@@ -92,12 +94,15 @@ feature "adding comments to a fact", type: :feature do
     #find text with comment - we need to do this before asserting on ordering
     #since expect..to..match is not async, and at this point the comment ajax
     #may not have been completed yet.
-    assert_comment_exists comment1
+    assert_comment_exists comment3
 
     items = all '.spec-evidence-box'
     expect(items[0].text).to match (Regexp.new factlink2.displaystring)
     expect(items[1].text).to match (Regexp.new comment3)
-    expect(items[2].text).to match (Regexp.new comment1)
+
+    if !FactlinkUI.Kennisland?
+      expect(items[2].text).to match (Regexp.new comment1)
+    end
   end
 
   scenario "after adding it can be removed" do
