b57f13b034d1395fb9abce6c5e8b1381472aa246
Mark IJbema <markijbema@gmail.com>
2014-04-23 11:52:07 +0200

    Remove active_resource
    afaik this was never used

--- core/config/application.rb
+++ core/config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "active_record/railtie"
 require "pavlov/alpha_compatibility"
 
