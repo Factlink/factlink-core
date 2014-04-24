6e6e5381a02c036cc31513976c30aeca0a9a78d4
Jan Paul Posma <janpaul@factlink.com>
2014-04-24 15:21:51 +0200

    Indented stuff

--- core/app/classes/formatted_anecdote_content.rb
+++ core/app/classes/formatted_anecdote_content.rb
@@ -7,14 +7,14 @@ class FormattedAnecdoteContent
 
   def html
     (content_tag(:p, content_tag(:strong, 'Introduction')) +
-    content_tag(:p, @anecdote['introduction']) +
-    content_tag(:p, content_tag(:strong, 'Sudden insight')) +
-    content_tag(:p, @anecdote['insight']) +
-    content_tag(:p, content_tag(:strong, 'Resources')) +
-    content_tag(:p, @anecdote['resources']) +
-    content_tag(:p, content_tag(:strong, 'Actions')) +
-    content_tag(:p, @anecdote['actions']) +
-    content_tag(:p, content_tag(:strong, 'Effect + evaluation')) +
-    content_tag(:p, @anecdote['effect'])).html_safe
+      content_tag(:p, @anecdote['introduction']) +
+      content_tag(:p, content_tag(:strong, 'Sudden insight')) +
+      content_tag(:p, @anecdote['insight']) +
+      content_tag(:p, content_tag(:strong, 'Resources')) +
+      content_tag(:p, @anecdote['resources']) +
+      content_tag(:p, content_tag(:strong, 'Actions')) +
+      content_tag(:p, @anecdote['actions']) +
+      content_tag(:p, content_tag(:strong, 'Effect + evaluation')) +
+      content_tag(:p, @anecdote['effect'])).html_safe
   end
 end
