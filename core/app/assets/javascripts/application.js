e9a88d0549d13a57f973836600e74dde86940aba
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-28 14:21:29 +0200

    Move polyfills into their own seperate section

--- core/app/assets/javascripts/application.js
+++ core/app/assets/javascripts/application.js
@@ -2,14 +2,16 @@
 //= require_tree ./globals
 
 // All the default libraries/files needed for all pages on Factlink
-//= require local_storage_polyfill
-//= require underscore
 //= require console_polyfill
 //= require function_bind_polyfill
+//= require local_storage_polyfill
+
 //= require jquery_ujs
 //= require jquery.scrollTo
 //= require jquery_ujs_override
 //= require jquery.placeholder
+
+//= require underscore
 //= require ohnoes
 
 //= require_tree ./base
