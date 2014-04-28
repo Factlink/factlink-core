7380e171b6d12a20b038e311f0856fe80d6f1a0f
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-28 14:20:48 +0200

    Move all jquery includes to one place in file

--- core/app/assets/javascripts/application.js
+++ core/app/assets/javascripts/application.js
@@ -2,18 +2,17 @@
 //= require_tree ./globals
 
 // All the default libraries/files needed for all pages on Factlink
-//= require jquery_ujs
 //= require local_storage_polyfill
-//= require jquery_ujs_override
-
 //= require underscore
 //= require console_polyfill
 //= require function_bind_polyfill
+//= require jquery_ujs
+//= require jquery.scrollTo
+//= require jquery_ujs_override
 //= require jquery.placeholder
 //= require ohnoes
 
 //= require_tree ./base
 //= require_tree ./lib
-//= require jquery.scrollTo
 //= require postFactlinkObject
 //= require factlink.backbone
