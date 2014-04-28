2117c161b2e691f93bd714bc24c9bb8fbf43828a
Luuk <luuk@hackerone.com>
2014-04-28 11:48:38 +0200

    merge master

--- core/config/initializers/sirportly.rb
+++
@@ -1,8 +0,0 @@
-webform_id = case Rails.env.to_sym
-  when :development then 91
-  when :test        then 1
-  when :staging     then 94
-  when :production  then 95
-  end
-
-FactlinkUI::Application.config.sirportly_webform_id = webform_id
