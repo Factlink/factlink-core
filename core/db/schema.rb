e5757a7798b9171ecc74a18f72dd19bd9547e5cc
Jan Paul Posma <janpaul@factlink.com>
2014-04-28 11:35:08 +0200

    Added migration

--- core/db/schema.rb
+++ core/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424133758) do
+ActiveRecord::Schema.define(version: 20140428092748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
