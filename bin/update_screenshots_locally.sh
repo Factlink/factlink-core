519c2d8973022b4594d09b2da0de1841087d8782
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-29 15:58:19 +0200

    Remove reference to core

--- bin/update_screenshots_locally.sh
+++ bin/update_screenshots_locally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd `dirname $0`/../core
+cd `dirname $0`/..
 ls tmp/capybara/ | egrep -v -e '-diff\.png|screenshot_201' | grep '\.png' | xargs -n 1 -I '{}' cp 'tmp/capybara/{}' spec/screenshots/screenshots/
