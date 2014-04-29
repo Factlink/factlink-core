bba7577ede1f76d5c63831839fbcf37081d3d72f
Jan Paul Posma <janpaul@factlink.com>
2014-04-29 14:22:13 +0200

    Use less-strange-looking "mystery man" default Gravatar icon

--- app/classes/gravatar.rb
+++ app/classes/gravatar.rb
@@ -1,6 +1,6 @@
 module Gravatar
   def self.url(hash:, size:)
-    "https://secure.gravatar.com/avatar/#{hash}?size=#{size}&rating=PG&default=retro"
+    "https://secure.gravatar.com/avatar/#{hash}?size=#{size}&rating=PG&default=mm"
   end
 
   def self.hash(email)
