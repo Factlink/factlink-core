ac4771f7fdc38ee6be3b4c52ac3ccc18d4974d9f
Jan Paul Posma <janpaul@factlink.com>
2014-04-29 12:41:14 +0200

    Also destroy activities that refer to deleted users

--- db/migrate/20140429094807_remove_invalid_activities.rb
+++ db/migrate/20140429094807_remove_invalid_activities.rb
@@ -1,7 +1,7 @@
 class RemoveInvalidActivities < ActiveRecord::Migration
   def up
     Activity.all.each do |a|
-      unless a.valid? && a.subject && a.user && !a.user.deleted
+      unless a.valid? && a.subject && a.user && !a.user.deleted && (a.subject_type.to_s != 'User' || !a.subject.deleted)
         a.destroy
       end
     end
