94cd8cdc2026155af176702a96f6ae88a00cead3
Mark IJbema <markijbema@gmail.com>
2014-04-29 11:49:02 +0200

    Moved core config into ruby

--- config/initializers/core.rb
+++ config/initializers/core.rb
@@ -1,4 +1,30 @@
-core_conf = YAML::load_file(Rails.root.join('config/core.yml'))[Rails.env]['core']
-FactlinkUI::Application.config.api_url =  URI.parse(core_conf['protocol'] + core_conf['hostname'] + ':' + core_conf['port'].to_s).to_s
-FactlinkUI::Application.config.core_url = URI.parse(core_conf['protocol'] + core_conf['hostname'] + ':' + core_conf['port'].to_s).to_s
-FactlinkUI::Application.config.hostname = core_conf['hostname']
+core_conf_complete = ActiveSupport::HashWithIndifferentAccess.new({
+  development: {
+    hostname: 'localhost',
+    port: 3000,
+    protocol: 'http://',
+  },
+  test: {
+    # Match urls used by Rails/Capybara
+    hostname: '127.0.0.1',
+    port: 3005,
+    protocol: 'http://',
+  },
+  staging: {
+    hostname: 'factlink-staging.inverselink.com',
+    port: 443,
+    protocol: 'https://',
+  },
+  production: {
+    hostname: 'factlink.com',
+    port: 443,
+    protocol: 'https://',
+  }
+})
+
+core_conf = core_conf_complete[Rails.env]
+
+FactlinkUI::Application.config.core_url =
+  URI.parse(core_conf[:protocol] + core_conf[:hostname] + ':' + core_conf[:port].to_s).to_s
+FactlinkUI::Application.config.hostname =
+  core_conf[:hostname]
