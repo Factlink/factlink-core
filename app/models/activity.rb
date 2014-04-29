5c0ac19b900bc411d0449155f8230fc4eb9d246b
Jan Paul Posma <janpaul@factlink.com>
2014-04-29 11:51:46 +0200

    Removed unused method

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,22 +8,4 @@ class Activity < ActiveRecord::Base
   end
 
   validates :action, inclusion: { in: valid_actions }
-
-  # WARNING: if this method returns false, we assume it will never become
-  #          valid again either, and remove/destroy freely.
-  # TODO: remove this, and instead do cascading delete in subjects
-  def still_valid?
-    valid? and user_still_valid? and subject_still_valid?
-  end
-
-  private
-
-  def user_still_valid?
-    real_user = User.where(id: user_id).first
-    real_user && !real_user.deleted
-  end
-
-  def subject_still_valid?
-    subject
-  end
 end
