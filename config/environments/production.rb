a69a4205bb162fdf97764950754c30d61d68133f
Martijn Russchen <martijn@factlink.com>
2014-04-29 16:17:33 +0200

    Move default_url_options to initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,6 @@ FactlinkUI::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "factlink.com", protocol: "https" }
 
   # Enable threaded mode
   # config.threadsafe!
