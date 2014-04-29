5923cdd481be86fac76515cbbaf9b67ed4ecde81
Jan Paul Posma <janpaul@factlink.com>
2014-04-29 11:46:06 +0200

    Moved sub_comments relation in comments to ActiveRecord

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :content, :created_by_id, :fact_data_id
   attr_accessible :markup_format # nil is plain text
   belongs_to :fact_data, class_name: 'FactData'
+  has_many :sub_comments, foreign_key: :parent_id, inverse_of: :parent, dependent: :destroy
 
   def created_by
     user = User.where(id: created_by_id).first
@@ -12,13 +13,5 @@ class Comment < ActiveRecord::Base
     self.created_by_id = user.id.to_s
   end
 
-  def sub_comments
-    SubComment.where(parent_id: self.id)
-  end
-
   #index({ fact_data: 1, opinion: 1, created_at: 1})
-
-  after_destroy do |comment|
-    SubComment.destroy_all(parent_id: comment.id.to_s) #delete_all doesn't work?
-  end
 end
