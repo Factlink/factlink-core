908842cb1e22aee37177b1823828e821e62f1c9d
Mark IJbema <markijbema@gmail.com>
2014-04-30 14:33:18 +0200

    Translated app name everywhere

--- spec/support/acceptance/authentication_helper.rb
+++ spec/support/acceptance/authentication_helper.rb
@@ -1,7 +1,7 @@
 module Acceptance
   module AuthenticationHelper
     def assert_on_login_page
-      page.find('h2', text: 'Sign in to Factlink')
+      page.find('h2', text: 'Sign in to ' + I18n.t(:app_name))
     end
   end
 end
