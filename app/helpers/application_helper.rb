4650799db482c9410335b08404f282bbe6e49f55
Mark IJbema <markijbema@gmail.com>
2014-04-30 08:58:30 +0200

    Created helper for footer items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # This helper contains application wide helpers
 # There shouldn't be anything specific in this file.
 module ApplicationHelper
+  def footer_item name, path
+    content_tag(:li, class: 'footer-item') do
+      link_to(name, path, class: 'footer-item-link')
+    end
+  end
 end
