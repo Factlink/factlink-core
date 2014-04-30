1fa84ad115d41e148bfa01662b88c67fe24171dc
maarten <maarten@vgijssel.nl>
2014-04-30 18:10:26 +0200

    Added pavlov add member interactor to the controller

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,4 +1,5 @@
 class Api::GroupsController < ApplicationController
   pavlov_action :create, Interactors::Groups::Create
   pavlov_action :index, Interactors::Groups::List
+  pavlov_action :add_member, Interactors::Groups::AddMember
 end
