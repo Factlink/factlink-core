676e57172e302a27d235ebe45b496464ae4ff3bf
maarten <maarten@vgijssel.nl>
2014-04-30 10:40:02 +0200

    Added add member method to the backend class

--- app/interactors/backend/groups.rb
+++ app/interactors/backend/groups.rb
@@ -1,12 +1,23 @@
 module Backend
   module Groups
     extend self
-    def create(groupname:, usernames:)
+    def create(groupname:, members:)
       group = Group.new
       group.groupname = groupname
       group.users << usernames.map{ |username| User.where(username: username).first }
       group.save!
       group #TODO: do we need a dead_group or something here?
+    end
+
+    def add_member(username:, groupname:)
+      user = Users.user_by_username username: username
+      group = group_by_groupname groupname: groupname
+      group.users << user
+      group.save!
+    end
+
+    def group_by_groupname(groupname:)
+      Group.find_by! groupname: groupname
     end
   end
 end
