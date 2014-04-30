162a20f3ea8c76375d4b0455c4773232e478ac87
Eamon Nerbonne <eamon@nerbonne.org>
2014-04-30 11:16:24 +0200

    Test multiple user creations simultaneously

--- spec/integration-real/interactors/group_spec.rb
+++ spec/integration-real/interactors/group_spec.rb
@@ -4,14 +4,28 @@ describe Interactors::Groups do
   include PavlovSupport
 
   describe 'Created groups appear in list' do
-    it 'filters out invalid activities' do
+    it 'Can create a group with no members' do
       user = create :user, admin: true
+      user2 = create :user
       as(user) do |pavlov|
         groups0 = pavlov.interactor :'groups/list'
         expect(groups0.size).to eq 0
-        pavlov.interactor :'groups/create', groupname: "bla", members: []
+        group = pavlov.interactor :'groups/create', groupname: "bla", members: []
         groups1 = pavlov.interactor :'groups/list'
         expect(groups1.size).to eq 1
+        expect(group.users.size).to eq 0
+      end
+    end
+    it 'Can create a group with two members' do
+      user = create :user, admin: true
+      user2 = create :user
+      as(user) do |pavlov|
+        group = pavlov.interactor :'groups/create', groupname: "bla", members: [
+            user.username, user2.username
+        ]
+        groups1 = pavlov.interactor :'groups/list'
+        expect(groups1.size).to eq 1
+        expect(group.users.size).to eq 2
       end
     end
   end
