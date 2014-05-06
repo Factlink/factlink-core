a95a344a9901c274977635281d694a199cfc48b2
Jan Paul Posma <janpaul@factlink.com>
2014-05-06 15:27:27 +0200

    Memoize config at class level

--- app/entities/dead_fact.rb
+++ app/entities/dead_fact.rb
@@ -34,18 +34,24 @@ DeadFact = StrictStruct.new(
   def html_content
     return nil unless FactlinkUI.Kennisland?
 
-    extra_config = {
-      add_attributes: {
-        'a' => { 'rel' => 'nofollow' },
-        'iframe' => { 'sandbox' => 'allow-forms allow-scripts' }
-      },
-      attributes: {
-        'iframe' => %w(src allowfullscreen frameborder width height)
-      }
-    }
-    config = Sanitize::Config::RELAXED.deep_merge(extra_config)
-    config[:elements] += ['iframe'] # iframes are used in youtube embedding.
+    Sanitize.clean(displaystring, self.class.sanitize_config)
+  end
 
-    Sanitize.clean(displaystring, config)
+  def self.sanitize_config
+    @sanitize_config ||= begin
+      extra_config = {
+        add_attributes: {
+          'a' => { 'rel' => 'nofollow' },
+          'iframe' => { 'sandbox' => 'allow-forms allow-scripts' }
+        },
+        attributes: {
+          'iframe' => %w(src allowfullscreen frameborder width height)
+        }
+      }
+      config = Sanitize::Config::RELAXED.deep_merge(extra_config)
+      config[:elements] += ['iframe'] # iframes are used in youtube embedding.
+
+      config
+    end
   end
 end
