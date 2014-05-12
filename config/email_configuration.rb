a45ae3e62b0445b9f3e0c1aa2322f363c24c9e61
Jan Paul Posma <janpaul@factlink.com>
2014-05-12 10:15:26 +0200

    Made Houndci happy

--- config/email_configuration.rb
+++ config/email_configuration.rb
@@ -1,16 +1,16 @@
 mandrill_settings = {
-  :port =>           '587',
-  :address =>        'smtp.mandrillapp.com',
-  :user_name =>      ENV['MANDRILL_USERNAME'],
-  :password =>       ENV['MANDRILL_APIKEY'],
-  :domain =>         'heroku.com',
-  :authentication => :plain
+  port:           '587',
+  address:        'smtp.mandrillapp.com',
+  user_name:      ENV['MANDRILL_USERNAME'],
+  password:       ENV['MANDRILL_APIKEY'],
+  domain:         'heroku.com',
+  authentication: :plain
 }
 
 mailcatcher_settings = {
-  :address => '127.0.0.1',
-  :port => 1025,
-  :domain => "factlink.com"
+  address: '127.0.0.1',
+  port: 1025,
+  domain: "factlink.com"
 }
 
 if ['production', 'staging'].include? Rails.env
