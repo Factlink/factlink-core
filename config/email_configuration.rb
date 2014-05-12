d64c1dbe272dfb22e3ad4ae660a4219ffd7885d6
Jan Paul Posma <janpaul@factlink.com>
2014-05-12 10:02:41 +0200

    Fixed environment variables

--- config/email_configuration.rb
+++ config/email_configuration.rb
@@ -1,23 +1,11 @@
-if ENV['MANDRILL_USERNAME'] && ENV['MANDRILL_APIKEY']
-  mandrill_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
-  }
-else
-  mandrill_settings = {
-    user_name: "mark@factlink.com",
-    password: 'YKJLeC_CBFnr1sxc6dtjXw',
-    domain: "factlink.com",
-    address: "smtp.mandrillapp.com",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-end
+mandrill_settings = {
+  :port =>           '587',
+  :address =>        'smtp.mandrillapp.com',
+  :user_name =>      ENV['MANDRILL_USERNAME'],
+  :password =>       ENV['MANDRILL_APIKEY'],
+  :domain =>         'heroku.com',
+  :authentication => :plain
+}
 
 mailcatcher_settings = {
   :address => '127.0.0.1',
