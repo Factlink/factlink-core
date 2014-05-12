3f19e2cbdd9c58543bcd7682a47490eb556bc026
Jan Paul Posma <janpaul@factlink.com>
2014-05-12 13:34:02 +0200

    Removed twitter/facebook local configs, use .env file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,38 +1,10 @@
-twitter_conf =
-  if ENV['TWITTER_ID'] && ENV['TWITTER_SECRET']
-    ActiveSupport::HashWithIndifferentAccess.new({
-      id: ENV['TWITTER_ID'],
-      secret: ENV['TWITTER_SECRET'],
-    })
-  else
-    # Development app
-    ActiveSupport::HashWithIndifferentAccess.new({
-      id: "e88X667rNJwV3qUU1FP38Q",
-      secret: "PzyaLeimuKB9vFnaQQePk76QSXwp11nUZKxpk83E",
-    })
-  end
-
-facebook_conf =
-  if ENV['FACEBOOK_ID'] && ENV['FACEBOOK_SECRET']
-    ActiveSupport::HashWithIndifferentAccess.new({
-      id: ENV['FACEBOOK_ID'],
-      secret: ENV['FACEBOOK_SECRET'],
-    })
-  else
-    # Development app
-    ActiveSupport::HashWithIndifferentAccess.new({
-      id: "283748838400237",
-      secret: "9d3776e82a8e050f615838bb9b32d13f",
-    })
-  end
-
-FactlinkUI::Application.config.facebook_app_id = facebook_conf['id']
+FactlinkUI::Application.config.facebook_app_id = ENV['FACEBOOK_ID']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, twitter_conf['id'], twitter_conf['secret']
+  provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
   provider :facebook,
-    facebook_conf['id'],
-    facebook_conf['secret'],
+    ENV['FACEBOOK_ID'],
+    ENV['FACEBOOK_SECRET'],
     scope: 'email',
     display: 'popup'
 end
